71358ff7f3696a58175a168a668cd5a46f0400df
Jianzhun Du <jianzhun.du@snowflake.com>
2021-05-28 01:02:53 +0000

    remove jvm stuff

--- test/integ/test_session.py
+++ test/integ/test_session.py
@@ -3,17 +3,12 @@
 #
 # Copyright (c) 2012-2021 Snowflake Computing Inc. All right reserved.
 #
-import pytest
-
 # TODO fix 'src.' in imports
 from src.snowflake.snowpark.row import Row
 
 
-@pytest.mark.parametrize("use_jvm_for_network", [True, False])
-@pytest.mark.parametrize("use_jvm_for_use_jvm_for_plans", [True, False])
-def test_select_1(session_cnx, db_parameters, use_jvm_for_network, use_jvm_for_use_jvm_for_plans):
+def test_select_1(session_cnx, db_parameters):
     """Tests retrieving a negative number of results."""
-    with session_cnx(db_parameters, use_jvm_for_network, use_jvm_for_use_jvm_for_plans) as session:
-        session.sql('use warehouse tests_pysnowpark').collect()
+    with session_cnx(db_parameters) as session:
         res = session.sql('select 1').collect()
         assert res == [Row([1])]
--- test/unit/test_basic.py
+++
@@ -1,6 +0,0 @@
-
-
-def test_obvious():
-    assert False != True
-
-
