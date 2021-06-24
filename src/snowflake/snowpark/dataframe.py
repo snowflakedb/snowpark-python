b41398370557f3ee3f24b00de4600438b30211fc
sfc-gh-okostakis <orestis.kostakis@snowflake.com>
2021-06-24 10:21:03 -0700

    SNOW-371888 df.createOrReplaceView() and df.createOrReplaceTempView() (#18)
    Implement createOrReplaceView(), createOrReplaceTempView(), imported tests from scala

--- src/snowflake/snowpark/dataframe.py
+++ src/snowflake/snowpark/dataframe.py
@@ -9,6 +9,13 @@ from typing import List, Optional, Tuple, Union
 
 from .column import Column
 from .internal.analyzer.analyzer_package import AnalyzerPackage
+from .internal.analyzer.sp_identifiers import TableIdentifier
+from .internal.analyzer.sp_views import (
+    CreateViewCommand as SPCreateViewCommand,
+    LocalTempView as SPLocalTempView,
+    PersistedView as SPPersistedView,
+    ViewType as SPViewType,
+)
 from .internal.sp_expressions import (
     Ascending as SPAscending,
     Attribute as SPAttribute,
@@ -19,6 +26,7 @@ from .internal.sp_expressions import (
     ResolvedStar as SPResolvedStar,
     SortOrder as SPSortOrder,
 )
+from .internal.utils import Utils
 from .plans.logical.basic_logical_operators import Join as SPJoin, Sort as SPSort
 from .plans.logical.hints import JoinHint as SPJoinHint
 from .plans.logical.logical_plan import Filter as SPFilter, Project as SPProject
@@ -431,6 +439,55 @@ class DataFrame:
     # TODO complete function. Requires TableFunction
     def __join_dataframe_table_function(self, table_function, columns) -> "DataFrame":
         pass
+
+    def createOrReplaceView(self, name: Union[str, List[str]]):
+        if type(name) == str:
+            formatted_name = name
+        elif isinstance(name, (list, tuple)):
+            if not all(type(i) == str for i in name):
+                raise ValueError(
+                    f"createOrReplaceView takes as input a string or list of strings."
+                )
+            formatted_name = ".".join(name)
+        else:
+            raise ValueError(
+                f"createOrReplaceView takes as input a string or list of strings."
+            )
+
+        return self.__do_create_or_replace_view(formatted_name, SPPersistedView())
+
+    def createOrReplaceTempView(self, name: Union[str, List[str]]):
+        if type(name) == str:
+            formatted_name = name
+        elif isinstance(name, (list, tuple)):
+            if not all(type(i) == str for i in name):
+                raise ValueError(
+                    f"createOrReplaceTempView() takes as input a string or list of strings."
+                )
+            formatted_name = ".".join(name)
+        else:
+            raise ValueError(
+                f"createOrReplaceTempView() takes as input a string or list of strings."
+            )
+
+        return self.__do_create_or_replace_view(formatted_name, SPLocalTempView())
+
+    def __do_create_or_replace_view(self, view_name: str, view_type: SPViewType):
+        Utils.validate_object_name(view_name)
+        name = TableIdentifier(view_name)
+        cmd = SPCreateViewCommand(
+            name=name,
+            user_specified_columns=[],
+            comment=None,
+            properties={},
+            original_text="",
+            child=self.__plan,
+            allow_existing=False,
+            replace=True,
+            view_type=view_type,
+        )
+
+        return self.session.conn.execute(self.session.analyzer.resolve(cmd))
 
     # Utils
     def __resolve(self, col_name: str) -> SPNamedExpression:
