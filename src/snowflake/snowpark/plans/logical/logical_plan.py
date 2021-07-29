9d5c98072e681c696a4552dc738f956625c7dbe0
Kristopher Wagner <kristopher.wagner@snowflake.com>
2021-07-29 05:27:51 +0000

    Implemented dataframe.sample() and added testing for it as well. The sample() function differs from the Thundersnow API and get inspiration from the pandas dataframe sample function parameters  to make it more pythonic

--- src/snowflake/snowpark/plans/logical/logical_plan.py
+++ src/snowflake/snowpark/plans/logical/logical_plan.py
@@ -4,7 +4,7 @@
 # Copyright (c) 2012-2021 Snowflake Computing Inc. All right reserved.
 #
 
-from typing import List
+from typing import List, Optional
 
 
 class LogicalPlan:
@@ -52,5 +52,26 @@ class Filter(OrderPreservingUnaryNode):
     def __init__(self, condition, child):
         super().__init__()
         self.condition = condition
+        self.child = child
+        self.children.append(child)
+
+
+class Sample(OrderPreservingUnaryNode):
+    """Represents a sample operation in the logical plan"""
+
+    def __init__(
+        self,
+        child: LogicalPlan,
+        probability_fraction: Optional[float] = None,
+        row_count: Optional[int] = None,
+    ):
+        super().__init__()
+        if probability_fraction is None and row_count is None:
+            raise ValueError(
+                "probability_fraction and row_count cannot both be None. "
+                "One of those values must be defined"
+            )
+        self.probability_fraction = probability_fraction
+        self.row_count = row_count
         self.child = child
         self.children.append(child)
