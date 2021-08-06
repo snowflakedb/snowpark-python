90f71b7895d700853c818fbd4427705ef0e7768d
Kristopher Wagner <kristopher.wagner@snowflake.com>
2021-08-06 22:34:22 +0000

    SNOW-415671
    Added tox file and added new build, test and code coverage jobs for snowpark

---
+++ test/conftest.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012-2021 Snowflake Computing Inc. All right reserved.
+#
+
+from pathlib import Path
+
+
+def pytest_collection_modifyitems(items) -> None:
+    """Applies tags to tests based on folders that they are in."""
+    top_test_dir = Path(__file__).parent
+    for item in items:
+        item_path = Path(str(item.fspath)).parent
+        relative_path = item_path.relative_to(top_test_dir)
+        for part in relative_path.parts:
+            item.add_marker(part)
