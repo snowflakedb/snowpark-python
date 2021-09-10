c439b4522c0cae77c8f2d945a7e065be7402bd50
Jianzhun Du <jianzhun.du@snowflake.com>
2021-09-10 18:26:39 +0000

    rebase

--- src/snowflake/snowpark/internal/server_connection.py
+++ src/snowflake/snowpark/internal/server_connection.py
@@ -183,7 +183,7 @@ class ServerConnection:
                 else:
                     return DecimalType(precision, scale)
             else:
-                return DecimalType(38, 0)
+                return DecimalType(38, 15)  # Spark 1.5.0 default
         if column_type_name == "REAL":
             return DoubleType()
         if column_type_name == "FIXED" and scale == 0:
