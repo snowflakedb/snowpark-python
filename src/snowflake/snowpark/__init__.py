036b4561f903db4c61c5e7701f10c3451842bb33
yixie <yijun.xie@snowflake.com>
2021-09-24 10:02:04 -0700

    expose Column, Row, Session, DataFrame and related classes in package snowpark

--- src/snowflake/snowpark/__init__.py
+++ src/snowflake/snowpark/__init__.py
@@ -4,4 +4,25 @@
 # Copyright (c) 2012-2021 Snowflake Computing Inc. All right reserved.
 #
 
-__all__ = []
+from snowflake.snowpark.column import Column  # Should CaseExpr be exposed?
+from snowflake.snowpark.dataframe import DataFrame
+from snowflake.snowpark.dataframe_reader import DataFrameReader
+from snowflake.snowpark.dataframe_writer import DataFrameWriter
+from snowflake.snowpark.relational_grouped_dataframe import RelationalGroupedDataFrame
+from snowflake.snowpark.row import Row
+from snowflake.snowpark.session import (
+    Session,  # Why do we choose Session over SnowparkSession?
+)
+
+# types, udf, functions, exceptions still use its own modules
+# rename snowpark_client_exception.py to exception.py or errors.py?
+
+__all__ = [
+    "Column",
+    "Row",
+    "Session",
+    "DataFrame",
+    "DataFrameWriter",
+    "DataFrameReader",
+    "RelationalGroupedDataFrame",
+]
