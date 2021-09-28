0f80719ba174e74b6593752cc57f053257378cf6
Jianzhun Du <jianzhun.du@snowflake.com>
2021-09-28 00:52:27 +0000

    validate

--- test/integ/scala/test_session_suite.py
+++ test/integ/scala/test_session_suite.py
@@ -3,6 +3,7 @@
 #
 # Copyright (c) 2012-2021 Snowflake Computing Inc. All right reserved.
 #
+from test.utils import Utils
 from typing import NamedTuple
 
 import pytest
@@ -11,45 +12,44 @@ from snowflake.snowpark import Row, Session
 from snowflake.snowpark.exceptions import (
     SnowparkInvalidObjectNameException,
     SnowparkMissingDbOrSchemaException,
+    SnowparkSessionException,
 )
+from snowflake.snowpark.types import IntegerType, StringType, StructField, StructType
 
 
-def test_createDataFrame_sequence(session_cnx):
-    with session_cnx() as session:
-        df = session.createDataFrame([[1, "one", 1.0], [2, "two", 2.0]])
-        assert [field.name for field in df.schema.fields] == ["_1", "_2", "_3"]
-        assert df.collect() == [Row(1, "one", 1.0), Row(2, "two", 2.0)]
+def test_create_dataframe_sequence(session):
+    df = session.createDataFrame([[1, "one", 1.0], [2, "two", 2.0]])
+    assert [field.name for field in df.schema.fields] == ["_1", "_2", "_3"]
+    assert df.collect() == [Row(1, "one", 1.0), Row(2, "two", 2.0)]
 
-        df = session.createDataFrame([1, 2])
-        assert [field.name for field in df.schema.fields] == ["VALUES"]
-        assert df.collect() == [Row(1), Row(2)]
+    df = session.createDataFrame([1, 2])
+    assert [field.name for field in df.schema.fields] == ["VALUES"]
+    assert df.collect() == [Row(1), Row(2)]
 
-        df = session.createDataFrame(["one", "two"])
-        assert [field.name for field in df.schema.fields] == ["VALUES"]
-        assert df.collect() == [Row("one"), Row("two")]
+    df = session.createDataFrame(["one", "two"])
+    assert [field.name for field in df.schema.fields] == ["VALUES"]
+    assert df.collect() == [Row("one"), Row("two")]
 
 
-def test_create_dataFrame_namedtuple(session_cnx):
+def test_create_dataframe_namedtuple(session):
     P1 = NamedTuple("P1", [("a", int), ("b", str), ("c", float)])
-    with session_cnx() as session:
-        df = session.createDataFrame([P1(1, "one", 1.0), P1(2, "two", 2.0)])
-        assert [field.name for field in df.schema.fields] == ["A", "B", "C"]
+    df = session.createDataFrame([P1(1, "one", 1.0), P1(2, "two", 2.0)])
+    assert [field.name for field in df.schema.fields] == ["A", "B", "C"]
 
 
 # this test requires the parameters used for connection has `public role`,
 # and the public role has the privilege to access the current database and
 # schema of the current role
-def test_get_schema_database_works_after_use_role(session_cnx):
-    with session_cnx() as session:
-        current_role = session.conn._get_string_datum("select current_role()")
-        try:
-            db = session.getCurrentDatabase()
-            schema = session.getCurrentSchema()
-            session._run_query("use role public")
-            assert session.getCurrentDatabase() == db
-            assert session.getCurrentSchema() == schema
-        finally:
-            session._run_query("use role {}".format(current_role))
+def test_get_schema_database_works_after_use_role(session):
+    current_role = session.conn._get_string_datum("select current_role()")
+    try:
+        db = session.getCurrentDatabase()
+        schema = session.getCurrentSchema()
+        session._run_query("use role public")
+        assert session.getCurrentDatabase() == db
+        assert session.getCurrentSchema() == schema
+    finally:
+        session._run_query("use role {}".format(current_role))
 
 
 def test_negative_test_for_missing_required_parameter_schema(db_parameters):
@@ -59,15 +59,56 @@ def test_negative_test_for_missing_required_parameter_schema(db_parameters):
     assert "The SCHEMA is not set for the current session." in str(ex_info)
 
 
-def test_negative_test_to_invalid_table_name(session_cnx):
-    with session_cnx() as session:
-        with pytest.raises(SnowparkInvalidObjectNameException) as ex_info:
-            session.table("negative.test.invalid.table.name")
-        assert "The object name 'negative.test.invalid.table.name' is invalid." in str(
-            ex_info
-        )
+def test_negative_test_to_invalid_table_name(session):
+    with pytest.raises(SnowparkInvalidObjectNameException) as ex_info:
+        session.table("negative.test.invalid.table.name")
+    assert "The object name 'negative.test.invalid.table.name' is invalid." in str(
+        ex_info
+    )
 
 
-def test_create_dataframe_from_seq_none(session_cnx):
-    with session_cnx() as session:
-        assert session.createDataFrame([None, 1]).collect() == [Row(None), Row(1)]
+def test_create_dataframe_from_seq_none(session):
+    assert session.createDataFrame([None, 1]).collect() == [Row(None), Row(1)]
+
+
+def test_create_dataframe_from_array(session):
+    data = [Row(1, "a"), Row(2, "b")]
+    schema = StructType(
+        [StructField("num", IntegerType()), StructField("str", StringType())]
+    )
+    df = session.createDataFrame(data, schema)
+    assert df.collect() == data
+
+    # negative
+    data1 = [Row("a", 1), Row(2, "b")]
+    with pytest.raises(TypeError) as ex_info:
+        session.createDataFrame(data1, schema)
+    assert "Unsupported datatype" in str(ex_info)
+
+
+def test_dataframe_created_before_session_close_are_not_usable_after_closing_session(
+    db_parameters,
+):
+    new_session = Session.builder.configs(db_parameters).create()
+    df = new_session.range(10)
+    read = new_session.read
+    new_session.close()
+
+    with pytest.raises(SnowparkSessionException) as ex_info:
+        df.collect()
+    assert ex_info.value.error_code == "1404"
+    with pytest.raises(SnowparkSessionException) as ex_info:
+        read.json("@mystage/prefix")
+    assert ex_info.value.error_code == "1404"
+
+
+def test_load_table_from_array_multipart_identifier(session):
+    name = Utils.random_name()
+    try:
+        Utils.create_table(session, name, "col int")
+        db = session.getCurrentDatabase()
+        sc = session.getCurrentSchema()
+        multipart = [db, sc, name]
+        assert len(session.table(multipart).schema.fields) == 1
+    finally:
+        Utils.drop_table(session, name)
--- test/integ/test_session.py
+++ test/integ/test_session.py
@@ -14,15 +14,14 @@ from snowflake.snowpark import Row, Session
 from snowflake.snowpark._internal.analyzer.analyzer_package import AnalyzerPackage
 
 
-def test_select_1(session_cnx):
-    with session_cnx() as session:
-        res = session.sql("select 1").collect()
-        assert res == [Row(1)]
+def test_select_1(session):
+    res = session.sql("select 1").collect()
+    assert res == [Row(1)]
 
 
-def test_invalid_configs(db_parameters):
+def test_invalid_configs(session, db_parameters):
     with pytest.raises(DatabaseError) as ex_info:
-        session = (
+        new_session = (
             Session.builder.configs(db_parameters)
             .config("user", "invalid_user")
             .config("password", "invalid_pwd")
@@ -30,118 +29,116 @@ def test_invalid_configs(db_parameters):
             .create()
         )
         assert "Incorrect username or password was specified" in str(ex_info)
-        session.close()
+        new_session.close()
+    # restore active session
+    Session._set_active_session(session)
 
 
-def test_no_default_database_and_schema(session_cnx, db_parameters):
-    invalid_parameters = db_parameters.copy()
-    invalid_parameters.pop("database")
-    invalid_parameters.pop("schema")
-    with session_cnx(invalid_parameters) as session:
-        assert not session.getDefaultDatabase()
-        assert not session.getDefaultSchema()
-        session.close()
+def test_no_default_database_and_schema(session, db_parameters):
+    new_session = (
+        Session.builder.configs(db_parameters)
+        ._remove_config("database")
+        ._remove_config("schema")
+        .create()
+    )
+    assert not new_session.getDefaultDatabase()
+    assert not new_session.getDefaultSchema()
+    new_session.close()
+    # restore active session
+    Session._set_active_session(session)
 
 
-def test_default_and_current_database_and_schema(session_cnx):
-    with session_cnx() as session:
-        default_database = session.getDefaultDatabase()
-        default_schema = session.getDefaultSchema()
+def test_default_and_current_database_and_schema(session):
+    default_database = session.getDefaultDatabase()
+    default_schema = session.getDefaultSchema()
+
+    assert Utils.equals_ignore_case(default_database, session.getCurrentDatabase())
+    assert Utils.equals_ignore_case(default_schema, session.getCurrentSchema())
+
+    try:
+        schema_name = Utils.random_name()
+        session._run_query("create schema {}".format(schema_name))
+
+        assert Utils.equals_ignore_case(default_database, session.getDefaultDatabase())
+        assert Utils.equals_ignore_case(default_schema, session.getDefaultSchema())
 
         assert Utils.equals_ignore_case(default_database, session.getCurrentDatabase())
-        assert Utils.equals_ignore_case(default_schema, session.getCurrentSchema())
-
-        try:
-            schema_name = Utils.random_name()
-            session._run_query("create schema {}".format(schema_name))
-
-            assert Utils.equals_ignore_case(
-                default_database, session.getDefaultDatabase()
-            )
-            assert Utils.equals_ignore_case(default_schema, session.getDefaultSchema())
-
-            assert Utils.equals_ignore_case(
-                default_database, session.getCurrentDatabase()
-            )
-            assert Utils.equals_ignore_case(
-                AnalyzerPackage.quote_name(schema_name), session.getCurrentSchema()
-            )
-        finally:
-            # restore
-            session._run_query("drop schema if exists {}".format(schema_name))
-            session._run_query("use schema {}".format(default_schema))
+        assert Utils.equals_ignore_case(
+            AnalyzerPackage.quote_name(schema_name), session.getCurrentSchema()
+        )
+    finally:
+        # restore
+        session._run_query("drop schema if exists {}".format(schema_name))
+        session._run_query("use schema {}".format(default_schema))
 
 
-def test_quote_all_database_and_schema_names(session_cnx):
-    with session_cnx() as session:
+def test_quote_all_database_and_schema_names(session):
+    def is_quoted(name: str) -> bool:
+        return name[0] == '"' and name[-1] == '"'
 
-        def is_quoted(name: str) -> bool:
-            return name[0] == '"' and name[-1] == '"'
-
-        assert is_quoted(session.getDefaultDatabase())
-        assert is_quoted(session.getDefaultSchema())
-        assert is_quoted(session.getCurrentDatabase())
-        assert is_quoted(session.getCurrentSchema())
+    assert is_quoted(session.getDefaultDatabase())
+    assert is_quoted(session.getDefaultSchema())
+    assert is_quoted(session.getCurrentDatabase())
+    assert is_quoted(session.getCurrentSchema())
 
 
-def test_active_session(session_cnx):
-    with session_cnx() as session:
-        assert session == Session._get_active_session()
+def test_active_session(session):
+    assert session == Session._get_active_session()
 
 
-def test_list_files_in_stage(session_cnx, resources_path):
+def test_list_files_in_stage(session, resources_path):
     stage_name = Utils.random_stage_name()
     special_name = f'"{stage_name}/aa"'
     test_files = TestFiles(resources_path)
-    with session_cnx() as session:
-        try:
-            Utils.create_stage(session, stage_name)
-            Utils.upload_to_stage(
-                session, stage_name, test_files.test_file_avro, compress=False
-            )
-            files = session._list_files_in_stage(stage_name)
-            assert len(files) == 1
-            assert os.path.basename(test_files.test_file_avro) in files
 
-            full_name = f"{session.getFullyQualifiedCurrentSchema()}.{stage_name}"
-            files2 = session._list_files_in_stage(full_name)
-            assert len(files2) == 1
-            assert os.path.basename(test_files.test_file_avro) in files2
+    try:
+        Utils.create_stage(session, stage_name)
+        Utils.upload_to_stage(
+            session, stage_name, test_files.test_file_avro, compress=False
+        )
+        files = session._list_files_in_stage(stage_name)
+        assert len(files) == 1
+        assert os.path.basename(test_files.test_file_avro) in files
 
-            prefix = "/prefix/prefix2"
-            with_prefix = stage_name + prefix
-            Utils.upload_to_stage(
-                session, with_prefix, test_files.test_file_avro, compress=False
-            )
-            files3 = session._list_files_in_stage(with_prefix)
-            assert len(files3) == 1
-            assert os.path.basename(test_files.test_file_avro) in files3
+        full_name = f"{session.getFullyQualifiedCurrentSchema()}.{stage_name}"
+        files2 = session._list_files_in_stage(full_name)
+        assert len(files2) == 1
+        assert os.path.basename(test_files.test_file_avro) in files2
 
-            # TODO: SNOW-425907 the following three test cases are not working
-            # because currently list_files_in_stage function only supports
-            # the simple parsing rule, for session stage in particular
-            # uncomment them once we figure out how to get the normalized
-            # stage location
+        prefix = "/prefix/prefix2"
+        with_prefix = stage_name + prefix
+        Utils.upload_to_stage(
+            session, with_prefix, test_files.test_file_avro, compress=False
+        )
+        files3 = session._list_files_in_stage(with_prefix)
+        assert len(files3) == 1
+        assert os.path.basename(test_files.test_file_avro) in files3
 
-            # quoted_name = f'"{stage_name}"{prefix}'
-            # files4 = session._list_files_in_stage(quoted_name)
-            # assert len(files4) == 1
-            # assert os.path.basename(test_files.test_file_avro) in files4
+        # TODO: SNOW-425907 the following three test cases are not working
+        # because currently list_files_in_stage function only supports
+        # the simple parsing rule, for session stage in particular
+        # uncomment them once we figure out how to get the normalized
+        # stage location
 
-            # full_name_with_prefix = (
-            #     f"{session.getFullyQualifiedCurrentSchema()}.{quoted_name}"
-            # )
-            # files5 = session._list_files_in_stage(full_name_with_prefix)
-            # assert len(files5) == 1
-            # assert os.path.basename(test_files.test_file_avro) in files5
-            #
-            # Utils.create_stage(session, special_name)
-            # Utils.upload_to_stage(
-            #     session, special_name, test_files.test_file_csv, compress=False
-            # )
-            # files6 = session._list_files_in_stage(special_name)
-            # assert len(files6) == 1
-            # assert os.path.basename(test_files.test_file_csv) in files6
-        finally:
-            Utils.drop_stage(session, stage_name)
-            Utils.drop_stage(session, special_name)
+        # quoted_name = f'"{stage_name}"{prefix}'
+        # files4 = session._list_files_in_stage(quoted_name)
+        # assert len(files4) == 1
+        # assert os.path.basename(test_files.test_file_avro) in files4
+
+        # full_name_with_prefix = (
+        #     f"{session.getFullyQualifiedCurrentSchema()}.{quoted_name}"
+        # )
+        # files5 = session._list_files_in_stage(full_name_with_prefix)
+        # assert len(files5) == 1
+        # assert os.path.basename(test_files.test_file_avro) in files5
+        #
+        # Utils.create_stage(session, special_name)
+        # Utils.upload_to_stage(
+        #     session, special_name, test_files.test_file_csv, compress=False
+        # )
+        # files6 = session._list_files_in_stage(special_name)
+        # assert len(files6) == 1
+        # assert os.path.basename(test_files.test_file_csv) in files6
+    finally:
+        Utils.drop_stage(session, stage_name)
+        Utils.drop_stage(session, special_name)
