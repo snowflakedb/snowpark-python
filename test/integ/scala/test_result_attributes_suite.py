de28934d9323122b44be1232ab9d080d76f138fc
Jianzhun Du <68252326+sfc-gh-jdu@users.noreply.github.com>
2021-09-29 20:43:24 -0700

    Validate session suite and result_attributes suite (#96)

--- test/integ/scala/test_result_attributes_suite.py
+++ test/integ/scala/test_result_attributes_suite.py
@@ -25,16 +25,15 @@ from snowflake.snowpark.types import (
 )
 
 
-def get_table_attributes(session: "Session", name: str) -> List["Attribute"]:
-    return session._get_result_attributes("select * from {name}".format(name=name))
+def get_table_attributes(session: Session, name: str) -> List[Attribute]:
+    return session._get_result_attributes(f"select * from {name}")
 
 
 def get_attributes_with_types(
-    session: "Session", name: str, types: List[str]
-) -> List["Attribute"]:
-    attributes = []
+    session: Session, name: str, types: List[str]
+) -> List[Attribute]:
     try:
-        schema = ",".join(["col_{} {}".format(idx, tp) for idx, tp in enumerate(types)])
+        schema = ",".join([f"col_{idx} {tp}" for idx, tp in enumerate(types)])
         utils.create_table(session, name, schema)
         attributes = get_table_attributes(session, name)
     finally:
@@ -42,52 +41,47 @@ def get_attributes_with_types(
     return attributes
 
 
-def test_integer_type(session_cnx):
+def test_integer_type(session):
     integers = ["number", "decimal", "numeric", "bigint", "int", "integer", "smallint"]
     table_name = utils.random_name()
-    with session_cnx() as session:
-        attributes = get_attributes_with_types(session, table_name, integers)
+    attributes = get_attributes_with_types(session, table_name, integers)
     for attribute in attributes:
         assert type(attribute.datatype) == LongType
 
 
-def test_float_type(session_cnx):
+def test_float_type(session):
     floats = ["float", "float4", "double", "real"]
     table_name = utils.random_name()
-    with session_cnx() as session:
-        attributes = get_attributes_with_types(session, table_name, floats)
+    attributes = get_attributes_with_types(session, table_name, floats)
     for attribute in attributes:
         assert type(attribute.datatype) == DoubleType
 
 
-def test_string_type(session_cnx):
+def test_string_type(session):
     strings = ["varchar", "char", "character", "string", "text"]
     table_name = utils.random_name()
-    with session_cnx() as session:
-        attributes = get_attributes_with_types(session, table_name, strings)
+    attributes = get_attributes_with_types(session, table_name, strings)
     for attribute in attributes:
         assert type(attribute.datatype) == StringType
 
 
-def test_binary_type(session_cnx):
+def test_binary_type(session):
     binarys = ["binary", "varbinary"]
     table_name = utils.random_name()
-    with session_cnx() as session:
-        attributes = get_attributes_with_types(session, table_name, binarys)
+    attributes = get_attributes_with_types(session, table_name, binarys)
     for attribute in attributes:
         assert type(attribute.datatype) == BinaryType
 
 
-def test_logical_type(session_cnx):
+def test_logical_type(session):
     logicals = ["boolean"]
     table_name = utils.random_name()
-    with session_cnx() as session:
-        attributes = get_attributes_with_types(session, table_name, logicals)
+    attributes = get_attributes_with_types(session, table_name, logicals)
     for attribute in attributes:
         assert type(attribute.datatype) == BooleanType
 
 
-def test_date_and_time_type(session_cnx):
+def test_date_and_time_type(session):
     dates = {
         "date": DateType,
         "datetime": TimestampType,
@@ -98,32 +92,28 @@ def test_date_and_time_type(session_cnx):
         "timestamp_tz": TimestampType,
     }
     table_name = utils.random_name()
-    with session_cnx() as session:
-        attributes = get_attributes_with_types(session, table_name, list(dates.keys()))
+    attributes = get_attributes_with_types(session, table_name, list(dates.keys()))
     for attribute, expected_type in zip(attributes, dates.values()):
         assert type(attribute.datatype) == expected_type
 
 
-def test_semi_structured_type(session_cnx):
+def test_semi_structured_type(session):
     semi_structures = ["variant", "object"]
     table_name = utils.random_name()
-    with session_cnx() as session:
-        attributes = get_attributes_with_types(session, table_name, semi_structures)
+    attributes = get_attributes_with_types(session, table_name, semi_structures)
     assert type(attributes[0].datatype) == VariantType
     assert type(attributes[1].datatype) == MapType
 
 
-def test_array_type(session_cnx):
+def test_array_type(session):
     semi_structures = ["array"]
     table_name = utils.random_name()
-    with session_cnx() as session:
-        attributes = get_attributes_with_types(session, table_name, semi_structures)
+    attributes = get_attributes_with_types(session, table_name, semi_structures)
     assert type(attributes[0].datatype) == ArrayType
 
 
-@pytest.mark.skip("SNOW-442047: show shares doesn't work; re-enable after the fix")
 def test_describe_schema_matches_execute_schema_for_show_queries(
-    session_cnx, db_parameters
+    session, db_parameters
 ):
     objs = [
         "tables",
@@ -141,7 +131,7 @@ def test_describe_schema_matches_execute_schema_for_show_queries(
         "procedures",
         "parameters",
         "functions",
-        "shares",
+        # "shares", TODO: SNOW-442047: show shares doesn't work; re-enable after the fix
         "roles",
         "grants",
         "warehouses",
@@ -150,16 +140,15 @@ def test_describe_schema_matches_execute_schema_for_show_queries(
         "regions",
         "integrations",
     ]
-    with session_cnx() as session:
-        for obj in objs:
-            query = "show {}".format(obj)
-            # describe query
-            show_query_schema_describe = session._get_result_attributes(query)
-            assert len(show_query_schema_describe) > 0
-            # execute query
-            session._run_query(query)
-            show_query_schema_execute = session.conn._cursor.description
-            assert len(show_query_schema_execute) > 0
-            assert [attribute.name for attribute in show_query_schema_describe] == [
-                '"' + column[0] + '"' for column in show_query_schema_execute
-            ]
+    for obj in objs:
+        query = "show {}".format(obj)
+        # describe query
+        show_query_schema_describe = session._get_result_attributes(query)
+        assert len(show_query_schema_describe) > 0
+        # execute query
+        session._run_query(query)
+        show_query_schema_execute = session.conn._cursor.description
+        assert len(show_query_schema_execute) > 0
+        assert [attribute.name for attribute in show_query_schema_describe] == [
+            '"' + column[0] + '"' for column in show_query_schema_execute
+        ]
