bb67911bd493ff3108160d6b80155fa6fa0c9728
Jianzhun Du <68252326+sfc-gh-jdu@users.noreply.github.com>
2021-10-01 11:18:37 -0700

    SNOW-471687: Use QA1 for jenkins regression test (#103)

--- scripts/jenkins_regress.sh
+++ scripts/jenkins_regress.sh
@@ -15,6 +15,7 @@ exit_code_decorator(){
 
 # test
 # decrypt profile
-gpg --quiet --batch --yes --decrypt --passphrase="$GPG_KEY" --output "$TEST_DIR/parameters.py" scripts/parameters.py.gpg
+# TODO SNOW-471687: use preprod3 instead of qa1 after preprod3 has python 3.8 change
+gpg --quiet --batch --yes --decrypt --passphrase="$GPG_KEY" --output "$TEST_DIR/parameters.py" scripts/parameters_qa1.py.gpg
 
 exit_code_decorator "python -m pytest $TEST_DIR"
