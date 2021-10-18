91ca84b6e04231673487f3d9f22650b1f28960d2
Haoran Yu <haoran.yu@snowflake.com>
2021-10-18 23:42:40 +0000

    Create standardization and transform()

---
+++ src/snowflake/snowpark/ml/transformers/standardization.py
@@ -0,0 +1,20 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012-2021 Snowflake Computing Inc. All rights reserved.
+#
+
+
+class Standardization:
+    def __init__(self, session=None):
+        self.session = session
+
+    __TEMP_TABLE = "table_temp"
+
+    def fit(self) -> None:
+        pass
+
+    def transform(self, value) -> float:
+        mean = self.session.table(self.__TEMP_TABLE).select("mean")
+        stddev = self.session.table(self.__TEMP_TABLE).select("stddev")
+        return (value - mean) / stddev
