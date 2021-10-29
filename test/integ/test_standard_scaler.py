ecbc14de2cc4660abebc34ee146506c11ba31b12
Haoran Yu <haoran.yu@snowflake.com>
2021-10-29 21:52:51 +0000

    Update test_standard_scaler.py

--- test/integ/test_standard_scaler.py
+++ test/integ/test_standard_scaler.py
@@ -3,19 +3,22 @@
 #
 # Copyright (c) 2012-2021 Snowflake Computing Inc. All rights reserved.
 #
+
 from snowflake.snowpark import Row
 from snowflake.snowpark.ml.transformers.standard_scaler import StandardScaler
 
+TABLENAME = "hayu.standardscaler.table_temp"
+
 
 def test_transform(session):
-    mean = 5.492480
-    stddev = 2.884792887
+    mean = float(session.table(TABLENAME).collect()[0][0])
+    stddev = float(session.table(TABLENAME).collect()[0][1])
 
-    df = session.createDataFrame([2.0, 5.0]).toDF("value")
+    df = session.createDataFrame([-1.0, 5.0]).toDF("value")
     expected_df = (
         session.createDataFrame(
             [
-                Row((2.0 - mean) / stddev),
+                Row((-1.0 - mean) / stddev),
                 Row((5.0 - mean) / stddev),
             ]
         )
@@ -26,4 +29,4 @@ def test_transform(session):
     actual_df = df.select(scaler.transform(df.col("value"))).collect()
 
     for value1, value2 in zip(expected_df, actual_df):
-        assert "{:.5f}".format(value1[0]) == "{:.5f}".format(value2[0])
+        assert "{:.6f}".format(value1[0]) == "{:.6f}".format(value2[0])
