57e735c06f9afa7b546557cf5a1f6869cadf0b63
Haoran Yu <haoran.yu@snowflake.com>
2021-10-29 23:03:07 +0000

    Add test_imputer.py

---
+++ test/integ/test_imputer.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2012-2021 Snowflake Computing Inc. All rights reserved.
+#
+from snowflake.snowpark.ml.transformers.Imputer import Imputer
+
+TABLENAME = "hayu.imputer.table_temp"
+
+
+def test_transform_numerical(session):
+    mean = float(session.table(TABLENAME).collect()[0][0])
+
+    df = session.createDataFrame([1.0, None]).toDF("value")
+    expected_df = session.createDataFrame([1.0, mean]).toDF(["expected"]).collect()
+    imputer = Imputer()
+    actual_df = df.select(imputer.transform(df.col("value"))).collect()
+
+    assert expected_df == actual_df
+
+
+def test_transform_categorical(session):
+    mode = session.table(TABLENAME).collect()[0][1]
+
+    df = session.createDataFrame(["a", None]).toDF("value")
+    expected_df = session.createDataFrame(["a", mode]).toDF(["expected"]).collect()
+    imputer = Imputer()
+    actual_df = df.select(imputer.transform(df.col("value"))).collect()
+
+    assert expected_df == actual_df
