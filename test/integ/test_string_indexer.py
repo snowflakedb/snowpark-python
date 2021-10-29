833b317976d935656bed1ef61f321dba8d674a1f
Haoran Yu <haoran.yu@snowflake.com>
2021-10-29 22:02:41 +0000

    Update test_string_indexer.py

--- test/integ/test_string_indexer.py
+++ test/integ/test_string_indexer.py
@@ -3,32 +3,19 @@
 #
 # Copyright (c) 2012-2021 Snowflake Computing Inc. All rights reserved.
 #
-import logging
-from test.utils import Utils
 
 from snowflake.snowpark import Row
 from snowflake.snowpark.ml.transformers.string_indexer import StringIndexer
 
-logger = logging.getLogger(__name__)
+TABLENAME = "hayu.stringindexer.table_temp"
 
 
 def test_transform(session):
-    df = session.createDataFrame([[8815942446845118937], [-3774021408702477068]]).toDF(
+    df = session.createDataFrame([["Northwest Territories"], ["Alberta"]]).toDF(
         ["value"]
     )
     indexer = StringIndexer()
-    output_df = df.select(indexer.transform(df.col("value"))).collect()
-    expected_df = (
-        session.createDataFrame([Row(17555), Row(41209)]).toDF("output").collect()
-    )
-    # expected_df = session.createDataFrame([Row(-1), Row(41209)]).toDF(["expected"]).collect()
+    expected_df = session.createDataFrame([Row(2), Row(11)]).toDF("expected").collect()
+    actual_df = df.select(indexer.transform(df.col("value"))).collect()
 
-    logger.info(f"INPUT: {df}")
-    logger.info(f"OUTPUT: {output_df}")
-    logger.info(f"EXPECTED: {expected_df}")
-
-    assert output_df == expected_df
-
-    # Utils.check_answer(
-    #     [Row(17555), Row(41209)], output_df
-    # )
+    assert expected_df == actual_df
