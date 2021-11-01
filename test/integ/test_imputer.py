82b374ccf729692ee7fab2249e6fff83e50f851a
Haoran Yu <haoran.yu@snowflake.com>
2021-11-01 18:15:01 +0000

    Update test_imputer.py

--- test/integ/test_imputer.py
+++ test/integ/test_imputer.py
@@ -3,7 +3,6 @@
 #
 # Copyright (c) 2012-2021 Snowflake Computing Inc. All rights reserved.
 #
-
 from snowflake.snowpark.ml.transformers.Imputer import Imputer
 
 TABLENAME = "hayu.imputer.table_temp"
