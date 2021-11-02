428fcf8efc7cb1c8047b3fc4595738c5e0b580da
Haoran Yu <haoran.yu@snowflake.com>
2021-11-02 07:10:34 +0000

    Add test_fit(), update test_transform()

--- test/integ/test_string_indexer.py
+++ test/integ/test_string_indexer.py
@@ -4,16 +4,43 @@
 # Copyright (c) 2012-2021 Snowflake Computing Inc. All rights reserved.
 #
 
+from test.utils import Utils
+
 from snowflake.snowpark import Row
 from snowflake.snowpark.ml.transformers.string_indexer import StringIndexer
 
-TABLENAME = "hayu.stringindexer.table_temp"
+TABLENAME = "table_temp"
+
+
+def test_fit(session):
+    input_df = session.createDataFrame(["a", "b", "c", "b", "d", "a"]).toDF(
+        "input_value"
+    )
+    expected_df = session.createDataFrame(
+        [
+            Row("a", 1),
+            Row("b", 2),
+            Row("c", 3),
+            Row("d", 4),
+        ]
+    ).toDF("distinct_value", "index")
+
+    indexer = StringIndexer(session=session, input_col="input_value")
+    indexer.fit(input_df)
+    actual_df = session.table(TABLENAME)
+
+    Utils.check_answer(expected_df, actual_df)
 
 
 def test_transform(session):
-    df = session.createDataFrame(["Northwest Territories", "Alberta"]).toDF("value")
-    indexer = StringIndexer()
-    expected_df = session.createDataFrame([Row(2), Row(11)]).toDF("expected").collect()
-    actual_df = df.select(indexer.transform(df.col("value"))).collect()
+    input_df = session.createDataFrame(["a", "b", "c", "b", "d", "a"]).toDF(
+        "input_value"
+    )
+    indexer = StringIndexer(session=session, input_col="input_value")
+    indexer.fit(input_df)
 
-    assert expected_df == actual_df
+    df = session.createDataFrame(["b", "d", "z", "a"]).toDF("value")
+    expected_df = session.createDataFrame([2, 4, -1, 1]).toDF("expected")
+    actual_df = df.select(indexer.transform(df["value"]))
+
+    Utils.check_answer(expected_df, actual_df)
