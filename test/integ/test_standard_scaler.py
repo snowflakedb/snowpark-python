af9e37bd391cb5c93e55fe262ce15b34f3f77357
Haoran Yu <haoran.yu@snowflake.com>
2021-11-02 06:52:13 +0000

    Update test_fit() and test_transform()

--- test/integ/test_standard_scaler.py
+++ test/integ/test_standard_scaler.py
@@ -5,42 +5,40 @@
 #
 
 from snowflake.snowpark import Row
+from snowflake.snowpark.functions import avg, stddev
 from snowflake.snowpark.ml.transformers.standard_scaler import StandardScaler
 
-TABLENAME = "hayu.standardscaler.table_temp"
+TABLENAME = "table_temp"
 
 
 def test_fit(session):
-    df = session.createDataFrame([-1.0, 2.0, 3.5, 4.0]).toDF("value")
-    expected_mean = 2.125
-    expected_stddev = 1.948557158515
+    input_df = session.createDataFrame([-1.0, 2.0, 3.5, 4.0]).toDF("input_value")
+    expected_mean = input_df.select(avg(input_df["input_value"])).collect()[0][0]
+    expected_stddev = input_df.select(stddev(input_df["input_value"])).collect()[0][0]
 
-    scaler = StandardScaler(session=session, input_col="value")
-    scaler.fit(df)
+    scaler = StandardScaler(session=session, input_col="input_value")
+    scaler.fit(input_df)
     actual_mean = float(session.table(TABLENAME).collect()[0][0])
     actual_stddev = float(session.table(TABLENAME).collect()[0][1])
 
-    assert "{:.6f}".format(expected_mean) == "{:.6f}".format(actual_mean)
-    assert "{:.6f}".format(expected_stddev) == "{:.6f}".format(actual_stddev)
+    assert "{:.6f}".format(actual_mean) == "{:.6f}".format(expected_mean)
+    assert "{:.6f}".format(actual_stddev) == "{:.6f}".format(expected_stddev)
 
 
 def test_transform(session):
-    mean = float(session.table(TABLENAME).collect()[0][0])
-    stddev = float(session.table(TABLENAME).collect()[0][1])
+    input_df = session.createDataFrame([-1.0, 2.0, 3.5, 4.0]).toDF("input_value")
+    expected_mean = input_df.select(avg(input_df["input_value"])).collect()[0][0]
+    expected_stddev = input_df.select(stddev(input_df["input_value"])).collect()[0][0]
 
-    df = session.createDataFrame([-1.0, 5.0]).toDF("value")
-    expected_df = (
-        session.createDataFrame(
-            [
-                Row((-1.0 - mean) / stddev),
-                Row((5.0 - mean) / stddev),
-            ]
-        )
-        .toDF("expected")
-        .collect()
-    )
-    scaler = StandardScaler()
-    actual_df = df.select(scaler.transform(df.col("value"))).collect()
+    scaler = StandardScaler(session=session, input_col="input_value")
+    scaler.fit(input_df)
 
-    for value1, value2 in zip(expected_df, actual_df):
+    values = [-0.6, 5.0, 10.2]
+    df = session.createDataFrame(values).toDF("value")
+    expected_df = session.createDataFrame(
+        [Row((v - expected_mean) / expected_stddev) for v in values]
+    ).toDF("expected")
+    actual_df = df.select(scaler.transform(df["value"]))
+
+    for value1, value2 in zip(actual_df.collect(), expected_df.collect()):
         assert "{:.6f}".format(value1[0]) == "{:.6f}".format(value2[0])
