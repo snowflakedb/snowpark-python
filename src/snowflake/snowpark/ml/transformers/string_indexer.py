57e8d1ef8e4e5b8d96adffebcdc45f03fcc156d9
Haoran Yu <haoran.yu@snowflake.com>
2021-11-05 00:43:10 +0000

    Check input types

--- src/snowflake/snowpark/ml/transformers/string_indexer.py
+++ src/snowflake/snowpark/ml/transformers/string_indexer.py
@@ -17,9 +17,19 @@ class StringIndexer:
         self.input_col = input_col
 
     def fit(self, input_df: DataFrame) -> str:
+        if type(input_df) is not DataFrame:
+            raise TypeError(
+                f"StringIndexer.fit() input type must be DataFrame. Got: {type(input_df)}"
+            )
+
         query = input_df.select(self.input_col)._DataFrame__plan.queries[-1].sql
         res = self.session.sql(f"call {self.__BUNDLE}.fit($${query}$$)").collect()
         return res[0][0]
 
     def transform(self, col: Column) -> Column:
+        if type(col) is not Column:
+            raise TypeError(
+                f"StringIndexer.transform() input type must be Column. Got: {type(col)}"
+            )
+
         return builtin(f"{self.__BUNDLE}.transform")(col)
