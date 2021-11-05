3bee512aec8dffb5a785fe3e5411dcb16a538cc1
Haoran Yu <haoran.yu@snowflake.com>
2021-11-05 18:49:34 +0000

    Add input type checks

--- src/snowflake/snowpark/ml/transformers/Imputer.py
+++ src/snowflake/snowpark/ml/transformers/Imputer.py
@@ -16,12 +16,27 @@ class Imputer:
         self.session = session
         self.input_col = input_col
 
-    def fit(self, input_df: DataFrame, isNumerical: bool) -> str:
+    def fit(self, input_df: DataFrame, is_numerical: bool) -> str:
+        if type(input_df) is not DataFrame:
+            raise TypeError(
+                f"Imputer.fit() input input_df type must be DataFrame. Got: {type(input_df)}"
+            )
+
+        if type(is_numerical) is not bool:
+            raise TypeError(
+                f"Imputer.fit() input is_numerical type must be bool. Got: {type(is_numerical)}"
+            )
+
         query = input_df.select(self.input_col)._DataFrame__plan.queries[-1].sql
         res = self.session.sql(
-            f"call {self.__BUNDLE}.fit($${query}$$, {isNumerical})"
+            f"call {self.__BUNDLE}.fit($${query}$$, {is_numerical})"
         ).collect()
         return res[0][0]
 
     def transform(self, col: Column) -> Column:
+        if type(col) is not Column:
+            raise TypeError(
+                f"Imputer.transform() input type must be Column. Got: {type(col)}"
+            )
+
         return builtin(f"{self.__BUNDLE}.transform")(col)
