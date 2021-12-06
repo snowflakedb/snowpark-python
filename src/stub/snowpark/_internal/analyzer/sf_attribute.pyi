9b4a884ec0d265d99baa8dfe1d534940d1d58569
yixie <yijun.xie@snowflake.com>
2021-12-06 14:27:47 -0800

    remove internal stub

--- src/stub/snowpark/_internal/analyzer/sf_attribute.pyi
+++
@@ -1,9 +0,0 @@
-from typing import Any
-
-from snowflake.snowpark._internal.sp_types.sp_data_types import DataType as DataType
-
-class Attribute:
-    name: Any
-    datatype: Any
-    nullable: Any
-    def __init__(self, name: str, datatype: DataType, nullable: bool = ...) -> None: ...
--- src/stub/snowpark/_internal/analyzer/sp_identifiers.pyi
+++
@@ -1,9 +0,0 @@
-from typing import Any, Optional
-
-class IdentifierWithDatabase:
-    database: Optional[str]
-
-class TableIdentifier(IdentifierWithDatabase):
-    table: Any
-    database: Any
-    def __init__(self, table: str, database_name: Optional[str] = ...) -> None: ...
