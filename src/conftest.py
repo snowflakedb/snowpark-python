befc2d3a58d8948935adb7cf13f3e186cb8dc3b7
yixie <yijun.xie@snowflake.com>
2022-02-15 23:05:21 -0800

    Rename pytest fixture to add_snowpark_session

--- src/conftest.py
+++ src/conftest.py
@@ -11,7 +11,7 @@ from snowflake.snowpark import Session
 @pytest.fixture(
     autouse=True, scope="session"
 )  # scope session is pytest session, not the snowpark session.
-def add_np(doctest_namespace):
+def add_snowpark_session(doctest_namespace):
     with open("tests/parameters.py", encoding="utf-8") as f:
         exec(f.read(), globals())
     print(globals()["CONNECTION_PARAMETERS"])
