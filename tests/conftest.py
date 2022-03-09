ad04e39680ffe0c7d9d9c072554c2fc865b780c2
Kris Wagner <32375658+sfc-gh-kwagner@users.noreply.github.com>
2022-03-09 12:25:36 -0800

    SNOW-553202: Configure doctests to run as part of our test merge gates (#255)
    Added doctest target and updated commands to run doctests first

--- tests/conftest.py
+++ tests/conftest.py
@@ -13,8 +13,19 @@ logging.getLogger("snowflake.connector").setLevel(logging.ERROR)
 def pytest_collection_modifyitems(items) -> None:
     """Applies tags to tests based on folders that they are in."""
     top_test_dir = Path(__file__).parent
+    top_doctest_dir = top_test_dir.parent.joinpath("src/snowflake/snowpark")
     for item in items:
         item_path = Path(str(item.fspath)).parent
-        relative_path = item_path.relative_to(top_test_dir)
-        for part in relative_path.parts:
-            item.add_marker(part)
+        try:
+            relative_path = item_path.relative_to(top_test_dir)
+            for part in relative_path.parts:
+                item.add_marker(part)
+        except ValueError as e:
+            # item_path.relative_to(top_test_dir) will throw an error if
+            # the path isn't in the tests dir. We also accept doctest files
+            # in src/snowflake/snowpark (and set a marker for them) but
+            # we raise an exception for all other dirs that are passed in
+            if item_path == top_doctest_dir:
+                item.add_marker("doctest")
+            else:
+                raise e
