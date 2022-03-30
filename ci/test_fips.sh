ba3f58b3584ed4176b84946fb8e5c16c58a8e274
Kristopher Wagner <kristopher.wagner@snowflake.com>
2022-03-30 00:26:08 +0000

    Added flag to make OpenSSL run in FIPS mode

--- ci/test_fips.sh
+++ ci/test_fips.sh
@@ -10,6 +10,7 @@ SNOWPARK_WHL="$(ls $SNOWPARK_DIR/dist/*.whl | sort -r | head -n 1)"
 
 python3.8 -m venv fips_env
 source fips_env/bin/activate
+export OPENSSL_FIPS=1
 pip install -U setuptools pip
 pip install "${SNOWPARK_WHL}[pandas,secure-local-storage,development]"
 pip install "cryptography<3.3.0" --force-reinstall --no-binary cryptography
@@ -17,6 +18,7 @@ pip install "cryptography<3.3.0" --force-reinstall --no-binary cryptography
 echo "!!! Environment description !!!"
 echo "Default installed OpenSSL version"
 openssl version
+sysctl crypto.fips_enabled
 python -c "import ssl; print('Python openssl library: ' + ssl.OPENSSL_VERSION)"
 python -c  "from cryptography.hazmat.backends.openssl import backend;print('Cryptography openssl library: ' + backend.openssl_version_text())"
 python -c "import hashlib;print(hashlib.md5(b'G').digest());"
