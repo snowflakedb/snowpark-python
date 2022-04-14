872381da6b179ec96d027685703c55bce9095ad6
Jianzhun Du <68252326+sfc-gh-jdu@users.noreply.github.com>
2022-04-14 09:54:37 -0700

    SNOW-540230: Fix or mark several flaky tests (#304)

--- tests/integ/scala/test_dataframe_range_suite.py
+++ tests/integ/scala/test_dataframe_range_suite.py
@@ -92,7 +92,11 @@ def test_range_with_randomized_parameters(session):
         )
         assert len(res) != 0
         assert res[0][0] == expected_count
-        assert res[0][1] == expected_sum
+        if expected_count == 0:
+            # this dataframe from range is an empty dataframe, so SUM(ID) should be None
+            assert res[0][1] is None
+        else:
+            assert res[0][1] == expected_sum
 
 
 def test_range_with_max_and_min(session):
