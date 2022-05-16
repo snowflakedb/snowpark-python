af6875114872113c62b93776fa61d05eae87d0e2
Kristopher Wagner <kristopher.wagner@snowflake.com>
2022-05-16 22:34:11 +0000

    Updated script to get rid of whitespace in commands

--- scripts/tox_install_cmd.sh
+++ scripts/tox_install_cmd.sh
@@ -2,4 +2,14 @@
 
 set -e
 
-python -m pip install -U $@
+declare -a pip_options=()
+input_options=( "$@" )
+
+for val in "${input_options[@]}"; do
+  echo $val
+  pip_options+=(${val// /})
+done
+
+echo "${pip_options[*]}"
+
+python -m pip install -U ${pip_options[@]}
