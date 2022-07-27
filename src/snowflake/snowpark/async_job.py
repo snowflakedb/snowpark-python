888957834b0158c912c03d0969e3131b8f2ce338
Yuyang Wang <yuyang.wang@snowflake.com>
2022-07-27 15:52:34 -0700

    demo code

--- src/snowflake/snowpark/async_job.py
+++ src/snowflake/snowpark/async_job.py
@@ -2,11 +2,23 @@
 #
 # Copyright (c) 2012-2022 Snowflake Computing Inc. All rights reserved.
 #
+from snowflake.snowpark._internal.server_connection import SnowflakeConnection
 
 
 class AsyncJob:
-    def __init__(self, query_id: str) -> None:
+    def __init__(
+        self,
+        query_id: str = None,
+        query: str = None,
+        conn: SnowflakeConnection = None,
+    ) -> None:
         self.query_id = query_id
+        self.query = query
+        self._conn = conn
+        self._cursor = self._conn.cursor()
+
+        if not conn:
+            raise ValueError("connection cannot be empty")
         return
 
     @property
@@ -17,18 +29,33 @@ class AsyncJob:
     def query_id(self, value: str) -> None:
         self.query_id = value
 
+    @property
+    def query(self) -> str:
+        return self.query
+
+    @query.setter
+    def query(self, value: str) -> None:
+        self.query = value
+
+    # iterator
+    def __iter__(self):
+        pass
+
+    def __next__(self):
+        pass
+
     def is_done(self) -> bool:
         # return a bool value to indicate whether the query is finished
-        pass
+        status = self._conn.get_query_status(self.query_id)
+        is_running = self._conn.is_still_running(status)
+
+        return not is_running
 
     def cancel(self) -> None:
         # stop and cancel current query id
-        pass
+        self._conn._cancel_query(self.query, self.query_id)
 
     def collect(self):
         # return result of the query, in the form of a list of Row object
-        pass
-
-    def iterator(self):
-        # return result of the query, in the form of a iterator of Row object
-        pass
+        self._cursor.get_results_from_sfqid(self.query_id)
+        return self._cursor.fetchall()
