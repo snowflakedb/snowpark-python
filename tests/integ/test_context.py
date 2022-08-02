3d6f8eb1929f194eac6cd89301b2097599ab33d0
Afroz Alam <afroz.alam@snowflake.com>
2022-08-02 06:50:17 -0700

    SNOW-637199 init

---
+++ tests/integ/test_context.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+#
+# Copyright (c) 2012-2022 Snowflake Computing Inc. All rights reserved.
+#
+
+from snowflake.snowpark.context import get_active_session
+
+
+def test_get_active_session(session):
+    assert session == get_active_session()
