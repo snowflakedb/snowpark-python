0afcd3381e62fe1a2338d4d9831b9e0f3421ff3b
Afroz Alam <afroz.alam@snowflake.com>
2022-08-02 12:30:19 -0700

    SNOW-637199 added docstring for get_active_session

--- src/snowflake/snowpark/context.py
+++ src/snowflake/snowpark/context.py
@@ -2,9 +2,11 @@
 #
 # Copyright (c) 2012-2022 Snowflake Computing Inc. All rights reserved.
 #
-from snowflake.snowpark import Session
-from snowflake.snowpark.session import _get_active_session
+
+from snowflake.snowpark.session import Session, _get_active_session
 
 
-def get_active_session() -> "Session":
+def get_active_session() -> Session:
+    """Returns the current active snowpark session. Throws an error when
+    multiple sessions are active or when no sessions are created."""
     return _get_active_session()
