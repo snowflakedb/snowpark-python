1a93bdd64f491f90133b1d797469a72d8545f885
Afroz Alam <afroz.alam@snowflake.com>
2022-08-04 14:01:34 -0700

    SNOW-637199 docstrings that follow google format

--- src/snowflake/snowpark/context.py
+++ src/snowflake/snowpark/context.py
@@ -2,11 +2,16 @@
 #
 # Copyright (c) 2012-2022 Snowflake Computing Inc. All rights reserved.
 #
-
+"""Context module for Snowpark."""
 from snowflake.snowpark.session import Session, _get_active_session
 
 
 def get_active_session() -> Session:
-    """Returns the current active snowpark session. Throws an error when
-    multiple sessions are active or when no sessions are created."""
+    """Returns the current active snowpark session.
+
+    Raises: SnowparkSessionException: If there are more than one active sessions or no active sessions.
+
+    Returns:
+        A :class:`Session` object for current session.
+    """
     return _get_active_session()
