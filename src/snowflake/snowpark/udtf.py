7b7e5b64404d0c813e9fc4bd74ec38a609cfbb53
Shixuan Fan <shixuan.fan@snowflake.com>
2022-08-10 21:47:42 +0000

    SNOW-641436 Fix UDTF related doc
    Description
    
    As title
    
    Testing
    
    N/A

--- src/snowflake/snowpark/udtf.py
+++ src/snowflake/snowpark/udtf.py
@@ -93,9 +93,9 @@ class UDTFRegistration:
     permanently. The methods that register a UDTF return a :class:`UserDefinedTableFunction` object,
     which you can also use to call the UDTF.
 
-    Registering a UDTF is like registering a scalar UDF, you can use :meth:`register` or :func:`snowflake.snowpark.functions.udf`
+    Registering a UDTF is like registering a scalar UDF, you can use :meth:`register` or :func:`snowflake.snowpark.functions.udtf`
     to explicitly register it. You can also use the decorator `@udtf`. They all use ``cloudpickle`` to transfer the code from the client to the server.
-    Another way is to use :meth:`register_from_file`. Refer to module :class:`snowflake.snowpark.udf.UDFRegistration` for when to use them.
+    Another way is to use :meth:`register_from_file`. Refer to module :class:`snowflake.snowpark.udtf.UDTFRegistration` for when to use them.
 
     To query a registered UDTF is the same as to query other table functions.
     Refer to :meth:`~snowflake.snowpark.Session.table_function` and :meth:`~snowflake.snowpark.DataFrame.join_table_function`.
