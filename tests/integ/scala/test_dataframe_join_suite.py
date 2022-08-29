e49083aaf55864a419a7d34aa7f0fea7000ff492
yixie <yijun.xie@snowflake.com>
2022-08-29 14:44:51 -0700

    clean up a wrongly checked-in change

--- tests/integ/scala/test_dataframe_join_suite.py
+++ tests/integ/scala/test_dataframe_join_suite.py
@@ -145,9 +145,6 @@ def test_join_with_multiple_conditions(session):
     "SQL Simplifier:  Ambiguous columns will be fixed together with join simplifier"
 )
 def test_join_with_ambiguous_column_in_condidtion(session):
-    from snowflake.snowpark import context
-
-    context._USE_SQL_SIMPLIFIER = True
     df = session.create_dataframe([1, 2]).to_df(["a"])
     df2 = session.create_dataframe([[i, f"test{i}"] for i in range(1, 3)]).to_df(
         ["a", "b"]
