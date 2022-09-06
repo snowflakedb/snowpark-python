c5dbed47e497eb12cf81a1cc07c748ae924d95f7
Adam Ling <adam.ling@snowflake.com>
2022-09-06 11:44:43 -0700

    update tests

--- src/snowflake/snowpark/session.py
+++ src/snowflake/snowpark/session.py
@@ -1658,47 +1658,56 @@ class Session:
         return query_listener
 
     def _table_exists(self, table_name: str):
-        # implementation based upon: https://docs.snowflake.com/en/sql-reference/name-resolution.html
-        qualified_table_name = table_name.split(".")
-        if len(qualified_table_name) == 1:
-            # name in the form "table"
-            tables = self._run_query(f"show tables like '{table_name}'")
-        elif len(qualified_table_name) == 2:
-            # name in the form of "schema.table" omitting database
-            # schema: qualified_table_name[0]
-            # table: qualified_table_name[1]
-            tables = self._run_query(
-                f"show tables like '{qualified_table_name[1]}' in schema {qualified_table_name[0]}"
-            )
-        elif len(qualified_table_name) == 3:
-            # name in the form of "database.schema.table"
-            # database: qualified_table_name[0]
-            # schema: qualified_table_name[1]
-            # table: qualified_table_name[2]
-            if qualified_table_name[1] == "":
-                # Double-Dot Notation: "database..table" in which the schema is empty
+        try:
+            # implementation based upon: https://docs.snowflake.com/en/sql-reference/name-resolution.html
+            qualified_table_name = table_name.split(".")
+            if len(qualified_table_name) == 1:
+                # name in the form "table"
+                tables = self._run_query(f"show tables like '{table_name}'")
+            elif len(qualified_table_name) == 2:
+                # name in the form of "schema.table" omitting database
+                # schema: qualified_table_name[0]
+                # table: qualified_table_name[1]
                 tables = self._run_query(
-                    f"show tables like '{qualified_table_name[2]}' in database {qualified_table_name[0]}"
+                    f"show tables like '{qualified_table_name[1]}' in schema {qualified_table_name[0]}"
                 )
+            elif len(qualified_table_name) == 3:
+                # name in the form of "database.schema.table"
+                # database: qualified_table_name[0]
+                # schema: qualified_table_name[1]
+                # table: qualified_table_name[2]
+                if qualified_table_name[1] == "":
+                    # Double-Dot Notation: "database..table" in which the schema is empty
+                    tables = self._run_query(
+                        f"show tables like '{qualified_table_name[2]}' in database {qualified_table_name[0]}"
+                    )
+                else:
+                    # "database.schema.table", schema is not empty
+                    tables = self._run_query(
+                        f"show tables like '{qualified_table_name[2]}' in database {qualified_table_name[0]}"
+                    )
+                    # table is a tuple and the 3rd col(0 based idx) is schema_name
+                    return any(
+                        [
+                            table
+                            for table in tables
+                            if table[3].lower() == qualified_table_name[1].lower()
+                        ]
+                    )
             else:
-                # "database.schema.table", schema is not empty
-                tables = self._run_query(
-                    f"show tables like '{qualified_table_name[2]}' in database {qualified_table_name[0]}"
+                raise SnowparkClientExceptionMessages.GENERAL_INVALID_OBJECT_NAME(
+                    table_name
                 )
-                # table is a tuple and the 3rd col(0 based idx) is schema_name
-                return any(
-                    [
-                        table
-                        for table in tables
-                        if table[3].lower() == qualified_table_name[1].lower()
-                    ]
-                )
-        else:
-            raise SnowparkClientExceptionMessages.GENERAL_INVALID_OBJECT_NAME(
-                table_name
-            )
 
-        return tables is not None and len(tables) > 0
+            return tables is not None and len(tables) > 0
+        except ProgrammingError as exc:
+            if exc.errno == 2043:
+                # SQL compilation error: Object does not exist, or operation cannot be performed
+                return False
+            _logger.debug(
+                f"Error occurred when checking table existence for {table_name}, error: {exc!r}"
+            )
+            raise
 
     def _explain_query(self, query: str) -> Optional[str]:
         try:
