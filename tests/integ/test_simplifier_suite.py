ea4a0964ab4f85bdaad36edb5743628168c66f51
Afroz Alam <afroz.alam@snowflake.com>
2022-09-09 16:37:35 -0700

    SNOW-6248882 add output checks

--- tests/integ/test_simplifier_suite.py
+++ tests/integ/test_simplifier_suite.py
@@ -317,12 +317,16 @@ def test_select_with_table_function_join(session):
     df1 = df.select("a", split_to_table("c", lit(" ")))
     df2 = df.select("a", multiplier_udtf(df.b))
     # test multiple selects are flattened
-    df3 = df1.select("a", "seq").select("a")
+    expected = [Row(1), Row(1), Row(1), Row(2), Row(2), Row(2)]
+    df3 = df1.select("a", "seq").select("a").select("a")
+    Utils.check_answer(expected, df3)
     assert df1.queries["queries"][-1].count("SELECT") == df3.queries["queries"][
         -1
     ].count("SELECT")
 
-    df4 = df2.select("a", "two_x").select("a")
+    expected = [Row(1), Row(2)]
+    df4 = df2.select("a", "two_x").select("a").select("a")
+    Utils.check_answer(expected, df4)
     assert df2.queries["queries"][-1].count("SELECT") == df4.queries["queries"][
         -1
     ].count("SELECT")
@@ -352,13 +356,17 @@ def test_select_with_table_function_join(session):
     )
 
     # test expressions are not flattened
+    expected = [Row(3), Row(3), Row(3), Row(4), Row(4), Row(4)]
     df9 = df1.select((col("a") + 1).as_("a")).select((col("a") + 1).as_("a"))
+    Utils.check_answer(expected, df9)
     assert (
         df9.queries["queries"][-1].count("SELECT")
         == df1.queries["queries"][-1].count("SELECT") + 1
     )
 
+    expected = [Row(3), Row(4)]
     df10 = df2.select((col("a") + 1).as_("a")).select((col("a") + 1).as_("a"))
+    Utils.check_answer(expected, df10)
     assert (
         df10.queries["queries"][-1].count("SELECT")
         == df2.queries["queries"][-1].count("SELECT") + 1
@@ -381,28 +389,36 @@ def test_join_table_function(session):
     df2 = df.join_table_function(multiplier_udtf("b"))
 
     # test column flattens
+    expected = [Row(1), Row(1), Row(1), Row(2), Row(2), Row(2)]
     df3 = df1.select("a", "b", "seq")
-    df4 = df3.select("a", "seq").select("a")
+    df4 = df3.select("a", "seq").select("a").select("a")
+    Utils.check_answer(expected, df4)
     assert df3.queries["queries"][-1].count("SELECT") == df4.queries["queries"][
         -1
     ].count("SELECT")
 
+    expected = [Row(1), Row(2)]
     df5 = df2.select("a", "b", "two_x")
-    df6 = df5.select("a", "two_x").select("a")
+    df6 = df5.select("a", "two_x").select("a").select("a")
+    Utils.check_answer(expected, df6)
     assert df5.queries["queries"][-1].count("SELECT") == df6.queries["queries"][
         -1
     ].count("SELECT")
 
     # test column renames flatten for built-in fns
+    expected = [Row(2), Row(2), Row(2), Row(3), Row(3), Row(3)]
     df7 = df1.select("a", "seq")
-    df8 = df7.select((col("a") + 1).as_("a"), "seq").select("a")
+    df8 = df7.select((col("a") + 1).as_("a"), "seq").select("a").select("a")
+    Utils.check_answer(expected, df8)
     assert df7.queries["queries"][-1].count("SELECT") == df8.queries["queries"][
         -1
     ].count("SELECT")
 
     # test column rename flatten for user defined fns
+    expected = [Row(2), Row(3)]
     df9 = df2.select("a", "two_x")
-    df10 = df9.select((col("a") + 1).as_("a"), "two_x").select("a")
+    df10 = df9.select((col("a") + 1).as_("a"), "two_x").select("a").select("a")
+    Utils.check_answer(expected, df10)
     assert df9.queries["queries"][-1].count("SELECT") == df10.queries["queries"][
         -1
     ].count("SELECT")
@@ -413,14 +429,18 @@ def test_join_table_function(session):
     )
     df12 = df.join_table_function(multiplier_udtf("b").as_("x2", "x3"))
 
+    expected = [Row(1), Row(1), Row(1), Row(2), Row(2), Row(2)]
     df13 = df11.select("a", "b", "idx")
-    df14 = df13.select("a", "idx").select("a")
+    df14 = df13.select("a", "idx").select("a").select("a")
+    Utils.check_answer(expected, df14)
     assert df13.queries["queries"][-1].count("SELECT") == df14.queries["queries"][
         -1
     ].count("SELECT")
 
+    expected = [Row(1), Row(2)]
     df15 = df12.select("a", "b", "x2")
-    df16 = df15.select("a", "x2").select("a")
+    df16 = df15.select("a", "x2").select("a").select("a")
+    Utils.check_answer(expected, df16)
     assert df15.queries["queries"][-1].count("SELECT") == df16.queries["queries"][
         -1
     ].count("SELECT")
