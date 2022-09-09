471f8bf747fd44a8732d6189bcdb3e1f4363edda
Adam Ling <adam.ling@snowflake.com>
2022-09-09 16:05:19 -0700

    add comments

--- src/snowflake/snowpark/session.py
+++ src/snowflake/snowpark/session.py
@@ -259,6 +259,7 @@ class Session:
         self._plan_builder = SnowflakePlanBuilder(self)
         self._last_action_id = 0
         self._last_canceled_id = 0
+        # consider making _session_parameters check a helpful function
         self._use_scoped_temp_objects = bool(
             _use_scoped_temp_objects
             and (
