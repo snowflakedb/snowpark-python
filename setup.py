27475a531ec4f1f05d1902ab1bad7acf77ba5005
Yijun Xie <yijun.xie@snowflake.com>
2022-11-01 01:43:23 +0000

    SNOW-682115: Get ready to release 1.0.0 (#593)

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ from setuptools import setup
 THIS_DIR = os.path.dirname(os.path.realpath(__file__))
 SRC_DIR = os.path.join(THIS_DIR, "src")
 SNOWPARK_SRC_DIR = os.path.join(SRC_DIR, "snowflake", "snowpark")
-CONNECTOR_DEPENDENCY_VERSION = "2.7.12"
+CONNECTOR_DEPENDENCY_VERSION = ">=2.7.12, <3.0.0"
 
 # read the version
 VERSION = ()
@@ -37,7 +37,7 @@ setup(
     keywords="Snowflake db database cloud analytics warehouse",
     url="https://www.snowflake.com/",
     project_urls={
-        "Documentation": "https://docs.snowflake.com/en/developer-guide/snowpark/reference/python",
+        "Documentation": "https://docs.snowflake.com/en/developer-guide/snowpark/python/index.html",
         "Source": "https://github.com/snowflakedb/snowpark-python",
         "Issues": "https://github.com/snowflakedb/snowpark-python/issues",
         "Changelog": "https://github.com/snowflakedb/snowpark-python/blob/main/CHANGELOG.md",
@@ -47,8 +47,8 @@ setup(
         "setuptools>=40.6.0",
         "wheel",
         "cloudpickle>=1.6.0,<=2.0.0",
-        f"snowflake-connector-python>={CONNECTOR_DEPENDENCY_VERSION}",
-        "typing-extensions>=4.1.0",
+        f"snowflake-connector-python{CONNECTOR_DEPENDENCY_VERSION}",
+        "typing-extensions>=4.1.0, <5.0.0",
     ],
     namespace_packages=["snowflake"],
     # When a new package (directory) is added, we should also add it here
@@ -65,10 +65,10 @@ setup(
     },
     extras_require={
         "pandas": [
-            f"snowflake-connector-python[pandas]>={CONNECTOR_DEPENDENCY_VERSION}",
+            f"snowflake-connector-python[pandas]{CONNECTOR_DEPENDENCY_VERSION}",
         ],
         "secure-local-storage": [
-            f"snowflake-connector-python[secure-local-storage]>={CONNECTOR_DEPENDENCY_VERSION}",
+            f"snowflake-connector-python[secure-local-storage]{CONNECTOR_DEPENDENCY_VERSION}",
         ],
         "development": [
             "pytest",
@@ -79,7 +79,7 @@ setup(
         ],
     },
     classifiers=[
-        "Development Status :: 4 - Beta",
+        "Development Status :: 5 - Production/Stable",
         "Environment :: Console",
         "Environment :: Other Environment",
         "Intended Audience :: Developers",
