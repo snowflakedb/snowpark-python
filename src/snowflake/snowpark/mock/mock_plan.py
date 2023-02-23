8295e99a9e96557694beb3e35ed03b0232e9e0cf
Adam Ling <adam.ling@snowflake.com>
2023-02-22 21:38:55 -0800

    implement filter and sort

--- src/snowflake/snowpark/mock/mock_plan.py
+++ src/snowflake/snowpark/mock/mock_plan.py
@@ -13,6 +13,7 @@ from snowflake.snowpark._internal.analyzer.snowflake_plan_node import (
     LogicalPlan,
     SnowflakeValues,
 )
+from snowflake.snowpark._internal.analyzer.sort_expression import Ascending, NullsFirst
 from snowflake.snowpark._internal.analyzer.unary_expression import (
     Alias,
     UnresolvedAlias,
@@ -65,8 +66,8 @@ def execute_mock_plan(plan: MockExecutionPlan) -> TableEmulator:
     if isinstance(source_plan, MockSelectStatement):
         projection: Optional[List[Expression]] = source_plan.projection
         from_: Optional[MockSelectable] = source_plan.from_
-        # where: Optional[Expression] = source_plan.where
-        # order_by: Optional[List[Expression]] = source_plan.order_by
+        where: Optional[Expression] = source_plan.where
+        order_by: Optional[List[Expression]] = source_plan.order_by
         # limit_: Optional[int] = source_plan.limit_
         # offset: Optional[int] = source_plan.offset
 
@@ -76,6 +77,27 @@ def execute_mock_plan(plan: MockExecutionPlan) -> TableEmulator:
             column_name = plan.session._analyzer.analyze(exp)
             column_series = calculate_expression(exp, from_df)
             result_df[column_name] = column_series
+
+        if where:
+            # TODO: more tests, various operators
+            # TODO: column name contains quotes
+            where_query = plan.session._analyzer.analyze(where, escape_column_name=True)
+            result_df = result_df.query(where_query)
+
+        sort_columns_array = []
+        sort_orders_array = []
+        null_first_last_array = []
+        for exp in order_by:
+            sort_columns_array.append(plan.session._analyzer.analyze(exp.child))
+            sort_orders_array.append(isinstance(exp.direction, Ascending))
+            null_first_last_array.append(exp.null_ordering == NullsFirst)
+
+        if sort_columns_array:
+            # TODO: pandas DataFrame doesn't support the input of a list of na_position
+            result_df = result_df.sort_values(
+                by=sort_columns_array, ascending=sort_orders_array
+            )
+
         return result_df
 
 
