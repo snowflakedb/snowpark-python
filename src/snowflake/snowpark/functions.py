73fd59b206bb06b337be9b6c7bfab07174b7b605
Leonhard Spiegelberg <leonhard.spiegelberg@snowflake.com>
2023-02-28 16:56:16 -0800

    add udtf example

--- src/snowflake/snowpark/functions.py
+++ src/snowflake/snowpark/functions.py
@@ -3519,6 +3519,53 @@ def udtf(
 
     See Also:
         :class:`~snowflake.snowpark.udtf.UDTFRegistration`
+
+
+    Example::
+
+        >>> from snowflake.snowpark.types import IntegerType, StructField, StructType
+        >>> class PrimeSieve:
+        ...     def process(self, n):
+        ...         is_prime = [True] * (n + 1)
+        ...         is_prime[0] = False
+        ...         is_prime[1] = False
+        ...         p = 2
+        ...         while p * p <= n:
+        ...             if is_prime[p]:
+        ...                 # set all multiples of p to False
+        ...                 for i in range(p * p, n + 1, p):
+        ...                     is_prime[i] = False
+        ...             p += 1
+        ...         # yield all prime numbers
+        ...         for p in range(2, n + 1):
+        ...             if is_prime[p]:
+        ...                 yield (p,)
+        >>> prime_udtf = udtf(PrimeSieve, output_schema=StructType([StructField("number", IntegerType())]), input_types=[IntegerType()])
+        >>> session.table_function(prime_udtf(lit(20))).collect()
+        [Row(NUMBER=2), Row(NUMBER=3), Row(NUMBER=5), Row(NUMBER=7), Row(NUMBER=11), Row(NUMBER=13), Row(NUMBER=17), Row(NUMBER=19)]
+
+        Instead of calling `udtf` it is also possible to use udtf as a decorator.
+
+    Example::
+
+        >>> @udtf(name="alt_int",replace=True, output_schema=StructType([StructField("number", IntegerType())]), input_types=[IntegerType()])
+        ... class Alternator:
+        ...     def __init__(self):
+        ...         self._positive = True
+        ...
+        ...     def process(self, n):
+        ...         for i in range(n):
+        ...             if self._positive:
+        ...                 yield (1,)
+        ...             else:
+        ...                 yield (-1,)
+        ...             self._positive = not self._positive
+        >>> session.table_function("alt_int", lit(3)).collect()
+        [Row(NUMBER=1), Row(NUMBER=-1), Row(NUMBER=1)]
+        >>> session.table_function("alt_int", lit(2)).collect()
+        [Row(NUMBER=1), Row(NUMBER=-1)]
+        >>> session.table_function("alt_int", lit(1)).collect()
+        [Row(NUMBER=1)]
     """
     session = session or snowflake.snowpark.session._get_active_session()
     if handler is None:
