791b094c9cfc5e4ec76af6fe3e9a78a2e680438b
Shixuan Fan <shixuan.fan@snowflake.com>
2023-03-09 10:17:32 -0800

    SNOW-753246 Add Python 3.10 merge gate (#702)
    * py310 test
    
    * use str
    
    * fix Iterable
    
    * fix Iterable and Optional test
    
    * fix all nested type tests

--- src/snowflake/snowpark/_internal/analyzer/analyzer_utils.py
+++ src/snowflake/snowpark/_internal/analyzer/analyzer_utils.py
@@ -4,7 +4,7 @@
 #
 import re
 import typing
-from typing import Any, Dict, Iterable, List, Optional, Tuple, Union
+from typing import Any, Dict, List, Optional, Tuple, Union
 
 from snowflake.snowpark._internal.analyzer.binary_plan_node import (
     Except,
@@ -30,6 +30,14 @@ from snowflake.snowpark._internal.utils import (
 )
 from snowflake.snowpark.row import Row
 from snowflake.snowpark.types import DataType
+
+# Python 3.8 needs to use typing.Iterable because collections.abc.Iterable is not subscriptable
+# Python 3.9 can use both
+# Python 3.10 needs to use collections.abc.Iterable because typing.Iterable is removed
+try:
+    from typing import Iterable
+except ImportError:
+    from collections.abc import Iterable
 
 LEFT_PARENTHESIS = "("
 RIGHT_PARENTHESIS = ")"
