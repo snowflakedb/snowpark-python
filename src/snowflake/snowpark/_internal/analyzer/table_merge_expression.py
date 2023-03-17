491599ae1c52e112070bb9b68ce58d847f350f1a
Shixuan Fan <shixuan.fan@snowflake.com>
2023-03-17 12:47:05 -0700

    SNOW-761991 Update license header to 2023 (#728)
    Description
    
    It's 2023 you know :)
    
    Testing
    
    N/A

--- src/snowflake/snowpark/_internal/analyzer/table_merge_expression.py
+++ src/snowflake/snowpark/_internal/analyzer/table_merge_expression.py
@@ -1,6 +1,7 @@
 #
-# Copyright (c) 2012-2022 Snowflake Computing Inc. All rights reserved.
+# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
 #
+
 from typing import Dict, List, Optional
 
 from snowflake.snowpark._internal.analyzer.expression import Expression
--- tests/unit/test_analyzer_util_suite.py
+++ tests/unit/test_analyzer_util_suite.py
@@ -1,6 +1,7 @@
 #
-# Copyright (c) 2012-2022 Snowflake Computing Inc. All rights reserved.
+# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
 #
+
 import pytest
 
 from snowflake.snowpark._internal.analyzer.analyzer_utils import (
