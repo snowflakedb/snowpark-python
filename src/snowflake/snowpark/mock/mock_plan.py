9d901f3ec6027aba177ec24ebb67c9e1ded067fb
Adam Ling <adam.ling@snowflake.com>
2023-05-03 14:01:44 -0700

    SNOW-786744: Implement 10 most used snowflake functions for local testing (#814)

--- src/snowflake/snowpark/mock/mock_plan.py
+++ src/snowflake/snowpark/mock/mock_plan.py
@@ -4,6 +4,7 @@
 from functools import cached_property, partial
 from typing import List, NoReturn, Optional, Union
 
+import numpy as np
 import pandas as pd
 
 from snowflake.snowpark._internal.analyzer.analyzer_utils import UNION, UNION_ALL
@@ -59,6 +60,7 @@ from snowflake.snowpark.mock.mock_select_statement import (
 )
 from snowflake.snowpark.mock.snowflake_data_type import ColumnEmulator, TableEmulator
 from snowflake.snowpark.mock.util import convert_wildcard_to_regex, custom_comparator
+from snowflake.snowpark.types import _NumericType
 
 
 class MockExecutionPlan(LogicalPlan):
@@ -93,12 +95,18 @@ class MockExecutionPlan(LogicalPlan):
 def execute_mock_plan(plan: MockExecutionPlan) -> TableEmulator:
     source_plan = plan.source_plan if isinstance(plan, MockExecutionPlan) else plan
     if isinstance(source_plan, SnowflakeValues):
-        return TableEmulator(
+        table = TableEmulator(
             source_plan.data,
             columns=[x.name for x in source_plan.output],
             sf_types={x.name: x.datatype for x in source_plan.output},
             dtype=object,
         )
+        for column_name in table.columns:
+            sf_type = table.sf_types[column_name]
+            table[column_name].set_sf_type(table.sf_types[column_name])
+            if not isinstance(sf_type, _NumericType):
+                table[column_name].replace(np.nan, None, inplace=True)
+        return table
     if isinstance(source_plan, MockSelectExecutionPlan):
         return execute_mock_plan(source_plan.execution_plan)
     if isinstance(source_plan, MockSelectSnowflakePlan):
@@ -250,10 +258,22 @@ def calculate_expression(
             kw["is_distinct"] = exp.is_distinct
         if exp.name == "grouping":
             column_count = 0  # 0 indicate all columns
-
         if exp.name == "percentile_cont":
             # This one's syntax is different from other aggregation functions
             raise NotImplementedError("percentile_cont is not implemented yet")
+
+        # ==== for functions =====
+        # TODO: this needs a re-design
+        if exp.name == "to_date":
+            kw["fmt"] = (
+                str(evaluated_children[1]) if len(evaluated_children) > 1 else None
+            )
+        if exp.name == "contains":
+            return MOCK_FUNCTION_IMPLEMENTATION_MAP[exp.name](
+                evaluated_children[0], evaluated_children[1]
+            )
+        if exp.name == "abs":
+            return MOCK_FUNCTION_IMPLEMENTATION_MAP[exp.name](evaluated_children[0])
 
         output_columns = (
             evaluated_children[:column_count]
@@ -268,10 +288,10 @@ def calculate_expression(
         )
     if isinstance(exp, IsNull):
         child_column = calculate_expression(exp.child, input_data, analyzer)
-        return child_column.isnull()
+        return ColumnEmulator(data=[bool(data is None) for data in child_column])
     if isinstance(exp, IsNotNull):
         child_column = calculate_expression(exp.child, input_data, analyzer)
-        return ~(child_column.isnull())
+        return ColumnEmulator(data=[bool(data is not None) for data in child_column])
     if isinstance(exp, IsNaN):
         child_column = calculate_expression(exp.child, input_data, analyzer)
         return child_column.isna()
