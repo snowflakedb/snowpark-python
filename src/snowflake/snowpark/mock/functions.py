6ef923b502f9a6b0e2400b956ae4ce38f9838480
Adam Ling <adam.ling@snowflake.com>
2023-05-12 16:24:26 -0700

    SNOW-808886: local testing implement user defined mock function feature (#834)

--- src/snowflake/snowpark/mock/functions.py
+++ src/snowflake/snowpark/mock/functions.py
@@ -4,33 +4,52 @@
 
 import datetime
 import math
-from typing import Callable, List, Union
+from typing import Callable, Union
 
 from snowflake.snowpark.mock.snowflake_data_type import ColumnEmulator, TableEmulator
 from snowflake.snowpark.types import _NumericType
 
 RETURN_TYPE = Union[ColumnEmulator, TableEmulator]
 
-MOCK_FUNCTION_IMPLEMENTATION_MAP = {}
+_MOCK_FUNCTION_IMPLEMENTATION_MAP = {}
 
 
-def register_func_implementation(
-    snowpark_func_name: str, func_implementation: Callable
+def _register_func_implementation(
+    snowpark_func: Union[str, Callable], func_implementation: Callable
 ):
-    MOCK_FUNCTION_IMPLEMENTATION_MAP[snowpark_func_name] = func_implementation
-
-
-def unregister_func_implementation(snowpark_func_name: str):
     try:
-        del MOCK_FUNCTION_IMPLEMENTATION_MAP[snowpark_func_name]
+        _MOCK_FUNCTION_IMPLEMENTATION_MAP[snowpark_func.__name__] = func_implementation
+    except AttributeError:
+        _MOCK_FUNCTION_IMPLEMENTATION_MAP[snowpark_func] = func_implementation
+
+
+def _unregister_func_implementation(snowpark_func: Union[str, Callable]):
+    try:
+        try:
+            del _MOCK_FUNCTION_IMPLEMENTATION_MAP[snowpark_func.__name__]
+        except AttributeError:
+            del _MOCK_FUNCTION_IMPLEMENTATION_MAP[snowpark_func]
     except KeyError:
         pass
 
 
-def mock_min(columns: List[ColumnEmulator], **kwargs) -> ColumnEmulator:
-    if isinstance(columns[0].sf_type, _NumericType):
-        return ColumnEmulator(data=round(columns[0].min(), 5))
-    res = ColumnEmulator(data=columns[0].dropna().min())
+def patch(function):
+    def decorator(mocking_function):
+        _register_func_implementation(function, mocking_function)
+
+        def wrapper(*args, **kwargs):
+            mocking_function(*args, **kwargs)
+
+        return wrapper
+
+    return decorator
+
+
+@patch("min")
+def mock_min(column: ColumnEmulator) -> ColumnEmulator:
+    if isinstance(column.sf_type, _NumericType):
+        return ColumnEmulator(data=round(column.min(), 5))
+    res = ColumnEmulator(data=column.dropna().min())
     try:
         if math.isnan(res[0]):
             return ColumnEmulator(data=[None])
@@ -39,10 +58,11 @@ def mock_min(columns: List[ColumnEmulator], **kwargs) -> ColumnEmulator:
         return ColumnEmulator(data=res)
 
 
-def mock_max(columns: List[ColumnEmulator], **kwargs) -> ColumnEmulator:
-    if isinstance(columns[0].sf_type, _NumericType):
-        return ColumnEmulator(data=round(columns[0].max(), 5))
-    res = ColumnEmulator(data=columns[0].dropna().max())
+@patch("max")
+def mock_max(column: ColumnEmulator) -> ColumnEmulator:
+    if isinstance(column.sf_type, _NumericType):
+        return ColumnEmulator(data=round(column.max(), 5))
+    res = ColumnEmulator(data=column.dropna().max())
     try:
         if math.isnan(res[0]):
             return ColumnEmulator(data=[None])
@@ -51,26 +71,31 @@ def mock_max(columns: List[ColumnEmulator], **kwargs) -> ColumnEmulator:
         return ColumnEmulator(data=res)
 
 
-def mock_sum(columns: List[ColumnEmulator], **kwargs) -> ColumnEmulator:
-    return ColumnEmulator(data=columns[0].sum())
+@patch("sum")
+def mock_sum(column: ColumnEmulator) -> ColumnEmulator:
+    return ColumnEmulator(data=column.sum())
 
 
-def mock_avg(columns: List[ColumnEmulator], **kwargs) -> ColumnEmulator:
-    return ColumnEmulator(data=round(columns[0].mean(), 5))
+@patch("avg")
+def mock_avg(column: ColumnEmulator) -> ColumnEmulator:
+    return ColumnEmulator(data=round(column.mean(), 5))
 
 
-def mock_count(columns: List[ColumnEmulator], **kwargs) -> ColumnEmulator:
-    return ColumnEmulator(data=round(columns[0].count(), 5))
+@patch("count")
+def mock_count(column: ColumnEmulator) -> ColumnEmulator:
+    return ColumnEmulator(data=round(column.count(), 5))
 
 
-def mock_median(columns: List[ColumnEmulator], **kwargs) -> ColumnEmulator:
-    return ColumnEmulator(data=round(columns[0].median(), 5))
+@patch("median")
+def mock_median(column: ColumnEmulator) -> ColumnEmulator:
+    return ColumnEmulator(data=round(column.median(), 5))
 
 
-def mock_covar_pop(columns: List[ColumnEmulator], **kwargs) -> ColumnEmulator:
+@patch("covar_pop")
+def mock_covar_pop(column1: ColumnEmulator, column2: ColumnEmulator) -> ColumnEmulator:
     non_nan_cnt = 0
     x_sum, y_sum, x_times_y_sum = 0, 0, 0
-    for x, y in zip(columns[0], columns[1]):
+    for x, y in zip(column1, column2):
         if x is not None and y is not None and not math.isnan(x) and not math.isnan(y):
             non_nan_cnt += 1
             x_times_y_sum += x * y
@@ -80,24 +105,22 @@ def mock_covar_pop(columns: List[ColumnEmulator], **kwargs) -> ColumnEmulator:
     return ColumnEmulator(data=data)
 
 
-def mock_listagg(columns: List[ColumnEmulator], **kwargs):
-    delimiter = kwargs.get("delimiter")
-    is_distinct = kwargs.get("is_distinct")
-    columns_data = ColumnEmulator(columns[0].unique()) if is_distinct else columns[0]
+@patch("listagg")
+def mock_listagg(column: ColumnEmulator, delimiter, is_distinct):
+    columns_data = ColumnEmulator(column.unique()) if is_distinct else column
     return ColumnEmulator(data=delimiter.join([v for v in columns_data.dropna()]))
 
 
-def mock_to_date(columns: List[ColumnEmulator], **kwargs):
-    date_fmt = kwargs.get("fmt")
-    auto_detect = bool(not date_fmt)
-    date_fmt = date_fmt or "%Y-%m-%d"
+@patch("to_date")
+def mock_to_date(column: ColumnEmulator, fmt: Union[ColumnEmulator, str] = None):
+    auto_detect = bool(not fmt)
+    date_fmt = fmt or "%Y-%m-%d"
     date_fmt = date_fmt.replace("YYYY", "%Y")
     date_fmt = date_fmt.replace("MM", "%m")
     date_fmt = date_fmt.replace("MON", "%b")
     date_fmt = date_fmt.replace("DD", "%d")
-    columns_data = columns[0]
     res = []
-    for data in columns_data:
+    for data in column:
         if auto_detect and data.isnumeric():
             # spec here: https://docs.snowflake.com/en/sql-reference/functions/to_date#usage-notes
             timestamp_values = int(data)
@@ -113,6 +136,7 @@ def mock_to_date(columns: List[ColumnEmulator], **kwargs):
     return ColumnEmulator(data=res)
 
 
+@patch("contains")
 def mock_contains(expr1: ColumnEmulator, expr2: Union[str, ColumnEmulator]):
     if isinstance(expr1, str) and isinstance(expr2, str):
         return ColumnEmulator(data=[bool(str(expr2) in str(expr1))])
@@ -125,24 +149,9 @@ def mock_contains(expr1: ColumnEmulator, expr2: Union[str, ColumnEmulator]):
     return ColumnEmulator(data=res)
 
 
+@patch("abs")
 def mock_abs(expr):
     if isinstance(expr, ColumnEmulator):
         return expr.abs()
     else:
         return abs(expr)
-
-
-MOCK_FUNCTION_IMPLEMENTATION_MAP["min"] = mock_min
-MOCK_FUNCTION_IMPLEMENTATION_MAP["max"] = mock_max
-MOCK_FUNCTION_IMPLEMENTATION_MAP["sum"] = mock_sum
-MOCK_FUNCTION_IMPLEMENTATION_MAP["median"] = mock_median
-MOCK_FUNCTION_IMPLEMENTATION_MAP["mean"] = mock_avg
-MOCK_FUNCTION_IMPLEMENTATION_MAP["avg"] = mock_avg
-MOCK_FUNCTION_IMPLEMENTATION_MAP["count"] = mock_count
-MOCK_FUNCTION_IMPLEMENTATION_MAP["covar_pop"] = mock_covar_pop
-MOCK_FUNCTION_IMPLEMENTATION_MAP["listagg"] = mock_listagg
-MOCK_FUNCTION_IMPLEMENTATION_MAP["to_date"] = MOCK_FUNCTION_IMPLEMENTATION_MAP[
-    "date_format"
-] = mock_to_date
-MOCK_FUNCTION_IMPLEMENTATION_MAP["contains"] = mock_contains
-MOCK_FUNCTION_IMPLEMENTATION_MAP["abs"] = mock_abs
