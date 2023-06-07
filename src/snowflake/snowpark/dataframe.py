1f6a956333a8e1b07fc033233d9b2f20f4f3ec4f
Vivek Nayak <vivek.nayak@snowflake.com>
2023-06-07 11:15:25 -0700

    SNOW-807303: Deleting commented code for incorrect implementation of name normalization

--- src/snowflake/snowpark/dataframe.py
+++ src/snowflake/snowpark/dataframe.py
@@ -1655,7 +1655,6 @@ class DataFrame:
         exprs = parse_positional_args_to_list(*cols)
         names = self._get_column_names_from_column_or_name_list(exprs)
         normalized_names = list({quote_name(n) for n in names})
-        # processed_names = self._get_processed_names_from_column_or_names(normalized_names)
         exclude_plan = Exclude(normalized_names, self._plan)
 
         if self._select_statement:
@@ -1705,7 +1704,6 @@ class DataFrame:
 
         names = self._get_column_names_from_column_or_name_list(column_or_name_list)
         normalized_name_list = [quote_name(n) for n in names]
-        # processed_names = self._get_processed_names_from_column_or_names(names)
         rename_map = {k: v for k, v in zip(normalized_name_list, rename_list)}
         rename_plan = Rename(rename_map, self._plan)
 
@@ -3744,40 +3742,6 @@ Query List:
                 raise SnowparkClientExceptionMessages.DF_CANNOT_DROP_COLUMN_NAME(str(c))
 
         return names
-
-    # def _get_processed_names_from_column_or_names(
-    #         self, exprs: List #TODO: Understand what the input type should be over here
-    # ) -> List[Expression]: #TODO: Understand what the return type should be over here
-    #     names = []
-    #     for e in exprs:
-    #         if isinstance(e, Column):
-    #             names.append(e._named())
-    #         elif isinstance(e, str):
-    #             names.append(Column(e)._named())
-    #         elif isinstance(e, TableFunctionCall):
-    #             if table_func:
-    #                 raise ValueError(
-    #                     f"At most one table function can be called inside a select(). "
-    #                     f"Called '{table_func.user_visible_name}' and '{e.user_visible_name}'."
-    #                 )
-    #             table_func = e
-    #             func_expr = _create_table_function_expression(func=table_func)
-    #             join_plan = self._session._analyzer.resolve(
-    #                 TableFunctionJoin(self._plan, func_expr)
-    #             )
-    #
-    #             if isinstance(e, _ExplodeFunctionCall):
-    #                 new_cols = _get_cols_after_explode_join(e, self._plan)
-    #             else:
-    #                 _, new_cols = _get_cols_after_join_table(
-    #                     func_expr, self._plan, join_plan
-    #                 )
-    #             names.extend(new_cols)
-    #         else:
-    #             raise TypeError(
-    #                 "The input of select() must be Column, column name, TableFunctionCall, or a list of them"
-    #             )
-    #     return names
 
     def _convert_cols_to_exprs(
         self,
