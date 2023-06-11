4125c58f97d67dbe8488f1e956be74a293d4dd6b
Vivek Nayak <vivek.nayak@snowflake.com>
2023-06-11 16:30:47 -0700

    SNOW-834932: Adding extra test case to test_rename_to_dropped_column_name

--- tests/integ/test_simplifier_suite.py
+++ tests/integ/test_simplifier_suite.py
@@ -1001,6 +1001,11 @@ def test_rename_to_dropped_column_name(session):
     session.sql_simplifier_enabled = True
     df1 = session.create_dataframe([[1, 2, 3]], schema=["a", "b", "c"])
     df2 = df1.drop("a").drop("b")
+
+    df_with_new_columns = df2.withColumn("a", col("c")).withColumn("b", sql_expr("1"))
+    assert df_with_new_columns.columns == ["C", "A", "B"]
+    Utils.check_answer(df_with_new_columns, [Row(3, 3, 1)])
+
     df3 = df2.withColumn("a", df2["c"])
     df4 = df3.withColumn("b", sql_expr("1"))
     assert df4.columns == ["C", "A", "B"]
