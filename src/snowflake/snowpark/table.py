0506194a65ab95e50e42ea1763a23ece5bc36f72
Sophie Tan <sophie.tan@snowflake.com>
2023-06-20 16:54:39 -0400

    [Local testing] Support `Table.drop_table` (#899)
    * Add implementation and tests
    
    * Undo test config change

--- src/snowflake/snowpark/table.py
+++ src/snowflake/snowpark/table.py
@@ -662,6 +662,9 @@ class Table(DataFrame):
         Note that subsequent operations such as :meth:`DataFrame.select`, :meth:`DataFrame.collect` on this ``Table`` instance and the derived DataFrame will raise errors because the underlying
         table in the Snowflake database no longer exists.
         """
-        self._session.sql(
-            f"drop table {self.table_name}"
-        )._internal_collect_with_tag_no_telemetry()
+        if hasattr(self._session._conn, "table_registry"):
+            self._session._conn.table_registry.drop_table(self.table_name)
+        else:
+            self._session.sql(
+                f"drop table {self.table_name}"
+            )._internal_collect_with_tag_no_telemetry()
