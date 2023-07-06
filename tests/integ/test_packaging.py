90887be5090cba9d9bf451cfa4870d48e8a7d86c
Vivek Nayak <vivek.nayak@snowflake.com>
2023-07-05 20:39:20 -0700

    Code cleanup

--- tests/integ/test_packaging.py
+++ tests/integ/test_packaging.py
@@ -803,7 +803,7 @@ def test_add_requirements_unsupported_with_persist_path(
 @pytest.mark.skipif(
     IS_IN_STORED_PROC or IS_WINDOWS or not is_pandas_and_numpy_available,
     reason="Numpy and pandas needed and subprocess process calls might occur (not allowed inside stored proc). "
-    "Also, replicate_local_environment() won't work for Windows",
+    "Also, replicate_local_environment() is currently causing fatal errors in Windows environments",
 )
 def test_replicate_local_environment(session):
     with patch.object(session, "_is_anaconda_terms_acknowledged", lambda: True):
