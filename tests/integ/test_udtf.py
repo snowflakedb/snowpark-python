10af52de659f24f503e9578a6ee0761fe8d4b5ae
Afroz Alam <afroz.alam@snowflake.com>
2023-08-08 16:44:51 -0700

    SNOW-878135 is_permanent behavior fix (#989)
    * SNOW-878135: Prioritize is_permanent over non-None stage_name for permanent sprocs/udfs
    
    * changelog updates
    
    * fix test
    
    * address comments
    
    * fix lint
    
    * use is_permanent in helper function; update changelog
    
    * add tests

--- tests/integ/test_udtf.py
+++ tests/integ/test_udtf.py
@@ -3,14 +3,17 @@
 #
 
 import decimal
+import logging
 import sys
 from typing import Tuple
 
 import pytest
 
 from snowflake.snowpark import Row, Table
+from snowflake.snowpark._internal.utils import TempObjectType
 from snowflake.snowpark.exceptions import SnowparkSQLException
 from snowflake.snowpark.functions import lit, udtf
+from snowflake.snowpark.session import Session
 from snowflake.snowpark.types import (
     BinaryType,
     BooleanType,
@@ -276,6 +279,46 @@ def test_secure_udtf(session):
     )
     ddl_sql = f"select get_ddl('function', '{UDTFEcho.name}(int)')"
     assert "SECURE" in session.sql(ddl_sql).collect()[0][0]
+
+
+@pytest.mark.skipif(IS_IN_STORED_PROC, reason="Cannot create session in SP")
+def test_permanent_udtf_negative(session, db_parameters, caplog):
+    stage_name = Utils.random_stage_name()
+    udtf_name = Utils.random_name_for_temp_object(TempObjectType.TABLE_FUNCTION)
+
+    class UDTFEcho:
+        def process(
+            self,
+            num: int,
+        ) -> Iterable[Tuple[int]]:
+            return [(num,)]
+
+    with Session.builder.configs(db_parameters).create() as new_session:
+        new_session.sql_simplifier_enabled = session.sql_simplifier_enabled
+        try:
+            with caplog.at_level(logging.WARN):
+                echo_udtf = udtf(
+                    UDTFEcho,
+                    output_schema=StructType([StructField("A", IntegerType())]),
+                    input_types=[IntegerType()],
+                    name=udtf_name,
+                    is_permanent=False,
+                    stage_location=stage_name,
+                    session=new_session,
+                )
+            assert (
+                "is_permanent is False therefore stage_location will be ignored"
+                in caplog.text
+            )
+
+            with pytest.raises(
+                SnowparkSQLException, match=f"Unknown table function {udtf_name}"
+            ):
+                session.table_function(echo_udtf(lit(1))).collect()
+
+            Utils.check_answer(new_session.table_function(echo_udtf(lit(1))), [Row(1)])
+        finally:
+            new_session._run_query(f"drop function if exists {udtf_name}(int)")
 
 
 @pytest.mark.xfail(reason="SNOW-757054 flaky test", strict=False)
