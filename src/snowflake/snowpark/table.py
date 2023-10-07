12b47092e7a49370bc56272e088c33d18c0349d8
Sophie Tan <sophie.tan@snowflake.com>
2023-10-06 20:33:04 -0400

    [Local Testing] SNOW-930302 Support DataFrame.sample and Table.sample (#1078)

--- src/snowflake/snowpark/table.py
+++ src/snowflake/snowpark/table.py
@@ -4,6 +4,7 @@
 #
 
 import sys
+from logging import getLogger
 from typing import Dict, List, NamedTuple, Optional, Union, overload
 
 import snowflake.snowpark
@@ -17,6 +18,7 @@ from snowflake.snowpark._internal.analyzer.table_merge_expression import (
     TableUpdate,
     UpdateMergeExpression,
 )
+from snowflake.snowpark._internal.analyzer.unary_plan_node import Sample
 from snowflake.snowpark._internal.error_message import SnowparkClientExceptionMessages
 from snowflake.snowpark._internal.telemetry import add_api_call, set_api_call_source
 from snowflake.snowpark._internal.type_utils import ColumnOrLiteral
@@ -31,6 +33,8 @@ if sys.version_info <= (3, 9):
     from typing import Iterable
 else:
     from collections.abc import Iterable
+
+_logger = getLogger(__name__)
 
 
 class UpdateResult(NamedTuple):
@@ -295,7 +299,7 @@ class Table(DataFrame):
         frac: Optional[float] = None,
         n: Optional[int] = None,
         *,
-        seed: Optional[float] = None,
+        seed: Optional[int] = None,
         sampling_method: Optional[str] = None,
     ) -> "DataFrame":
         """Samples rows based on either the number of rows to be returned or a percentage of rows to be returned.
@@ -322,7 +326,9 @@ class Table(DataFrame):
         """
         if sampling_method is None and seed is None:
             return super().sample(frac=frac, n=n)
+
         DataFrame._validate_sample_input(frac, n)
+
         if sampling_method and sampling_method.upper() not in (
             "BERNOULLI",
             "ROW",
@@ -331,6 +337,26 @@ class Table(DataFrame):
         ):
             raise ValueError(
                 f"'sampling_method' value {sampling_method} must be None or one of 'BERNOULLI', 'ROW', 'SYSTEM', or 'BLOCK'."
+            )
+
+        from snowflake.snowpark.mock.connection import MockServerConnection
+
+        if isinstance(self._session._conn, MockServerConnection):
+            if sampling_method in ("SYSTEM", "BLOCK"):
+                _logger.warning(
+                    "[Local Testing] SYSTEM/BLOCK sampling is not supported for Local Testing, falling back to ROW sampling"
+                )
+
+            sample_plan = Sample(
+                self._plan, probability_fraction=frac, row_count=n, seed=seed
+            )
+            return self._with_plan(
+                self._session._analyzer.create_select_statement(
+                    from_=self._session._analyzer.create_select_snowflake_plan(
+                        sample_plan, analyzer=self._session._analyzer
+                    ),
+                    analyzer=self._session._analyzer,
+                )
             )
 
         # The analyzer will generate a sql with subquery. So we build the sql directly without using the analyzer.
