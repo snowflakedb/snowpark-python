b2d771024489e52358bb78fa6c24e10bf60ea32f
Afroz Alam <afroz.alam@snowflake.com>
2023-10-31 18:33:55 -0700

    address comments

--- src/snowflake/snowpark/_internal/analyzer/snowflake_plan.py
+++ src/snowflake/snowpark/_internal/analyzer/snowflake_plan.py
@@ -575,11 +575,13 @@ class SnowflakePlanBuilder:
         # the attributes set to ($1, VariantType()) which cannot be used as valid column name
         # in save as table. So we rename ${number} with COL{number}.
         hidden_column_pattern = r"\$(\d+)"
-        column_definition = attribute_to_schema_string(child.attributes)
+        column_definition_with_hidden_columns = attribute_to_schema_string(
+            child.attributes
+        )
         column_definition = re.sub(
             hidden_column_pattern,
             lambda match: f"COL{match.group(1)}",
-            column_definition,
+            column_definition_with_hidden_columns,
         )
 
         def get_create_and_insert_plan(child: SnowflakePlan, replace=False, error=True):
