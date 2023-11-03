4b4179fbc89eb13bad7c51bc7662d62f7a1921fc
Adam Ling <adam.ling@snowflake.com>
2023-11-02 17:17:32 -0700

    clean up

--- tests/integ/test_column.py
+++ tests/integ/test_column.py
@@ -99,14 +99,14 @@ def test_cast_decimal(session, number_word):
 def test_cast_map_type(session):
     df = session.create_dataframe([['{"key": "1"}']], schema=["a"])
     result = df.select(parse_json(df["a"]).cast("object")).collect()
-    assert json.loads(result[0][0])
+    assert json.loads(result[0][0]) == {"key": "1"}
 
 
 @pytest.mark.localtest
 def test_cast_array_type(session):
     df = session.create_dataframe([["[1,2,3]"]], schema=["a"])
     result = df.select(parse_json(df["a"]).cast("array")).collect()
-    assert json.loads(result[0][0])
+    assert json.loads(result[0][0]) == [1, 2, 3]
 
 
 @pytest.mark.localtest
