48ce3ed0df664a09870c0056b1061b887c25950e
Sophie Tan <sophie.tan@snowflake.com>
2023-11-06 17:35:10 -0500

    [Local Testing] SNOW-904260 support dataframe na functions (#1069)
    * Add function iff
    
    * Add support for na functions, blocked by type coercion
    
    * Fix tests
    
    * Workaround type coercion
    
    * Workaround coercion
    
    * Address comments
    
    * Delete dead code
    
    * Fix tests

--- src/snowflake/snowpark/dataframe.py
+++ src/snowflake/snowpark/dataframe.py
@@ -3350,12 +3350,6 @@ class DataFrame:
         Returns a :class:`DataFrameNaFunctions` object that provides functions for
         handling missing values in the DataFrame.
         """
-        from snowflake.snowpark.mock.connection import MockServerConnection
-
-        if isinstance(self._session._conn, MockServerConnection):
-            raise NotImplementedError(
-                "[Local Testing] DataFrameNaFunctions is not implemented."
-            )
         return self._na
 
     @property
