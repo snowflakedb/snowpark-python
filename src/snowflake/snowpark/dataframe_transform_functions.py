af8b93b2b85632a90c792f7d9b5f2ed9e1c3aecf
Ravi Kumar Suresh Babu <ravikumar.sureshbabu@snowflake.com>
2023-11-28 07:05:44 -0800

    changes

--- src/snowflake/snowpark/dataframe_transform_functions.py
+++ src/snowflake/snowpark/dataframe_transform_functions.py
@@ -2,10 +2,11 @@
 # Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
 #
 
-from typing import Callable, Dict, List
+from typing import Callable, Dict, List, Optional
 
 import snowflake.snowpark
-from snowflake.snowpark.functions import expr
+from snowflake.snowpark.column import _to_col_if_str
+from snowflake.snowpark.functions import avg, expr, rank, when
 from snowflake.snowpark.window import Window
 
 
@@ -120,3 +121,102 @@ class DataFrameTransformFunctions:
                     agg_df = agg_df.with_column(formatted_col_name, agg_col)
 
         return agg_df
+
+    def encode(
+        self,
+        cols: List[str],
+        encoding_types: List[str],
+        targets: Optional[List[str]] = None,
+        col_formatter: Callable[[str, str], str] = _default_col_formatter,
+    ) -> "snowflake.snowpark.dataframe.DataFrame":
+        """
+        Encodes columns in the DataFrame using specified encoding types.
+
+        Args:
+            cols: A list of column names to encode.
+            encoding_types: A list of encoding types corresponding to each column. Supported types include 'label', 'one_hot', and 'target'.
+            targets: An optional list of target columns for 'target' encoding. For non-target encodings, None should be specified.
+            col_formatter: An optional function for formatting output column names. The default format is '<input_col>_<encoding_type>'.
+                        This function takes two arguments: 'input_col' (str) for the column name and 'encoding_type' (str) for the type of encoding,
+                        and returns a formatted string for the column name.
+
+        Returns:
+            A Snowflake DataFrame with additional columns corresponding to each encoded column.
+
+        Raises:
+            ValueError: If an unsupported encoding type is specified or if required parameters for specific encoding types are not provided.
+            TypeError: If input arguments are not of expected types.
+
+        Example:
+            encoded_df = df.transform.encode(
+                cols=['WEATHER_DESCRIPTION', 'ANOTHER_COLUMN'],
+                encoding_types=['target', 'one_hot'],
+                targets=['DAILY_HIGH_TEMP', None],
+                col_formatter=col_formatter_func
+            )
+        """
+        self._validate_string_list_argument(cols, "cols")
+        self._validate_string_list_argument(encoding_types, "encoding_types")
+        self._validate_formatter_argument(col_formatter)
+
+        if targets is None:
+            targets = [None] * len(cols)
+
+        if not (len(cols) == len(encoding_types)):
+            raise ValueError("Length of cols, encoding_types must be equal.")
+
+        encoded_df = self._df
+        for col_name, encoding_type, target in zip(cols, encoding_types, targets):
+            formatted_col_name = col_formatter(col_name, encoding_type)
+            column = _to_col_if_str(col_name, "transform.encode")
+
+            if encoding_type == "label":
+                # Create a DataFrame with distinct values and their labels
+                distinct_values = (
+                    self._df.select(column)
+                    .distinct()
+                    .with_column(
+                        formatted_col_name, rank().over(Window.order_by(column))
+                    )
+                )
+
+                # Add the label encoding to the df.
+                distinct_values = distinct_values.with_column_renamed(
+                    col_name, "temp_join_key"
+                )
+                encoded_df = encoded_df.join(
+                    distinct_values,
+                    encoded_df[col_name] == distinct_values["temp_join_key"],
+                )
+                encoded_df = encoded_df.drop("temp_join_key")
+
+            elif encoding_type == "one_hot":
+                distinct_col_vals = encoded_df.select(column).distinct().collect()
+                for row in distinct_col_vals:
+                    val = row[0]
+                    column_name = formatted_col_name + f"_{val}"
+                    encoded_df = encoded_df.with_column(
+                        column_name, when(column == val, 1).otherwise(0)
+                    )
+
+            elif encoding_type == "target":
+                if not target:
+                    raise ValueError(
+                        "Target column must be specified for target encoding"
+                    )
+                target_column = _to_col_if_str(target, "transform.encode")
+                target_mean = encoded_df.group_by(col_name).agg(
+                    avg(target_column).alias(formatted_col_name)
+                )
+                target_mean = target_mean.with_column_renamed(col_name, "temp_join_key")
+                encoded_df = encoded_df.join(
+                    target_mean, encoded_df[col_name] == target_mean["temp_join_key"]
+                )
+                encoded_df = encoded_df.drop("temp_join_key")
+
+            else:
+                raise ValueError(
+                    f"Invalid encoding type for column {col_name}: {encoding_type}"
+                )
+
+        return encoded_df
