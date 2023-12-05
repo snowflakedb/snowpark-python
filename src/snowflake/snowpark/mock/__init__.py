db8830d232c7f00c2e6ddb959395920ed5359b62
Sophie Tan <sophie.tan@snowflake.com>
2023-12-05 10:48:36 -0800

    [Local Testing] Expose CUSTOM_JSON_ENCODER and CUSTOM_JSON_DECODER as public APIs (#1164)
    * Expose CUSTOM_JSON_ENCODER anf CUSTOM_JSON_DECODER as public APIs
    
    * Address comments
    
    * Address comments

--- src/snowflake/snowpark/mock/__init__.py
+++ src/snowflake/snowpark/mock/__init__.py
@@ -1,8 +1,15 @@
 #
 # Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
 #
-
 from ._functions import patch
 from ._snowflake_data_type import ColumnEmulator, ColumnType, TableEmulator
 
-__all__ = ["patch", "ColumnEmulator", "ColumnType", "TableEmulator"]
+CUSTOM_JSON_ENCODER = None
+CUSTOM_JSON_DECODER = None
+
+__all__ = [
+    "patch",
+    "ColumnEmulator",
+    "ColumnType",
+    "TableEmulator",
+]
