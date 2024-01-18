d4b960fb3729867dbf080f1b818fcd323959ca35
Shixuan Fan <shixuan.fan@snowflake.com>
2024-01-18 13:33:04 -0800

    SNOW-967727 Add functions sign/signum (#1206)
    Description
    
    As title
    
    Testing
    
    doc test

--- src/snowflake/snowpark/functions.py
+++ src/snowflake/snowpark/functions.py
@@ -2410,6 +2410,30 @@ def round(e: ColumnOrName, scale: Union[ColumnOrName, int, float] = 0) -> Column
     return builtin("round")(c, scale_col)
 
 
+def sign(col: ColumnOrName) -> Column:
+    """
+    Returns the sign of its argument:
+
+        - -1 if the argument is negative.
+        - 1 if it is positive.
+        - 0 if it is 0.
+
+    Args:
+        col: The column to evaluate its sign
+
+    Example::
+        >>> df = session.create_dataframe([(-2, 2, 0)], ["a", "b", "c"])
+        >>> df.select(sign("a").alias("a_sign"), sign("b").alias("b_sign"), sign("c").alias("c_sign")).show()
+        ----------------------------------
+        |"A_SIGN"  |"B_SIGN"  |"C_SIGN"  |
+        ----------------------------------
+        |-1        |1         |0         |
+        ----------------------------------
+        <BLANKLINE>
+    """
+    return builtin("sign")(_to_col_if_str(col, "sign"))
+
+
 def split(
     str: ColumnOrName,
     pattern: ColumnOrName,
@@ -8106,6 +8130,7 @@ monotonically_increasing_id = seq8
 from_unixtime = to_timestamp
 sort_array = array_sort
 map_from_arrays = arrays_to_object
+signum = sign
 
 
 def unix_timestamp(e: ColumnOrName, fmt: Optional["Column"] = None) -> Column:
