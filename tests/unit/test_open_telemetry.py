f13817ef6fef255a0989f72880b01f02136f9a9e
Yuyang Wang <yuyang.wang@snowflake.com>
2024-03-20 11:55:16 -0700

    debug

--- tests/unit/test_open_telemetry.py
+++ tests/unit/test_open_telemetry.py
@@ -20,19 +20,17 @@ from opentelemetry.sdk.trace.export import BatchSpanProcessor, SpanExporter
 
 class DictExporter(SpanExporter):
     def __init__(self):
-        self.exported_spans = []
+        self.exported_spans = {}
 
     def export(self, spans) -> None:
         for span in spans:
-
-            span_dict = {
+            self.exported_spans[span.name] = {
                 "name": span.name,
                 "attributes": dict(span.attributes),
             }
-            self.exported_spans.append(span_dict)
 
 
-def test_open_telemetry_span_from_dataframe_writer():
+def test_open_telemetry_span_from_dataframe_writer_and_dataframe():
     # set up exporter
     resource = Resource(attributes={SERVICE_NAME: "snowpark-python-open-telemetry"})
     trace_provider = TracerProvider(resource=resource)
@@ -46,36 +44,25 @@ def test_open_telemetry_span_from_dataframe_writer():
     session = snowflake.snowpark.session.Session(mock_connection)
     session._conn._telemetry_client = mock.MagicMock()
     df = session.create_dataframe([1, 2, 3, 4]).to_df("a")
+    # test dataframe writer
     df.write.mode("overwrite").save_as_table("saved_table", table_type="temporary")
     lineno = str(inspect.currentframe().f_lineno - 1)
     # wait for open telemetry to capture the span
-    while len(dict_exporter.exported_spans) == 0:
+    while "DataFrameWriter.save_as_table" not in dict_exporter.exported_spans:
         time.sleep(1)
-    span = dict_exporter.exported_spans[0]
+    span = dict_exporter.exported_spans["DataFrameWriter.save_as_table"]
     assert span["attributes"]["method.chain"] == "DataFrame.to_df().save_as_table()"
     assert os.path.basename(span["attributes"]["code.filepath"]) == "test_open_telemetry.py"
     assert span["attributes"]["code.lineno"] == lineno
 
-
-def test_open_telemetry_span_from_dataframe():
-    resource = Resource(attributes={SERVICE_NAME: "snowpark-python-open-telemetry"})
-    trace_provider = TracerProvider(resource=resource)
-    dict_exporter = DictExporter()
-    processor = BatchSpanProcessor(dict_exporter)
-    trace_provider.add_span_processor(processor)
-    trace.set_tracer_provider(trace_provider)
-
-    mock_connection = mock.create_autospec(ServerConnection)
-    mock_connection._conn = mock.MagicMock()
-    session = snowflake.snowpark.session.Session(mock_connection)
-    session._conn._telemetry_client = mock.MagicMock()
-    df = session.create_dataframe([1, 2, 3, 4]).to_df("a")
+    # test from dataframe
     df.collect()
     lineno = str(inspect.currentframe().f_lineno - 1)
     # wait for open telemetry to capture the span
-    while len(dict_exporter.exported_spans) == 0:
+    while "DataFrame.collect" not in dict_exporter.exported_spans:
         time.sleep(1)
-    span = dict_exporter.exported_spans[0]
+    span = dict_exporter.exported_spans["DataFrame.collect"]
     assert span["attributes"]["method.chain"] == "DataFrame.to_df().collect()"
     assert os.path.basename(span["attributes"]["code.filepath"]) == "test_open_telemetry.py"
     assert span["attributes"]["code.lineno"] == lineno
+
