7be33a9bc797e4d8fc2f1e6bd39f38f9b7b242f7
Jamison Rose <Jamison.Rose@snowflake.com>
2024-03-22 13:24:29 -0700

    SNOW-1183317: [Local Testing] Support registering udf (#1316)

--- src/snowflake/snowpark/mock/__init__.py
+++ src/snowflake/snowpark/mock/__init__.py
@@ -1,11 +1,31 @@
 #
 # Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
 #
+from json import JSONEncoder
+
 from ._functions import patch
 from ._snowflake_data_type import ColumnEmulator, ColumnType, TableEmulator
 
-CUSTOM_JSON_ENCODER = None
+
+class NumpyEncoder(JSONEncoder):
+    def default(self, obj):
+        import numpy
+
+        if isinstance(obj, numpy.integer):
+            return int(obj)
+        if isinstance(obj, numpy.floating):
+            return float(obj)
+        if isinstance(obj, numpy.ndarray):
+            return obj.tolist()
+        if isinstance(obj, numpy.bool_):
+            return bool(obj)
+
+        return super().default(obj)
+
+
+CUSTOM_JSON_ENCODER = NumpyEncoder
 CUSTOM_JSON_DECODER = None
+
 
 __all__ = [
     "patch",
