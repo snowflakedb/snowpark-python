3f1be29bbcf7e6ac202a17b9527587a0ce1aba3b
Naren Krishna <naren.krishna@snowflake.com>
2024-04-03 11:22:59 -0700

    SNOW-1271956: Update Snowpark Python license year to 2024 (#1359)
    Signed-off-by: Naren Krishna <naren.krishna@snowflake.com>

--- src/snowflake/snowpark/stored_procedure.py
+++ src/snowflake/snowpark/stored_procedure.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python3
 #
-# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+# Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
 #
 
 """Stored procedures in Snowpark. Refer to :class:`~snowflake.snowpark.stored_procedure.StoredProcedure` for details and sample code."""
