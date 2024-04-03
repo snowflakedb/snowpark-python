be71e76a43eace0f3d932ad67b8b2eeb76c6c603
Naren Krishna <naren.krishna@snowflake.com>
2024-04-03 11:22:59 -0700

    SNOW-1271956: Update Snowpark Python license year to 2024 (#1359)
    Signed-off-by: Naren Krishna <naren.krishna@snowflake.com>

--- src/snowflake/snowpark/_internal/analyzer/select_statement.py
+++ src/snowflake/snowpark/_internal/analyzer/select_statement.py
@@ -1,5 +1,5 @@
 #
-# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+# Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
 #
 
 from abc import ABC, abstractmethod
