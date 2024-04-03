ba60d7dea51fcc94b8f974b24efbf8b38a0c182d
Naren Krishna <naren.krishna@snowflake.com>
2024-04-03 16:48:39 -0700

    Change license for more tests to 2024
    Signed-off-by: Naren Krishna <naren.krishna@snowflake.com>

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #
-# Copyright (c) 2012-2023 Snowflake Computing Inc. All rights reserved.
+# Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
 #
 
 import logging
