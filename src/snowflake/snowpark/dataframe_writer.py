bcdd668f9241c892b36e17c3051696fecd216d4b
Arturo Herrera Aguilar <127373818+sfc-gh-aherreraaguilar@users.noreply.github.com>
2024-04-04 11:48:06 -0600

    Update src/snowflake/snowpark/dataframe_writer.py
    Co-authored-by: Afroz Alam <afroz.alam@snowflake.com>

--- src/snowflake/snowpark/dataframe_writer.py
+++ src/snowflake/snowpark/dataframe_writer.py
@@ -2,10 +2,6 @@
 # Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
 #
 
-#
-# Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
-#
-
 import sys
 from typing import Dict, List, Literal, Optional, Union, overload
 
