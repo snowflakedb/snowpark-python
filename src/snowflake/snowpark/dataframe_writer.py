f35e29ebc4cd835166ab2e7904f0dfdbde566feb
Jianzhun Du <68252326+sfc-gh-jdu@users.noreply.github.com>
2024-04-04 16:06:53 -0700

    SNOW-1285811: Upgrade and force pre-commit on merge gate (#1360)

--- src/snowflake/snowpark/dataframe_writer.py
+++ src/snowflake/snowpark/dataframe_writer.py
@@ -11,11 +11,11 @@ from snowflake.snowpark._internal.analyzer.snowflake_plan_node import (
     SaveMode,
     SnowflakeCreateTable,
 )
+from snowflake.snowpark._internal.open_telemetry import open_telemetry_context_manager
 from snowflake.snowpark._internal.telemetry import (
     add_api_call,
     dfw_collect_api_telemetry,
 )
-from snowflake.snowpark._internal.open_telemetry import open_telemetry_context_manager
 from snowflake.snowpark._internal.type_utils import ColumnOrName, ColumnOrSqlExpr
 from snowflake.snowpark._internal.utils import (
     SUPPORTED_TABLE_TYPES,
@@ -170,14 +170,18 @@ class DataFrameWriter:
         """
         with open_telemetry_context_manager(self.save_as_table, self._dataframe):
             save_mode = (
-                str_to_enum(mode.lower(), SaveMode, "'mode'") if mode else self._save_mode
+                str_to_enum(mode.lower(), SaveMode, "'mode'")
+                if mode
+                else self._save_mode
             )
             full_table_name = (
                 table_name if isinstance(table_name, str) else ".".join(table_name)
             )
             validate_object_name(full_table_name)
             table_name = (
-                parse_table_name(table_name) if isinstance(table_name, str) else table_name
+                parse_table_name(table_name)
+                if isinstance(table_name, str)
+                else table_name
             )
             if column_order is None or column_order.lower() not in ("name", "index"):
                 raise ValueError("'column_order' must be either 'name' or 'index'")
@@ -337,34 +341,42 @@ class DataFrameWriter:
             block=block,
         )
 
-    def csv(self, path: str, overwrite: bool = False, compression: str = None, single: bool = True,
-            partition_by: ColumnOrName = None) -> Union[List[Row], AsyncJob]:
+    def csv(
+        self,
+        path: str,
+        overwrite: bool = False,
+        compression: str = None,
+        single: bool = True,
+        partition_by: ColumnOrName = None,
+    ) -> Union[List[Row], AsyncJob]:
         """Executes internally a `COPY INTO <location> <https://docs.snowflake.com/en/sql-reference/sql/copy-into-location.html>`__ to unload data from a ``DataFrame`` into one or more CSV files in a stage or external stage.
 
-                Args:
-                    path: The destination stage location.
-                    overwrite: Specifies if it should overwrite the file if exists, the default value is ``False``.
-                    compression: String (constant) that specifies to compresses the unloaded data files using the specified compression algorithm. Use the options documented in the `Format Type Options <https://docs.snowflake.com/en/sql-reference/sql/copy-into-location.html#format-type-options-formattypeoptions>`__
-                    single: Boolean that specifies whether to generate a single file or multiple files. If FALSE, a filename prefix must be included in ``<path>``
-                    partition_by: Specifies an expression used to partition the unloaded table rows into separate files. It can be a :class:`Column`, a column name, or a SQL expression.
+        Args:
+            path: The destination stage location.
+            overwrite: Specifies if it should overwrite the file if exists, the default value is ``False``.
+            compression: String (constant) that specifies to compresses the unloaded data files using the specified compression algorithm. Use the options documented in the `Format Type Options <https://docs.snowflake.com/en/sql-reference/sql/copy-into-location.html#format-type-options-formattypeoptions>`__
+            single: Boolean that specifies whether to generate a single file or multiple files. If FALSE, a filename prefix must be included in ``<path>``
+            partition_by: Specifies an expression used to partition the unloaded table rows into separate files. It can be a :class:`Column`, a column name, or a SQL expression.
 
-                Returns:
-                    A list of :class:`Row` objects containing unloading results.
+        Returns:
+            A list of :class:`Row` objects containing unloading results.
 
-                Example::
+        Example::
 
-                    >>> # save this dataframe to a parquet file on the session stage
-                    >>> df = session.create_dataframe([["John", "Berry"], ["Rick", "Berry"], ["Anthony", "Davis"]], schema = ["FIRST_NAME", "LAST_NAME"])
-                    >>> remote_file_path = f"{session.get_session_stage()}/names.csv"
-                    >>> copy_result = df.write.csv(remote_file_path, overwrite=True)
-                    >>> copy_result[0].rows_unloaded
-                    3
-                """
-        return self.copy_into_location(path,
-                                       file_format_type="CSV",
-                                       partition_by=partition_by,
-                                       overwrite=overwrite,
-                                       format_type_options=dict(compression=compression),
-                                       single=single)
+            >>> # save this dataframe to a parquet file on the session stage
+            >>> df = session.create_dataframe([["John", "Berry"], ["Rick", "Berry"], ["Anthony", "Davis"]], schema = ["FIRST_NAME", "LAST_NAME"])
+            >>> remote_file_path = f"{session.get_session_stage()}/names.csv"
+            >>> copy_result = df.write.csv(remote_file_path, overwrite=True)
+            >>> copy_result[0].rows_unloaded
+            3
+        """
+        return self.copy_into_location(
+            path,
+            file_format_type="CSV",
+            partition_by=partition_by,
+            overwrite=overwrite,
+            format_type_options=dict(compression=compression),
+            single=single,
+        )
 
     saveAsTable = save_as_table
