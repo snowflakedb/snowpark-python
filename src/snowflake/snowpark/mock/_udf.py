cfd5e5b991c5102bd9c82c8a38323ced7aa3ddc7
Sophie Tan <sophie.tan@snowflake.com>
2024-04-22 10:02:49 -0700

    Push missing changes

--- src/snowflake/snowpark/mock/_udf.py
+++ src/snowflake/snowpark/mock/_udf.py
@@ -1,7 +1,6 @@
 #
 # Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
 #
-import os
 from types import ModuleType
 from typing import Callable, Dict, List, Optional, Tuple, Union
 
@@ -11,7 +10,7 @@ from snowflake.snowpark._internal.udf_utils import (
 )
 from snowflake.snowpark._internal.utils import TempObjectType
 from snowflake.snowpark.exceptions import SnowparkSQLException
-from snowflake.snowpark.mock._stage_registry import extract_stage_name_and_prefix
+from snowflake.snowpark.mock._udf_utils import extract_import_dir_and_module_name
 from snowflake.snowpark.types import DataType
 from snowflake.snowpark.udf import UDFRegistration, UserDefinedFunction
 
@@ -40,49 +39,11 @@ class MockUDFRegistration(UDFRegistration):
         Imports a python file or a directory of python module structure or a zip of the former.
         Returns the name of the Python module to be imported.
         When udf_name is not None, the import is added to the UDF associated with the name;
-        Otherwise, it is a session level import and will be added to every future UDF call.
+        Otherwise, it is a session level import and will be used if no UDF-level imports are specified.
         """
-        file_name, file_extension = os.path.splitext(os.path.basename(file_path))
-
-        is_on_stage = file_path.startswith("@")
-
-        if is_on_stage:
-            stage_registry = self._session._conn.stage_registry
-            stage_name, stage_prefix = extract_stage_name_and_prefix(file_path)
-            local_path = (
-                stage_registry[stage_name]._working_directory + "/" + stage_prefix
-            )
-        else:
-            local_path = file_path
-
-        is_python_import = file_extension in (
-            ".py",
-            ".zip",
-            "",
-        )  # directory is always considered as python module
-
-        if not is_python_import:
-            absolute_module_path = local_path
-            module_name = ""
-        else:
-            if (
-                import_path and not is_on_stage
-            ):  # import_path is only considered for local python files
-                module_root_dir = local_path[
-                    0 : local_path.rfind(import_path.replace(".", "/"))
-                ]
-            elif file_extension == ".py":
-                module_root_dir = os.path.join(local_path, "..")
-            elif file_extension == ".zip":
-                module_root_dir = local_path
-            else:  # directory
-                module_root_dir = os.path.join(local_path, "..")
-
-            absolute_module_path = os.path.abspath(module_root_dir)
-            module_name = file_name.split(".")[
-                0
-            ]  # the split is for the edge case when the filename contains ., e.g. test.py.zip
-
+        absolute_module_path, module_name = extract_import_dir_and_module_name(
+            file_path, self._session._conn.stage_registry, import_path
+        )
         if udf_name:
             self._udf_level_imports[udf_name].add(absolute_module_path)
         else:
