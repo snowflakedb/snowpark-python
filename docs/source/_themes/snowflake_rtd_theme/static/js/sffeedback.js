679f3bc8fad52e8b5e2f9cc4a239dc761b65afa8
Varnika Budati <varnika.budati@snowflake.com>
2024-04-25 11:26:37 -0700

    Remove the snowflake_rtd_theme docs theme

--- docs/source/_themes/snowflake_rtd_theme/static/js/sffeedback.js
+++
@@ -1,9 +0,0 @@
-// Script to append the current page URL to the mailto: link for doc-feedback.
-window.onload = function() {
-  var linkId = 'doc-feedback-link';
-  var docFeedbackLink = document.getElementById(linkId);
-  if ((docFeedbackLink !== null) && (typeof docFeedbackLink.href !== 'undefined' && docFeedbackLink.href.length > 0)) {
-    var mailtoParams = '?subject=' + encodeURIComponent('Feedback on ' + document.location.href) + '&body=' + encodeURIComponent('Please suggest feedback on ' + document.location.href) + ':';
-    docFeedbackLink.href += mailtoParams;
-  }
-};
--- docs/source/_themes/snowflake_rtd_theme/static/js/sftoggle.js
+++
@@ -1,8 +0,0 @@
-jQuery(document).ready(function() {
-    jQuery('.hamburger').click(function(e) {
-        jQuery(this).toggleClass('sftoggle');
-        jQuery('.left-sidebar').toggleClass('sftoggle');
- 
-        e.preventDefault();
-    });
-});
