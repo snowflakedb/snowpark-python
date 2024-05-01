eeb02252500d3a2948d91353d6043a2f6ea4252e
Arthur Zwiegincew <arthur.zwiegincew@snowflake.com>
2024-05-01 10:54:48 -0700

    Rely on the default config

--- tests/thin-client/steel-thread.py
+++ tests/thin-client/steel-thread.py
@@ -1,20 +1,7 @@
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col, contains
 
-connection_parameters = {
-  "host": "snowflake.dev.local",
-  "port": 53100,
-  "protocol": "http",
-  "account": "SNOWFLAKE",
-  "user": "admin",
-  "password": "test",
-  "role": "ACCOUNTADMIN",
-  "warehouse": "TESTWH_SNOWPANDAS",
-  "database": "TESTDB_SNOWPANDAS",
-  "schema": "public"
-}
-
-session = Session.builder.configs(connection_parameters).create()
+session = Session.builder.create()
 df = session.table('test_table')
 df = df.filter("STR LIKE '%e%'")
 df.show()
