23d9ff00944e829a8c7f22bee7068ab338db4ecd
Arthur Zwiegincew <arthur.zwiegincew@snowflake.com>
2024-05-03 15:17:56 -0700

    [private branch] Maintain the AST as we build up Snowpark expressions: table(), filter() (#1468)
    A very basic initial attempt at serializing the AST.
    
    I'm trying to maintain a parallel codebase for phases 0 and 1 for now,
    since it would be a shame to do this work twice. Once we complete and
    ship phase 0, we'll be able to drastically simplify the phase 1 client.
    
    Unlike what I mentioned before, this implementation doesn't flush
    dependencies of eagerly evaluated expressions. Instead, any client-side
    value is appended to the pending batch. This is simpler to implement and
    will likely work well, although we may need to do some dependency
    analysis on the server to ensure we don't issue unnecessary queries.

--- src/snowflake/snowpark/_internal/server_connection.py
+++ src/snowflake/snowpark/_internal/server_connection.py
@@ -694,6 +694,13 @@ class ServerConnection:
             else default_value
         )
 
+    def ast_query(self, request_id__ast) -> Any:
+        (request_id, ast) = request_id__ast
+        req = {
+            "dataframeAst": ast,
+        }
+        return self._conn._rest.request(f"/queries/v1/query-request?requestId={request_id}", req)
+
 
 def _fix_pandas_df_fixed_type(
     pd_df: "pandas.DataFrame", results_cursor: SnowflakeCursor
