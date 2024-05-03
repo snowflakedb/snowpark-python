7a92099334f0c03ce54c03c6b23fd7622efd8b29
Arthur Zwiegincew <arthur.zwiegincew@snowflake.com>
2024-05-03 15:17:56 -0700

    [private branch] Maintain the AST as we build up Snowpark expressions: table(), filter() (#1468)
    A very basic initial attempt at serializing the AST.
    
    I'm trying to maintain a parallel codebase for phases 0 and 1 for now,
    since it would be a shame to do this work twice. Once we complete and
    ship phase 0, we'll be able to drastically simplify the phase 1 client.
    
    Unlike what I mentioned before, this implementation doesn't flush
    dependencies of eagerly evaluated expressions. Instead, any client-side
    value is appended to the pending batch. This is simpler to implement and
    will likely work well, although we may need to do some dependency
    analysis on the server to ensure we don't issue unnecessary queries.

--- src/snowflake/snowpark/session.py
+++ src/snowflake/snowpark/session.py
@@ -48,6 +48,7 @@ from snowflake.snowpark._internal.analyzer.table_function import (
     TableFunctionRelation,
 )
 from snowflake.snowpark._internal.analyzer.unary_expression import Cast
+from snowflake.snowpark._internal.ast import AstBatch
 from snowflake.snowpark._internal.error_message import SnowparkClientExceptionMessages
 from snowflake.snowpark._internal.packaging_utils import (
     DEFAULT_PACKAGES,
@@ -494,6 +495,8 @@ class Session:
         self._conf = self.RuntimeConfig(self, options or {})
         self._tmpdir_handler: Optional[tempfile.TemporaryDirectory] = None
         self._runtime_version_from_requirement: str = None
+
+        self._ast_batch = AstBatch(self)
 
         _logger.info("Snowpark Session information: %s", self._session_info)
 
