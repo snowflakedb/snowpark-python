2a1777b3fe9fab6006770fa4d8ad8285f7b1adaf
Samedh Desai <samedh.desai@snowflake.com>
2024-05-08 11:00:07 -0700

    Update tests/integ/modin/binary/test_binary_op.py
    Co-authored-by: Naren Krishna <naren.krishna@snowflake.com>

--- tests/integ/modin/binary/test_binary_op.py
+++ tests/integ/modin/binary/test_binary_op.py
@@ -783,7 +783,7 @@ class TestFillValue:
         def op_helper(ser):
             other = rhs
             if (
-                isinstance(other, pd.Index) or isinstance(other, native_pd.Index)
+                isinstance(other, (pd.Index, native_pd.Index))
             ) and isinstance(ser, native_pd.Series):
                 # Native pandas does not support binary operations between a Series and list-like objects -
                 # Series <op> list-like works as expected for all cases except when rhs is an Index object.
