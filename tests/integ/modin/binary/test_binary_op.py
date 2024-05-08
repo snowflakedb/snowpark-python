d3d59766b9afdbddf4657951cbeeb088d7bfc023
Samedh Desai <samedh.desai@snowflake.com>
2024-05-08 11:28:32 -0700

    test fixes

--- tests/integ/modin/binary/test_binary_op.py
+++ tests/integ/modin/binary/test_binary_op.py
@@ -782,9 +782,9 @@ class TestFillValue:
         # fill_value is supposed to be used when either the lhs or rhs is NaN, not when both are NaN.
         def op_helper(ser):
             other = rhs
-            if (
-                isinstance(other, (pd.Index, native_pd.Index))
-            ) and isinstance(ser, native_pd.Series):
+            if (isinstance(other, (pd.Index, native_pd.Index))) and isinstance(
+                ser, native_pd.Series
+            ):
                 # Native pandas does not support binary operations between a Series and list-like objects -
                 # Series <op> list-like works as expected for all cases except when rhs is an Index object.
                 index_as_list = other.tolist()
