5f1fe8d0b14c85801d29f4e40aace3d146ebd8b8
Samedh Desai <samedh.desai@snowflake.com>
2024-05-08 13:11:30 -0700

    test fixes

--- src/snowflake/snowpark/modin/pandas/index.py
+++ src/snowflake/snowpark/modin/pandas/index.py
@@ -7,6 +7,46 @@ import pandas as native_pd
 
 # modin index class, wrapper to native pandas for now
 class Index:
+    """
+    Immutable sequence used for indexing and alignment.
+
+    The basic object storing axis labels for all pandas objects.
+
+    .. versionchanged:: 2.0.0
+
+       Index can hold all numpy numeric dtypes (except float16). Previously only
+       int64/uint64/float64 dtypes were accepted.
+
+    Parameters
+    ----------
+    data : array-like (1-dimensional)
+    dtype : str, numpy.dtype, or ExtensionDtype, optional
+        Data type for the output Index. If not specified, this will be
+        inferred from `data`.
+        See the :ref:`user guide <basics.dtypes>` for more usages.
+    copy : bool, default False
+        Copy input data.
+    name : object
+        Name to be stored in the index.
+    tupleize_cols : bool (default: True)
+        When True, attempt to create a MultiIndex if possible.
+
+    Notes
+    -----
+    An Index instance can **only** contain hashable objects.
+    An Index instance *can not* hold numpy float16 dtype.
+
+    Examples
+    --------
+    pd.Index([1, 2, 3])
+    Index([1, 2, 3], dtype='int64')
+
+    pd.Index(list('abc'))
+    Index(['a', 'b', 'c'], dtype='object')
+
+    pd.Index([1, 2, 3], dtype="uint8")
+    Index([1, 2, 3], dtype='uint8')
+    """
 
     # same fields as native pandas index constructor
     def __new__(
