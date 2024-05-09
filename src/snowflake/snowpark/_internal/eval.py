52bb329efe03708249c28cea0f24941b960060d6
Leonhard Spiegelberg <leonhard.spiegelberg@snowflake.com>
2024-05-09 10:36:23 -0700

    wip

--- src/snowflake/snowpark/_internal/eval.py
+++ src/snowflake/snowpark/_internal/eval.py
@@ -3,13 +3,25 @@
 #
 
 # evaluate AST given as base64 encoded protobuf
+import argparse
+import base64
+import json
+import sys
 
 
 def process_ast(base64_encoded_ast: str) -> None:
-    # use this as entry point in pex bundle.
+    ast = base64.b64decode(base64_encoded_ast)
 
     print("Hello world: " + base64_encoded_ast)
-    pass
+
+    from snowflake.snowpark import Session
+
+    session = Session.builder.getOrCreate()
+    print(f"Currently used warehouse is: {session.get_current_warehouse()}")
+
+    # return result as dict to further encode as json
+    response = {"warehouse": session.get_current_warehouse()}
+    return response
 
 
 def main():
@@ -20,11 +32,22 @@ def main():
     # then terminate process.
     # that should work.
 
-    from snowflake.snowpark import Session
+    # use this as entry point in pex bundle.
+    # in the future, use gRPC to communicate encoded ast, for now use argparse and command-line for a quick test.
+    # TODO: shift to gRPC/protobuf
+    parser = argparse.ArgumentParser(
+        prog="Snowpark Coprocessor",
+        description="Evaluates given AST in base64 encoded form.",
+        epilog="(c) 2024 Snowflake, experimental feature.",
+    )
+    parser.add_argument("ast", nargs=1)  # positional argument
+    args = parser.parse_args()
 
-    session = Session.builder.getOrCreate()
-    print(f'Currently used warehouse is: {session.get_current_warehouse()}')
-    process_ast("test")
+    response = process_ast(args.ast)
+    sys.stdout.flush()
+    sys.stdout("--- RESPONSE ---")
+    sys.stdout.write(json.dumps(response, indent=2))
+    sys.stdout.flush()
 
 
 if __name__ == "__main__":
