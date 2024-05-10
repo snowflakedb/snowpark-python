8235ceae8214bf0a69b15c06f3fc552711706394
Naren Krishna <naren.krishna@snowflake.com>
2024-05-10 09:36:04 -0700

    SNOW-1374306: Add docstring for resample
    Signed-off-by: Naren Krishna <naren.krishna@snowflake.com>

--- src/snowflake/snowpark/modin/plugin/docstrings/base.py
+++ src/snowflake/snowpark/modin/plugin/docstrings/base.py
@@ -2048,6 +2048,84 @@ class BasePandasDataset:  # pragma: no cover: we use this class's docstrings, bu
     def resample():
         """
         Resample time-series data.
+
+        Convenience method for frequency conversion and resampling of time series.
+        The object must have a datetime-like index (DatetimeIndex, PeriodIndex, or TimedeltaIndex),
+        or the caller must pass the label of a datetime-like series/index to the ``on``/``level`` keyword parameter.
+
+        Parameters
+        ----------
+        rule : DateOffset, Timedelta or str
+            The offset string or object representing target conversion.
+        axis : {0 or 'index', 1 or 'columns'}, default 0
+            Which axis to use for up- or down-sampling. For Series this parameter is unused and defaults to 0.
+            Must be DatetimeIndex, TimedeltaIndex or PeriodIndex.
+
+            Deprecated since version 2.0.0: Use frame.T.resample(…) instead.
+        closed : {'right', 'left'}, default None
+            Which side of bin interval is closed. The default is 'left' for all frequency offsets except for
+            'ME', 'YE', 'QE', 'BME', 'BA', 'BQE', and 'W' which all have a default of 'right'.
+        label : {'right', 'left'}, default None
+            Which bin edge label to label bucket with. The default is 'left' for all frequency offsets except for
+            'ME', 'YE', 'QE', 'BME', 'BA', 'BQE', and 'W' which all have a default of 'right'.
+        convention : {'start', 'end', 's', 'e'}, default 'start'
+            For PeriodIndex only, controls whether to use the start or end of rule.
+
+            Deprecated since version 2.2.0: Convert PeriodIndex to DatetimeIndex before resampling instead.
+        kind : {'timestamp', 'period'}, optional, default None
+            Pass 'timestamp' to convert the resulting index to a DateTimeIndex
+            or 'period' to convert it to a PeriodIndex. By default, the input representation is retained.
+        on : str, optional
+            For a DataFrame, column to use instead of index for resampling. Column must be datetime-like.
+        level : str or int, optional
+            For a MultiIndex, level (name or number) to use for resampling. level must be datetime-like.
+        origin : Timestamp or str, default 'start_day'
+            The timestamp on which to adjust the grouping. The timezone of origin must match the timezone of the index.
+            If a string, must be one of the following:
+                'epoch': origin is 1970-01-01
+                'start': origin is the first value of the timeseries
+                'start_day': origin is the first day at midnight of the timeseries
+                'end': origin is the last value of the timeseries
+                'end_day': origin is the ceiling midnight of the last day
+
+            .. versionadded:: 1.3.0
+        offset : Timedelta or str, default is None
+            An offset timedelta added to the origin.
+        group_keys : bool, default False
+            Whether to include the group keys in the result index when using ``.apply()`` on the resampled object.
+
+        Returns
+        -------
+        pandas.api.typing.Resampler
+            Resampler object.
+
+        See Also
+        --------
+        Series.resample: Resample a Series.
+        DataFrame.resample : Resample a dataframe.
+        groupby: Group Series/DataFrame by mapping, function, label, or list of labels.
+        asfreq: Reindex a Series/DataFrame with the given frequency without grouping.
+
+        Examples
+        --------
+        >>> index = pd.date_range('1/1/2000', periods=9, freq='min')
+        >>> series = pd.Series(range(9), index=index)
+        >>> series
+        2000-01-01 00:00:00    0
+        2000-01-01 00:01:00    1
+        2000-01-01 00:02:00    2
+        2000-01-01 00:03:00    3
+        2000-01-01 00:04:00    4
+        2000-01-01 00:05:00    5
+        2000-01-01 00:06:00    6
+        2000-01-01 00:07:00    7
+        2000-01-01 00:08:00    8
+        Freq: None, dtype: int64
+        >>> series.resample('3min').sum()
+        2000-01-01 00:00:00     3
+        2000-01-01 00:03:00    12
+        2000-01-01 00:06:00    21
+        Freq: None, dtype: int64
         """
 
     def reset_index():
