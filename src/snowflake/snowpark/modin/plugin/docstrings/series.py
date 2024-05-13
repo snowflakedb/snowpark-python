25c31e41388fc783edc7018de2b8bc7b4ff775ea
Samedh Desai <samedh.desai@snowflake.com>
2024-05-13 11:31:34 -0700

    doctest cleanups

--- src/snowflake/snowpark/modin/plugin/docstrings/series.py
+++ src/snowflake/snowpark/modin/plugin/docstrings/series.py
@@ -2450,8 +2450,8 @@ class Series:  # pragma: no cover: we use this class's docstrings, but we never 
 
         Slicing a single row from a single column will produce a single
         scalar DataFrame:
-
-        >>> df_0a = df.loc[df.index < 1, ['a']]
+        # TODO: SNOW-1372242: Remove instances of to_pandas when lazy index is implemented
+        >>> df_0a = df.loc[df.index.to_pandas() < 1, ['a']]
         >>> df_0a
            a
         0  1
