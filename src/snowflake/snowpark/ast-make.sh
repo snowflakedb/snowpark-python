a1231bfaefb953273cec5198198677118bb6d6ae
Arthur Zwiegincew <arthur.zwiegincew@snowflake.com>
2024-05-15 11:27:53 -0700

    Removed ast-make.sh since we removed ast.proto.

--- src/snowflake/snowpark/ast-make.sh
+++
@@ -1,3 +0,0 @@
-# Builds the Python client library for the AST protobuf definition.
-# TODO: make this a part of setup.py and ensure that protoc is a compile-time dependency.
-protoc -I=. --python_out=_internal/proto ast.proto
