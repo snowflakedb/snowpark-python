d6d9964075e23cea9d24e28e64e4e778da5fc627
Varnika Budati <varnika.budati@snowflake.com>
2024-05-15 13:24:39 -0700

    added tests for resample docstrings

--- tests/integ/modin/resample/test_resample.py
+++ tests/integ/modin/resample/test_resample.py
@@ -4,15 +4,17 @@
 import random
 import string
 
+import modin.pandas as pd
 import numpy as np
 import pandas as native_pd
 import pytest
 
+import snowflake.snowpark.modin.plugin  # noqa: F401
 from snowflake.snowpark.modin.plugin._internal.resample_utils import (
     IMPLEMENTED_AGG_METHODS,
     IMPLEMENTED_DATEOFFSET_STRINGS,
 )
-from tests.integ.modin.sql_counter import sql_count_checker
+from tests.integ.modin.sql_counter import SqlCounter, sql_count_checker
 from tests.integ.modin.utils import (
     create_test_dfs,
     create_test_series,
@@ -457,5 +459,206 @@ def test_resample_ffill_large_gaps(interval):
     eval_snowpark_pandas_result(
         *create_test_dfs({"a": [1, None, 4, 5, 7, None, 8]}, index=datecol),
         lambda df: df.resample(rule=f"{interval}D").ffill(),
+        check_freq=False,
+    )
+
+
+def test_resample_docstrings_ffill():
+    lst1 = ["2020-01-03", "2020-01-04", "2020-01-05", "2020-01-07", "2020-01-08"]
+    # Native pandas needs all string datetime values to be of the same format, Snowpark pandas does not care.
+    native_lst2 = [
+        "2023-01-03 1:00:00",
+        "2023-01-04 00:00:00",
+        "2023-01-05 23:00:00",
+        "2023-01-06 00:00:00",
+        "2023-01-07 2:00:00",
+        "2023-01-10 00:00:00",
+    ]
+    snow_lst2 = [
+        "2023-01-03 1:00:00",
+        "2023-01-04",
+        "2023-01-05 23:00:00",
+        "2023-01-06",
+        "2023-01-07 2:00:00",
+        "2023-01-10",
+    ]
+
+    # Series example 1
+    native_ser = native_pd.Series([1, 2, 3, 4, 5], index=native_pd.to_datetime(lst1))
+    snow_ser = pd.Series(native_ser, index=pd.to_datetime(lst1))
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_ser, native_ser, lambda s: s.resample("1D").ffill(), check_freq=False
+        )
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_ser, native_ser, lambda s: s.resample("3D").ffill(), check_freq=False
+        )
+
+    # Series example 2
+    native_ser = native_pd.Series(
+        [1, 2, 3, 4, None, 6], index=native_pd.to_datetime(native_lst2)
+    )
+    snow_ser = pd.Series([1, 2, 3, 4, None, 6], index=pd.to_datetime(snow_lst2))
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_ser, native_ser, lambda s: s.resample("1D").ffill(), check_freq=False
+        )
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_ser, native_ser, lambda s: s.resample("2D").ffill(), check_freq=False
+        )
+
+    # DataFrame example 1
+    snow_idx = pd.to_datetime(lst1)
+    native_idx = native_pd.to_datetime(lst1)
+    snow_df = pd.DataFrame(
+        {
+            "a": range(len(snow_idx)),
+            "b": range(len(snow_idx) + 10, len(snow_idx) * 2 + 10),
+        },
+        index=snow_idx,
+    )
+    native_df = native_pd.DataFrame(
+        {
+            "a": range(len(native_idx)),
+            "b": range(len(native_idx) + 10, len(native_idx) * 2 + 10),
+        },
+        index=native_idx,
+    )
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_df, native_df, lambda df: df.resample("1D").ffill(), check_freq=False
+        )
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_df, native_df, lambda df: df.resample("3D").ffill(), check_freq=False
+        )
+
+    # DataFrame example 2
+    snow_idx = pd.to_datetime(snow_lst2)
+    native_idx = native_pd.to_datetime(native_lst2)
+    snow_df = pd.DataFrame(
+        {
+            "a": range(len(snow_idx)),
+            "b": range(len(snow_idx) + 10, len(snow_idx) * 2 + 10),
+        },
+        index=snow_idx,
+    )
+    native_df = native_pd.DataFrame(
+        {
+            "a": range(len(native_idx)),
+            "b": range(len(native_idx) + 10, len(native_idx) * 2 + 10),
+        },
+        index=native_idx,
+    )
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_df, native_df, lambda df: df.resample("1D").ffill(), check_freq=False
+        )
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_df, native_df, lambda df: df.resample("2D").ffill(), check_freq=False
+        )
+
+
+def test_resample_docstrings_df_max():
+    native_idx = native_pd.date_range("2020-01-01", periods=4, freq="1D")
+    snow_idx = pd.date_range("2020-01-01", periods=4, freq="1D")
+    # DataFrame example 1
+    data = [[1, 8], [1, 2], [2, 5], [2, 6]]
+    native_df = native_pd.DataFrame(data, columns=["a", "b"], index=native_idx)
+    snow_df = pd.DataFrame(data, columns=["a", "b"], index=snow_idx)
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_df, native_df, lambda df: df.resample("2D").max(), check_freq=False
+        )
+
+    # DataFrame example 2
+    data = {"A": [1, 2, 3, np.nan], "B": [np.nan, np.nan, 3, 4]}
+    native_idx = native_pd.date_range("2020-01-01", periods=4, freq="1S")
+    snow_idx = pd.date_range("2020-01-01", periods=4, freq="1S")
+    native_df = native_pd.DataFrame(data, index=native_idx)
+    snow_df = pd.DataFrame(data, index=snow_idx)
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_df, native_df, lambda df: df.resample("2S").max(), check_freq=False
+        )
+
+
+def test_resample_docstrings_df_mean():
+    # DataFrame example 1
+    data = {"A": [1, 1, 2, 1, 2], "B": [np.nan, 2, 3, 4, 5]}
+    native_idx = native_pd.date_range("2020-01-01", periods=5, freq="1D")
+    snow_idx = pd.date_range("2020-01-01", periods=5, freq="1D")
+    native_df = native_pd.DataFrame(data, index=native_idx)
+    snow_df = pd.DataFrame(data, index=snow_idx)
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_df, native_df, lambda df: df.resample("2D").mean(), check_freq=False
+        )
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_df,
+            native_df,
+            lambda df: df.resample("2D")["B"].mean(),
+            check_freq=False,
+        )
+
+    # DataFrame example 2
+    data = {"A": [1, 2, 3, np.nan], "B": [np.nan, np.nan, 3, 4]}
+    native_idx = native_pd.date_range("2020-01-01", periods=4, freq="1S")
+    snow_idx = pd.date_range("2020-01-01", periods=4, freq="1S")
+    native_df = native_pd.DataFrame(data, index=native_idx)
+    snow_df = pd.DataFrame(data, index=snow_idx)
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_df, native_df, lambda df: df.resample("2S").mean(), check_freq=False
+        )
+
+
+@pytest.mark.parametrize(
+    "func", ["var", "sum", "std", "min", "median", "count", "mean", "max"]
+)
+def test_resample_docstrings_series(func):
+    # Series example 1
+    native_lst1 = native_pd.date_range("2020-01-01", periods=4, freq="1D")
+    snow_lst1 = pd.date_range("2020-01-01", periods=4, freq="1D")
+    native_ser = native_pd.Series([1, 2, 3, 4], index=native_lst1)
+    snow_ser = pd.Series([1, 2, 3, 4], index=snow_lst1)
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_ser,
+            native_ser,
+            lambda s: getattr(s.resample("2D"), func)(),
+            check_freq=False,
+        )
+
+    # Series example 2
+    native_lst2 = native_pd.date_range("2020-01-01", periods=4, freq="S")
+    snow_lst2 = pd.date_range("2020-01-01", periods=4, freq="S")
+    native_ser = native_pd.Series([1, 2, np.nan, 4], index=native_lst2)
+    snow_ser = pd.Series([1, 2, np.nan, 4], index=snow_lst2)
+    with SqlCounter(query_count=2, join_count=1):
+        eval_snowpark_pandas_result(
+            snow_ser,
+            native_ser,
+            lambda s: getattr(s.resample("2S"), func)(),
+            check_freq=False,
+        )
+
+
+@sql_count_checker(query_count=3, join_count=1)
+@pytest.mark.parametrize("func", ["var", "sum", "std", "min", "median", "count"])
+def test_resample_docstrings_df(func):
+    native_idx = native_pd.date_range("2020-01-01", periods=4, freq="1D")
+    snow_idx = pd.date_range("2020-01-01", periods=4, freq="1D")
+    data = [[1, 8, 2], [1, 2, 5], [2, 5, 8], [2, 6, 9]]
+    native_df = native_pd.DataFrame(data, columns=["a", "b", "c"], index=native_idx)
+    snow_df = pd.DataFrame(data, columns=["a", "b", "c"], index=snow_idx)
+    eval_snowpark_pandas_result(
+        snow_df,
+        native_df,
+        lambda df: getattr(df.resample("2D"), func)(),
         check_freq=False,
     )
