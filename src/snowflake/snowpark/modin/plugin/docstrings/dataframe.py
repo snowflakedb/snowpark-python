f57fd108a09d44e46340eadfea51c72555e002cb
Rehan Durrani <rehan.durrani@snowflake.com>
2024-05-15 20:12:37 -0700

    Address review comments

--- src/snowflake/snowpark/modin/plugin/docstrings/dataframe.py
+++ src/snowflake/snowpark/modin/plugin/docstrings/dataframe.py
@@ -1994,8 +1994,8 @@ class DataFrame:  # pragma: no cover: we use this class's docstrings, but we nev
         -----
         Raise NotImplementedError if
 
-            * margins, observed, or sort is given;
-            * or index, columns, or values is not str;
+            * observed or sort is given;
+            * or index, columns, or values is not str, a list of str, or None;
             * or DataFrame contains MultiIndex;
             * or any argfunc is not "count", "mean", "min", "max", or "sum"
 
