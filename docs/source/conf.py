bea5c18435b07d8fe0116bb86cb0479f2ffa5102
Jonathan Shi <jonathan.shi@snowflake.com>
2024-05-16 16:40:33 -0700

    remove unnecessary links (need to replace "Series or DataFrame" in groupby methods w/ link)

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -65,9 +65,9 @@ exclude_patterns = []
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
 #
-# html_theme = 'alabaster'
+html_theme = 'alabaster'
 # html_theme = 'sphinx_rtd_theme'
-html_theme = "empty"
+# html_theme = "empty"
 
 html_theme_path = [
     "_themes",
