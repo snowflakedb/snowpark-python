6d8b89aa4553ce4c851f6857a132e2b0760525df
Ovidiu Platon <ovidiu.platon@snowflake.com>
2024-05-20 10:11:17 -0700

    Use explicit connection parameters at the client instead of relying on connections.toml
    Also use a different table and filter for the test dataframe

--- tests/thin-client/steel-thread.py
+++ tests/thin-client/steel-thread.py
@@ -1,7 +1,18 @@
 from snowflake.snowpark import Session
 from snowflake.snowpark.functions import col, contains
 
-session = Session.builder.create()
-df = session.table('test_table')
-df = df.filter("STR LIKE '%e%'")
+CONNECTION_PARAMETERS = {
+    'host': "snowflake.dev.local",
+    'protocol': "http",
+    'port': 53200,
+    'account': 'snowflake',
+    'user': "admin",
+    'password': "test",
+    'schema': 'PUBLIC',
+    'database': 'TESTDB',
+    'warehouse': 'REGRESS',
+}
+session = Session.builder.configs(CONNECTION_PARAMETERS).create()
+df = session.table('testtab')
+df = df.filter("C2 < 40")
 df.show()
