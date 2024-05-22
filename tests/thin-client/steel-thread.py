03fd96c46b8e14b0869581c06fa52c09ec72fe11
Ovidiu Platon <ovidiu.platon@snowflake.com>
2024-05-22 09:23:27 -0700

    Customize the steel-thread.py test to match the test schema I've been using

--- tests/thin-client/steel-thread.py
+++ tests/thin-client/steel-thread.py
@@ -1,18 +1,21 @@
+#
+# Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
+#
+
 from snowflake.snowpark import Session
-from snowflake.snowpark.functions import col, contains
 
 CONNECTION_PARAMETERS = {
-    'host': "snowflake.dev.local",
-    'protocol': "http",
-    'port': 53200,
-    'account': 'snowflake',
-    'user': "admin",
-    'password': "test",
-    'schema': 'PUBLIC',
-    'database': 'TESTDB',
-    'warehouse': 'REGRESS',
+    "host": "snowflake.dev.local",
+    "protocol": "http",
+    "port": 53100,
+    "account": "snowflake",
+    "user": "admin",
+    "password": "test",
+    "schema": "TSCHEMA",
+    "database": "TDB",
+    "warehouse": "REGRESS",
 }
 session = Session.builder.configs(CONNECTION_PARAMETERS).create()
-df = session.table('testtab')
-df = df.filter("C2 < 40")
+df = session.table("ttab")
+df = df.filter("C1 < 20")
 df.show()
