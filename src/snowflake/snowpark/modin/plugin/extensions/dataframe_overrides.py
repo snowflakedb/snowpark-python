92ac20debdbb08048a47929c355ae294457bda96
Jonathan Shi <149419494+sfc-gh-joshi@users.noreply.github.com>
2024-05-28 10:44:50 -0700

    SNOW-1264688: Remove overrides of infer_object/nunique (#1580)

--- src/snowflake/snowpark/modin/plugin/extensions/dataframe_overrides.py
+++ src/snowflake/snowpark/modin/plugin/extensions/dataframe_overrides.py
@@ -12,16 +12,9 @@ from typing import Any
 import pandas as native_pd
 
 from snowflake.snowpark.modin import pandas as pd  # noqa: F401
-from snowflake.snowpark.modin.pandas import DataFrame, Series
 from snowflake.snowpark.modin.pandas.api.extensions import register_dataframe_accessor
 from snowflake.snowpark.modin.plugin._internal.telemetry import (
     snowpark_pandas_telemetry_method_decorator,
-)
-from snowflake.snowpark.modin.plugin.compiler.snowflake_query_compiler import (
-    SnowflakeQueryCompiler,
-)
-from snowflake.snowpark.modin.plugin.utils.error_message import (
-    dataframe_not_implemented,
 )
 from snowflake.snowpark.modin.utils import _inherit_docstrings
 
@@ -64,69 +57,3 @@ def memory_usage(self, index: bool = True, deep: bool = False) -> Any:
     # TODO: SNOW-1264697: push implementation down to query compiler
     columns = (["Index"] if index else []) + self._get_columns().array.tolist()
     return native_pd.Series([0] * len(columns), index=columns)
-
-
-@_inherit_docstrings(native_pd.DataFrame.infer_objects, apilink="pandas.DataFrame")
-@register_dataframe_accessor("infer_objects")
-@snowpark_pandas_telemetry_method_decorator
-@dataframe_not_implemented()
-def infer_objects(
-    self,
-) -> SnowflakeQueryCompiler:  # pragma: no cover # noqa: RT01, D200
-    """
-    Attempt to infer better dtypes for object columns.
-    """
-    return self.__constructor__(query_compiler=self._query_compiler.infer_objects())
-
-
-@_inherit_docstrings(native_pd.DataFrame.nunique, apilink="pandas.DataFrame")
-@register_dataframe_accessor("nunique")
-@snowpark_pandas_telemetry_method_decorator
-def nunique(self, axis: int = 0, dropna: bool = True) -> Series:
-    """
-    Count number of distinct elements in specified axis.
-
-    Return Series with number of distinct elements. Can ignore NaN
-    values. Snowpark pandas API does not distinguish between NaN values and treats them all as the same.
-
-    Parameters
-    ----------
-    axis : {0 or 'index', 1 or 'columns'}, default 0
-        The axis to use. 0 or 'index' for row-wise, 1 or 'columns' for
-        column-wise.
-    dropna : bool, default True
-        Don't include NaN in the counts.
-
-    Returns
-    -------
-    Series
-
-    Examples
-    --------
-    >>> import snowflake.snowpark.modin.pandas as pd
-    >>> df = pd.DataFrame({'A': [4, 5, 6], 'B': [4, 1, 1]})
-    >>> df.nunique()
-    A    3
-    B    2
-    dtype: int8
-
-    >>> df.nunique(axis=1)
-    0    1
-    1    2
-    2    2
-    dtype: int8
-
-    >>> df = pd.DataFrame({'A': [None, pd.NA, None], 'B': [1, 2, 1]})
-    >>> df.nunique()
-    A    0
-    B    2
-    dtype: int8
-
-    >>> df.nunique(dropna=False)
-    A    1
-    B    2
-    dtype: int8
-
-    """
-    # TODO: SNOW-1264688: remove this override
-    return super(DataFrame, self).nunique(axis=axis, dropna=dropna)
