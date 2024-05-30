a2b4a834b8019712f9b80c78cfcf7efa5a1760b6
Hazem Elmeleegy <hazem.elmeleegy@snowflake.com>
2024-05-30 09:14:11 -0700

    SNOW-1231747: Add Support for Series.str.get

--- src/snowflake/snowpark/modin/plugin/docstrings/series_utils.py
+++ src/snowflake/snowpark/modin/plugin/docstrings/series_utils.py
@@ -107,7 +107,55 @@ class StringMethods:
         pass
 
     def get():
-        pass
+        """
+        Extract element from each component at specified position or with specified key.
+
+        Extract element from lists, tuples, dict, or strings in each element in the Series/Index.
+
+        Parameters
+        ----------
+        i : int
+            Position or key of element to extract.
+
+        Returns
+        -------
+        Series or Index
+
+        Examples
+        --------
+        >>> s = pd.Series(["String",
+        ...            (1, 2, 3),
+        ...            ["a", "b", "c"],
+        ...            123,
+        ...            -456,
+        ...            {1: "Hello", "2": "World"}])
+        >>> s
+        0                        String
+        1                     (1, 2, 3)
+        2                     [a, b, c]
+        3                           123
+        4                          -456
+        5    {1: 'Hello', '2': 'World'}
+        dtype: object
+
+        >>> s.str.get(1)
+        0        t
+        1        2
+        2        b
+        3      NaN
+        4      NaN
+        5    Hello
+        dtype: object
+
+        >>> s.str.get(-1)
+        0      g
+        1      3
+        2      c
+        3    NaN
+        4    NaN
+        5    None
+        dtype: object
+        """
 
     def join():
         pass
