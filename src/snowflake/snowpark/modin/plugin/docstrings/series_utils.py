32eb776297891768091e41319909e09054873148
Hazem Elmeleegy <hazem.elmeleegy@snowflake.com>
2024-05-31 19:40:43 -0700

    fix doctest

--- src/snowflake/snowpark/modin/plugin/docstrings/series_utils.py
+++ src/snowflake/snowpark/modin/plugin/docstrings/series_utils.py
@@ -139,12 +139,12 @@ class StringMethods:
         dtype: object
 
         >>> s.str.get(-1)
-        0     g
-        1  None
-        2  None
-        3  None
-        4  None
-        5  None
+        0       g
+        1    None
+        2    None
+        3    None
+        4    None
+        5    None
         dtype: object
         """
 
