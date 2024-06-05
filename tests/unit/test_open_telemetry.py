e78c4663feea4cfe8f9b6d047b9329ba28da1a25
Yuyang Wang <yuyang.wang@snowflake.com>
2024-06-05 13:21:21 -0700

    fix test

--- tests/unit/test_open_telemetry.py
+++ tests/unit/test_open_telemetry.py
@@ -156,8 +156,6 @@ def test_inline_register_udaf(dict_exporter):
     assert span.attributes["snow.executable.handler"] == "PythonSumUDAF"
 
     # test register with @udaf
-    lineno = inspect.currentframe().f_lineno + 8
-
     @udaf(
         name="sum_udaf",
         session=session,
@@ -181,6 +179,7 @@ def test_inline_register_udaf(dict_exporter):
         def finish(self):
             return self._sum
 
+    lineno = inspect.currentframe().f_lineno - 17
     spans = spans_to_dict(dict_exporter.get_finished_spans())
     assert "register" in spans
     span = spans["register"]
@@ -287,8 +286,6 @@ def test_inline_register_udtf(dict_exporter):
     assert span.attributes["snow.executable.handler"] == "GeneratorUDTF"
 
     # test register with @udtf
-    lineno = inspect.currentframe().f_lineno + 7
-
     @udtf(
         output_schema=StructType([StructField("number", IntegerType())]),
         name="generate_udtf_with_decorator",
@@ -299,6 +296,7 @@ def test_inline_register_udtf(dict_exporter):
             for i in range(n):
                 yield (i,)
 
+    lineno = inspect.currentframe().f_lineno - 5
     spans = spans_to_dict(dict_exporter.get_finished_spans())
     assert "register" in spans
     span = spans["register"]
@@ -378,12 +376,11 @@ def test_inline_register_udf(dict_exporter):
     assert span.attributes["snow.executable.handler"] == "add_udf"
 
     # test register with decorator @udf
-    lineno = inspect.currentframe().f_lineno + 3
-
     @udf(name="minus", session=session)
     def minus_udf(x: int, y: int) -> int:
         return x - y
 
+    lineno = inspect.currentframe().f_lineno - 3
     spans = spans_to_dict(dict_exporter.get_finished_spans())
     assert "register" in spans
     span = spans["register"]
