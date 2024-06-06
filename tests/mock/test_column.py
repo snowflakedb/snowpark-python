ff6d57b3ecb4f6fb90d32ea252ece41ce235bc9e
Sophie Tan <sophie.tan@snowflake.com>
2024-06-06 14:27:57 -0700

    Add tests

---
+++ tests/mock/test_column.py
@@ -0,0 +1,20 @@
+#
+# Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
+#
+
+from snowflake.snowpark.functions import col, when
+from snowflake.snowpark.row import Row
+
+
+def test_casewhen_with_non_zero_row_index(session):
+    df = session.create_dataframe([[1, 2], [3, 4]], schema=["a", "b"])
+    assert df.filter(col("a") > 1).select(
+        when(col("a").is_null(), 5).when(col("a") == 1, 6).otherwise(7).as_("a")
+    ).collect() == [Row(A=7)]
+
+
+def test_regexp_with_non_zero_row_index(session):
+    df = session.create_dataframe([["1", 2], ["3", 4]], schema=["a", "b"])
+    assert df.filter(col("b") > 2).select(
+        col("a").like("1").alias("res")
+    ).collect() == [Row(RES=False)]
