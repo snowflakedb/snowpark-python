SNOW-1356685: Fix precision loss bug in DataFrame and Series with dtype=np.uint64
Signed-off-by: Naren Krishna <naren.krishna@snowflake.com>

tests/integ/modin/series/test_unique.py
@@ -26,13 +26,14 @@ HOMOGENEOUS_INPUT_DATA_FOR_SERIES = [
     [12.0, 11.999999, 11.999999],
     ["A", "A", "C", "C", "A"],
     [None, "A", None, "B"],
+    _make_nan_interleaved_float_series(),
+    native_pd.Series([1, 2, 2**63, 2**63], dtype=np.uint64),
     pytest.param(
-        native_pd.Series([1, 2, 2**63, 2**63], dtype=np.uint64),
+        native_pd.Series([1, 2, -(2**63) - 1, -(2**64)]),
         marks=pytest.mark.xfail(
-            reason="SNOW-1356685: Dtype with unsigned int results in precision error"
+            reason="Represent overflow using float instead of integer",
         ),
     ),
-    _make_nan_interleaved_float_series(),
 ]
 
 