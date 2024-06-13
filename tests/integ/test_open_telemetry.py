7ca0ae4e27301a41beaf1f32e7b78522213f14ec
Yuyang Wang <yuyang.wang@snowflake.com>
2024-06-13 14:34:42 -0700

    test fix

--- tests/integ/test_open_telemetry.py
+++ tests/integ/test_open_telemetry.py
@@ -159,10 +159,6 @@ def test_inline_register_udaf(session, dict_exporter):
     dict_exporter.clear()
 
 
-@pytest.mark.skipif(
-    "config.getoption('local_testing_mode', default=False)",
-    reason="UDTF not supported in Local Testing",
-)
 def test_register_udtf_from_file(session, dict_exporter):
     test_file = os.path.normpath(
         os.path.join(
@@ -215,10 +211,6 @@ def test_register_udtf_from_file(session, dict_exporter):
     dict_exporter.clear()
 
 
-@pytest.mark.skipif(
-    "config.getoption('local_testing_mode', default=False)",
-    reason="UDTF not supported in Local Testing",
-)
 def test_inline_register_udtf(session, dict_exporter):
     # test register with udtf.register
 
