828290eae7835ac999056876342a72d106bf02a3
Samedh Desai <samedh.desai@snowflake.com>
2024-06-21 13:41:13 -0700

    use helper

--- src/snowflake/snowpark/modin/plugin/_internal/index.py
+++ src/snowflake/snowpark/modin/plugin/_internal/index.py
@@ -300,7 +300,7 @@ class Index:
         array([1, 2, 3])
         """
         return (
-            self._query_compiler.reset_index()
+            self._convert_index_column_to_data_column()
             .drop(columns=self._query_compiler.columns)
             .to_numpy()
             # reshape data to a "row vector" to match native pandas behavior
