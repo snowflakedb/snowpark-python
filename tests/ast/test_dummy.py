eafead615ec4fd8e867afc4277bb70abe60f44da
Leonhard Spiegelberg <leonhard.spiegelberg@snowflake.com>
2024-06-27 09:46:44 -0700

    fix select with column refactor

--- tests/ast/test_dummy.py
+++ tests/ast/test_dummy.py
@@ -7,7 +7,20 @@ from snowflake.snowpark.session import Session
 
 
 def test_example():
-
     session = Session.builder.getOrCreate()
+    df = session.createDataFrame([1, 2, 3], schema=["A"])
+    df.select("A", col("A"))
 
-    session.createDataFrame([1, 2, 3]).select(col("*")).collect()
+    # df = df.select(col("A").regexp("test"))
+    #
+    # df = df.select(col("A").startswith("test"))
+    #
+    # df = df.select(col("A").endswith("test"))
+    #
+    # df = df.select(col("A").substr(col("B"), col("C")))
+    #
+    # df = df.select(col("A").collate("test"))
+    #
+    # df = df.select(col("A").contains("test"))
+
+    # .select(col("*")).collect())
