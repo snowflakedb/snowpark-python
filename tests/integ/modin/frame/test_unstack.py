4d02722a261eef07d9a58e83fb9736ae98cce2f2
Naren Krishna <naren.krishna@snowflake.com>
2024-07-02 08:39:01 -0700

    fix unstack for index/column names
    Signed-off-by: Naren Krishna <naren.krishna@snowflake.com>

--- tests/integ/modin/frame/test_unstack.py
+++ tests/integ/modin/frame/test_unstack.py
@@ -11,10 +11,20 @@ from tests.integ.modin.sql_counter import sql_count_checker
 from tests.integ.modin.utils import eval_snowpark_pandas_result
 
 
+@pytest.mark.parametrize(
+    "index_names",
+    [
+        [None, None],
+        ["hello", "world"],
+        [None, "world"],
+        ["hello", None],
+    ],
+)
 @sql_count_checker(query_count=1)
-def test_unstack_input_no_multiindex():
+def test_unstack_input_no_multiindex(index_names):
     index = native_pd.MultiIndex.from_tuples(
-        [("one", "a"), ("one", "b"), ("two", "a"), ("two", "b")]
+        tuples=[("one", "a"), ("one", "b"), ("two", "a"), ("two", "b")],
+        names=index_names,
     )
     # Note we call unstack below to create a dataframe without a multiindex before
     # calling unstack again
@@ -23,31 +33,19 @@ def test_unstack_input_no_multiindex():
     eval_snowpark_pandas_result(snow_df, native_df, lambda df: df.unstack())
 
 
+@pytest.mark.parametrize(
+    "index_names",
+    [
+        [None, None],
+        ["hello", "world"],
+        ["hello", None],
+        [None, "world"],
+    ],
+)
 @sql_count_checker(query_count=1)
-def test_unstack_input_no_multiindex_with_names():
-    index = native_pd.MultiIndex.from_tuples(
-        [("one", "a"), ("one", "b"), ("two", "a"), ("two", "b")],
-        names=["hello", "world"],
-    )
-    # Note we call unstack below to create a dataframe without a multiindex before
-    # calling unstack again
-    native_df = native_pd.Series(np.arange(1.0, 5.0), index=index).unstack(level=0)
-    snow_df = pd.DataFrame(native_df)
-    eval_snowpark_pandas_result(snow_df, native_df, lambda df: df.unstack())
-
-
-@sql_count_checker(query_count=1)
-def test_unstack_multiindex():
-    index = pd.MultiIndex.from_product([[2, 1], ["a", "b"]])
-    native_df = native_pd.DataFrame(np.random.randn(4), index=index, columns=["A"])
-    snow_df = pd.DataFrame(native_df)
-    eval_snowpark_pandas_result(snow_df, native_df, lambda df: df.unstack())
-
-
-@sql_count_checker(query_count=1)
-def test_unstack_multiindex_with_names():
+def test_unstack_multiindex(index_names):
     index = pd.MultiIndex.from_product(
-        iterables=[[2, 1], ["a", "b"]], names=["hello", "world"]
+        iterables=[[2, 1], ["a", "b"]], names=index_names
     )
     native_df = native_pd.DataFrame(np.random.randn(4), index=index, columns=["A"])
     snow_df = pd.DataFrame(native_df)
@@ -75,3 +73,16 @@ def test_unstack_sort_notimplemented():
         match="Snowpark pandas DataFrame/Series.unstack does not yet support the `sort` parameter",
     ):
         snow_df.unstack(sort=False)
+
+
+@sql_count_checker(query_count=0)
+def test_unstack_non_integer_level_notimplemented():
+    index = pd.MultiIndex.from_product([[2, 1], ["a", "b"]])
+    native_df = native_pd.DataFrame(np.random.randn(4), index=index, columns=["A"])
+    snow_df = pd.DataFrame(native_df)
+
+    with pytest.raises(
+        NotImplementedError,
+        match="Snowpark pandas DataFrame/Series.unstack does not yet support a non-integer `level` parameter",
+    ):
+        snow_df.unstack(level=[0, 1])
