40ae51ee1928097719270ad0b34755e1ca57ebcb
Varnika Budati <varnika.budati@snowflake.com>
2024-07-10 12:12:40 -0700

    Added logic for Index.sort_values and tests

--- tests/integ/modin/index/test_index_methods.py
+++ tests/integ/modin/index/test_index_methods.py
@@ -111,18 +111,6 @@ def test_index_value_counts(native_index):
     assert_series_equal(snow_index.value_counts(), native_index.value_counts())
 
 
-@sql_count_checker(query_count=4)
-@pytest.mark.parametrize("native_index", NATIVE_INDEX_TEST_DATA[:-1])
-def test_index_sort_values(native_index):
-    snow_index = pd.Index(native_index)
-    with SqlCounter(query_count=4):
-        assert_index_equal(snow_index.sort_values(), native_index.sort_values())
-        native_tup = native_index.sort_values(return_indexer=True)
-        snow_tup = snow_index.sort_values(return_indexer=True)
-        assert_index_equal(native_tup[0], snow_tup[0])
-        assert np.array_equal(native_tup[1], snow_tup[1])
-
-
 @sql_count_checker(query_count=8)
 def test_index_union():
     idx1 = pd.Index([1, 2, 3, 4])
