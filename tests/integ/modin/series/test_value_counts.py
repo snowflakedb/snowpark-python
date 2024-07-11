c2dffb48a26e328ab469a5be50e9926d184c7346
Varnika Budati <varnika.budati@snowflake.com>
2024-07-11 16:10:23 -0700

    Update docstring for Series.value_counts and add comments in the test

--- tests/integ/modin/series/test_value_counts.py
+++ tests/integ/modin/series/test_value_counts.py
@@ -31,11 +31,11 @@ NATIVE_SERIES_TEST_DATA = [
     native_pd.Series(
         [1, 2, 3, 2, 3, 5, 6, 7, 8, 4, 4, 5, 6, 7, 1, 2, 1, 2, 3, 4, 3, 4, 5, 6, 7]
     ),
-    # native_pd.Series([1.1, 2.2, 1.0, 1, 1.1, 2.2, 1, 1, 1, 2, 2, 2, 2.2]),
-    # native_pd.Series([1, 3, 1, 1, 1, 3, 1, 1, 1, 2, 2, 2, 3]),
-    # native_pd.Series(
-    #     [True, False, True, False, True, False, True, False, True, True], dtype=bool
-    # ),
+    native_pd.Series([1.1, 2.2, 1.0, 1, 1.1, 2.2, 1, 1, 1, 2, 2, 2, 2.2]),
+    native_pd.Series([1, 3, 1, 1, 1, 3, 1, 1, 1, 2, 2, 2, 3]),
+    native_pd.Series(
+        [True, False, True, False, True, False, True, False, True, True], dtype=bool
+    ),
     native_pd.Series(
         [
             "a",
@@ -124,25 +124,21 @@ def test_value_counts_bins():
 
 @pytest.mark.parametrize("native_series", NATIVE_SERIES_TEST_DATA)
 @pytest.mark.parametrize("normalize", [True, False])
-@pytest.mark.parametrize("sort", [True])
+@pytest.mark.parametrize("sort", [True, False])
 @pytest.mark.parametrize("ascending", [True, False])
 @pytest.mark.parametrize("dropna", [True, False])
-@sql_count_checker(query_count=2)
+@sql_count_checker(query_count=1)
 def test_series_value_counts(native_series, normalize, sort, ascending, dropna):
+    # If `sort=True`, sort by the count. If `sort=False`, maintain the original ordering.
+    # `ascending` controls whether to sort the count in ascending or descending order.
+
+    # When there is a tie between counts, the order is still deterministic, but
+    # may be different from the result from native pandas. Snowpark pandas will
+    # always respect the order of insertion during ties. Native pandas is not
+    # deterministic since the original order/order of insertion is based on the
+    # Python hashmap which may produce different results on different versions.
+    # Refer to: https://github.com/pandas-dev/pandas/issues/15833
     snow_series = pd.Series(native_series)
-    print(f"\n___PANDAS VERSION___\n{native_pd.__version__}")
-    print("\n___NATIVE PANDAS VALUE COUNTS RESULT___")
-    print(
-        native_series.value_counts(
-            normalize=normalize, sort=sort, ascending=ascending, dropna=dropna
-        )
-    )
-    print("\nSNOWPARK PANDAS VALUE COUNTS RESULT___")
-    print(
-        snow_series.value_counts(
-            normalize=normalize, sort=sort, ascending=ascending, dropna=dropna
-        )
-    )
     eval_snowpark_pandas_result(
         snow_series,
         native_series,
