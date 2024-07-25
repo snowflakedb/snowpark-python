685c1e8791625cff21c5b12ec01befe1ecacf061
Yun Zou <yun.zou@snowflake.com>
2024-07-25 11:03:48 -0700

    fix error

--- src/snowflake/snowpark/_internal/compiler/common_subdataframe_elimination.py
+++ src/snowflake/snowpark/_internal/compiler/common_subdataframe_elimination.py
@@ -12,16 +12,28 @@ from snowflake.snowpark._internal.analyzer.select_statement import (
     SetStatement,
 )
 from snowflake.snowpark._internal.analyzer.snowflake_plan import SnowflakePlan
-from snowflake.snowpark._internal.analyzer.snowflake_plan_node import (
-    LogicalPlan,
-    WithObjectRef,
-    WithQueryBlock,
-)
-from snowflake.snowpark._internal.optimizer.query_generator import QueryGenerator
+from snowflake.snowpark._internal.analyzer.snowflake_plan_node import LogicalPlan
+from snowflake.snowpark._internal.compiler.query_generator import QueryGenerator
 from snowflake.snowpark._internal.utils import (
     TempObjectType,
     random_name_for_temp_object,
 )
 
 TreeNode = Union[SnowflakePlan, Selectable]
+
+
+class CommonSubDataframeElimination:
+    _logical_plans: List[LogicalPlan]
+    _node_count_map: Dict[LogicalPlan, int]
+    _node_parents_map: Dict[LogicalPlan, Set[LogicalPlan]]
+    _duplicated_nodes: Set[LogicalPlan]
+    _query_generator: QueryGenerator
+
+    def __init__(self, logical_plans: List[LogicalPlan], query_generator: QueryGenerator) -> None:
+        self._logical_plans = logical_plans
+        self._query_generator = query_generator
+        self._node_count_map = defaultdict(int)
+        self._node_parents_map = defaultdict(set)
+        self._duplicated_nodes = set()
+
 
