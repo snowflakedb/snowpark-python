2c3a6188887518ad3876edfa581bc8d5bcf22cc4
Jonathan Shi <jonathan.shi@snowflake.com>
2024-07-25 14:31:54 -0700

    fix ascending tests

--- tests/integ/modin/groupby/test_value_counts.py
+++ tests/integ/modin/groupby/test_value_counts.py
@@ -36,6 +36,7 @@ TEST_DATA = [
 
 @pytest.mark.parametrize("test_data", TEST_DATA)
 @pytest.mark.parametrize("by", ["by"])  # , ["by", "value1"], ["by", "value2"]])
+@pytest.mark.parametrize("groupby_sort", [True, False])
 @pytest.mark.parametrize(
     "subset",
     [None, ["value1"], ["value2"], ["value1", "value2"]],
@@ -43,10 +44,10 @@ TEST_DATA = [
 @pytest.mark.parametrize("normalize", [True, False])
 @pytest.mark.parametrize("dropna", [True, False])
 @sql_count_checker(query_count=1)
-def test_value_counts_basic(test_data, by, subset, normalize, dropna):
-    # Tests here use check_like=True because Snowpark pandas will always preserve the original
-    # order of rows within groups, while native pandas provides this guarantee only for the
-    # grouping and count/proportion columns. We check this ordering behavior in a separate test.
+def test_value_counts_basic(test_data, by, groupby_sort, subset, normalize, dropna):
+    # In this test, we use check_like=True because Snowpark pandas will always preserve the original
+    # order of rows within groups, while native pandas provides this guarantee only for the grouping
+    # and count/proportion columns. We check this ordering behavior in a separate test.
     #
     # Note that when as_index=False, check_like is insufficient because columns that were originally
     # part of the MultiIndex are demoted to data columns before value_counts returns, so check_like
@@ -66,13 +67,13 @@ def test_value_counts_basic(test_data, by, subset, normalize, dropna):
         # since NaNs are sorted last, we want the sentinel to sort to the end as well
         VALUE_COUNTS_TEST_SENTINEL = "zzzzzz"
         snow_df, native_df = create_test_dfs(test_data)
-        snow_result = snow_df.groupby(by=by).value_counts(
+        snow_result = snow_df.groupby(by=by, sort=groupby_sort).value_counts(
             subset=subset,
             normalize=normalize,
             dropna=dropna,
         )
         native_df = native_df.fillna(value=VALUE_COUNTS_TEST_SENTINEL)
-        native_result = native_df.groupby(by=by).value_counts(
+        native_result = native_df.groupby(by=by, sort=groupby_sort).value_counts(
             subset=subset,
             normalize=normalize,
             dropna=dropna,
@@ -86,7 +87,7 @@ def test_value_counts_basic(test_data, by, subset, normalize, dropna):
     else:
         eval_snowpark_pandas_result(
             *create_test_dfs(test_data),
-            lambda df: df.groupby(by=by).value_counts(
+            lambda df: df.groupby(by=by, sort=groupby_sort).value_counts(
                 subset=subset,
                 normalize=normalize,
                 dropna=dropna,
@@ -96,8 +97,9 @@ def test_value_counts_basic(test_data, by, subset, normalize, dropna):
 
 
 @pytest.mark.parametrize(
-    "test_data, groupby_kwargs, sort, expected_result",
+    "test_data, groupby_kwargs, value_counts_kwargs, expected_result",
     [
+        # value_counts(sort)
         (
             TEST_DATA[0],
             {
@@ -105,7 +107,10 @@ def test_value_counts_basic(test_data, by, subset, normalize, dropna):
                 "as_index": True,
                 "sort": True,
             },
-            True,
+            {
+                "sort": True,
+                "ascending": False,
+            },
             # In pandas, the row [c, dd, 2] appears before [c, ee, 1] despite the latter
             # appearing earlier in the original frame
             #
@@ -142,7 +147,105 @@ def test_value_counts_basic(test_data, by, subset, normalize, dropna):
                 "as_index": True,
                 "sort": True,
             },
-            False,
+            {
+                "sort": False,
+                "ascending": False,
+            },
+            # by  value1  value2
+            # a   bb      3         1
+            #     aa      1         2
+            # b   aa      2         1
+            #     bb      1         1
+            #     cc      3         1
+            # c   ee      1         1
+            #     dd      2         1
+            # Name: count, dtype: int64
+            native_pd.Series(
+                [1, 2, 1, 1, 1, 1, 1],
+                name="count",
+                index=pd.MultiIndex.from_tuples(
+                    [
+                        ("a", "bb", 3),
+                        ("a", "aa", 1),
+                        ("b", "aa", 2),
+                        ("b", "bb", 1),
+                        ("b", "cc", 3),
+                        ("c", "ee", 1),
+                        ("c", "dd", 2),
+                    ],
+                    names=["by", "value1", "value2"],
+                ),
+            ),
+        ),
+        # groupby(as_index)
+        (
+            TEST_DATA[0],
+            {
+                "by": ["by"],
+                "as_index": False,
+                "sort": True,
+            },
+            {
+                "sort": True,
+                "ascending": False,
+            },
+            #   by value1  value2  count
+            # 0  a     aa       1      2
+            # 1  a     bb       3      1
+            # 2  b     aa       2      1
+            # 3  b     bb       1      1
+            # 4  b     cc       3      1
+            # 5  c     ee       1      1
+            # 6  c     dd       2      1
+            native_pd.DataFrame(
+                {
+                    "by": ["a", "a", "b", "b", "b", "c", "c"],
+                    "value1": ["aa", "bb", "aa", "bb", "cc", "ee", "dd"],
+                    "value2": [1, 3, 2, 1, 3, 1, 2],
+                    "count": [2, 1, 1, 1, 1, 1, 1],
+                }
+            ),
+        ),
+        (
+            TEST_DATA[0],
+            {
+                "by": ["by"],
+                "as_index": False,
+                "sort": True,
+            },
+            {
+                "sort": False,
+                "ascending": False,
+            },
+            #   by value1  value2  count
+            # 0  a     bb       3      1
+            # 1  a     aa       1      2
+            # 2  b     aa       2      1
+            # 3  b     bb       1      1
+            # 4  b     cc       3      1
+            # 5  c     ee       1      1
+            # 6  c     dd       2      1
+            native_pd.DataFrame(
+                {
+                    "by": ["a", "a", "b", "b", "b", "c", "c"],
+                    "value1": ["bb", "aa", "aa", "bb", "cc", "ee", "dd"],
+                    "value2": [3, 1, 2, 1, 3, 1, 2],
+                    "count": [1, 2, 1, 1, 1, 1, 1],
+                }
+            ),
+        ),
+        # value_counts(ascending)
+        (
+            TEST_DATA[0],
+            {
+                "by": ["by"],
+                "as_index": True,
+                "sort": True,
+            },
+            {
+                "sort": True,
+                "ascending": True,
+            },
             # by  value1  value2
             # a   bb      3         1
             #     aa      1         2
@@ -170,53 +273,101 @@ def test_value_counts_basic(test_data, by, subset, normalize, dropna):
             ),
         ),
         (
-            TEST_DATA[0],
+            TEST_DATA[2],
             {
                 "by": ["by"],
-                "as_index": False,
-                "sort": True,
+                "as_index": True,
+                "sort": False,
             },
-            True,
-            #   by value1  value2  count
-            # 0  a     aa       1      2
-            # 1  a     bb       3      1
-            # 2  b     aa       2      1
-            # 3  b     bb       1      1
-            # 4  b     cc       3      1
-            # 5  c     ee       1      1
-            # 6  c     dd       2      1
-            native_pd.DataFrame(
-                {
-                    "by": ["a", "a", "b", "b", "b", "c", "c"],
-                    "value1": ["aa", "bb", "aa", "bb", "cc", "ee", "dd"],
-                    "value2": [1, 3, 2, 1, 3, 1, 2],
-                    "count": [2, 1, 1, 1, 1, 1, 1],
-                }
+            {
+                "sort": False,
+            },
+            # by      value1  value2
+            # male    low     US        1
+            #         medium  FR        1
+            # female  high    US        1
+            # male    low     FR        2
+            # female  high    FR        1
+            # Name: count, dtype: int64
+            native_pd.Series(
+                [1, 1, 1, 2, 1],
+                name="count",
+                index=pd.MultiIndex.from_tuples(
+                    [
+                        ("male", "low", "US"),
+                        ("male", "medium", "FR"),
+                        ("female", "high", "US"),
+                        ("male", "low", "FR"),
+                        ("female", "high", "FR"),
+                    ],
+                    names=["by", "value1", "value2"],
+                ),
             ),
         ),
         (
-            TEST_DATA[0],
+            TEST_DATA[2],
             {
                 "by": ["by"],
-                "as_index": False,
+                "as_index": True,
+                "sort": False,
+            },
+            {
+                "sort": True,
+                "ascending": True,
+            },
+            # by      value1  value2
+            # male    low     US        1
+            #         medium  FR        1
+            # female  high    US        1
+            #                 FR        1
+            # male    low     FR        2
+            # Name: count, dtype: int64
+            native_pd.Series(
+                [1, 1, 1, 1, 2],
+                name="count",
+                index=pd.MultiIndex.from_tuples(
+                    [
+                        ("male", "low", "US"),
+                        ("male", "medium", "FR"),
+                        ("female", "high", "US"),
+                        ("female", "high", "FR"),
+                        ("male", "low", "FR"),
+                    ],
+                    names=["by", "value1", "value2"],
+                ),
+            ),
+        ),
+        (
+            TEST_DATA[2],
+            {
+                "by": ["by"],
+                "as_index": True,
                 "sort": True,
             },
-            False,
-            #   by value1  value2  count
-            # 0  a     bb       3      1
-            # 1  a     aa       1      2
-            # 2  b     aa       2      1
-            # 3  b     bb       1      1
-            # 4  b     cc       3      1
-            # 5  c     ee       1      1
-            # 6  c     dd       2      1
-            native_pd.DataFrame(
-                {
-                    "by": ["a", "a", "b", "b", "b", "c", "c"],
-                    "value1": ["bb", "aa", "aa", "bb", "cc", "ee", "dd"],
-                    "value2": [3, 1, 2, 1, 3, 1, 2],
-                    "count": [1, 2, 1, 1, 1, 1, 1],
-                }
+            {
+                "sort": True,
+                "ascending": True,
+            },
+            # by      value1  value2
+            # female  high    US        1
+            #                 FR        1
+            # male    low     US        1
+            #         medium  FR        1
+            #         low     FR        2
+            # Name: count, dtype: int64
+            native_pd.Series(
+                [1, 1, 1, 1, 2],
+                name="count",
+                index=pd.MultiIndex.from_tuples(
+                    [
+                        ("female", "high", "US"),
+                        ("female", "high", "FR"),
+                        ("male", "low", "US"),
+                        ("male", "medium", "FR"),
+                        ("male", "low", "FR"),
+                    ],
+                    names=["by", "value1", "value2"],
+                ),
             ),
         ),
     ],
@@ -225,14 +376,16 @@ def test_value_counts_basic(test_data, by, subset, normalize, dropna):
 def test_value_counts_pandas_issue_59307(
     test_data,
     groupby_kwargs,
-    sort,
+    value_counts_kwargs,
     expected_result,
 ):
     # Tests that Snowpark pandas preserves the order of rows from the input frame within groups.
     # When groupby(sort=True), native pandas guarantees order is preserved only for the grouping columns.
     # https://github.com/pandas-dev/pandas/issues/59307
     assert_snowpark_pandas_equal_to_pandas(
-        pd.DataFrame(test_data).groupby(**groupby_kwargs).value_counts(sort=sort),
+        pd.DataFrame(test_data)
+        .groupby(**groupby_kwargs)
+        .value_counts(**value_counts_kwargs),
         expected_result,
     )
 
@@ -248,23 +401,6 @@ def test_value_counts_bad_subset(test_data, subset):
         expect_exception=True,
         expect_exception_type=KeyError,
         assert_exception_equal=False,
-    )
-
-
-@pytest.mark.parametrize("test_data", TEST_DATA)
-@pytest.mark.parametrize("as_index", [True, False])
-@pytest.mark.parametrize("groupby_sort", [True, False])
-@pytest.mark.parametrize("sort", [True, False])
-@pytest.mark.parametrize("ascending", [True, False])
-@sql_count_checker(query_count=1)
-def test_value_counts_sort_ascending(
-    test_data, as_index, groupby_sort, sort, ascending
-):
-    eval_snowpark_pandas_result(
-        *create_test_dfs(test_data),
-        lambda x: x.groupby(
-            by=["by"], as_index=as_index, sort=groupby_sort
-        ).value_counts(sort=sort, ascending=ascending),
     )
 
 
