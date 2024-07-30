3f7cc4b24815960b4936bc65fb182c3b5851efbd
Naresh Kumar <naresh.kumar@snowflake.com>
2024-07-30 15:40:08 -0700

    SNOW-1558877: Add snowpark pandas DatetimeIndex class

---
+++ tests/integ/modin/index/test_datetime_index_methods.py
@@ -0,0 +1,29 @@
+#
+# Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
+#
+
+import modin.pandas as pd
+import pandas as native_pd
+import pytest
+
+import snowflake.snowpark.modin.plugin  # noqa: F401
+
+
+def test_datetime_index_construction():
+    # create from native pandas datetime index.
+    index = native_pd.DatetimeIndex(["2021-01-01", "2021-01-02", "2021-01-03"])
+    snow_index = pd.Index(index)
+    assert isinstance(snow_index, pd.DatetimeIndex)
+
+    # create from query compiler with timestamp type.
+    df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]}, index=index)
+    snow_index = df.index
+    assert isinstance(snow_index, pd.DatetimeIndex)
+
+
+def test_datetime_index_construction_negative():
+    # Try to create datatime index query compiler with int type.
+    df = pd.DataFrame({"a": [1, 2, 3], "b": [4, 5, 6]})
+    msg = "DatetimeIndex can only be created from a query compiler with TimestampType"
+    with pytest.raises(ValueError, match=msg):
+        pd.DatetimeIndex(df._query_compiler)
