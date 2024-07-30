0bcd6d4921b50e0cba81947b9eaa7699b7091bb5
Afroz Alam <afroz.alam@snowflake.com>
2024-07-30 13:20:24 -0700

    add pipeline breaker node logic

--- src/snowflake/snowpark/_internal/compiler/large_query_breakdown.py
+++ src/snowflake/snowpark/_internal/compiler/large_query_breakdown.py
@@ -3,32 +3,53 @@
 #
 
 from collections import defaultdict
-from typing import TYPE_CHECKING, List, Optional, Union
+from typing import TYPE_CHECKING, List, Optional, Union as UnionType
 
 from sortedcontainers import SortedList
 
+from snowflake.snowpark._internal.analyzer.binary_plan_node import Union
 from snowflake.snowpark._internal.analyzer.query_plan_analysis_utils import (
     get_complexity_score,
 )
-from snowflake.snowpark._internal.analyzer.select_statement import Selectable
+from snowflake.snowpark._internal.analyzer.select_statement import (
+    SET_UNION,
+    Selectable,
+    SelectStatement,
+    SetStatement,
+)
 from snowflake.snowpark._internal.analyzer.snowflake_plan import SnowflakePlan
 from snowflake.snowpark._internal.analyzer.snowflake_plan_node import (
     SaveMode,
     SnowflakeCreateTable,
 )
+from snowflake.snowpark._internal.analyzer.unary_plan_node import (
+    Aggregate,
+    Pivot,
+    Sample,
+    Sort,
+    Unpivot,
+)
 from snowflake.snowpark._internal.compiler.utils import (
     is_active_transaction,
     replace_child,
 )
+from snowflake.snowpark._internal.utils import (
+    TempObjectType,
+    random_name_for_temp_object,
+)
 from snowflake.snowpark.session import Session
 
 if TYPE_CHECKING:
-    PlanTreeNode = Union[SnowflakePlan, Selectable]
+    PlanTreeNode = UnionType[SnowflakePlan, Selectable]
+
+COMPLEXITY_SCORE_LOWER_BOUND = 300
+COMPLEXITY_SCORE_UPPER_BOUND = 1000
 
 
 class LargeQueryBreakdown:
-    COMPLEXITY_SCORE_LOWER_BOUND = 300
-    COMPLEXITY_SCORE_UPPER_BOUND = 1000
+    """This class is used to break down large query plans into smaller partitions based on
+    estimated complexity score of the plan nodes.
+    """
 
     def __init__(self, session: Session) -> None:
         self.session = session
@@ -44,12 +65,12 @@ class LargeQueryBreakdown:
 
     def breakdown_plan(self, root: PlanTreeNode) -> List[PlanTreeNode]:
         complexity_score = get_complexity_score(root.cumulative_node_complexity)
-        if complexity_score < self.COMPLEXITY_SCORE_UPPER_BOUND:
+        if complexity_score <= COMPLEXITY_SCORE_UPPER_BOUND:
             return [root]
 
         plans = []
 
-        while complexity_score > self.COMPLEXITY_SCORE_UPPER_BOUND:
+        while complexity_score > COMPLEXITY_SCORE_UPPER_BOUND:
             partition = self.get_partitioned_plan(root)
             if partition is None:
                 break
@@ -96,11 +117,31 @@ class LargeQueryBreakdown:
         return temp_table_plan
 
     def is_node_valid_to_breakdown(self, score: int, node: PlanTreeNode) -> bool:
+        """Method to check if a node is valid to breakdown based on complexity score and node type."""
         return (
-            self.COMPLEXITY_SCORE_LOWER_BOUND
-            < score
-            < self.COMPLEXITY_SCORE_UPPER_BOUND
-        )
+            COMPLEXITY_SCORE_LOWER_BOUND < score < COMPLEXITY_SCORE_UPPER_BOUND
+        ) and self.is_node_pipeline_breaker(node)
+
+    def is_node_pipeline_breaker(self, node: PlanTreeNode) -> bool:
+        if isinstance(node, (Pivot, Unpivot, Sort, Aggregate)):
+            return True
+
+        if isinstance(node, Sample):
+            return node.row_count is not None
+
+        if isinstance(node, Union):
+            return not node.is_all
+
+        if isinstance(node, SelectStatement):
+            return node.order_by is not None
+
+        if isinstance(node, SetStatement):
+            return any(operand.operator == SET_UNION for operand in node.set_operands)
+
+        if isinstance(node, SnowflakePlan):
+            return node.source_plan is not None and self.is_node_pipeline_breaker(
+                node.source_plan
+            )
 
     def update_ancestors(
         self, parent_map: defaultdict, child: PlanTreeNode, temp_table_name: str
@@ -121,3 +162,8 @@ class LargeQueryBreakdown:
             # TODO: reset SnowflakePlan for node
             # reset_snowflake_plan(node)
             node.reset_cumulative_node_complexity()
+
+    def get_temp_table_name(self) -> str:
+        return self.session.get_fully_qualified_name_if_possible(
+            f'"{random_name_for_temp_object(TempObjectType.TABLE)}"'
+        )
