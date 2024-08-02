fe62166231315aca57e0916a8cc3bf5b53eb849f
Hazem Elmeleegy <hazem.elmeleegy@snowflake.com>
2024-08-02 14:45:41 -0700

    fix errors

--- src/snowflake/snowpark/modin/plugin/docstrings/series_utils.py
+++ src/snowflake/snowpark/modin/plugin/docstrings/series_utils.py
@@ -1548,11 +1548,11 @@ class CombinedDatetimelikeProperties:
         ...                   periods=4)})
         >>> df.assign(quarter=df.dates.dt.quarter,
         ...           is_quarter_start=df.dates.dt.is_quarter_start)
-               dates  quarter  is_quarter_end
-        0 2017-03-30        1           False
-        1 2017-03-31        1           False
-        2 2017-04-01        2            True
-        3 2017-04-02        2           False
+               dates  quarter  is_quarter_start
+        0 2017-03-30        1             False
+        1 2017-03-31        1             False
+        2 2017-04-01        2              True
+        3 2017-04-02        2             False
         """
 
     @property
