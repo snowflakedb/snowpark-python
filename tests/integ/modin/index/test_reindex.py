c930c2d14b410708e6aad58f6d8a22497a643f33
Rehan Durrani <rehan.durrani@snowflake.com>
2024-08-12 13:58:16 -0700

    Refactor testing

--- tests/integ/modin/index/test_reindex.py
+++ tests/integ/modin/index/test_reindex.py
@@ -12,28 +12,16 @@ import pytest
 import snowflake.snowpark.modin.plugin  # noqa: F401
 from snowflake.snowpark.exceptions import SnowparkSQLException
 from tests.integ.modin.sql_counter import SqlCounter, sql_count_checker
-from tests.integ.modin.utils import assert_index_equal
+from tests.integ.modin.utils import assert_index_equal, eval_snowpark_pandas_result
 
 
 def assert_reindex_result_equal(
-    snow_result: tuple[pd.Index, np.ndarray], native_result: tuple[pd.Index, np.ndarray]
+    snow_idx: pd.Index, native_idx: native_pd.Index, func: Callable
 ) -> None:
+    snow_result = func(snow_idx)
+    native_result = func(native_idx)
     assert_index_equal(snow_result[0], native_result[0])
     np.testing.assert_array_equal(snow_result[1], native_result[1])
-
-
-def assert_reindex_exceptions_match(
-    snow_idx: pd.Index,
-    native_idx: native_pd.Index,
-    func: Callable,
-    excp_type: Exception,
-    excp_match: str,
-) -> None:
-    with pytest.raises(excp_type, match=excp_match):
-        func(native_idx)
-
-    with pytest.raises(excp_type, match=excp_match):
-        func(snow_idx)
 
 
 def create_datetime_index(snow_series: pd.Series) -> pd.Index:
@@ -58,68 +46,72 @@ def create_datetime_index(snow_series: pd.Series) -> pd.Index:
 
 
 @sql_count_checker(query_count=0)
-def test_reindex_invalid_limit_parameter():
+def test_invalid_limit_parameter():
     native_idx = native_pd.Index(list("ABC"))
     snow_idx = pd.Index(native_idx)
-    assert_reindex_exceptions_match(
+    eval_snowpark_pandas_result(
         snow_idx,
         native_idx,
         lambda idx: idx.reindex(list("CAB"), limit=1),
-        ValueError,
-        "limit argument only valid if doing pad, backfill or nearest reindexing",
+        expect_exception=True,
+        expect_exception_type=ValueError,
+        expect_exception_match="limit argument only valid if doing pad, backfill or nearest reindexing",
+        assert_exception_equal=True,
     )
 
 
 @sql_count_checker(query_count=2, join_count=2)
-def test_reindex_index_basic_reorder():
+def test_basic_reorder():
     native_idx = native_pd.Index(list("ABC"))
     snow_idx = pd.Index(native_idx)
-    native_result = native_idx.reindex(list("CAB"))
-    snow_result = snow_idx.reindex(list("CAB"))
-    assert_reindex_result_equal(snow_result, native_result)
+    assert_reindex_result_equal(
+        snow_idx, native_idx, lambda idx: idx.reindex(list("CAB"))
+    )
 
 
 @sql_count_checker(query_count=2, join_count=2)
-def test_reindex_index_basic_add_elements():
+def test_basic_add_elements():
     native_idx = native_pd.Index(list("ABC"))
     snow_idx = pd.Index(native_idx)
-    native_result = native_idx.reindex(list("CABDEF"))
-    snow_result = snow_idx.reindex(list("CABDEF"))
-    assert_reindex_result_equal(snow_result, native_result)
+    assert_reindex_result_equal(
+        snow_idx, native_idx, lambda idx: idx.reindex(list("CABDEF"))
+    )
 
 
 @sql_count_checker(query_count=2, join_count=2)
-def test_reindex_index_basic_remove_elements():
+def test_basic_remove_elements():
     native_idx = native_pd.Index(list("ABC"))
     snow_idx = pd.Index(native_idx)
-    native_result = native_idx.reindex(list("CA"))
-    snow_result = snow_idx.reindex(list("CA"))
-    assert_reindex_result_equal(snow_result, native_result)
+    assert_reindex_result_equal(
+        snow_idx, native_idx, lambda idx: idx.reindex(list("CA"))
+    )
 
 
 @sql_count_checker(query_count=2, join_count=2)
-def test_reindex_index_basic_add_remove_elements():
+def test_basic_add_remove_elements():
     native_idx = native_pd.Index(list("ABC"))
     snow_idx = pd.Index(native_idx)
-    native_result = native_idx.reindex(list("CADEFG"))
-    snow_result = snow_idx.reindex(list("CADEFG"))
-    assert_reindex_result_equal(snow_result, native_result)
+    assert_reindex_result_equal(
+        snow_idx, native_idx, lambda idx: idx.reindex(list("CADEFG"))
+    )
 
 
 @sql_count_checker(query_count=2, join_count=2)
 @pytest.mark.parametrize("method", ["bfill", "backfill", "pad", "ffill"])
 @pytest.mark.parametrize("limit", [None, 1, 2, 100])
-def test_reindex_index_fill_method(method, limit):
+def test_fill_method(method, limit):
     native_idx = native_pd.Index(list("ACE"))
     snow_idx = pd.Index(native_idx)
-    native_result = native_idx.reindex(list("ABCDEFGH"), method=method, limit=limit)
-    snow_result = snow_idx.reindex(list("ABCDEFGH"), method=method, limit=limit)
-    assert_reindex_result_equal(snow_result, native_result)
+    assert_reindex_result_equal(
+        snow_idx,
+        native_idx,
+        lambda idx: idx.reindex(list("ABCDEFGH"), method=method, limit=limit),
+    )
 
 
 @pytest.mark.parametrize("method", ["bfill", "backfill", "pad", "ffill"])
 @pytest.mark.parametrize("limit", [None, 1, 2, 100])
-def test_reindex_index_fill_method_non_monotonic_new_index(method, limit):
+def test_fill_method_non_monotonic_new_index(method, limit):
     # The negative is because pandas does not support `limit` parameter
     # if the index and target are not both monotonic, while we do.
     native_idx = native_pd.Index(list("ABC"))
@@ -127,34 +119,36 @@ def test_reindex_index_fill_method_non_monotonic_new_index(method, limit):
     if limit is not None:
         with SqlCounter(query_count=1, join_count=1):
             method_str = {"bfill": "backfill", "ffill": "pad"}.get(method, method)
-            assert_reindex_exceptions_match(
+            eval_snowpark_pandas_result(
                 snow_idx,
                 native_idx,
                 lambda idx: idx.reindex(list("CEBFGA"), method=method, limit=limit),
-                ValueError,
-                f"limit argument for '{method_str}' method only well-defined if index and target are monotonic",
+                expect_exception=True,
+                expect_exception_type=ValueError,
+                expect_exception_match=f"limit argument for '{method_str}' method only well-defined if index and target are monotonic",
+                assert_exception_equal=True,
             )
     else:
         with SqlCounter(query_count=2, join_count=2):
-            native_result = native_idx.reindex(
-                list("CADEFG"), method=method, limit=limit
+            assert_reindex_result_equal(
+                snow_idx,
+                native_idx,
+                lambda idx: idx.reindex(list("CADEFG"), method=method, limit=limit),
             )
-            snow_result = snow_idx.reindex(list("CADEFG"), method=method, limit=limit)
-            assert_reindex_result_equal(snow_result, native_result)
 
 
 @sql_count_checker(query_count=2, join_count=2)
-def test_reindex_index_ordered_index_unordered_new_index():
+def test_ordered_index_unordered_new_index():
     native_idx = native_pd.Index([5, 6, 8])
     snow_idx = pd.Index(native_idx)
-    native_result = native_idx.reindex([6, 8, 7], method="ffill")
-    snow_result = snow_idx.reindex([6, 8, 7], method="ffill")
-    assert_reindex_result_equal(snow_result, native_result)
+    assert_reindex_result_equal(
+        snow_idx, native_idx, lambda idx: idx.reindex([6, 8, 7], method="ffill")
+    )
 
 
 @pytest.mark.parametrize("limit", [None, 1, 2, 100])
 @pytest.mark.parametrize("method", ["bfill", "backfill", "pad", "ffill"])
-def test_reindex_index_datetime_with_fill(limit, method):
+def test_datetime_with_fill(limit, method):
     query_count = 3 if limit else 2
     join_count = 2
     with SqlCounter(query_count=query_count, join_count=join_count):
@@ -162,45 +156,43 @@ def test_reindex_index_datetime_with_fill(limit, method):
         snow_date_index = create_datetime_index(
             pd.date_range("1/1/2010", periods=6, freq="D")
         )
-        native_result = native_date_index.reindex(
-            native_pd.date_range("12/29/2009", periods=10, freq="D"),
-            method=method,
-            limit=limit,
+        assert_reindex_result_equal(
+            snow_date_index,
+            native_date_index,
+            lambda idx: idx.reindex(
+                native_pd.date_range("12/29/2009", periods=10, freq="D"),
+                method=method,
+                limit=limit,
+            ),
         )
-        snow_result = snow_date_index.reindex(
-            pd.date_range("12/29/2009", periods=10, freq="D"),
-            method=method,
-            limit=limit,
-        )
-        assert_reindex_result_equal(snow_result, native_result)
 
 
 @sql_count_checker(query_count=2, join_count=2)
-def test_reindex_index_non_overlapping_index():
+def test_non_overlapping_index():
     native_idx = native_pd.Index(list("ABC"))
     snow_idx = pd.Index(native_idx)
-    native_result = native_idx.reindex(list("EFG"))
-    snow_result = snow_idx.reindex(list("EFG"))
-    assert_reindex_result_equal(snow_result, native_result)
+    assert_reindex_result_equal(
+        snow_idx, native_idx, lambda idx: idx.reindex(list("EFG"))
+    )
 
 
 @sql_count_checker(query_count=2, join_count=2)
-def test_reindex_index_non_overlapping_datetime_index():
+def test_non_overlapping_datetime_index():
     native_date_index = native_pd.date_range("1/1/2010", periods=6, freq="D")
     snow_date_index = create_datetime_index(
         pd.date_range("1/1/2010", periods=6, freq="D")
     )
-    native_result = native_date_index.reindex(
-        native_pd.date_range("12/29/2008", periods=10, freq="D")
+    assert_reindex_result_equal(
+        snow_date_index,
+        native_date_index,
+        lambda idx: idx.reindex(
+            native_pd.date_range("12/29/2008", periods=10, freq="D")
+        ),
     )
-    snow_result = snow_date_index.reindex(
-        pd.date_range("12/29/2008", periods=10, freq="D")
-    )
-    assert_reindex_result_equal(snow_result, native_result)
 
 
 @sql_count_checker(query_count=0)
-def test_reindex_index_non_overlapping_different_types_index_negative():
+def test_non_overlapping_different_types_index_negative():
     date_index = create_datetime_index(pd.date_range("1/1/2010", periods=6, freq="D"))
 
     with pytest.raises(SnowparkSQLException, match=".*Timestamp 'A' is not recognized"):
@@ -217,7 +209,7 @@ def test_reindex_index_non_overlapping_different_types_index_negative():
         (list("AABC"), list("AAAABC"), np.array([0, 2, 0, 2, 1, -1])),
     ],
 )
-def test_reindex_index_duplicate_values(new_index, result_index, indices):
+def test_duplicate_values(new_index, result_index, indices):
     native_idx = native_pd.Index(list("ABA"))
     snow_idx = pd.Index(native_idx)
 
@@ -226,11 +218,12 @@ def test_reindex_index_duplicate_values(new_index, result_index, indices):
     ):
         native_idx.reindex(new_index)
     snow_result = snow_idx.reindex(new_index)
-    assert_reindex_result_equal(snow_result, (native_pd.Index(result_index), indices))
+    assert_index_equal(snow_result[0], native_pd.Index(result_index))
+    np.testing.assert_array_equal(snow_result[1], indices)
 
 
 @sql_count_checker(query_count=0)
-def test_reindex_multiindex_negative():
+def test_multiindex_negative():
     native_idx = native_pd.MultiIndex.from_tuples(list(zip(range(4), range(4))))
     snow_idx = pd.Index(native_idx)
 
@@ -242,7 +235,7 @@ def test_reindex_multiindex_negative():
 
 
 @sql_count_checker(query_count=2, join_count=2)
-def test_reindex_non_monotonic_index_no_overlapping_negative():
+def test_non_monotonic_index_no_overlapping_negative():
     native_idx = native_pd.Index(["car", "bike", "train"])
     snow_idx = pd.Index(native_idx)
 
@@ -252,18 +245,21 @@ def test_reindex_non_monotonic_index_no_overlapping_negative():
         native_idx.reindex(["a"], method="ffill")
 
     snow_result = snow_idx.reindex(["a"], method="ffill")
-    assert_reindex_result_equal(snow_result, (native_pd.Index(["a"]), np.array([-1])))
+    assert_index_equal(snow_result[0], native_pd.Index(["a"]))
+    np.testing.assert_array_equal(snow_result[1], np.array([-1]))
 
 
 @sql_count_checker(query_count=1, join_count=1)
-def test_reindex_non_monotonic_index():
+def test_non_monotonic_index():
     native_idx = native_pd.Index(["car", "bike", "train"])
     snow_idx = pd.Index(native_idx)
 
-    assert_reindex_exceptions_match(
+    eval_snowpark_pandas_result(
         snow_idx,
         native_idx,
         lambda idx: idx.reindex(["car", "cat"], method="ffill"),
-        ValueError,
-        "index must be monotonic increasing or decreasing",
+        expect_exception=True,
+        expect_exception_type=ValueError,
+        expect_exception_match="index must be monotonic increasing or decreasing",
+        assert_exception_equal=True,
     )
