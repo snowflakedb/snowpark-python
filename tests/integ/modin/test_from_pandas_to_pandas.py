6910b145ee899cf070a6ec3e4d17e2d729807130
sfc-gh-mvashishtha <mahesh.vashishtha@snowflake.com>
2024-08-12 22:55:21 -0700

    Fix grammar
    Signed-off-by: sfc-gh-mvashishtha <mahesh.vashishtha@snowflake.com>

--- tests/integ/modin/test_from_pandas_to_pandas.py
+++ tests/integ/modin/test_from_pandas_to_pandas.py
@@ -157,8 +157,8 @@ def test_type_mismatch_index_type(name, indices_dict):
 
 @pytest.mark.parametrize("name", FROM_TO_PANDAS_VALUE_TYPE_MISMATCH_INDICES)
 def test_value_type_mismatch_index_type(name, indices_dict):
-    # We can create pandas dataframe of the datetime and timedelta values, so
-    # we execute some queries for those cases.
+    # We can create a pandas dataframe out of the datetime and timedelta
+    # values, so we execute some queries for those cases.
     expected_query_count = 6 if ("datetime" in name or "timedelta" in name) else 0
     with SqlCounter(query_count=expected_query_count):
         index = indices_dict[name]
