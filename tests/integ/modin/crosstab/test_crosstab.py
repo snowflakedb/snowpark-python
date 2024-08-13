f812c9c59a7481eb32003a958456804dc4ef0cbf
Rehan Durrani <rehan.durrani@snowflake.com>
2024-08-13 12:02:28 -0700

    Add values tests

--- tests/integ/modin/crosstab/test_crosstab.py
+++ tests/integ/modin/crosstab/test_crosstab.py
@@ -44,7 +44,7 @@ def test_basic_crosstab():
 
 
 @sql_count_checker(query_count=4, join_count=4, union_count=3)
-def test_basic_crosstab_margins():
+def test_margins():
     a = np.array(
         ["foo", "foo", "foo", "foo", "bar", "bar", "bar", "bar", "foo", "foo", "foo"],
         dtype=object,
@@ -90,7 +90,7 @@ def test_basic_crosstab_margins():
 
 @sql_count_checker(query_count=5, join_count=1)
 @pytest.mark.parametrize("normalize", [0, 1, True, "all", "index", "columns"])
-def test_basic_crosstab_normalize(normalize):
+def test_normalize(normalize):
     a = np.array(
         ["foo", "foo", "foo", "foo", "bar", "bar", "bar", "bar", "foo", "foo", "foo"],
         dtype=object,
@@ -120,5 +120,120 @@ def test_basic_crosstab_normalize(normalize):
     )
     snow_df = pd.crosstab(
         a, [b, c], rownames=["a"], colnames=["b", "c"], normalize=normalize
+    )
+    assert_snowpark_pandas_equal_to_pandas(snow_df, native_df)
+
+
+@sql_count_checker(query_count=6, join_count=12, union_count=8)
+@pytest.mark.parametrize("normalize", [0, 1, True, "all", "index", "columns"])
+def test_normalize_and_margins(normalize):
+    a = np.array(
+        ["foo", "foo", "foo", "foo", "bar", "bar", "bar", "bar", "foo", "foo", "foo"],
+        dtype=object,
+    )
+    b = np.array(
+        ["one", "one", "one", "two", "one", "one", "one", "two", "two", "two", "one"],
+        dtype=object,
+    )
+    c = np.array(
+        [
+            "dull",
+            "dull",
+            "shiny",
+            "dull",
+            "dull",
+            "shiny",
+            "shiny",
+            "dull",
+            "shiny",
+            "shiny",
+            "shiny",
+        ],
+        dtype=object,
+    )
+    native_df = native_pd.crosstab(
+        a,
+        [b, c],
+        rownames=["a"],
+        colnames=["b", "c"],
+        normalize=normalize,
+        margins=True,
+    )
+    snow_df = pd.crosstab(
+        a,
+        [b, c],
+        rownames=["a"],
+        colnames=["b", "c"],
+        normalize=normalize,
+        margins=True,
+    )
+    assert_snowpark_pandas_equal_to_pandas(snow_df, native_df)
+
+
+@sql_count_checker(query_count=4, join_count=7)
+@pytest.mark.parametrize("aggfunc", ["mean", "sum"])
+def test_values(aggfunc):
+    native_df = native_pd.DataFrame(
+        {
+            "species": ["dog", "cat", "dog", "dog", "cat", "cat", "dog", "cat"],
+            "favorite_food": [
+                "chicken",
+                "fish",
+                "fish",
+                "beef",
+                "chicken",
+                "beef",
+                "fish",
+                "beef",
+            ],
+            "age": [7, 2, 8, 5, 9, 3, 6, 1],
+        }
+    )
+    native_df_result = native_pd.crosstab(
+        native_df["species"].values,
+        native_df["favorite_food"].values,
+        values=native_df["age"].values,
+        aggfunc=aggfunc,
+    )
+    snow_df = pd.crosstab(
+        native_df["species"].values,
+        native_df["favorite_food"].values,
+        values=native_df["age"].values,
+        aggfunc=aggfunc,
+    )
+    assert_snowpark_pandas_equal_to_pandas(snow_df, native_df_result)
+
+
+@sql_count_checker(query_count=4, join_count=7)
+@pytest.mark.parametrize("aggfunc", ["mean", "sum"])
+def test_values_series_like(aggfunc):
+    native_df = native_pd.DataFrame(
+        {
+            "species": ["dog", "cat", "dog", "dog", "cat", "cat", "dog", "cat"],
+            "favorite_food": [
+                "chicken",
+                "fish",
+                "fish",
+                "beef",
+                "chicken",
+                "beef",
+                "fish",
+                "beef",
+            ],
+            "age": [7, 2, 8, 5, 9, 3, 6, 1],
+        }
+    )
+    snow_df = pd.DataFrame(native_df)
+    native_df = native_pd.crosstab(
+        native_df["species"],
+        native_df["favorite_food"],
+        values=native_df["age"],
+        aggfunc=aggfunc,
+    )
+    snow_df = pd.crosstab(
+        snow_df["species"],
+        snow_df["favorite_food"],
+        values=snow_df["age"],
+        aggfunc=aggfunc,
     )
     assert_snowpark_pandas_equal_to_pandas(snow_df, native_df)
