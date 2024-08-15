cb39d20a207da173cf00ed97b39136d20af398bd
Afroz Alam <afroz.alam@snowflake.com>
2024-08-14 17:31:58 -0700

    fix

--- ci/test_fips.sh
+++ ci/test_fips.sh
@@ -11,7 +11,7 @@ SNOWPARK_WHL="$(ls $SNOWPARK_DIR/dist/*.whl | sort -r | head -n 1)"
 python3.8 -m venv fips_env
 source fips_env/bin/activate
 export PATH=/usr/local/bin:$PATH
-export LD_LIBRARY_PATH=/usr/local/lib/:$LD_LIBRARY_PATH
+export LD_LIBRARY_PATH=/usr/local/lib/:/usr/local/lib64/:$LD_LIBRARY_PATH
 
 pip install -U setuptools pip
 pip install "${SNOWPARK_WHL}[pandas,secure-local-storage,development,opentelemetry]"
