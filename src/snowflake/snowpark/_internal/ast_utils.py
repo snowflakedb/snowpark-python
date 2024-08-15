6a67ba6afff92e7864c29765a8075b18b4279de5
Hemit Shah <hemit.shah@snowflake.com>
2024-08-15 14:44:32 -0700

    wip for tracing all calls to with_src_position

--- src/snowflake/snowpark/_internal/ast_utils.py
+++ src/snowflake/snowpark/_internal/ast_utils.py
@@ -35,7 +35,11 @@ from snowflake.snowpark._internal.type_utils import (
 FAIL_ON_MISSING_AST = True
 
 
-def build_expr_from_python_val(expr_builder: proto.Expr, obj: Any) -> None:
+def build_expr_from_python_val(
+        expr_builder: proto.Expr, 
+        obj: Any,
+        caller_frame_depth: int = 3,
+) -> None:
     """Infer the Const AST expression from obj, and populate the provided ast.Expr() instance
 
     Args:
@@ -49,50 +53,50 @@ def build_expr_from_python_val(expr_builder: proto.Expr, obj: Any) -> None:
     from snowflake.snowpark.row import Row
 
     if obj is None:
-        with_src_position(expr_builder.null_val)
+        with_src_position(expr_builder.null_val, caller_frame_depth=caller_frame_depth)
 
     # Keep objects most high up in the class hierarchy first, i.e. a Row is a tuple.
     elif isinstance(obj, Column):
         expr_builder.CopyFrom(obj._ast)
 
     elif isinstance(obj, Row):
-        sp_row_ast = with_src_position(expr_builder.sp_row)
+        sp_row_ast = with_src_position(expr_builder.sp_row, caller_frame_depth=caller_frame_depth)
         if hasattr(obj, "_named_values") and obj._named_values is not None:
             for field in obj._fields:
                 sp_row_ast.names.list.append(field)
                 build_expr_from_python_val(
-                    sp_row_ast.vs.add(), obj._named_values[field]
+                    sp_row_ast.vs.add(), obj._named_values[field], caller_frame_depth=caller_frame_depth+1
                 )
         else:
             for field in obj:
-                build_expr_from_python_val(sp_row_ast.vs.add(), field)
+                build_expr_from_python_val(sp_row_ast.vs.add(), field, caller_frame_depth=caller_frame_depth+1)
 
     elif isinstance(obj, bool):
-        bool_val_ast = with_src_position(expr_builder.bool_val)
+        bool_val_ast = with_src_position(expr_builder.bool_val, caller_frame_depth=caller_frame_depth)
         bool_val_ast.v = obj
 
     elif isinstance(obj, int):
-        int64_val_ast = with_src_position(expr_builder.int64_val)
+        int64_val_ast = with_src_position(expr_builder.int64_val, caller_frame_depth=caller_frame_depth)
         int64_val_ast.v = obj
 
     elif isinstance(obj, float):
-        float64_val_ast = with_src_position(expr_builder.float64_val)
+        float64_val_ast = with_src_position(expr_builder.float64_val, caller_frame_depth=caller_frame_depth)
         float64_val_ast.v = obj
 
     elif isinstance(obj, str):
-        string_val_ast = with_src_position(expr_builder.string_val)
+        string_val_ast = with_src_position(expr_builder.string_val, caller_frame_depth=caller_frame_depth)
         string_val_ast.v = obj
 
     elif isinstance(obj, bytes):
-        binary_val_ast = with_src_position(expr_builder.binary_val)
+        binary_val_ast = with_src_position(expr_builder.binary_val, caller_frame_depth=caller_frame_depth)
         binary_val_ast.v = obj
 
     elif isinstance(obj, bytearray):
-        binary_val_ast = with_src_position(expr_builder.binary_val)
+        binary_val_ast = with_src_position(expr_builder.binary_val, caller_frame_depth=caller_frame_depth)
         binary_val_ast.v = bytes(obj)
 
     elif isinstance(obj, decimal.Decimal):
-        big_decimal_val_ast = with_src_position(expr_builder.big_decimal_val)
+        big_decimal_val_ast = with_src_position(expr_builder.big_decimal_val, caller_frame_depth=caller_frame_depth)
         dec_tuple = obj.as_tuple()
         unscaled_val = reduce(lambda val, digit: val * 10 + digit, dec_tuple.digits)
         if dec_tuple.sign != 0:
@@ -104,7 +108,7 @@ def build_expr_from_python_val(expr_builder: proto.Expr, obj: Any) -> None:
         big_decimal_val_ast.scale = dec_tuple.exponent
 
     elif isinstance(obj, datetime.datetime):
-        python_timestamp_val_ast = with_src_position(expr_builder.python_timestamp_val)
+        python_timestamp_val_ast = with_src_position(expr_builder.python_timestamp_val, caller_frame_depth=caller_frame_depth)
         if obj.tzinfo is not None:
             python_timestamp_val_ast.tz.offset_seconds = int(
                 obj.tzinfo.utcoffset(obj).total_seconds()
@@ -124,13 +128,13 @@ def build_expr_from_python_val(expr_builder: proto.Expr, obj: Any) -> None:
         python_timestamp_val_ast.microsecond = obj.microsecond
 
     elif isinstance(obj, datetime.date):
-        python_date_val_ast = with_src_position(expr_builder.python_date_val)
+        python_date_val_ast = with_src_position(expr_builder.python_date_val, caller_frame_depth=caller_frame_depth)
         python_date_val_ast.year = obj.year
         python_date_val_ast.month = obj.month
         python_date_val_ast.day = obj.day
 
     elif isinstance(obj, datetime.time):
-        python_time_val_ast = with_src_position(expr_builder.python_time_val)
+        python_time_val_ast = with_src_position(expr_builder.python_time_val, caller_frame_depth=caller_frame_depth)
         datetime_val = datetime.datetime.combine(datetime.date.today(), obj)
         if obj.tzinfo is not None:
             python_time_val_ast.tz.offset_seconds = int(
@@ -148,21 +152,21 @@ def build_expr_from_python_val(expr_builder: proto.Expr, obj: Any) -> None:
         python_time_val_ast.microsecond = obj.microsecond
 
     elif isinstance(obj, dict):
-        seq_map_val_ast = with_src_position(expr_builder.seq_map_val)
+        seq_map_val_ast = with_src_position(expr_builder.seq_map_val, caller_frame_depth=caller_frame_depth)
         for key, value in obj.items():
             kv_tuple_ast = seq_map_val_ast.kvs.add()
-            build_expr_from_python_val(kv_tuple_ast.vs.add(), key)
-            build_expr_from_python_val(kv_tuple_ast.vs.add(), value)
+            build_expr_from_python_val(kv_tuple_ast.vs.add(), key, caller_frame_depth=caller_frame_depth+1)
+            build_expr_from_python_val(kv_tuple_ast.vs.add(), value, caller_frame_depth=caller_frame_depth+1)
 
     elif isinstance(obj, list):
-        list_val_ast = with_src_position(expr_builder.list_val)
+        list_val_ast = with_src_position(expr_builder.list_val, caller_frame_depth=caller_frame_depth)
         for v in obj:
-            build_expr_from_python_val(list_val_ast.vs.add(), v)
+            build_expr_from_python_val(list_val_ast.vs.add(), v, caller_frame_depth=caller_frame_depth+1)
 
     elif isinstance(obj, tuple):
-        tuple_val_ast = with_src_position(expr_builder.tuple_val)
+        tuple_val_ast = with_src_position(expr_builder.tuple_val, caller_frame_depth=caller_frame_depth)
         for v in obj:
-            build_expr_from_python_val(tuple_val_ast.vs.add(), v)
+            build_expr_from_python_val(tuple_val_ast.vs.add(), v, caller_frame_depth=caller_frame_depth+1)
 
     else:
         raise NotImplementedError("not supported type: %s" % type(obj))
@@ -219,9 +223,9 @@ def build_builtin_fn_apply(
         **kwargs: Keyword arguments to pass to function.
 
     """
-    expr = with_src_position(ast.apply_expr)
+    expr = with_src_position(ast.apply_expr, caller_frame_depth=3)
     _set_fn_name(builtin_name, expr.fn.builtin_fn)
-    with_src_position(expr.fn.builtin_fn)
+    with_src_position(expr.fn.builtin_fn, caller_frame_depth=3)
     build_fn_apply_args(ast, *args, **kwargs)
 
 
@@ -230,9 +234,9 @@ def build_udf_apply(
     udf_name: str,
     *args: Tuple[Union[proto.Expr, Any]],
 ) -> None:
-    expr = with_src_position(ast.apply_expr)
+    expr = with_src_position(ast.apply_expr, caller_frame_depth=3)
     _set_fn_name(udf_name, expr.fn.udf)
-    with_src_position(expr.fn.udf)
+    with_src_position(expr.fn.udf, caller_frame_depth=3)
     build_fn_apply_args(ast, *args)
 
 
@@ -250,9 +254,9 @@ def build_session_table_fn_apply(
         *args: Positional arguments to pass to function.
         **kwargs: Keyword arguments to pass to function.
     """
-    expr = with_src_position(ast.apply_expr)
+    expr = with_src_position(ast.apply_expr, caller_frame_depth=3)
     _set_fn_name(name, expr.fn.session_table_fn)
-    with_src_position(expr.fn.session_table_fn)
+    with_src_position(expr.fn.session_table_fn, caller_frame_depth=3)
     build_fn_apply_args(ast, *args, **kwargs)
 
 
@@ -272,7 +276,7 @@ def build_table_fn_apply(
 
     Requires that ast.apply_expr.fn.table_fn.call_type is set to a valid TableFnCallType.
     """
-    expr = with_src_position(ast.apply_expr)
+    expr = with_src_position(ast.apply_expr, caller_frame_depth=3)
     assert (
         ast.apply_expr.fn.table_fn.call_type.WhichOneof("variant") is not None
     ), f"Explicitly set the call type before calling this function {str(ast.apply_expr.fn.table_fn)}"
@@ -281,7 +285,7 @@ def build_table_fn_apply(
             name is not None
         ), f"Table function name must be provided {str(ast.apply_expr.fn.table_fn)}"
         _set_fn_name(name, expr.fn.table_fn)
-    with_src_position(expr.fn.table_fn)
+    with_src_position(expr.fn.table_fn, caller_frame_depth=3)
     build_fn_apply_args(ast, *args, **kwargs)
 
 
@@ -307,7 +311,7 @@ def build_fn_apply_args(
             expr.pos_args.append(arg._ast)
         else:
             pos_arg = proto.Expr()
-            build_expr_from_python_val(pos_arg, arg)
+            build_expr_from_python_val(pos_arg, arg, caller_frame_depth=5)
             expr.pos_args.append(pos_arg)
 
     for name, arg in kwargs.items():
@@ -319,7 +323,7 @@ def build_fn_apply_args(
             assert arg._ast, f"Column object {name}={arg} has no _ast member set."
             kwarg._2.CopyFrom(arg._ast)
         else:
-            build_expr_from_python_val(kwarg._2, arg)
+            build_expr_from_python_val(kwarg._2, arg, caller_frame_depth=5)
         expr.named_args.append(kwarg)
 
 
@@ -338,7 +342,8 @@ assignment_re = re.compile(r"^\s*([a-zA-Z_]\w*)\s*=.*$", re.DOTALL)
 def with_src_position(
     expr_ast: proto.Expr, 
     assign: Optional[proto.Assign] = None,
-    max_stack_lookback: int = 3,
+    caller_frame_depth: int = 2,
+    debug: bool = True
 ) -> proto.Expr:
     """
     Sets the src_position on the supplied Expr AST node and returns it.
@@ -347,8 +352,7 @@ def with_src_position(
     Args:
         expr_ast: The AST node to set the src_position on.
         assign: The Assign AST node to set the symbol value on.
-        max_stack_lookback: The maximum number of stack frames to look back from the caller's caller if 
-                            the caller's caller is still in the snowpark package.
+        caller_frame_depth: The number of frames to step back from the current frame to find the code of interest.
     """
     src = expr_ast.src
     frame = inspect.currentframe()
@@ -364,27 +368,34 @@ def with_src_position(
             src.file = "<unknown>"
             return expr_ast
         
-        # If frame is not None, we can step back to the caller's caller via f_back twice.
-        # Since all uses of this function should be from public APIs, this should immediately reach the
-        # code of interest most of the time. Could still be None depending on the execution context.
-        frame = frame.f_back.f_back
-        filename = frame.f_code.co_filename if frame is not None else "<unknown>"
+        # If frame is not None, use the provided stack depth to step back to the relevant frame.
+        # This frame should be the first frame outside of the snowpark package, and contain the code of interest.
+        # NOTE: The inspect module provides many other APIs to get information about the current frame and its callers.
+        # All of these (except for currentframe) unnecessarily incur the overhead of resolving the filename for each
+        # frame and making sure the file exists (even if the context parameter is set to 0 to avoid capturing lineno, 
+        # source line, etc.). Since we should know exactly how many frames to step back, we can and should avoid this 
+        # overhead. Using the inspect.currentframe() (a wrapper around sys._getframe that handles the None case) is the
+        # most efficient way to get the current frame. Stepping back from this frame via frame.f_back is also the most 
+        # efficient method to walk the stack.
+        stack_index = 0
+        while stack_index < caller_frame_depth and frame is not None:
+            frame, prev_frame = frame.f_back, frame
+            stack_index += 1
 
-        stack_depth = 0
-        # If the caller's caller is in the snowpark package, keep stepping back until we're out of it.
-        snowpark_path = Path(__file__).parents[1]
-        while stack_depth < max_stack_lookback and \
-        frame is not None and snowpark_path in Path(filename).parents:
-            frame = frame.f_back
-            filename = frame.f_code.co_filename
-            stack_depth += 1
-
-        # Again, once we've stepped out of the snowpark package, we should be in the code of interest.
+        # Once we've stepped out of the snowpark package, we should be in the code of interest.
         # However, the code of interest may execute in an environment that is not accessible via the filesystem.
         # e.g. Jupyter notebooks, REPLs, calls to exec, etc.
+        filename = frame.f_code.co_filename if frame is not None else "<unknown>"
         if frame is None or not Path(filename).is_file():
             src.file = "<unknown>"
             return expr_ast
+        
+        if debug:
+            snowpark_path = Path(__file__).parents[1]
+            last_snowpark_file = prev_frame.f_code.co_filename
+            assert snowpark_path in Path(last_snowpark_file).parents
+            first_non_snowpark_file = frame.f_code.co_filename
+            assert snowpark_path not in Path(first_non_snowpark_file).parents
         
         # The context argument specifies the number of lines of context to capture around the current line.
         # If IO performance is an issue, this can be set to 0 but this will disable symbol capture. Some
@@ -449,7 +460,7 @@ def build_expr_from_snowpark_column_or_col_name(
     if isinstance(value, snowflake.snowpark.Column):
         build_expr_from_snowpark_column(expr_builder, value)
     elif isinstance(value, str):
-        expr = with_src_position(expr_builder.string_val)
+        expr = with_src_position(expr_builder.string_val, caller_frame_depth=3)
         expr.v = value
     else:
         raise TypeError(
@@ -472,7 +483,7 @@ def build_expr_from_snowpark_column_or_sql_str(
     if isinstance(value, snowflake.snowpark.Column):
         build_expr_from_snowpark_column(expr_builder, value)
     elif isinstance(value, str):
-        expr = with_src_position(expr_builder.sp_column_sql_expr)
+        expr = with_src_position(expr_builder.sp_column_sql_expr, caller_frame_depth=3)
         expr.sql = value
     else:
         raise TypeError(
@@ -532,7 +543,7 @@ def build_expr_from_snowpark_column_or_table_fn(
 
 
 def fill_ast_for_column(
-    expr: proto.Expr, name1: str, name2: Optional[str], fn_name="col"
+    expr: proto.Expr, name1: str, name2: Optional[str], fn_name="col", caller_frame_depth=3
 ) -> None:
     """
     Fill in expr node to encode Snowpark Column created through col(...) / column(...).
@@ -546,14 +557,14 @@ def fill_ast_for_column(
 
     # Handle the special case * (as a SQL column expr).
     if name2 == "*":
-        ast = with_src_position(expr.sp_column_sql_expr)
+        ast = with_src_position(expr.sp_column_sql_expr, caller_frame_depth=caller_frame_depth)
         ast.sql = "*"
         if name1 is not None:
             ast.df_alias.value = name1
         return expr
 
     if name1 == "*" and name2 is None:
-        ast = with_src_position(expr.sp_column_sql_expr)
+        ast = with_src_position(expr.sp_column_sql_expr, caller_frame_depth=caller_frame_depth)
         ast.sql = "*"
         return expr
 
@@ -573,17 +584,17 @@ def fill_ast_for_column(
 
 
 def create_ast_for_column(
-    name1: str, name2: Optional[str], fn_name="col"
+    name1: str, name2: Optional[str], fn_name="col", caller_frame_depth=4
 ) -> proto.Expr:
     """
     Helper function to create Ast for Snowpark Column. Cf. fill_ast_for_column on parameter details.
     """
     ast = proto.Expr()
-    fill_ast_for_column(ast, name1, name2, fn_name)
+    fill_ast_for_column(ast, name1, name2, fn_name, caller_frame_depth=caller_frame_depth)
     return ast
 
 
-def snowpark_expression_to_ast(expr: Expression) -> proto.Expr:
+def snowpark_expression_to_ast(expr: Expression, caller_frame_depth = 0) -> proto.Expr:
     """
     Converts Snowpark expression expr to protobuf ast.
     Args:
@@ -600,10 +611,10 @@ def snowpark_expression_to_ast(expr: Expression) -> proto.Expr:
         # we don't need an AST.
         return None
     elif isinstance(expr, Attribute):
-        return create_ast_for_column(expr.name, None)
+        return create_ast_for_column(expr.name, None, caller_frame_depth=5)
     elif isinstance(expr, Literal):
         ast = proto.Expr()
-        build_expr_from_python_val(ast, expr.value)
+        build_expr_from_python_val(ast, expr.value, caller_frame_depth=4)
         return ast
     elif isinstance(expr, UnresolvedAttribute):
         # Unresolved means treatment as sql expression.
