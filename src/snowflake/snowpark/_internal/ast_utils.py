5ed2cc29fc4665dfd2bc0a0a65f8707056e60c95
Hemit Shah <hemit.shah@snowflake.com>
2024-08-19 10:06:27 -0700

    [SNOW-1491175] Remove set_src_position, overhaul with_src_position for better performance (#2074)
    [SNOW-1491175](https://snowflakecomputing.atlassian.net/browse/SNOW-1491175)
    
    Remove all uses of `set_src_position` and `get_first_non_snowpark_stack_frame`, update the necessary test cases, and use the `inspect` module with better practices including
    - Deleting the retrieved frame to prevent reference cycles and memory leaks
    - Avoid capturing code context for every file along the stack
    - Skip two frames out of the Snowpark library code to retrieve relevant user code immediately when possible
    - Add comments discussing functionality in detail for future improvements
    
    [SNOW-1491175]: https://snowflakecomputing.atlassian.net/browse/SNOW-1491175?atlOrigin=eyJpIjoiNWRkNTljNzYxNjVmNDY3MDlhMDU5Y2ZhYzA5YTRkZjUiLCJwIjoiZ2l0aHViLWNvbS1KU1cifQ
    
    Co-authored-by: Leonhard Spiegelberg <leonhard.spiegelberg@snowflake.com>

--- src/snowflake/snowpark/_internal/ast_utils.py
+++ src/snowflake/snowpark/_internal/ast_utils.py
@@ -36,6 +36,8 @@ from snowflake.snowpark._internal.utils import str_to_enum
 # AST or field is required to populate the AST field of a different Snowpark object instance.
 FAIL_ON_MISSING_AST = True
 
+# The path to the snowpark package.
+SNOWPARK_LIB_PATH = Path(__file__).parent.parent.resolve()
 
 def build_expr_from_python_val(expr_builder: proto.Expr, obj: Any) -> None:
     """Infer the Const AST expression from obj, and populate the provided ast.Expr() instance
@@ -51,120 +53,120 @@ def build_expr_from_python_val(expr_builder: proto.Expr, obj: Any) -> None:
     from snowflake.snowpark.row import Row
 
     if obj is None:
-        set_src_position(expr_builder.null_val.src)
+        with_src_position(expr_builder.null_val)
 
     # Keep objects most high up in the class hierarchy first, i.e. a Row is a tuple.
     elif isinstance(obj, Column):
         expr_builder.CopyFrom(obj._ast)
 
     elif isinstance(obj, Row):
-        set_src_position(expr_builder.sp_row.src)
+        ast = with_src_position(expr_builder.sp_row)
         if hasattr(obj, "_named_values") and obj._named_values is not None:
             for field in obj._fields:
-                expr_builder.sp_row.names.list.append(field)
+                ast.names.list.append(field)
                 build_expr_from_python_val(
-                    expr_builder.sp_row.vs.add(), obj._named_values[field]
+                    ast.vs.add(), obj._named_values[field]
                 )
         else:
             for field in obj:
-                build_expr_from_python_val(expr_builder.sp_row.vs.add(), field)
+                build_expr_from_python_val(ast.vs.add(), field)
 
     elif isinstance(obj, bool):
-        set_src_position(expr_builder.bool_val.src)
-        expr_builder.bool_val.v = obj
+        ast = with_src_position(expr_builder.bool_val)
+        ast.v = obj
 
     elif isinstance(obj, int):
-        set_src_position(expr_builder.int64_val.src)
-        expr_builder.int64_val.v = obj
+        ast = with_src_position(expr_builder.int64_val)
+        ast.v = obj
 
     elif isinstance(obj, float):
-        set_src_position(expr_builder.float64_val.src)
-        expr_builder.float64_val.v = obj
+        ast = with_src_position(expr_builder.float64_val)
+        ast.v = obj
 
     elif isinstance(obj, str):
-        set_src_position(expr_builder.string_val.src)
-        expr_builder.string_val.v = obj
+        ast = with_src_position(expr_builder.string_val)
+        ast.v = obj
 
     elif isinstance(obj, bytes):
-        set_src_position(expr_builder.binary_val.src)
-        expr_builder.binary_val.v = obj
+        ast = with_src_position(expr_builder.binary_val)
+        ast.v = obj
 
     elif isinstance(obj, bytearray):
-        set_src_position(expr_builder.binary_val.src)
-        expr_builder.binary_val.v = bytes(obj)
+        ast = with_src_position(expr_builder.binary_val)
+        ast.v = bytes(obj)
 
     elif isinstance(obj, decimal.Decimal):
-        set_src_position(expr_builder.big_decimal_val.src)
+        ast = with_src_position(expr_builder.big_decimal_val)
         dec_tuple = obj.as_tuple()
         unscaled_val = reduce(lambda val, digit: val * 10 + digit, dec_tuple.digits)
         if dec_tuple.sign != 0:
             unscaled_val *= -1
         req_bytes = (unscaled_val.bit_length() + 7) // 8
-        expr_builder.big_decimal_val.unscaled_value = unscaled_val.to_bytes(
+        ast.unscaled_value = unscaled_val.to_bytes(
             req_bytes, "big", signed=True
         )
-        expr_builder.big_decimal_val.scale = dec_tuple.exponent
+        ast.scale = dec_tuple.exponent
 
     elif isinstance(obj, datetime.datetime):
-        set_src_position(expr_builder.python_timestamp_val.src)
+        ast = with_src_position(expr_builder.python_timestamp_val)
         if obj.tzinfo is not None:
-            expr_builder.python_timestamp_val.tz.offset_seconds = int(
+            ast.tz.offset_seconds = int(
                 obj.tzinfo.utcoffset(obj).total_seconds()
             )
             tz = obj.tzinfo.tzname(obj)
             if tz is not None:
-                expr_builder.python_timestamp_val.tz.name.value = tz
+                ast.tz.name.value = tz
         else:
             obj = obj.astimezone(datetime.timezone.utc)
 
-        expr_builder.python_timestamp_val.year = obj.year
-        expr_builder.python_timestamp_val.month = obj.month
-        expr_builder.python_timestamp_val.day = obj.day
-        expr_builder.python_timestamp_val.hour = obj.hour
-        expr_builder.python_timestamp_val.minute = obj.minute
-        expr_builder.python_timestamp_val.second = obj.second
-        expr_builder.python_timestamp_val.microsecond = obj.microsecond
+        ast.year = obj.year
+        ast.month = obj.month
+        ast.day = obj.day
+        ast.hour = obj.hour
+        ast.minute = obj.minute
+        ast.second = obj.second
+        ast.microsecond = obj.microsecond
 
     elif isinstance(obj, datetime.date):
-        set_src_position(expr_builder.python_date_val.src)
-        expr_builder.python_date_val.year = obj.year
-        expr_builder.python_date_val.month = obj.month
-        expr_builder.python_date_val.day = obj.day
+        ast = with_src_position(expr_builder.python_date_val)
+        ast.year = obj.year
+        ast.month = obj.month
+        ast.day = obj.day
 
     elif isinstance(obj, datetime.time):
-        set_src_position(expr_builder.python_time_val.src)
+        ast = with_src_position(expr_builder.python_time_val)
         datetime_val = datetime.datetime.combine(datetime.date.today(), obj)
         if obj.tzinfo is not None:
-            expr_builder.python_time_val.tz.offset_seconds = int(
+            ast.tz.offset_seconds = int(
                 obj.tzinfo.utcoffset(datetime_val).total_seconds()
             )
             tz = obj.tzinfo.tzname(datetime_val)
             if tz is not None:
-                expr_builder.python_time_val.tz.name.value = tz
+                ast.tz.name.value = tz
         else:
             obj = datetime_val.astimezone(datetime.timezone.utc)
 
-        expr_builder.python_time_val.hour = obj.hour
-        expr_builder.python_time_val.minute = obj.minute
-        expr_builder.python_time_val.second = obj.second
-        expr_builder.python_time_val.microsecond = obj.microsecond
+        ast.hour = obj.hour
+        ast.minute = obj.minute
+        ast.second = obj.second
+        ast.microsecond = obj.microsecond
 
     elif isinstance(obj, dict):
-        set_src_position(expr_builder.seq_map_val.src)
+        ast = with_src_position(expr_builder.seq_map_val)
         for key, value in obj.items():
-            kv_tuple_ast = expr_builder.seq_map_val.kvs.add()
-            build_expr_from_python_val(kv_tuple_ast.vs.add(), key)
-            build_expr_from_python_val(kv_tuple_ast.vs.add(), value)
+            kv_ast = ast.kvs.add()
+            build_expr_from_python_val(kv_ast.vs.add(), key)
+            build_expr_from_python_val(kv_ast.vs.add(), value)
 
     elif isinstance(obj, list):
-        set_src_position(expr_builder.list_val.src)
+        ast = with_src_position(expr_builder.list_val)
         for v in obj:
-            build_expr_from_python_val(expr_builder.list_val.vs.add(), v)
+            build_expr_from_python_val(ast.vs.add(), v)
 
     elif isinstance(obj, tuple):
-        set_src_position(expr_builder.tuple_val.src)
+        ast = with_src_position(expr_builder.tuple_val)
         for v in obj:
-            build_expr_from_python_val(expr_builder.tuple_val.vs.add(), v)
+            build_expr_from_python_val(ast.vs.add(), v)
 
     else:
         raise NotImplementedError("not supported type: %s" % type(obj))
@@ -234,7 +236,7 @@ def build_builtin_fn_apply(
     """
     expr = with_src_position(ast.apply_expr)
     _set_fn_name(builtin_name, expr.fn.builtin_fn)
-    set_src_position(expr.fn.builtin_fn.src)
+    with_src_position(expr.fn.builtin_fn)
     build_fn_apply_args(ast, *args, **kwargs)
 
 
@@ -245,7 +247,7 @@ def build_udf_apply(
 ) -> None:
     expr = with_src_position(ast.apply_expr)
     _set_fn_name(udf_name, expr.fn.udf)
-    set_src_position(expr.fn.udf.src)
+    with_src_position(expr.fn.udf)
     build_fn_apply_args(ast, *args)
 
 
@@ -265,7 +267,7 @@ def build_session_table_fn_apply(
     """
     expr = with_src_position(ast.apply_expr)
     _set_fn_name(name, expr.fn.session_table_fn)
-    set_src_position(expr.fn.session_table_fn.src)
+    with_src_position(expr.fn.session_table_fn)
     build_fn_apply_args(ast, *args, **kwargs)
 
 
@@ -294,7 +296,7 @@ def build_table_fn_apply(
             name is not None
         ), f"Table function name must be provided {str(ast.apply_expr.fn.table_fn)}"
         _set_fn_name(name, expr.fn.table_fn)
-    set_src_position(expr.fn.table_fn.src)
+    with_src_position(expr.fn.table_fn)
     build_fn_apply_args(ast, *args, **kwargs)
 
 
@@ -346,84 +348,98 @@ def set_builtin_fn_alias(ast: proto.Expr, alias: str) -> None:
     _set_fn_name(alias, ast.apply_expr.fn.builtin_fn)
 
 
-def get_first_non_snowpark_stack_frame() -> inspect.FrameInfo:
-    """Searches up through the call stack using inspect library to find the first stack frame
-    of a caller within a file which does not lie within the Snowpark library itself.
-
-    Returns:
-        inspect.FrameInfo: The FrameInfo object of the lowest caller outside of the Snowpark repo.
-    """
-    # TODO: Once `with_src_position()` is used exclusively, this can be abandoned.
-    idx = 0
-    call_stack = inspect.stack()
-    curr_frame = call_stack[idx]
-    snowpark_path = Path(__file__).parents[1]
-    while snowpark_path in Path(curr_frame.filename).parents:
-        idx += 1
-        curr_frame = call_stack[idx]
-    return curr_frame
-
-
-# TODO: remove this function and convert all callers to with_src_position.
-def set_src_position(src: proto.SrcPosition) -> None:
-    """Uses the method to retrieve the first non snowpark stack frame, and sets the SrcPosition IR entity
-    with the filename, and lineno which can be retrieved. In Python 3.11 and up the end line and column
-    offsets can also be retrieved from the FrameInfo.positions field.
-
-    Args:
-        src (proto.SrcPosition): SrcPosition builder.
-    """
-    frame = get_first_non_snowpark_stack_frame()
-
-    src.file = frame.filename
-    src.start_line = frame.lineno
-
-    if sys.version_info >= (3, 11):
-        pos = frame.positions
-        if pos.lineno is not None:
-            src.start_line = pos.lineno
-        if pos.end_lineno is not None:
-            src.end_line = pos.end_lineno
-        if pos.col_offset is not None:
-            src.start_column = pos.col_offset
-        if pos.end_col_offset is not None:
-            src.end_column = pos.end_col_offset
-
-
 assignment_re = re.compile(r"^\s*([a-zA-Z_]\w*)\s*=.*$", re.DOTALL)
 
-
 def with_src_position(
-    expr_ast: proto.Expr, assign: Optional[proto.Assign] = None
+    expr_ast: proto.Expr, 
+    assign: Optional[proto.Assign] = None,
+    caller_frame_depth: Optional[int] = None,
+    debug: bool = False,
 ) -> proto.Expr:
     """
     Sets the src_position on the supplied Expr AST node and returns it.
     N.B. This function assumes it's always invoked from a public API, meaning that the caller's caller
     is always the code of interest.
+    Args:
+        expr_ast: The AST node to set the src_position on.
+        assign: The Assign AST node to set the symbol value on.
+        caller_frame_depth: The number of frames to step back from the current frame to find the code of interest.
+                            If this is not provided, the filename for each frame is probed to find the code of interest.
     """
-    frame = (
-        get_first_non_snowpark_stack_frame()
-    )  # TODO: implement the assumption above to minimize overhead.
-    source_line = frame.code_context[0].strip() if frame.code_context else ""
-
     src = expr_ast.src
-    src.file = frame.filename
-    src.start_line = frame.lineno
-    if sys.version_info >= (3, 11):
-        pos = frame.positions
-        if pos.lineno is not None:
-            src.start_line = pos.lineno
-        if pos.end_lineno is not None:
-            src.end_line = pos.end_lineno
-        if pos.col_offset is not None:
-            src.start_column = pos.col_offset
-        if pos.end_col_offset is not None:
-            src.end_column = pos.end_col_offset
+    frame = inspect.currentframe()
 
-    if assign is not None:
-        match = assignment_re.fullmatch(source_line)
-        if match is not None:
-            assign.symbol.value = match.group(1)
+    # Best practices for the inspect library are to remove references to frame objects once done with them
+    # to avoid reference cycles and memory leaks. The above frame assignment is removed in the finally block.
+    try:
+        # Need this None guard as depending on the implementation of sys._getframe, frame may be None.
+        # Note the assignment to src.file is needed as several upstream uses of this method rely on 
+        # setting src fields for explicit presence of the encapsulating message in the AST.
+        # e.g., Null values have no fields, so the assignment to src fields ensures their presence.
+        if frame is None:
+            src.file = ""
+            return expr_ast
+        
+        # NOTE: The inspect module provides many other APIs to get information about the current frame and its callers.
+        # All of these (except for currentframe) unnecessarily incur the overhead of resolving the filename for each
+        # frame and making sure the file exists (even if the context parameter is set to 0 to avoid capturing lineno, 
+        # source line, etc.). Since we should know exactly how many frames to step back, we can and should avoid this 
+        # overhead. Using the inspect.currentframe() (a wrapper around sys._getframe that handles the None case) is the
+        # most efficient way to get the current frame. Stepping back from this frame via frame.f_back is also the most 
+        # efficient method to walk the stack as each frame object contains the minimal amount of needed context.
+        if caller_frame_depth is None:
+            # If the frame is not None, one guarantee we have is that two frames back is the caller's caller, and this
+            # frame contains the code of interest from the user if they are using a simple public API with no further
+            # nesting or indirection. This is the most common case.
+            frame, prev_frame = frame.f_back.f_back, frame.f_back
+            while frame is not None and SNOWPARK_LIB_PATH in Path(frame.f_code.co_filename).parents:
+                frame, prev_frame = frame.f_back, frame
+        else:
+            # If the frame is not None, use the provided stack depth to step back to the relevant frame.
+            # This frame should be the first frame outside of the snowpark package, and contain the code of interest.
+            curr_frame_depth = 0
+            while frame is not None and curr_frame_depth < caller_frame_depth:
+                frame, prev_frame = frame.f_back, frame
+                curr_frame_depth += 1
+
+        if debug:
+            last_snowpark_file = prev_frame.f_code.co_filename
+            assert SNOWPARK_LIB_PATH in Path(last_snowpark_file).parents
+            first_non_snowpark_file = frame.f_code.co_filename
+            assert SNOWPARK_LIB_PATH not in Path(first_non_snowpark_file).parents
+
+        # Once we've stepped out of the snowpark package, we should be in the code of interest.
+        # However, the code of interest may execute in an environment that is not accessible via the filesystem.
+        # e.g. Jupyter notebooks, REPLs, calls to exec, etc.
+        filename = frame.f_code.co_filename if frame is not None else ""
+        if frame is None or not Path(filename).is_file():
+            src.file = ""
+            return expr_ast
+                
+        # The context argument specifies the number of lines of context to capture around the current line.
+        # If IO performance is an issue, this can be set to 0 but this will disable symbol capture. Some
+        # potential alternatives to consider here are the linecache and traceback modules.
+        frame_info = inspect.getframeinfo(frame, context=1)
+        src.file = frame_info.filename
+        src.start_line = frame_info.lineno
+        if sys.version_info >= (3, 11):
+            pos = frame_info.positions
+            if pos.lineno is not None:
+                src.start_line = pos.lineno
+            if pos.end_lineno is not None:
+                src.end_line = pos.end_lineno
+            if pos.col_offset is not None:
+                src.start_column = pos.col_offset
+            if pos.end_col_offset is not None:
+                src.end_column = pos.end_col_offset
+
+        if assign is not None:
+            if code := frame_info.code_context:
+                source_line = code[frame_info.index]
+                if match := assignment_re.fullmatch(source_line):
+                    assign.symbol.value = match.group(1)
+    finally:
+        del frame
 
     return expr_ast
 
