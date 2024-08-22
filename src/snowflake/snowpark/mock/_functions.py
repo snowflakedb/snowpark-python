70310ea8fa7d2ca416fa092a7c86ea6a52a7f129
Adam Ling <adam.ling@snowflake.com>
2024-08-22 14:42:25 -0700

    SNOW-1569916: fix local testing default timestamp timezone issue (#2114)

--- src/snowflake/snowpark/mock/_functions.py
+++ src/snowflake/snowpark/mock/_functions.py
@@ -22,6 +22,8 @@ import snowflake.snowpark
 from snowflake.snowpark._internal.analyzer.expression import FunctionExpression
 from snowflake.snowpark.mock._options import numpy, pandas
 from snowflake.snowpark.mock._snowflake_data_type import (
+    _TIMESTAMP_TYPE_MAPPING,
+    _TIMESTAMP_TYPE_TIMEZONE_MAPPING,
     ColumnEmulator,
     ColumnType,
     TableEmulator,
@@ -943,7 +945,11 @@ def mock_to_timestamp(
     try_cast: bool = False,
 ):
     result = mock_to_timestamp_ntz(column, fmt, try_cast)
-    result.sf_type = ColumnType(TimestampType(), column.sf_type.nullable)
+
+    result.sf_type = ColumnType(
+        TimestampType(_TIMESTAMP_TYPE_TIMEZONE_MAPPING[_TIMESTAMP_TYPE_MAPPING]),
+        column.sf_type.nullable,
+    )
     return result
 
 
