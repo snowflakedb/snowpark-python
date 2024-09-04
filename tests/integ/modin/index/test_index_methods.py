d225c24dec86202538b116e8d67ae50aa9220426
azhan <andong.zhan@snowflake.com>
2024-09-04 16:29:16 -0700

    fix test

--- tests/integ/modin/index/test_index_methods.py
+++ tests/integ/modin/index/test_index_methods.py
@@ -42,10 +42,6 @@ def test_index_creation_from_lazy_index():
     assert_index_equal(i1, i2)
 
 
-def test():
-    pd.Index([1]) | 2
-
-
 @pytest.mark.parametrize("native_df", TEST_DFS)
 @sql_count_checker(query_count=2)
 def test_df_index_copy(native_df):
