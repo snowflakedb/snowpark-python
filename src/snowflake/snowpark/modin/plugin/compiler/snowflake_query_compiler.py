a0fd53cd59211c6e8163f7f0a04083b254ab6973
Naren Krishna <naren.krishna@snowflake.com>
2024-09-03 19:24:56 -0700

    SNOW-1649392: Fix bug in `Series.dt.isocalendar` using a named Series (#2212)
    SNOW-1649392
    
    Bug fix to rename the columns in `Series.dt.isocalendar` using the
    existing Series name instead of assuming `MODIN_UNNAMED_SERIES_LABEL`
    
    ---------
    
    Signed-off-by: Naren Krishna <naren.krishna@snowflake.com>

--- src/snowflake/snowpark/modin/plugin/compiler/snowflake_query_compiler.py
+++ src/snowflake/snowpark/modin/plugin/compiler/snowflake_query_compiler.py
@@ -10790,14 +10790,15 @@ class SnowflakeQueryCompiler(BaseQueryCompiler):
         return self.dt_property("dayofweek")
 
     def dt_isocalendar(self) -> "SnowflakeQueryCompiler":
+        col_name = self.columns[0]
         year_col = self.dt_property("yearofweekiso").rename(
-            columns_renamer={MODIN_UNNAMED_SERIES_LABEL: "year"}
+            columns_renamer={col_name: "year"}
         )
         week_col = self.dt_property("weekiso").rename(
-            columns_renamer={MODIN_UNNAMED_SERIES_LABEL: "week"}
+            columns_renamer={col_name: "week"}
         )
         day_col = self.dt_property("dayofweekiso").rename(
-            columns_renamer={MODIN_UNNAMED_SERIES_LABEL: "day"}
+            columns_renamer={col_name: "day"}
         )
         return year_col.concat(axis=1, other=[week_col, day_col])
 
