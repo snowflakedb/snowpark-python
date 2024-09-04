38b7705eb680fcfe0d82523a6f55e1cf79a0473e
Afroz Alam <afroz.alam@snowflake.com>
2024-09-04 10:52:25 -0700

    Add telemetry for before and after complexities

--- src/snowflake/snowpark/_internal/telemetry.py
+++ src/snowflake/snowpark/_internal/telemetry.py
@@ -42,6 +42,7 @@ class TelemetryField(Enum):
     )
     TYPE_AUTO_CLEAN_UP_TEMP_TABLE_ENABLED = "snowpark_auto_clean_up_temp_table_enabled"
     TYPE_LARGE_QUERY_BREAKDOWN_ENABLED = "snowpark_large_query_breakdown_enabled"
+    TYPE_COMPILATION_STAGE_STATISTICS = "snowpark_compilation_stage_statistics"
     TYPE_ERROR = "snowpark_error"
     # Message keys for telemetry
     KEY_START_TIME = "start_time"
@@ -76,6 +77,8 @@ class TelemetryField(Enum):
     QUERY_PLAN_HEIGHT = "query_plan_height"
     QUERY_PLAN_NUM_DUPLICATE_NODES = "query_plan_num_duplicate_nodes"
     QUERY_PLAN_COMPLEXITY = "query_plan_complexity"
+    BEFORE_COMPLEXITY_SCORE = "before_complexity_score"
+    AFTER_COMPLEXITY_SCORES = "after_complexity_scores"
 
 
 # These DataFrame APIs call other DataFrame APIs
@@ -421,6 +424,26 @@ class TelemetryClient:
             TelemetryField.KEY_DATA.value: {
                 TelemetryField.SESSION_ID.value: session_id,
                 TelemetryField.LARGE_QUERY_BREAKDOWN_ENABLED.value: True,
+            },
+        }
+        self.send(message)
+
+    def send_complexity_breakdown_post_compilation_stage(
+        self,
+        session_id: int,
+        plan_uuid: str,
+        before_complexity_score: int,
+        after_complexity_scores: List[int],
+    ) -> None:
+        message = {
+            **self._create_basic_telemetry_data(
+                TelemetryField.TYPE_COMPILATION_STAGE_STATISTICS.value
+            ),
+            TelemetryField.KEY_DATA.value: {
+                TelemetryField.SESSION_ID.value: session_id,
+                TelemetryField.PLAN_UUID.value: plan_uuid,
+                TelemetryField.BEFORE_COMPLEXITY_SCORE.value: before_complexity_score,
+                TelemetryField.AFTER_COMPLEXITY_SCORES.value: after_complexity_scores,
             },
         }
         self.send(message)
