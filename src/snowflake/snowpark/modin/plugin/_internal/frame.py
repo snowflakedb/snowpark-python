172d1bd14dd6b8af663c5c238ebf8d61b2742787
Naresh Kumar <113932371+sfc-gh-nkumar@users.noreply.github.com>
2024-09-06 14:37:45 -0700

    SNOW-1637948: Add support for TimedeltaIndex methods floor, ceil and round (#2243)
    Fixes SNOW-1637948
    
    Add support for TimedeltaIndex methods floor, ceil and round

--- src/snowflake/snowpark/modin/plugin/_internal/frame.py
+++ src/snowflake/snowpark/modin/plugin/_internal/frame.py
@@ -1246,6 +1246,7 @@ class InternalFrame:
         self,
         snowpark_func: Callable[[Any], SnowparkColumn],
         include_index: bool = False,
+        return_type: Optional[SnowparkPandasType] = None,
     ) -> "InternalFrame":
         """
         Apply snowpark function callable to all data columns of an InternalFrame. If
@@ -1254,6 +1255,7 @@ class InternalFrame:
 
         Arguments:
             snowpark_func: Snowpark function to apply to columns of underlying snowpark df.
+            return_type: The optional SnowparkPandasType for the new column.
             include_index: Whether to apply the function to index columns as well.
 
         Returns:
@@ -1264,7 +1266,8 @@ class InternalFrame:
             snowflake_ids.extend(self.index_column_snowflake_quoted_identifiers)
 
         return self.update_snowflake_quoted_identifiers_with_expressions(
-            {col_id: snowpark_func(col(col_id)) for col_id in snowflake_ids}
+            {col_id: snowpark_func(col(col_id)) for col_id in snowflake_ids},
+            [return_type] * len(snowflake_ids) if return_type else None,
         ).frame
 
     def select_active_columns(self) -> "InternalFrame":
