ce1ffa65b8e7795266aeec8f343f22e0b40e5c84
Varnika Budati <varnika.budati@snowflake.com>
2024-09-09 11:24:59 -0700

    fix test

--- tests/integ/modin/groupby/test_groupby_basic_agg.py
+++ tests/integ/modin/groupby/test_groupby_basic_agg.py
@@ -951,7 +951,7 @@ def test_groupby_with_level(df_multi, level):
     )
 
 
-@sql_count_checker(query_count=1)
+@sql_count_checker(query_count=1, join_count=2)
 def test_groupby_with_hier_columns():
     tuples = list(
         zip(
