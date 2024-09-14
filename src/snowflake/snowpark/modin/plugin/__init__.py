e94e2e0946ca458535a10e4fb6e7b09354f0d2e5
Jonathan Shi <149419494+sfc-gh-joshi@users.noreply.github.com>
2024-09-13 17:54:49 -0700

    SNOW-1063346: Remove modin/pandas/dataframe.py (#2223)
    <!---
    Please answer these questions before creating your pull request. Thanks!
    --->
    
    1. Which Jira issue is this PR addressing? Make sure that there is an
    accompanying issue to your PR.
    
       <!---
       In this section, please add a Snowflake Jira issue number.
       
    Note that if a corresponding GitHub issue exists, you should still
    include
       the Snowflake Jira issue number. For example, for GitHub issue
    https://github.com/snowflakedb/snowpark-python/issues/1400, you should
       add "SNOW-1335071" here.
        --->
    
       Fixes SNOW-1063346
    
    2. Fill out the following pre-review checklist:
    
    - [ ] I am adding a new automated test(s) to verify correctness of my
    new code
    - [ ] If this test skips Local Testing mode, I'm requesting review from
    @snowflakedb/local-testing
       - [ ] I am adding new logging messages
       - [ ] I am adding a new telemetry message
       - [ ] I am adding new credentials
       - [ ] I am adding a new dependency
    - [ ] If this is a new feature/behavior, I'm adding the Local Testing
    parity changes.
    
    3. Please describe how your code solves the related issue.
    
    This PR removes dataframe.py (the Snowpark pandas one, not the Snowpark
    Python one), following #2167 and #2205. Once more, preserved overrides
    are given a reason in code comments. The following implemented methods
    have been added to `dataframe_overrides.py`:
    - __init__
    - __dataframe__
    - __and__, __rand__, __or__, __ror__
    - apply, applymap
    - columns
    - corr
    - dropna
    - fillna
    - groupby
    - info
    - insert
    - isin
    - join
    - mask
    - melt
    - merge
    - replace
    - rename
    - pivot_table
    - pow
    - rpow
    - select_dtypes
    - set_axis
    - set_index
    - shape
    - squeeze
    - sum
    - stack
    - transpose
    - unstack
    - value_counts
    - where
    - iterrows
    - itertuples
    - __repr__
    - _repr_html_
    - _to_datetime
    - _to_pandas
    - __setitem__

--- src/snowflake/snowpark/modin/plugin/__init__.py
+++ src/snowflake/snowpark/modin/plugin/__init__.py
@@ -69,6 +69,7 @@ _inherit_docstrings = snowflake.snowpark.modin.utils._inherit_docstrings
 
 inherit_modules = [
     (docstrings.base.BasePandasDataset, modin.pandas.base.BasePandasDataset),
+    (docstrings.dataframe.DataFrame, modin.pandas.dataframe.DataFrame),
     (docstrings.series.Series, modin.pandas.series.Series),
     (docstrings.series_utils.StringMethods, modin.pandas.series_utils.StringMethods),
     (
@@ -90,17 +91,3 @@ for (doc_module, target_object) in inherit_modules:
 snowflake.snowpark._internal.utils.should_warn_dynamic_pivot_is_in_private_preview = (
     False
 )
-
-
-# TODO: SNOW-1504302: Modin upgrade - use Snowpark pandas DataFrame for isocalendar
-# OSS Modin's DatetimeProperties frontend class wraps the returned query compiler with `modin.pandas.DataFrame`.
-# Since we currently replace `pd.DataFrame` with our own Snowpark pandas DataFrame object, this causes errors
-# since OSS Modin explicitly imports its own DataFrame class here. This override can be removed once the frontend
-# DataFrame class is removed from our codebase.
-def isocalendar(self):  # type: ignore
-    from snowflake.snowpark.modin.pandas import DataFrame
-
-    return DataFrame(query_compiler=self._query_compiler.dt_isocalendar())
-
-
-modin.pandas.series_utils.DatetimeProperties.isocalendar = isocalendar
