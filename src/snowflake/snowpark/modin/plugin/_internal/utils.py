af3db84c4bc0ce4d5064f155b8feebd2f6a0224c
Yun Zou <yun.zou@snowflake.com>
2024-09-16 15:55:04 -0700

    fix error

--- src/snowflake/snowpark/modin/plugin/_internal/utils.py
+++ src/snowflake/snowpark/modin/plugin/_internal/utils.py
@@ -261,7 +261,7 @@ def _create_read_only_table(
     readonly_table_name = (
         f"{random_name_for_temp_object(TempObjectType.TABLE)}{READ_ONLY_TABLE_SUFFIX}"
     )
-
+    use_scoped_temp_table = session._use_scoped_temp_objects
     # If we need to materialize into a temp table our create table expression
     # needs to be SELECT * FROM (object).
     if materialize_into_temp_table:
@@ -287,7 +287,7 @@ def _create_read_only_table(
         statement_params.update(new_params)
         # TODO: use snowpark save_as_table when user facing scoped object is supported
         session.sql(
-            f"CREATE OR REPLACE {get_temp_type_for_object(use_scoped_temp_objects=True, is_generated=True)} TABLE {temp_table_name} AS {ctas_query}"
+            f"CREATE OR REPLACE {get_temp_type_for_object(use_scoped_temp_objects=use_scoped_temp_table, is_generated=True)} TABLE {temp_table_name} AS {ctas_query}"
         ).collect(statement_params=statement_params)
         table_name = temp_table_name
 
@@ -302,7 +302,7 @@ def _create_read_only_table(
     )
     # TODO: pushing read only table creation down to snowpark for general usage
     session.sql(
-        f"CREATE OR REPLACE {get_temp_type_for_object(use_scoped_temp_objects=True, is_generated=True)} READ ONLY TABLE {readonly_table_name} CLONE {table_name}"
+        f"CREATE OR REPLACE {get_temp_type_for_object(use_scoped_temp_objects=use_scoped_temp_table, is_generated=True)} READ ONLY TABLE {readonly_table_name} CLONE {table_name}"
     ).collect(statement_params=statement_params)
 
     return readonly_table_name
