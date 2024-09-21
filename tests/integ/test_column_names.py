f4009503cb98683ef76dd7a7078b0ac6f7dfd5ab
Afroz Alam <afroz.alam@snowflake.com>
2024-09-20 17:15:41 -0700

    fix

--- tests/integ/test_column_names.py
+++ tests/integ/test_column_names.py
@@ -42,7 +42,7 @@ from snowflake.snowpark.types import (
     TimestampType,
     VariantType,
 )
-from tests.utils import Utils
+from tests.utils import IS_IN_STORED_PROC, Utils
 
 paramList = [True, False]
 
@@ -485,6 +485,7 @@ def test_function_expression(session, local_testing_mode):
     )
 
 
+@pytest.mark.skipif(IS_IN_STORED_PROC, reason="Temp function not supported in stored proc environment")
 @pytest.mark.udf
 @pytest.mark.parametrize("use_qualified_name", [True, False])
 def test_udf(session, use_qualified_name, local_testing_mode):
