272e4e1ee5da84f8ac0abfefda95aab3b0bf4d7e
Jonathan Shi <149419494+sfc-gh-joshi@users.noreply.github.com>
2024-09-23 14:17:43 -0700

    SNOW-1673230: Remove modin/pandas/utils.py (#2332)
    <!---
    Please answer these questions before creating your pull request. Thanks!
    --->
    
    1. Which Jira issue is this PR addressing? Make sure that there is an
    accompanying issue to your PR.
    
       <!---
       In this section, please add a Snowflake Jira issue number.
       
    Note that if a corresponding GitHub issue exists, you should still
    include
       the Snowflake Jira issue number. For example, for GitHub issue
    https://github.com/snowflakedb/snowpark-python/issues/1400, you should
       add "SNOW-1335071" here.
        --->
    
       Fixes SNOW-1673230
    
    2. Fill out the following pre-review checklist:
    
    - [ ] I am adding a new automated test(s) to verify correctness of my
    new code
    - [ ] If this test skips Local Testing mode, I'm requesting review from
    @snowflakedb/local-testing
       - [ ] I am adding new logging messages
       - [ ] I am adding a new telemetry message
       - [ ] I am adding new credentials
       - [ ] I am adding a new dependency
    - [ ] If this is a new feature/behavior, I'm adding the Local Testing
    parity changes.
    
    3. Please describe how your code solves the related issue.
    
    This PR removes `src/snowflake/snowpark/modin/pandas/utils.py`. Some of
    these functions were identical to their counterparts in
    `modin/pandas/utils.py` and `modin/pandas/io.py`, and have been replaced
    accordingly. The remaining functions, which are specific to Snowpark
    pandas, have been moved to
    `src/snowflake/snowpark/modin/plugin/extensions/utils.py`.

--- src/snowflake/snowpark/modin/plugin/extensions/indexing_overrides.py
+++ src/snowflake/snowpark/modin/plugin/extensions/indexing_overrides.py
@@ -46,6 +46,7 @@ import pandas
 from modin.pandas import Series
 from modin.pandas.base import BasePandasDataset
 from modin.pandas.dataframe import DataFrame
+from modin.pandas.utils import is_scalar
 from pandas._libs.tslibs import Resolution, parsing
 from pandas._typing import AnyArrayLike, Scalar
 from pandas.api.types import is_bool, is_list_like
@@ -59,8 +60,7 @@ from pandas.core.dtypes.common import (
 from pandas.core.indexing import IndexingError
 
 import snowflake.snowpark.modin.pandas as pd
-import snowflake.snowpark.modin.pandas.utils as frontend_utils
-from snowflake.snowpark.modin.pandas.utils import is_scalar
+import snowflake.snowpark.modin.plugin.extensions.utils as frontend_utils
 from snowflake.snowpark.modin.plugin._internal.indexing_utils import (
     MULTIPLE_ELLIPSIS_INDEXING_ERROR_MESSAGE,
     TOO_FEW_INDEXERS_INDEXING_ERROR_MESSAGE,
