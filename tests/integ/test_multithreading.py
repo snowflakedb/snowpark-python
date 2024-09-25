ebd736e67dca8da5ed419ce05575edd6afbe670d
Afroz Alam <afroz.alam@snowflake.com>
2024-09-25 10:06:57 -0700

    SNOW-1418523: make udf and sproc registration thread safe (#2289)

--- tests/integ/test_multithreading.py
+++ tests/integ/test_multithreading.py
@@ -6,9 +6,23 @@ import hashlib
 import os
 import tempfile
 from concurrent.futures import ThreadPoolExecutor, as_completed
+from typing import List, Tuple  # noqa: F401
 from unittest.mock import patch
 
 import pytest
+
+from snowflake.snowpark.session import Session
+from snowflake.snowpark.types import IntegerType
+
+try:
+    import dateutil
+
+    # six is the dependency of dateutil
+    import six
+
+    is_dateutil_available = True
+except ImportError:
+    is_dateutil_available = False
 
 from snowflake.snowpark.functions import lit
 from snowflake.snowpark.row import Row
@@ -195,3 +209,296 @@ def test_file_io(session, resources_path, temp_stage, use_stream):
             assert set(os.listdir(download_dir)) == {
                 os.path.basename(file_path) for file_path in resources_files
             }
+
+
+def test_concurrent_add_packages(session):
+    # this is a list of packages available in snowflake anaconda. If this
+    # test fails due to packages not being available, please update the list
+    package_list = {
+        "graphviz",
+        "numpy",
+        "pandas",
+        "scipy",
+        "scikit-learn",
+        "matplotlib",
+    }
+
+    try:
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [
+                executor.submit(session.add_packages, package)
+                for package in package_list
+            ]
+
+            for future in as_completed(futures):
+                future.result()
+
+            assert session.get_packages() == {
+                package: package for package in package_list
+            }
+    finally:
+        session.clear_packages()
+
+
+def test_concurrent_remove_package(session):
+    def remove_package(session_, package_name):
+        try:
+            session_.remove_package(package_name)
+            return True
+        except ValueError:
+            return False
+        except Exception as e:
+            raise e
+
+    try:
+        session.add_packages("numpy")
+        with ThreadPoolExecutor(max_workers=10) as executor:
+
+            futures = [
+                executor.submit(remove_package, session, "numpy") for _ in range(10)
+            ]
+            success_count, failure_count = 0, 0
+            for future in as_completed(futures):
+                if future.result():
+                    success_count += 1
+                else:
+                    failure_count += 1
+
+            # assert that only one thread was able to remove the package
+            assert success_count == 1
+            assert failure_count == 9
+    finally:
+        session.clear_packages()
+
+
+@pytest.mark.skipif(not is_dateutil_available, reason="dateutil is not available")
+def test_concurrent_add_import(session, resources_path):
+    test_files = TestFiles(resources_path)
+    import_files = [
+        test_files.test_udf_py_file,
+        os.path.relpath(test_files.test_udf_py_file),
+        test_files.test_udf_directory,
+        os.path.relpath(test_files.test_udf_directory),
+        six.__file__,
+        os.path.relpath(six.__file__),
+        os.path.dirname(dateutil.__file__),
+    ]
+    try:
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            for file in import_files:
+                executor.submit(
+                    session.add_import,
+                    file,
+                )
+
+        assert set(session.get_imports()) == {
+            os.path.abspath(file) for file in import_files
+        }
+    finally:
+        session.clear_imports()
+
+
+def test_concurrent_remove_import(session, resources_path):
+    test_files = TestFiles(resources_path)
+
+    def remove_import(session_, import_file):
+        try:
+            session_.remove_import(import_file)
+            return True
+        except KeyError:
+            return False
+        except Exception as e:
+            raise e
+
+    try:
+        session.add_import(test_files.test_udf_py_file)
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            futures = [
+                executor.submit(remove_import, session, test_files.test_udf_py_file)
+                for _ in range(10)
+            ]
+
+            success_count, failure_count = 0, 0
+            for future in as_completed(futures):
+                if future.result():
+                    success_count += 1
+                else:
+                    failure_count += 1
+
+            # assert that only one thread was able to remove the import
+            assert success_count == 1
+            assert failure_count == 9
+    finally:
+        session.clear_imports()
+
+
+def test_concurrent_sp_register(session, tmpdir):
+    try:
+        session.add_packages("snowflake-snowpark-python")
+
+        def register_and_test_sp(session_, thread_id):
+            prefix = Utils.random_alphanumeric_str(10)
+            sp_file_path = os.path.join(tmpdir, f"{prefix}_add_{thread_id}.py")
+            sproc_body = f"""
+from snowflake.snowpark import Session
+from snowflake.snowpark.functions import (
+    col,
+    lit
+)
+def add_{thread_id}(session_: Session, x: int) -> int:
+    return (
+        session_.create_dataframe([[x, ]], schema=["x"])
+        .select(col("x") + lit({thread_id}))
+        .collect()[0][0]
+    )
+"""
+            with open(sp_file_path, "w") as f:
+                f.write(sproc_body)
+                f.flush()
+
+            add_sp_from_file = session_.sproc.register_from_file(
+                sp_file_path, f"add_{thread_id}"
+            )
+            add_sp = session_.sproc.register(
+                lambda sess_, x: sess_.sql(f"select {x} + {thread_id}").collect()[0][0],
+                return_type=IntegerType(),
+                input_types=[IntegerType()],
+            )
+
+            assert add_sp_from_file(1) == thread_id + 1
+            assert add_sp(1) == thread_id + 1
+
+        with ThreadPoolExecutor(max_workers=10) as executor:
+            for i in range(10):
+                executor.submit(register_and_test_sp, session, i)
+    finally:
+        session.clear_packages()
+
+
+def test_concurrent_udf_register(session, tmpdir):
+    df = session.range(-5, 5).to_df("a")
+
+    def register_and_test_udf(session_, thread_id):
+        prefix = Utils.random_alphanumeric_str(10)
+        file_path = os.path.join(tmpdir, f"{prefix}_add_{thread_id}.py")
+        with open(file_path, "w") as f:
+            func = f"""
+def add_{thread_id}(x: int) -> int:
+    return x + {thread_id}
+"""
+            f.write(func)
+            f.flush()
+        add_i_udf_from_file = session_.udf.register_from_file(
+            file_path, f"add_{thread_id}"
+        )
+        add_i_udf = session_.udf.register(
+            lambda x: x + thread_id,
+            return_type=IntegerType(),
+            input_types=[IntegerType()],
+        )
+
+        Utils.check_answer(
+            df.select(add_i_udf(df.a), add_i_udf_from_file(df.a)),
+            [(thread_id + i, thread_id + i) for i in range(-5, 5)],
+        )
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for i in range(10):
+            executor.submit(register_and_test_udf, session, i)
+
+
+@pytest.mark.xfail(
+    "config.getoption('local_testing_mode', default=False)",
+    reason="UDTFs is not supported in local testing mode",
+    run=False,
+)
+def test_concurrent_udtf_register(session, tmpdir):
+    def register_and_test_udtf(session_, thread_id):
+        udtf_body = f"""
+from typing import List, Tuple
+
+class UDTFEcho:
+    def process(
+        self,
+        num: int,
+    ) -> List[Tuple[int]]:
+        return [(num + {thread_id},)]
+"""
+        prefix = Utils.random_alphanumeric_str(10)
+        file_path = os.path.join(tmpdir, f"{prefix}_udtf_echo_{thread_id}.py")
+        with open(file_path, "w") as f:
+            f.write(udtf_body)
+            f.flush()
+
+        d = {}
+        exec(udtf_body, {**globals(), **locals()}, d)
+        echo_udtf_from_file = session_.udtf.register_from_file(
+            file_path, "UDTFEcho", output_schema=["num"]
+        )
+        echo_udtf = session_.udtf.register(d["UDTFEcho"], output_schema=["num"])
+
+        df_local = session.table_function(echo_udtf(lit(1)))
+        df_from_file = session.table_function(echo_udtf_from_file(lit(1)))
+        assert df_local.collect() == [(thread_id + 1,)]
+        assert df_from_file.collect() == [(thread_id + 1,)]
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for i in range(10):
+            executor.submit(register_and_test_udtf, session, i)
+
+
+@pytest.mark.xfail(
+    "config.getoption('local_testing_mode', default=False)",
+    reason="UDAFs is not supported in local testing mode",
+    run=False,
+)
+def test_concurrent_udaf_register(session: Session, tmpdir):
+    df = session.create_dataframe([[1, 3], [1, 4], [2, 5], [2, 6]]).to_df("a", "b")
+
+    def register_and_test_udaf(session_, thread_id):
+        udaf_body = f"""
+class OffsetSumUDAFHandler:
+    def __init__(self) -> None:
+        self._sum = 0
+
+    @property
+    def aggregate_state(self):
+        return self._sum
+
+    def accumulate(self, input_value):
+        self._sum += input_value
+
+    def merge(self, other_sum):
+        self._sum += other_sum
+
+    def finish(self):
+        return self._sum + {thread_id}
+    """
+        prefix = Utils.random_alphanumeric_str(10)
+        file_path = os.path.join(tmpdir, f"{prefix}_udaf_{thread_id}.py")
+        with open(file_path, "w") as f:
+            f.write(udaf_body)
+            f.flush()
+        d = {}
+        exec(udaf_body, {**globals(), **locals()}, d)
+
+        offset_sum_udaf_from_file = session_.udaf.register_from_file(
+            file_path,
+            "OffsetSumUDAFHandler",
+            return_type=IntegerType(),
+            input_types=[IntegerType()],
+        )
+        offset_sum_udaf = session_.udaf.register(
+            d["OffsetSumUDAFHandler"],
+            return_type=IntegerType(),
+            input_types=[IntegerType()],
+        )
+
+        Utils.check_answer(
+            df.agg(offset_sum_udaf_from_file(df.a)), [Row(6 + thread_id)]
+        )
+        Utils.check_answer(df.agg(offset_sum_udaf(df.a)), [Row(6 + thread_id)])
+
+    with ThreadPoolExecutor(max_workers=10) as executor:
+        for i in range(10):
+            executor.submit(register_and_test_udaf, session, i)
