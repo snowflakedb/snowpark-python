66d6fb0337da66e0d8c60f832a6b9469d86886ec
sfc-gh-mvashishtha <mahesh.vashishtha@snowflake.com>
2024-09-27 16:11:11 -0700

    Add expanding.
    Signed-off-by: sfc-gh-mvashishtha <mahesh.vashishtha@snowflake.com>

--- tests/integ/modin/window/test_rolling.py
+++ tests/integ/modin/window/test_rolling.py
@@ -22,21 +22,12 @@ from tests.integ.modin.utils import (
     create_test_series,
     eval_snowpark_pandas_result,
 )
-
-agg_func_supported_for_timedelta = ["count"]
-agg_func_not_supported_for_timedelta = [
-    "sum",
-    "mean",
-    "var",
-    "std",
-    "min",
-    "max",
-    "sem",
-]
-agg_func = pytest.mark.parametrize(
-    "agg_func",
-    [*agg_func_supported_for_timedelta, *agg_func_not_supported_for_timedelta],
+from tests.integ.modin.window.utils import (
+    agg_func,
+    agg_func_not_supported_for_timedelta,
+    agg_func_supported_for_timedelta,
 )
+
 window = pytest.mark.parametrize("window", [1, 2, 3, 4, 6])
 min_periods = pytest.mark.parametrize("min_periods", [None, 1, 2])
 center = pytest.mark.parametrize("center", [True, False])
