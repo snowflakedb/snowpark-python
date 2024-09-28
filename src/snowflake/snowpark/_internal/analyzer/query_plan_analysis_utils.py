02227623ed244087008c910c99b10d3a946feb7d
Afroz Alam <afroz.alam@snowflake.com>
2024-09-28 15:35:36 -0700

    type fix

--- src/snowflake/snowpark/_internal/analyzer/query_plan_analysis_utils.py
+++ src/snowflake/snowpark/_internal/analyzer/query_plan_analysis_utils.py
@@ -77,7 +77,7 @@ def get_complexity_score(node: "LogicalPlan") -> int:
     """Calculates the complexity score based on the cumulative node complexity"""
     adjusted_cumulative_complexity = node.cumulative_node_complexity.copy()
     if hasattr(node, "referenced_ctes"):
-        for with_query_block in node.referenced_ctes: # type: ignore
+        for with_query_block in node.referenced_ctes:  # type: ignore
             child_node = with_query_block.children[0]
             for category, value in child_node.cumulative_node_complexity.items():
                 if category in adjusted_cumulative_complexity:
