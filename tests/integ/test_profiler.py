fa243b1956619afb134a3051ef66b1e632ab6f2e
Yuyang Wang <yuyang.wang@snowflake.com>
2024-09-30 11:37:12 -0700

    fix test

--- tests/integ/test_profiler.py
+++ tests/integ/test_profiler.py
@@ -160,3 +160,19 @@ def test_dump_profile_to_file(
     profiler_session.profiler.register_modules([])
     with open(file) as f:
         assert "Modules Profiled" in f.read()
+
+
+def test_sp_call_match(profiler_session):
+    pro = profiler_session.profiler
+    sp_call_sql = """WITH myProcedure AS PROCEDURE ()
+  RETURNS TABLE ( )
+  LANGUAGE PYTHON
+  RUNTIME_VERSION = '3.8'
+  PACKAGES = ( 'snowflake-snowpark-python==1.2.0', 'pandas==1.3.3' )
+  IMPORTS = ( '@my_stage/file1.py', '@my_stage/file2.py' )
+  HANDLER = 'my_function'
+  RETURNS NULL ON NULL INPUT
+AS 'fake'
+CALL myProcedure()INTO :result
+    """
+    assert pro._is_sp_call(sp_call_sql)
