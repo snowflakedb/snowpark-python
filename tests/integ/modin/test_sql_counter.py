c9a8a243850ef5643532046d51fcf48fa81cc7da
Eric Vandenberg <eric.vandenberg@snowflake.com>
2024-10-02 11:13:39 -0700

    Pull latest main

--- tests/integ/modin/test_sql_counter.py
+++ tests/integ/modin/test_sql_counter.py
@@ -9,17 +9,12 @@ import pytest
 import snowflake.snowpark.modin.plugin  # noqa: F401
 from snowflake.snowpark import QueryRecord
 from snowflake.snowpark.session import Session
-from tests.integ.modin.sql_counter import SqlCounter, sql_count_checker
 from tests.integ.modin.utils import assert_frame_equal
 from tests.integ.utils.sql_counter import SqlCounter, sql_count_checker
 
 
 class CustomException(BaseException):
     pass
-
-
-def is_sql_simplifier_enabled():
-    return Session.SessionBuilder().getOrCreate().sql_simplifier_enabled
 
 
 @sql_count_checker(query_count=3)
