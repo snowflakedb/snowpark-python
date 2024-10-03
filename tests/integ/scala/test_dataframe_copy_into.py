ef1285b75ed4670bc4d88e9ba424577f256e5a54
Leonhard Spiegelberg <leonhard.spiegelberg@snowflake.com>
2024-10-03 13:27:22 -0700

    try to fix the other test agai

--- tests/integ/scala/test_dataframe_copy_into.py
+++ tests/integ/scala/test_dataframe_copy_into.py
@@ -955,7 +955,8 @@ def test_copy_non_csv_transformation(
                     if not running_on_public_ci()
                     else datetime.time(11, 11, 11),
                     TS_NTZ=datetime.datetime(2022, 4, 1, 4, 11, 11)
-                    + datetime.timedelta(hours=-local_to_utc_offset_in_hours()),
+                    if not running_on_public_ci()
+                    else datetime.time(11, 11, 11),
                     TS=datetime.datetime(2022, 4, 1, 4, 11, 11),
                     V='{"key":"value"}',
                 )
