cfe7986477a586f205e43339ba81e5c135bf89e1
Afroz Alam <afroz.alam@snowflake.com>
2024-10-03 16:25:05 -0700

    SNOW-1566370: remove duplicate with query complexity (#2346)

--- src/snowflake/snowpark/_internal/compiler/plan_compiler.py
+++ src/snowflake/snowpark/_internal/compiler/plan_compiler.py
@@ -80,9 +80,7 @@ class PlanCompiler:
             # preparation for compilation
             # 1. make a copy of the original plan
             start_time = time.time()
-            complexity_score_before_compilation = get_complexity_score(
-                self._plan.cumulative_node_complexity
-            )
+            complexity_score_before_compilation = get_complexity_score(self._plan)
             logical_plans: List[LogicalPlan] = [copy.deepcopy(self._plan)]
             plot_plan_if_enabled(self._plan, "original_plan")
             deep_copy_end_time = time.time()
@@ -101,8 +99,7 @@ class PlanCompiler:
 
             cte_end_time = time.time()
             complexity_scores_after_cte = [
-                get_complexity_score(logical_plan.cumulative_node_complexity)
-                for logical_plan in logical_plans
+                get_complexity_score(logical_plan) for logical_plan in logical_plans
             ]
             for i, plan in enumerate(logical_plans):
                 plot_plan_if_enabled(plan, f"cte_optimized_plan_{i}")
@@ -116,8 +113,7 @@ class PlanCompiler:
 
             large_query_breakdown_end_time = time.time()
             complexity_scores_after_large_query_breakdown = [
-                get_complexity_score(logical_plan.cumulative_node_complexity)
-                for logical_plan in logical_plans
+                get_complexity_score(logical_plan) for logical_plan in logical_plans
             ]
             for i, plan in enumerate(logical_plans):
                 plot_plan_if_enabled(plan, f"large_query_breakdown_plan_{i}")
