8955596f1ca8fd7e51f0a754bb6a32e25a8ed104
Leonhard Spiegelberg <leonhard.spiegelberg@snowflake.com>
2024-10-04 10:22:14 -0700

    noqa for plugin

---
+++ tests/dummy.py
@@ -0,0 +1,16 @@
+#
+# Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
+#
+
+import modin.pandas as pd
+
+import snowflake.snowpark.modin.plugin  # noqa: F401
+from snowflake.snowpark.session import Session
+
+session = Session.builder.create()
+
+index = pd.Index([0, 1, 2])
+
+index.any()
+
+print("all ok")
