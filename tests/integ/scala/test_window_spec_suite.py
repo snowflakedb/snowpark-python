8760b789bc6622f1eedb288fabf7249b421d0555
Jamison <jamison.rose@snowflake.com>
2024-10-07 17:23:42 -0700

    Additional tests.

--- tests/integ/scala/test_window_spec_suite.py
+++ tests/integ/scala/test_window_spec_suite.py
@@ -3,6 +3,7 @@
 # Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
 #
 
+from datetime import date
 from decimal import Decimal
 
 import pytest
@@ -272,6 +273,34 @@ def test_window_function_should_fail_if_order_by_clause_is_not_specified(session
     with pytest.raises(SnowparkSQLException) as ex_info:
         df.select(row_number().over(Window.partition_by("value"))).collect()
     assert "requires ORDER BY in window specification" in str(ex_info)
+
+
+@pytest.mark.skipif(
+    "config.getoption('local_testing_mode', default=False)",
+    "SNOW-1360263: row_number currently failing in local testing",
+)
+def test_snow_1360263_repro(session):
+    data = [
+        Row(id=1, row_date=date(2024, 1, 1), value=1),
+        Row(id=2, row_date=date(2024, 1, 1), value=1),
+        Row(id=1, row_date=date(2024, 1, 2), value=1),
+        Row(id=1, row_date=date(2024, 1, 2), value=100),
+        Row(id=2, row_date=date(2024, 1, 2), value=1),
+    ]
+
+    test_data = session.create_dataframe(data)
+
+    # partition over id and row_date and get the records with the largest values
+    window = Window.partition_by("id", "row_date").order_by(col("value").desc())
+    df = test_data.with_column("row_num", row_number().over(window)).where(
+        col("row_num") == 1
+    )
+    assert df.order_by("ID", "ROW_DATE").collect() == [
+        Row(1, date(2024, 1, 1), 1, 1),
+        Row(1, date(2024, 1, 2), 100, 1),
+        Row(2, date(2024, 1, 1), 1, 1),
+        Row(2, date(2024, 1, 2), 1, 1),
+    ]
 
 
 @pytest.mark.skipif(
