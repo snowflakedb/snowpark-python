707eaac502778db04d18b2b91fb5c5eef2e907de
Jianzhun Du <jianzhun.du@snowflake.com>
2024-10-08 10:38:56 -0700

    fix

--- tests/integ/scala/test_snowflake_plan_suite.py
+++ tests/integ/scala/test_snowflake_plan_suite.py
@@ -190,7 +190,10 @@ def test_execution_queries_and_post_actions(session):
 )
 def test_plan_height(session, temp_table, sql_simplifier_enabled):
     df1 = session.table(temp_table)
-    assert df1._plan.plan_state[PlanState.PLAN_HEIGHT] == 1
+    if sql_simplifier_enabled:
+        assert df1._plan.plan_state[PlanState.PLAN_HEIGHT] == 2
+    else:
+        assert df1._plan.plan_state[PlanState.PLAN_HEIGHT] == 1
 
     df2 = session.create_dataframe([(1, 20), (3, 40)], schema=["a", "c"])
     df3 = session.create_dataframe(
@@ -250,7 +253,7 @@ def test_plan_num_duplicate_nodes_describe_query(session, temp_table):
     assert len(query_history.queries) == 1
 
 
-def test_create_scoped_temp_table(session):
+def test_create_scoped_temp_table(session, sql_simplifier_enabled):
     table_name = Utils.random_name_for_temp_object(TempObjectType.TABLE)
     try:
         Utils.create_table(session, table_name, "num int, str string(8)")
@@ -305,6 +308,11 @@ def test_create_scoped_temp_table(session):
             .sql
             == f' CREATE  TEMPORARY  TABLE {temp_table_name}("NUM" BIGINT, "STR" STRING(8))  '
         )
+        inner_select_sql = (
+            f" SELECT  *  FROM {table_name}"
+            if sql_simplifier_enabled
+            else f" SELECT  *  FROM ({table_name})"
+        )
         assert (
             session._plan_builder.save_as_table(
                 table_name=[temp_table_name],
@@ -326,7 +334,7 @@ def test_create_scoped_temp_table(session):
             )
             .queries[0]
             .sql
-            == f" CREATE  TEMPORARY  TABLE  {temp_table_name}    AS  SELECT  *  FROM ( SELECT  *  FROM ({table_name}))"
+            == f" CREATE  TEMPORARY  TABLE  {temp_table_name}    AS  SELECT  *  FROM ({inner_select_sql})"
         )
         expected_sql = f' CREATE  TEMPORARY  TABLE  {temp_table_name}("NUM" BIGINT, "STR" STRING(8))'
         assert expected_sql in (
