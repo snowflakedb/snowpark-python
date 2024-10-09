6fffc5e92a078c45d3eea55f569ccf8f9b74f1d4
Afroz Alam <afroz.alam@snowflake.com>
2024-10-09 16:32:00 -0700

    minor bugfix

--- src/snowflake/snowpark/_internal/analyzer/select_statement.py
+++ src/snowflake/snowpark/_internal/analyzer/select_statement.py
@@ -1185,9 +1185,13 @@ class SelectStatement(Selectable):
         self.analyzer.analyze(col, self.df_aliased_col_name_to_real_col_name)
         for plan in self.analyzer.subquery_plans:
             for query in plan.queries[:-1]:
+                if new.pre_actions is None:
+                    new.pre_actions = []
                 if query not in new.pre_actions:
                     new.pre_actions.append(query)
             for query in plan.post_actions:
+                if new.post_actions is None:
+                    new.post_actions = []
                 if query not in new.post_actions:
                     new.post_actions.append(query)
 
