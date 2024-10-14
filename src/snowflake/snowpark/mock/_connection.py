df1d2f6d1a3c9650de0d510ca6ca3b273b9d2ce9
Afroz Alam <afroz.alam@snowflake.com>
2024-10-14 16:32:51 -0700

    merge with param protection changes

--- src/snowflake/snowpark/mock/_connection.py
+++ src/snowflake/snowpark/mock/_connection.py
@@ -323,10 +323,7 @@ class MockServerConnection:
             self._oob_telemetry.disable()
         else:
             self._oob_telemetry.log_session_creation(self._connection_uuid)
-        # thread safe param protection
-        self._thread_safe_session_enabled = self._options.get(
-            "PYTHON_SNOWPARK_ENABLE_THREAD_SAFE_SESSION", False
-        )
+        self._thread_safe_session_enabled = True
 
     def log_not_supported_error(
         self,
