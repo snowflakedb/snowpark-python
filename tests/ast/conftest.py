1f39ada2c7cf4f3b1b5803c543170c23f0ca5c3e
Eric Vandenberg <eric.vandenberg@snowflake.com>
2024-10-15 14:10:58 -0700

    SNOW-1738538 Update expectation files to json ast output so more stable & readable

--- tests/ast/conftest.py
+++ tests/ast/conftest.py
@@ -15,6 +15,10 @@ def default_unparser_path():
     return explicit or default_default
 
 
+def default_encoding():
+    return 'json'
+
+
 def pytest_addoption(parser):
     parser.addoption(
         "--unparser-jar",
@@ -29,6 +33,13 @@ def pytest_addoption(parser):
         default=False,
         help="If set, overwrite test files with the actual output as the expected output.",
     )
+    parser.addoption(
+        "--encoding",
+        action="store",
+        default=default_encoding(),
+        type=str,
+        help="Encoding for AST expectation output, values can be 'json' (default) or 'b64' for base64."
+    )
 
 
 def pytest_configure(config):
@@ -40,6 +51,11 @@ def pytest_configure(config):
             f"Please set the correct path with --unparser-jar or SNOWPARK_UNPARSER_JAR."
         )
     pytest.update_expectations = config.getoption("--update-expectations")
+    pytest.encoding = config.getoption("--ast_encoding", default='json')
+    if pytest.encoding not in ['json', 'b64']:
+        logging.error(
+            f"Unrecognized encoding {pytest.encoding}, ignoring.  Using default {default_encoding()}."
+        )
 
 
 @pytest.fixture(scope="function")
