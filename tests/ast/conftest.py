d70fb2be169059ffcb5cdd929986c6557bbbf130
Eric Vandenberg <eric.vandenberg@snowflake.com>
2024-10-21 14:04:39 -0700

    SNOW-1738540 Introduce "nop_testing" execution mode that does not execute against any backend.

--- tests/ast/conftest.py
+++ tests/ast/conftest.py
@@ -151,8 +151,9 @@ class TestTables:
 def session():
     # Note: Do NOT use Session(MockServerConnection()), as this doesn't setup the correct registrations throughout snowpark.
     # Need to use the Session.builder to properly register this as active session etc.
-    with Session.builder.config("local_testing", True).create() as s:
+    with Session.builder.config("nop_testing", True).create() as s:
         s.ast_enabled = True
+        s.sql_simplifier_enabled = False
         yield s
 
 
