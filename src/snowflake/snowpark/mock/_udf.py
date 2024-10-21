0d1e5b9ef8aac9d0ca405e3caa711ae978d1a382
Ovidiu Platon <ovidiu.platon@snowflake.com>
2024-10-21 16:16:37 -0700

    Fix rebase mistakes in mock/_udf.py

--- src/snowflake/snowpark/mock/_udf.py
+++ src/snowflake/snowpark/mock/_udf.py
@@ -211,18 +211,18 @@ class MockUDFRegistration(UDFRegistration):
             if packages:
                 pass  # NO-OP
 
-        # register
-        self._registry[udf_name] = MockUserDefinedFunction(
-            func,
-            return_type,
-            input_types,
-            udf_name,
-            strict=strict,
-            packages=packages,
-            use_session_imports=imports is None,
-            _ast=ast,
-            _ast_id=stmt.var_id.bitfield1 if stmt else None,
-        )
+            # register
+            self._registry[udf_name] = MockUserDefinedFunction(
+                func,
+                return_type,
+                input_types,
+                udf_name,
+                strict=strict,
+                packages=packages,
+                use_session_imports=imports is None,
+                _ast=ast,
+                _ast_id=stmt.var_id.bitfield1 if stmt else None,
+            )
 
             if type(func) is tuple:  # update file registration
                 module_name = self._import_file(func[0], udf_name=udf_name)
