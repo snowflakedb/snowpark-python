f570884faa2fc3744cc8bc7988dcd0d96e488b2f
John Kew <john.kew@snowflake.com>
2024-10-22 09:58:20 -0700

    Add tests specifically for scikit-learn

---
+++ tests/integ/modin/test_scikit.py
@@ -0,0 +1,41 @@
+import modin.pandas as pd
+import numpy as np
+import pandas as native_pd
+import pytest
+from numpy.testing import assert_array_equal
+
+import snowflake.snowpark.modin.plugin  # noqa: F401
+from tests.integ.utils.sql_counter import SqlCounter, sql_count_checker
+
+
+# SNOW-1344931 Support MaxAbsScalar by supporting numpy.may_share_memory
+def test_scikit_maxabs():    
+    from sklearn.preprocessing import MaxAbsScaler
+    data = [[ 1., -1.,  2.],
+        [ 2.,  0.,  0.],
+        [ 0.,  1., -1.]]
+    X = pd.DataFrame(data)
+    X_native = native_pd.DataFrame(data)
+    # the following will result in a TypeError if
+    # `numpy.may_share_memory' is not implemented as an
+    # array function. Similar calls are made in other scikit learn
+    # functions
+    with SqlCounter(query_count=3):
+        result = MaxAbsScaler().fit_transform(X)
+        native_result = MaxAbsScaler().fit_transform(X_native)
+        assert_array_equal(np.array(result), np.array(native_result))
+        
+# SNOW-1518382 Support PCA by supporting numpy.may_share_memory
+# similar to the test for MaxAbsScalar
+def test_scikit_pca():    
+    from sklearn.decomposition import PCA
+    data = [[ 1., -1.,  2.],
+        [ 2.,  0.,  0.],
+        [ 0.,  1., -1.]]
+    X = pd.DataFrame(data)
+    X_native = native_pd.DataFrame(data)
+    with SqlCounter(query_count=2):
+        pca = PCA()
+        result = pca.fit(X)
+        native_result = pca.fit(X_native)
+        assert_array_equal(np.array(result), np.array(native_result))
