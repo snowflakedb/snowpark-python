bb1f9dacfebf934c699580e40ad5e6cc6443e0e6
Ovidiu Platon <ovidiu.platon@snowflake.com>
2024-10-22 12:17:35 -0700

    Server-side Snowpark: Modin changes

--- src/snowflake/snowpark/modin/conftest.py
+++ src/snowflake/snowpark/modin/conftest.py
@@ -3,17 +3,20 @@
 # Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
 #
 
-import modin.pandas as pd  # pragma: no cover
-import numpy as np  # pragma: no cover
-import pytest  # pragma: no cover
+from snowflake.snowpark._internal.utils import COMPATIBLE_WITH_MODIN
 
-import snowflake.snowpark.modin.plugin  # pragma: no cover # noqa: F401
+# Guard against importing modin due to Python 3.8 issue.
+if COMPATIBLE_WITH_MODIN:
+    import modin.pandas as pd  # pragma: no cover
+    import numpy as np  # pragma: no cover
+    import pytest  # pragma: no cover
 
+    import snowflake.snowpark.modin.plugin  # pragma: no cover # noqa: F401
 
-@pytest.fixture(autouse=True, scope="module")  # pragma: no cover
-def add_doctest_imports(doctest_namespace) -> None:  # pragma: no cover
-    """
-    Make `np` and `pd` names available for doctests.
-    """
-    doctest_namespace["np"] = np  # pragma: no cover
-    doctest_namespace["pd"] = pd  # pragma: no cover
+    @pytest.fixture(autouse=True, scope="module")  # pragma: no cover
+    def add_doctest_imports(doctest_namespace) -> None:  # pragma: no cover
+        """
+        Make `np` and `pd` names available for doctests.
+        """
+        doctest_namespace["np"] = np  # pragma: no cover
+        doctest_namespace["pd"] = pd  # pragma: no cover
