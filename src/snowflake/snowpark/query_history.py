039a8a56d25f8170663f918f7349e4cad44c5abd
Ovidiu Platon <ovidiu.platon@snowflake.com>
2024-10-22 11:12:21 -0700

    Server-side Snowpark: Update query notifications with dataframe AST functionality

--- src/snowflake/snowpark/query_history.py
+++ src/snowflake/snowpark/query_history.py
@@ -1,6 +1,7 @@
 #
 # Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
 #
+from abc import abstractmethod
 from typing import List, NamedTuple
 
 import snowflake.snowpark
@@ -25,7 +26,22 @@ class QueryRecord(NamedTuple):
             return f"QueryRecord(query_id={self.query_id}, sql_text={self.sql_text}, is_describe={self.is_describe}, thread_id={self.thread_id})"
 
 
-class QueryHistory:
+class QueryListener:
+    @abstractmethod
+    def _notify(self, query_record: QueryRecord, *args, **kwargs) -> None:
+        """
+        notify query listener of a query event
+        Args:
+            query_record: record of the query to notify the listener of
+            *args: optional arguments
+            **kwargs: optional keyword arguments
+        Returns:
+            None
+        """
+        pass  # pragma: no cover
+
+
+class QueryHistory(QueryListener):
     """A context manager that listens to and records SQL queries that are pushed down to the Snowflake database.
 
     See also:
@@ -49,7 +65,7 @@ class QueryHistory:
     def __exit__(self, exc_type, exc_val, exc_tb):
         self.session._conn.remove_query_listener(self)
 
-    def _add_query(self, query_record: QueryRecord):
+    def _notify(self, query_record: QueryRecord, *args, **kwargs) -> None:
         self._queries.append(query_record)
 
     @property
@@ -63,3 +79,23 @@ class QueryHistory:
     @property
     def include_thread_id(self) -> bool:
         return self._include_thread_id
+
+
+class AstListener(QueryListener):
+    def __init__(self, session: "snowflake.snowpark.session.Session") -> None:
+        self.session = session
+        self._ast_batches: List[bytes] = []
+
+    def __enter__(self):
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.session._conn.remove_query_listener(self)
+
+    def _notify(self, query_record: QueryRecord, *args, **kwargs) -> None:
+        if "dataframeAst" in kwargs:
+            self._ast_batches.append(kwargs["dataframeAst"])
+
+    @property
+    def base64_batches(self) -> List[QueryRecord]:
+        return self._ast_batches
