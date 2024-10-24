4d605c2e987932e89c4bdd97e5f686047c2655f2
Adam Ling <adam.ling@snowflake.com>
2024-10-24 11:14:40 -0700

    update

---
+++ tests/unit/test_deprecation.py
@@ -0,0 +1,26 @@
+#
+# Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
+#
+
+import importlib
+import sys
+
+import pytest
+
+
+def test_py38_deprecation(monkeypatch):
+    if not (sys.version_info.major == 3 and sys.version_info.minor == 8):
+        pytest.skip("This test is for Python 3.8 only")
+
+    monkeypatch.delitem(sys.modules, "snowflake.snowpark", raising=False)
+    with pytest.warns(
+        DeprecationWarning, match="Python Runtime 3.8 reached its End-Of-Life"
+    ) as record:
+        importlib.import_module("snowflake.snowpark")
+
+    assert len(record) == 1
+
+    # import again won't trigger the warning
+    with pytest.warns(None) as record:
+        importlib.import_module("snowflake.snowpark")
+    assert len(record) == 0
