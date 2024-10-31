8df47358ddb03ae8fc88ab91aa321132a442a7fc
Mahesh Vashishtha <mahesh.vashishtha@snowflake.com>
2024-10-31 12:28:30 -0700

    SNOW-1777142: Implement groupby.agg(len) (#2542)
    Fixes SNOW-1777142.
    
    Map `len` to the same Snowpark aggregation functions that `"size"` maps
    to.
    
    Also fix a bug where aggregating a single-column dataframe with a single
    callable function (e.g. `pd.DataFrame([0]).agg(len)`) would fail to
    transpose the result. One of the test cases that we already have for
    `agg("size")` revealed this bug for `agg(len)`.
    
    ---------
    
    Signed-off-by: sfc-gh-mvashishtha <mahesh.vashishtha@snowflake.com>

--- src/snowflake/snowpark/modin/plugin/_internal/aggregation_utils.py
+++ src/snowflake/snowpark/modin/plugin/_internal/aggregation_utils.py
@@ -456,12 +456,15 @@ _PANDAS_AGGREGATION_TO_SNOWPARK_PANDAS_AGGREGATION: MappingProxyType[
             axis_1_aggregation_skipna=_columns_count,
             preserves_snowpark_pandas_types=False,
         ),
-        "size": _SnowparkPandasAggregation(
-            # We must count the total number of rows regardless of if they're null.
-            axis_0_aggregation=lambda _: builtin("count_if")(pandas_lit(True)),
-            axis_1_aggregation_keepna=_columns_count_keep_nulls,
-            axis_1_aggregation_skipna=_columns_count_keep_nulls,
-            preserves_snowpark_pandas_types=False,
+        **_create_pandas_to_snowpark_pandas_aggregation_map(
+            (len, "size"),
+            _SnowparkPandasAggregation(
+                # We must count the total number of rows regardless of if they're null.
+                axis_0_aggregation=lambda _: builtin("count_if")(pandas_lit(True)),
+                axis_1_aggregation_keepna=_columns_count_keep_nulls,
+                axis_1_aggregation_skipna=_columns_count_keep_nulls,
+                preserves_snowpark_pandas_types=False,
+            ),
         ),
         **_create_pandas_to_snowpark_pandas_aggregation_map(
             ("mean", np.mean),
