6a3454e2d26eef2d727eefea2b0746db29390884
Afroz Alam <afroz.alam@snowflake.com>
2024-11-03 20:37:07 -0800

    update requirements

--- setup.py
+++ setup.py
@@ -24,6 +24,7 @@ INSTALL_REQ_LIST = [
     "cloudpickle==2.2.1;python_version~='3.11'",  # backend only supports cloudpickle 2.2.1 + python 3.11 at the moment
     "protobuf>=5.28",  # Snowpark IR
     "tzlocal",  # Snowpark IR
+    "snowflake>=1.0.0,<2", # Catalog
 ]
 REQUIRED_PYTHON_VERSION = ">=3.8, <3.12"
 
