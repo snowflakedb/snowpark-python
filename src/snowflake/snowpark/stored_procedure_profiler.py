ca475439cc0406f7388b5afb58af68f218b9f9f5
Yuyang Wang <yuyang.wang@snowflake.com>
2024-11-05 11:51:09 -0800

    SNOW-1762556:set_active_profiler should accept lower/mixed cases of LINE and MEMORY  (#2557)

--- src/snowflake/snowpark/stored_procedure_profiler.py
+++ src/snowflake/snowpark/stored_procedure_profiler.py
@@ -73,19 +73,22 @@ class StoredProcedureProfiler:
 
         Args:
             active_profiler_type: String that represent active_profiler, must be either 'LINE' or 'MEMORY'
-            (case-sensitive). Active profiler is 'LINE' by default.
+            (case-insensitive). Active profiler is 'LINE' by default.
 
         """
         with self._lock:
             self._active_profiler_number += 1
-        if active_profiler_type not in ["LINE", "MEMORY"]:
+        if active_profiler_type.upper() not in ["LINE", "MEMORY"]:
             raise ValueError(
                 f"active_profiler expect 'LINE', 'MEMORY', got {active_profiler_type} instead"
             )
-        sql_statement = (
-            f"alter session set ACTIVE_PYTHON_PROFILER = '{active_profiler_type}'"
-        )
-        self._session.sql(sql_statement)._internal_collect_with_tag_no_telemetry()
+        sql_statement = f"alter session set ACTIVE_PYTHON_PROFILER = '{active_profiler_type.upper()}'"
+        try:
+            self._session.sql(sql_statement)._internal_collect_with_tag_no_telemetry()
+        except Exception as e:
+            logger.warning(
+                f"Set active profiler failed because of {e}. Active profiler is previously set value or default 'LINE' now."
+            )
         with self._lock:
             if self._query_history is None:
                 self._query_history = self._session.query_history(
