756fd0f410be8c7e6c2b7368ce328a1f930c4dff
Andong Zhan <andong.zhan@snowflake.com>
2024-11-06 13:48:58 -0800

    SNOW-1491199 Generate Protobuf Python code for IR (#2518)
    <!---
    Please answer these questions before creating your pull request. Thanks!
    --->
    
    1. Which Jira issue is this PR addressing? Make sure that there is an
    accompanying issue to your PR.
    
       <!---
       In this section, please add a Snowflake Jira issue number.
    
    Note that if a corresponding GitHub issue exists, you should still
    include
       the Snowflake Jira issue number. For example, for GitHub issue
    https://github.com/snowflakedb/snowpark-python/issues/1400, you should
       add "SNOW-1335071" here.
        --->
    
       Fixes SNOW-1491199
    
    2. Fill out the following pre-review checklist:
    
    - [ ] I am adding a new automated test(s) to verify correctness of my
    new code
    - [ ] If this test skips Local Testing mode, I'm requesting review from
    @snowflakedb/local-testing
       - [ ] I am adding new logging messages
       - [ ] I am adding a new telemetry message
       - [ ] I am adding new credentials
       - [ ] I am adding a new dependency
    - [ ] If this is a new feature/behavior, I'm adding the Local Testing
    parity changes.
    - [x] I acknowledge that I have ensured my changes to be thread-safe.
    Follow the link for more information: [Thread-safe Developer
    Guidelines](https://docs.google.com/document/d/162d_i4zZ2AfcGRXojj0jByt8EUq-DrSHPPnTa4QvwbA/edit#bookmark=id.e82u4nekq80k)
    
    3. Please describe how your code solves the related issue.
    
    Please write a short description of how your code change solves the
    related issue.
    
    This PR removed Protobuf generated code from source and add logic to
    generate them when it is need.
    This pull request includes the addition of a script to install `protoc`
    and updates to multiple GitHub Actions workflows to use this script.
    Additionally, there is an update to the `CODEOWNERS` file.
    
    * Added a new script `install_protoc.sh` to install `protoc` on GitHub
    Actions runners. This script handles downloading and installing the
    appropriate version of `protoc` based on the operating system and
    architecture.
    
    * Updated the `CODEOWNERS` file to add ownership for the
    `tests/unit/ast/` directory to the `@snowflakedb/snowpark-ir` team.
    
    Daily pandas precommit test:
    https://github.com/snowflakedb/snowpark-python/actions/runs/11645868643/job/32429396169
    Daily notebook precommit test:
    https://github.com/snowflakedb/snowpark-python/actions/runs/11645871020
    Daily precommit test:
    https://github.com/snowflakedb/snowpark-python/actions/runs/11645600647/job/32428853317
    pandas Sproc precommit test:
    https://ci-dev-142.int.snowflakecomputing.com/job/SnowparkPandasStoredProcPrecommitTest/185/console
    
    After this PR, when developers try to pip install source code or use
    tox, they need to make sure `protoc` is installed. When pip install from
    wheel file, no `protoc` is required since the generated code should
    exist in the wheel file.

---
+++ tests/unit/ast/test_ast_proto.py
@@ -0,0 +1,9 @@
+#
+# Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
+#
+
+
+def test_import_ast_proto():
+    from snowflake.snowpark._internal.proto.generated import ast_pb2 as proto
+
+    assert proto.MAX_VERSION
