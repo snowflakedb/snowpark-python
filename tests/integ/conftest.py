96637ecae702d5ecba6f5073262f20e342b986bf
Eric Vandenberg <eric.vandenberg@snowflake.com>
2024-11-08 10:14:48 -0800

    SNOW-1787415 Fix dataframe join with sort deterministic (#2591)
    SNOW-1787415 Fix dataframe join with sort deterministic

--- tests/integ/conftest.py
+++ tests/integ/conftest.py
@@ -313,3 +313,13 @@ def temp_schema(connection, session, local_testing_mode) -> None:
             )
             yield temp_schema_name
             cursor.execute(f"DROP SCHEMA IF EXISTS {temp_schema_name}")
+
+
+@pytest.fixture(scope="function", autouse=True)
+def clear_session_ast_batch_on_validate_ast(session, validate_ast):
+    """
+    After each test, flush the AST batch so it does not pollute the next test validation.
+    """
+    yield
+    if validate_ast:
+        session._ast_batch.flush()
