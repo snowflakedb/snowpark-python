73079dff2ab9f8ea0c72689a0b0f3fae460816be
Labanya Mukhopadhyay <labanya.mukhopadhyay@snowflake.com>
2024-11-11 18:42:08 -0800

    fix align 1 to accomodate for duplicate overlapping labels
    Signed-off-by: Labanya Mukhopadhyay <labanya.mukhopadhyay@snowflake.com>

--- src/snowflake/snowpark/modin/plugin/_internal/align_utils.py
+++ src/snowflake/snowpark/modin/plugin/_internal/align_utils.py
@@ -1,10 +1,12 @@
 #
 # Copyright (c) 2012-2024 Snowflake Computing Inc. All rights reserved.
 #
+from collections import Counter
 from collections.abc import Hashable
-from typing import Literal, Optional
+from typing import Literal
 
 from snowflake.snowpark._internal.type_utils import ColumnOrName
+from snowflake.snowpark.functions import col
 from snowflake.snowpark.modin.plugin._internal.frame import InternalFrame
 from snowflake.snowpark.modin.plugin._internal.join_utils import align_on_index
 from snowflake.snowpark.modin.plugin._internal.utils import pandas_lit
@@ -12,7 +14,7 @@ from snowflake.snowpark.modin.plugin._internal.utils import pandas_lit
 
 def align_axis_0_left(
     frame: InternalFrame, other_frame: InternalFrame, join: str
-) -> tuple[InternalFrame, InternalFrame, list[str], list[str]]:
+) -> InternalFrame:
     """
     Gets the left align results.
 
@@ -22,11 +24,7 @@ def align_axis_0_left(
         join: type of alignment to be performed.
 
     Returns:
-        Tuple containing:
-            InternalFrame result of join_utils.align_on_index,
-            final left_frame,
-            list of left_frame_data_ids,
-            list of left_index_ids
+        New InternalFrame representing aligned left frame.
     """
     if join == "right":
         left_result, left_column_mapper = align_on_index(other_frame, frame, how="left")
@@ -46,12 +44,22 @@ def align_axis_0_left(
         left_frame = left_result.ordered_dataframe.select(
             left_frame_data_ids + left_index_ids
         )
-    return left_result, left_frame, left_frame_data_ids, left_index_ids
+
+    return InternalFrame.create(
+        ordered_dataframe=left_frame,
+        data_column_snowflake_quoted_identifiers=left_frame_data_ids,
+        data_column_pandas_labels=frame.data_column_pandas_labels,
+        data_column_pandas_index_names=frame.data_column_pandas_index_names,
+        data_column_types=frame.cached_data_column_snowpark_pandas_types,
+        index_column_snowflake_quoted_identifiers=left_index_ids,
+        index_column_pandas_labels=left_result.index_column_pandas_labels,
+        index_column_types=left_result.cached_index_column_snowpark_pandas_types,
+    )
 
 
 def align_axis_0_right(
     frame: InternalFrame, other_frame: InternalFrame, join: str
-) -> tuple[InternalFrame, InternalFrame, list[str], list[str]]:
+) -> InternalFrame:
     """
     Gets the right align results.
 
@@ -61,11 +69,7 @@ def align_axis_0_right(
         join: type of alignment to be performed.
 
     Returns:
-        Tuple containing:
-            InternalFrame result of join_utils.align_on_index,
-            final right_frame,
-            list of right_frame_data_ids,
-            list of right_index_ids
+        New InternalFrame representing aligned right frame.
     """
     if join == "left":
         right_result, right_column_mapper = align_on_index(frame, other_frame, how=join)
@@ -96,14 +100,23 @@ def align_axis_0_right(
         right_frame = right_result.ordered_dataframe.select(
             right_frame_data_ids + right_index_ids
         )
-    return right_result, right_frame, right_frame_data_ids, right_index_ids
+
+    return InternalFrame.create(
+        ordered_dataframe=right_frame,
+        data_column_snowflake_quoted_identifiers=right_frame_data_ids,
+        data_column_pandas_labels=other_frame.data_column_pandas_labels,
+        data_column_pandas_index_names=other_frame.data_column_pandas_index_names,
+        data_column_types=other_frame.cached_data_column_snowpark_pandas_types,
+        index_column_snowflake_quoted_identifiers=right_index_ids,
+        index_column_pandas_labels=right_result.index_column_pandas_labels,
+        index_column_types=right_result.cached_index_column_snowpark_pandas_types,
+    )
 
 
 def align_axis_1(
     frame1: InternalFrame,
     frame2: InternalFrame,
     join: Literal["inner", "outer", "left", "right"],
-    sort: Optional[bool] = False,
 ) -> tuple[InternalFrame, InternalFrame]:
     """
     Concatenate frames on index axis by taking using UNION operator.
@@ -117,45 +130,49 @@ def align_axis_1(
             'outer': Output frame contains union of columns from both frames.
             'left': Output frame contains columns from left frame.
             'right': Output frame contains columns from right frame.
-        sort: Sort column axis if True.
 
     Returns:
-        New InternalFrame after taking union of given frames.
+        tuple representing aligned left and right InternalFrames.
     """
     columns1 = frame1.data_columns_index
     columns2 = frame2.data_columns_index
 
-    if join == "inner":
-        # Preserves the order from calling index.
-        # For example:
-        # pd.Index([3, 1, 2]).intersection(pd.Index([1, 2, 3]) will result in
-        # pd.Index([3, 1, 2])
-        data_column_labels = columns1.intersection(columns2, sort=False)
-    elif join == "outer":
-        # Preserves the order from calling index. And for labels not in calling index
-        # preserves the order from argument index.
-        # For example:
-        # pd.Index([3, 1, 2]).union(pd.Index([1, 4, 2, 3, 5]) will result in
-        # pd.Index([3, 1, 2, 4, 5])
-        data_column_labels = columns1.union(columns2, sort=False)
-    elif join == "left":
-        data_column_labels = columns1
+    inner_data_column_labels = columns1.intersection(columns2, sort=False).tolist()
+
+    frame1_data_column_pandas_labels = frame1.data_column_pandas_labels
+    frame2_data_column_pandas_labels = frame2.data_column_pandas_labels
+    full_data_column_pandas_labels = get_full_label_list(
+        frame1_data_column_pandas_labels,
+        frame2_data_column_pandas_labels,
+        inner_data_column_labels,
+        join=join,
+    )
+    if join != "right":
+        frame1 = align_axis_1_left_helper(
+            frame1, full_data_column_pandas_labels, frame2_data_column_pandas_labels
+        )
+        frame2 = align_axis_1_right_helper(
+            frame2, full_data_column_pandas_labels, frame2_data_column_pandas_labels
+        )
+
     elif join == "right":
-        data_column_labels = columns2
+        frame1 = align_axis_1_right_helper(
+            frame1, full_data_column_pandas_labels, frame1_data_column_pandas_labels
+        )
+        frame2 = align_axis_1_left_helper(
+            frame2, full_data_column_pandas_labels, frame1_data_column_pandas_labels
+        )
     else:
         raise AssertionError(
             f"Invalid join type '{join}'. Accepted values are 'inner', 'outer', 'left', and 'right'."
         )
-    if sort:
-        data_column_labels = data_column_labels.sort_values()
-
-    frame1 = _select_columns(frame1, data_column_labels.tolist())
-    frame2 = _select_columns(frame2, data_column_labels.tolist())
     return frame1, frame2
 
 
-def _select_columns(
-    frame: InternalFrame, data_column_labels: list[Hashable]
+def align_axis_1_left_helper(
+    frame: InternalFrame,
+    data_column_labels: list[str],
+    other_frame_labels: list[str],
 ) -> InternalFrame:
     """
     Select only the given labels from given frame for align. If any data column label is missing
@@ -164,46 +181,65 @@ def _select_columns(
     Args:
         frame: An InternalFrame
         data_column_labels: A list of pandas labels.
+        other_frame_labels: list of other frame labels
 
     Returns:
-        New InternalFrame after only with given data columns.
+        New InternalFrame representing left aligned frame.
 
     """
     select_list: list[ColumnOrName] = []
 
-    # Add index columns
+    # Add index and ordering columns
     select_list.extend(frame.index_column_snowflake_quoted_identifiers)
-
-    # Add ordering columns
     select_list.extend(frame.ordering_column_snowflake_quoted_identifiers)
+
+    data_column_snowflake_identifiers = []
+    other_counter = Counter(other_frame_labels)
 
     snowflake_ids = frame.get_snowflake_quoted_identifiers_group_by_pandas_labels(
         data_column_labels, include_index=False
     )
-    # Add data columns
-    data_column_snowflake_identifiers = []
-    # A map to keep track number of times a label is already seen.
-    # Native pandas fails with IndexError when either frame has duplicate labels, with
-    # the exception when both frames have exact same lables and exact same order.
-    # In Snowpark pandas, we don't fail concat when duplicates lables are present but
-    # try to match as many columns as possible from the frames.
-    label_count_map: dict[Hashable, int] = {}
+
+    curr_label_count_map: dict[Hashable, int] = {}
+    curr_label_index_map: dict[Hashable, int] = {}
     for label, id_tuple in zip(data_column_labels, snowflake_ids):
-        if len(id_tuple) <= label_count_map.get(label, 0):
-            # if missing add new column to frame with NULL values.
+        if (
+            curr_label_count_map.get(label, 0) > 0
+            and curr_label_count_map.get(label, 0) % other_counter[label] == 0
+        ):
+            curr_label_index_map[label] = curr_label_index_map.get(label, 0) + 1
+        index = curr_label_index_map.get(label, 0)
+
+        if len(id_tuple) == 0:
+            # if missing, add new column to frame with NULL values.
             snowflake_id = (
                 frame.ordered_dataframe.generate_snowflake_quoted_identifiers(
                     pandas_labels=[label]
                 )[0]
             )
             select_list.append(pandas_lit("nan").cast("float").as_(snowflake_id))
+
+        elif id_tuple[index] in data_column_snowflake_identifiers:
+            # if col already exists, copy col values to frame.
+            snowflake_id = id_tuple[index]
+            new_column_snowflake_quoted_id = (
+                frame.ordered_dataframe.generate_snowflake_quoted_identifiers(
+                    pandas_labels=[label],
+                    excluded=frame.data_column_snowflake_quoted_identifiers,
+                )[0]
+            )
+            select_list.append(col(snowflake_id).as_(new_column_snowflake_quoted_id))
+            snowflake_id = new_column_snowflake_quoted_id
+
+            i = curr_label_count_map.get(label, 0)
+            curr_label_count_map[label] = i + 1
         else:
-            index = label_count_map.get(label, 0)
             snowflake_id = id_tuple[index]
             select_list.append(snowflake_id)
-            label_count_map[label] = index + 1
-
+            i = curr_label_count_map.get(label, 0)
+            curr_label_count_map[label] = i + 1
         data_column_snowflake_identifiers.append(snowflake_id)
+
     return InternalFrame.create(
         ordered_dataframe=frame.ordered_dataframe.select(select_list),
         data_column_pandas_labels=data_column_labels,
@@ -214,3 +250,156 @@ def _select_columns(
         data_column_types=None,
         index_column_types=None,
     )
+
+
+def align_axis_1_right_helper(
+    frame: InternalFrame,
+    data_column_labels: list[str],
+    frame_labels: list[str],
+) -> InternalFrame:
+    """
+    Select only the given labels from given frame for align. If any data column label is missing
+    in frame add new column with NULL values.
+
+    Args:
+        frame: An InternalFrame
+        data_column_labels: A list of pandas labels.
+        frame_labels: list of frame labels
+
+    Returns:
+       New InternalFrame representing right aligned frame.
+
+    """
+    select_list: list[ColumnOrName] = []
+
+    # Add index and ordering columns
+    select_list.extend(frame.index_column_snowflake_quoted_identifiers)
+    select_list.extend(frame.ordering_column_snowflake_quoted_identifiers)
+
+    data_column_snowflake_identifiers = []
+    counter = Counter(frame_labels)
+
+    snowflake_ids = frame.get_snowflake_quoted_identifiers_group_by_pandas_labels(
+        data_column_labels, include_index=False
+    )
+
+    curr_label_count_map: dict[Hashable, int] = {}
+    curr_label_index_map: dict[Hashable, int] = {}
+    for label, id_tuple in zip(data_column_labels, snowflake_ids):
+        if (
+            curr_label_count_map.get(label, 0) > 0
+            and curr_label_count_map.get(label, 0) % counter[label] != 0
+        ):
+            curr_label_index_map[label] = curr_label_index_map.get(label, 0) + 1
+        else:
+            curr_label_index_map[label] = 0
+        index = curr_label_index_map.get(label, 0)
+        if len(id_tuple) == 0:
+            # if missing, add new column to frame with NULL values.
+            snowflake_id = (
+                frame.ordered_dataframe.generate_snowflake_quoted_identifiers(
+                    pandas_labels=[label]
+                )[0]
+            )
+            select_list.append(pandas_lit("nan").cast("float").as_(snowflake_id))
+
+        elif id_tuple[index] in data_column_snowflake_identifiers:
+            # if col already exists, copy col values to frame.
+            snowflake_id = id_tuple[index]
+            new_column_snowflake_quoted_id = (
+                frame.ordered_dataframe.generate_snowflake_quoted_identifiers(
+                    pandas_labels=[label],
+                    excluded=frame.data_column_snowflake_quoted_identifiers,
+                )[0]
+            )
+            select_list.append(col(snowflake_id).as_(new_column_snowflake_quoted_id))
+            snowflake_id = new_column_snowflake_quoted_id
+
+            i = curr_label_count_map.get(label, 0)
+            curr_label_count_map[label] = i + 1
+        else:
+            snowflake_id = id_tuple[index]
+            select_list.append(snowflake_id)
+            i = curr_label_count_map.get(label, 0)
+            curr_label_count_map[label] = i + 1
+        data_column_snowflake_identifiers.append(snowflake_id)
+
+    return InternalFrame.create(
+        ordered_dataframe=frame.ordered_dataframe.select(select_list),
+        data_column_pandas_labels=data_column_labels,
+        data_column_snowflake_quoted_identifiers=data_column_snowflake_identifiers,
+        data_column_pandas_index_names=frame.data_column_pandas_index_names,
+        index_column_pandas_labels=frame.index_column_pandas_labels,
+        index_column_snowflake_quoted_identifiers=frame.index_column_snowflake_quoted_identifiers,
+        data_column_types=None,
+        index_column_types=None,
+    )
+
+
+def get_full_label_list(
+    frame1_labels: list[str],
+    frame2_labels: list[str],
+    inner_data_column_labels: list[str],
+    join: str,
+) -> list[str]:
+    """
+    Gets the final aligned frame labels.
+
+    Args:
+        frame1_labels: list of frame1 labels.
+        frame2_labels:list of frame2 labels
+        inner_data_column_labels: intersection of frame1 and frame2 labels.
+        join: Type of alignment to be performed.
+            left: use only keys from left frame, preserve key order.
+            right: use only keys from right frame, preserve key order.
+            outer: use union of keys from both frames, sort keys lexicographically.
+            inner: use intersection of keys from both frames, preserve the order of the left keys.
+
+    Returns:
+        List of the final frame labels.
+    """
+    count1 = Counter(frame1_labels)
+    count2 = Counter(frame2_labels)
+
+    result_list = []
+
+    # final label list is similar to a cross join of frame1 and frame 2 column labels. For ex,
+    # if frame1 has cols ["D", "B", "C", "A", "B", "A", "E"] and frame2 has cols ["A", "B", "B", "C", "D", "A", "A"],
+    # result_list for join="outer" is ["A", "A", "A", "A", "A", "A", "B", "B", "B", "B", "C", "D", "E"]
+
+    if join == "inner":
+        # Add elements from frame1 to result_list, based on frequency in frame2 if element is in intersection list
+        for item1 in frame1_labels:
+            if item1 in inner_data_column_labels:
+                result_list.extend([item1] * count2[item1])
+
+    elif join == "outer":
+        # Add elements from frame1 to result_list, based on frequency in frame2. Add remaining items from frame2 and
+        # then sort.
+        for item1 in frame1_labels:
+            if item1 in frame2_labels:
+                result_list.extend([item1] * count2[item1])
+            else:
+                result_list.extend([item1])
+        for item2 in frame2_labels:
+            if item2 not in frame1_labels:
+                result_list.extend([item2])
+        result_list.sort()
+
+    elif join == "left":
+        # Add elements from frame1 to result_list, based on frequency in frame2.
+        for item1 in frame1_labels:
+            if item1 in frame2_labels:
+                result_list.extend([item1] * count2[item1])
+            else:
+                result_list.extend([item1])
+
+    elif join == "right":
+        # Add elements from frame2 to result_list, based on frequency in frame1.
+        for item2 in frame2_labels:
+            if item2 in frame1_labels:
+                result_list.extend([item2] * count1[item2])
+            else:
+                result_list.extend([item2])
+
+    return result_list
