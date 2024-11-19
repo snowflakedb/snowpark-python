009df7218dbd916cfabba382c550799083e485dd
Afroz Alam <afroz.alam@snowflake.com>
2024-11-19 14:35:13 -0800

    NO-SNOW: Fix daily precommit test (#2643)

--- tests/integ/test_multithreading.py
+++ tests/integ/test_multithreading.py
@@ -56,7 +56,7 @@ from tests.utils import (
 )
 
 
-@pytest.fixture(scope="module")
+@pytest.fixture(scope="function")
 def threadsafe_session(
     db_parameters,
     session,
@@ -684,7 +684,7 @@ def test_concurrent_update_on_sensitive_configs(
     run=False,
 )
 def test_large_query_breakdown_with_cte(threadsafe_session):
-    bounds = (300, 600) if threadsafe_session.sql_simplifier_enabled else (60, 90)
+    bounds = (300, 600) if threadsafe_session.sql_simplifier_enabled else (50, 70)
     try:
         original_query_compilation_stage_enabled = (
             threadsafe_session._query_compilation_stage_enabled
@@ -763,8 +763,7 @@ def test_large_query_breakdown_with_cte(threadsafe_session):
 
             assert len(queries["queries"]) == 2
             assert queries["queries"][0].startswith("CREATE  SCOPED TEMPORARY  TABLE")
-            if threadsafe_session.sql_simplifier_enabled:
-                assert queries["queries"][1].startswith("WITH SNOWPARK_TEMP_CTE_")
+            assert queries["queries"][1].startswith("WITH SNOWPARK_TEMP_CTE_")
 
             assert len(queries["post_actions"]) == 1
             assert queries["post_actions"][0].startswith("DROP  TABLE  If  EXISTS")
