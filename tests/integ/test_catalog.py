fc6a2d789a90127071855b400fc342bb16be942e
Afroz Alam <afroz.alam@snowflake.com>
2024-11-20 21:21:04 -0800

    add tests for get and set

--- tests/integ/test_catalog.py
+++ tests/integ/test_catalog.py
@@ -6,7 +6,8 @@ import uuid
 import pytest
 
 from snowflake.snowpark.catalog import Catalog, Column
-from snowflake.snowpark.types import LongType, StringType
+from snowflake.snowpark.session import Session
+from snowflake.snowpark.types import IntegerType, LongType, StringType
 
 pytestmark = [
     pytest.mark.xfail(
@@ -61,10 +62,10 @@ def temp_schema1(session, temp_db1):
 
 
 @pytest.fixture(scope="function")
-def temp_schema2(session, temp_db1):
-    temp_schema = create_temp_schema(session, temp_db1)
+def temp_schema2(session, temp_db2):
+    temp_schema = create_temp_schema(session, temp_db2)
     yield temp_schema
-    session._run_query(f"drop schema if exists {temp_db1}.{temp_schema}")
+    session._run_query(f"drop schema if exists {temp_db2}.{temp_schema}")
 
 
 def create_temp_table(session, db: str, schema: str) -> str:
@@ -89,34 +90,85 @@ def temp_table2(session, temp_db1, temp_schema1):
     session._run_query(f"drop table if exists {temp_db1}.{temp_schema1}.{temp_table}")
 
 
-@pytest.fixture(scope="function")
-def temp_view1(session):
-    pass
+def create_temp_view(session, db: str, schema: str) -> str:
+    temp_schema = get_temp_name("SCHEMA")
+    session._run_query(
+        f"create or replace temp view {db}.{schema}.{temp_schema} as select 1 as a, '2' as b"
+    )
+    return temp_schema
 
 
 @pytest.fixture(scope="function")
-def temp_view2(session):
-    pass
+def temp_view1(session, temp_db1, temp_schema1):
+    temp_view = create_temp_view(session, temp_db1, temp_schema1)
+    yield temp_view
+    session._run_query(f"drop view if exists {temp_db1}.{temp_schema1}.{temp_view}")
 
 
 @pytest.fixture(scope="function")
-def temp_procedure1(session):
-    pass
+def temp_view2(session, temp_db1, temp_schema1):
+    temp_view = create_temp_view(session, temp_db1, temp_schema1)
+    yield temp_view
+    session._run_query(f"drop view if exists {temp_db1}.{temp_schema1}.{temp_view}")
+
+
+def create_temp_procedure(session: Session, db, schema) -> str:
+    temp_procedure = get_temp_name("PROCEDURE")
+    session.sproc.register(
+        lambda _, x: x + 1,
+        return_type=IntegerType(),
+        input_types=[IntegerType()],
+        name=f"{db}.{schema}.{temp_procedure}",
+        packages=["snowflake-snowpark-python"],
+    )
+    return temp_procedure
 
 
 @pytest.fixture(scope="function")
-def temp_procedure2(session):
-    pass
+def temp_procedure1(session, temp_db1, temp_schema1):
+    temp_procedure = create_temp_procedure(session, temp_db1, temp_schema1)
+    yield temp_procedure
+    session._run_query(
+        f"drop procedure if exists {temp_db1}.{temp_schema1}.{temp_procedure}(int)"
+    )
 
 
 @pytest.fixture(scope="function")
-def temp_udf1(session):
-    pass
+def temp_procedure2(session, temp_db1, temp_schema1):
+    temp_procedure = create_temp_procedure(session, temp_db1, temp_schema1)
+    yield temp_procedure
+    session._run_query(
+        f"drop procedure if exists {temp_db1}.{temp_schema1}.{temp_procedure}(int)"
+    )
+
+
+def create_temp_udf(session: Session, db, schema) -> str:
+    temp_udf = get_temp_name("UDF")
+    session.udf.register(
+        lambda x: x + 1,
+        return_type=IntegerType(),
+        input_types=[IntegerType()],
+        name=f"{db}.{schema}.{temp_udf}",
+    )
+    return temp_udf
 
 
 @pytest.fixture(scope="function")
-def temp_udf2(session):
-    pass
+def temp_udf1(session, temp_db1, temp_schema1):
+    temp_udf = create_temp_udf(session, temp_db1, temp_schema1)
+    yield temp_udf
+    session._run_query(
+        f"drop function if exists {temp_db1}.{temp_schema1}.{temp_udf}(int)"
+    )
+
+
+@pytest.fixture(scope="function")
+def temp_udf2(session, temp_db1, temp_schema1):
+    temp_udf = create_temp_udf(session, temp_db1, temp_schema1)
+    yield temp_udf
+    session._run_query(
+        f"drop function if exists {temp_db1}.{temp_schema1}.{temp_udf}(int)"
+    )
 
 
 def test_list_db(session, temp_db1, temp_db2):
@@ -158,12 +210,141 @@ def test_list_tables(session, temp_db1, temp_schema1, temp_table1, temp_table2):
     assert cols == [Column("A", LongType(), True), Column("B", StringType(), True)]
 
 
-def test_get_methods(session):
-    pass
+def test_list_views(session, temp_db1, temp_schema1, temp_view1, temp_view2):
+    catalog: Catalog = session.catalog
+
+    assert len(catalog.list_views(pattern="does_not_exist_*")) == 0
+    assert (
+        len(
+            catalog.list_views(
+                pattern="does_not_exist_*", database=temp_db1, schema=temp_schema1
+            )
+        )
+        == 0
+    )
+
+    view_list = catalog.list_views(database=temp_db1, schema=temp_schema1)
+    assert {view.name for view in view_list} == {temp_view1, temp_view2}
 
 
-def test_set_methods(session):
-    pass
+@pytest.mark.xfail(reason="SNOW-1787268: Bug in snowflake api functions iter")
+def test_list_functions(session):
+    catalog: Catalog = session.catalog
+
+    assert len(catalog.list_functions(pattern="does_not_exist_*")) == 0
+    seq_functions = catalog.list_functions(pattern="seq_*")
+    assert {func.name for func in seq_functions} == {"seq0", "seq2"}
+
+
+def test_list_procedures(
+    session, temp_db1, temp_schema1, temp_procedure1, temp_procedure2
+):
+    catalog: Catalog = session.catalog
+
+    assert len(catalog.list_procedures(pattern="does_not_exist_*")) == 0
+    assert (
+        len(
+            catalog.list_procedures(
+                pattern="does_not_exist_*", database=temp_db1, schema=temp_schema1
+            )
+        )
+        == 0
+    )
+
+    procedure_list = catalog.list_procedures(
+        database=temp_db1,
+        schema=temp_schema1,
+        pattern=f"{CATALOG_TEMP_OBJECT_PREFIX}_PROCEDURE_*",
+    )
+    assert {procedure.name for procedure in procedure_list} == {
+        temp_procedure1,
+        temp_procedure2,
+    }
+
+
+@pytest.mark.xfail(reason="SNOW-1787268: Bug in snowflake api functions iter")
+def test_list_udfs(session, temp_db1, temp_schema1, temp_udf1, temp_udf2):
+    catalog: Catalog = session.catalog
+
+    assert len(catalog.list_functions(pattern="does_not_exist_*")) == 0
+    assert (
+        len(
+            catalog.list_functions(
+                pattern="does_not_exist_*", database=temp_db1, schema=temp_schema1
+            )
+        )
+        == 0
+    )
+    udf_list = catalog.list_functions(
+        database=temp_db1,
+        schema=temp_schema1,
+        pattern=f"{CATALOG_TEMP_OBJECT_PREFIX}_UDF_*",
+    )
+    assert {udf.name for udf in udf_list} == {temp_udf1, temp_udf2}
+
+
+def test_get_db_schema(session):
+    catalog: Catalog = session.catalog
+    current_db = session.get_current_database()
+    current_schema = session.get_current_schema()
+    assert catalog.get_current_database().name == current_db
+    assert catalog.get_current_schema().name == current_schema
+
+
+def test_get_table_view(session, temp_db1, temp_schema1, temp_table1, temp_view1):
+    catalog: Catalog = session.catalog
+    table = catalog.get_table(temp_table1, database=temp_db1, schema=temp_schema1)
+    assert table.name == temp_table1
+    assert table.database_name == temp_db1
+    assert table.schema_name == temp_schema1
+
+    view = catalog.get_view(temp_view1, database=temp_db1, schema=temp_schema1)
+    assert view.name == temp_view1
+    assert view.database_name == temp_db1
+    assert view.schema_name == temp_schema1
+
+
+def test_get_function_procedure_udf(
+    session, temp_db1, temp_schema1, temp_procedure1, temp_udf1
+):
+    catalog: Catalog = session.catalog
+
+    # function = catalog.get_function("seq1", [])
+    # assert function.name == "seq1"
+
+    procedure = catalog.get_procedure(
+        temp_procedure1, [IntegerType()], database=temp_db1, schema=temp_schema1
+    )
+    assert procedure.name == temp_procedure1
+    assert procedure.database_name == temp_db1
+    assert procedure.schema_name == temp_schema1
+
+    udf = catalog.get_user_defined_function(
+        temp_udf1, [IntegerType()], database=temp_db1, schema=temp_schema1
+    )
+    assert udf.name == temp_udf1
+    assert udf.database_name == temp_db1
+    assert udf.schema_name == temp_schema1
+
+
+def test_set_db_schema(session, temp_db1, temp_db2, temp_schema1, temp_schema2):
+    catalog = session.catalog
+
+    original_db = session.get_current_database()
+    original_schema = session.get_current_schema()
+    try:
+        catalog.set_current_database(temp_db1)
+        catalog.set_current_schema(temp_schema1)
+        assert session.get_current_database() == f'"{temp_db1}"'
+        assert session.get_current_schema() == f'"{temp_schema1}"'
+
+        catalog.set_current_database(temp_db2)
+        catalog.set_current_schema(temp_schema2)
+        assert session.get_current_database() == f'"{temp_db2}"'
+        assert session.get_current_schema() == f'"{temp_schema2}"'
+    finally:
+        session.use_database(original_db)
+        session.use_schema(original_schema)
 
 
 def test_exists_methods(session):
