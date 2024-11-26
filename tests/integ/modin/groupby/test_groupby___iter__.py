e85e6fbd770de0374b00f82b029b49edf42eef7a
Hazem Elmeleegy <hazem.elmeleegy@snowflake.com>
2024-11-25 20:26:47 -0800

    address comments

--- tests/integ/modin/groupby/test_groupby___iter__.py
+++ tests/integ/modin/groupby/test_groupby___iter__.py
@@ -7,31 +7,34 @@ import pytest
 
 import snowflake.snowpark.modin.plugin  # noqa: F401
 from tests.integ.modin.utils import assert_frame_equal, assert_series_equal
-from tests.integ.utils.sql_counter import sql_count_checker
+from tests.integ.utils.sql_counter import SqlCounter, sql_count_checker
 
 
-@sql_count_checker(query_count=3, join_count=4)
-@pytest.mark.parametrize("by", ["a", ["a"]])
-def test_frame_groupby___iter__(by):
+@sql_count_checker(query_count=4, join_count=6)
+@pytest.mark.parametrize(
+    "by, expected_query_count, expected_join_count",
+    [
+        ("a", 3, 4),
+        (["a"], 3, 4),
+        (["a", "b"], 4, 6),
+    ],
+)
+def test_frame_groupby___iter__(by, expected_query_count, expected_join_count):
     data = [[1, 2, 3], [1, 5, 6], [7, 8, 9]]
     native_df = native_pd.DataFrame(data, columns=["a", "b", "c"])
     snow_df = pd.DataFrame(native_df)
 
-    native_result = []
-    for x, y in native_df.groupby(by=by):
-        native_result.append([x, y])
+    with SqlCounter(query_count=expected_query_count, join_count=expected_join_count):
+        native_result = list(iter(native_df.groupby(by=by)))
+        snow_result = list(iter(snow_df.groupby(by=by)))
 
-    snow_result = []
-    for x, y in snow_df.groupby(by=by):
-        snow_result.append([x, y])
-
-    # check that sizes are identical
-    assert len(snow_result) == len(native_result)
-    for i in range(len(snow_result)):
-        # check that corresponding keys are identical
-        assert snow_result[i][0] == native_result[i][0]
-        # check that corresponding frames are identical
-        assert_frame_equal(snow_result[i][1], native_result[i][1])
+        # check that sizes are identical
+        assert len(snow_result) == len(native_result)
+        for i in range(len(snow_result)):
+            # check that corresponding keys are identical
+            assert snow_result[i][0] == native_result[i][0]
+            # check that corresponding frames are identical
+            assert_frame_equal(snow_result[i][1], native_result[i][1])
 
 
 @sql_count_checker(query_count=3, join_count=4)
@@ -40,13 +43,8 @@ def test_series_groupby___iter__():
     native_ser = native_pd.Series([1, 2, 3], index=lst)
     snow_ser = pd.Series(native_ser)
 
-    native_result = []
-    for x, y in native_ser.groupby(level=0):
-        native_result.append([x, y])
-
-    snow_result = []
-    for x, y in snow_ser.groupby(level=0):
-        snow_result.append([x, y])
+    native_result = list(iter(native_ser.groupby(level=0)))
+    snow_result = list(iter(snow_ser.groupby(level=0)))
 
     # check that sizes are identical
     assert len(snow_result) == len(native_result)
