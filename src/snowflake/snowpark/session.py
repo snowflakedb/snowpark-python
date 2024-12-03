ed5c17f41e7428c33133db178c05781d079efebf
Varnika Budati <varnika.budati@snowflake.com>
2024-12-03 09:36:33 -0800

    SNOW-1842532 Post-merge artifact removal (#2697)
    1. Which Jira issue is this PR addressing? Make sure that there is an
    accompanying issue to your PR.
       Fixes SNOW-1842532
    
    2. Fill out the following pre-review checklist:
    
    - [ ] I am adding a new automated test(s) to verify correctness of my
    new code
    - [ ] If this test skips Local Testing mode, I'm requesting review from
    @snowflakedb/local-testing
       - [ ] I am adding new logging messages
       - [ ] I am adding a new telemetry message
       - [ ] I am adding new credentials
       - [ ] I am adding a new dependency
    - [ ] If this is a new feature/behavior, I'm adding the Local Testing
    parity changes.
    - [x] I acknowledge that I have ensured my changes to be thread-safe.
    Follow the link for more information: [Thread-safe Developer
    Guidelines](https://github.com/snowflakedb/snowpark-python/blob/main/CONTRIBUTING.md#thread-safe-development)
    
    3. Please describe how your code solves the related issue.
    
    Removed an unnecessary comment, removed the example from the docstring
    of `auto_clean_up_temp_table_enabled` which was a merge artifact. See
    https://github.com/snowflakedb/snowpark-python/pull/2457/files.

--- src/snowflake/snowpark/session.py
+++ src/snowflake/snowpark/session.py
@@ -818,28 +818,6 @@ class Session:
         :meth:`DataFrame.cache_result` in the current session when the DataFrame is no longer referenced (i.e., gets garbage collected).
         The default value is ``False``.
 
-        Example::
-
-            >>> import gc
-            >>>
-            >>> def f(session: Session) -> str:
-            ...     df = session.create_dataframe(
-            ...         [[1, 2], [3, 4]], schema=["a", "b"]
-            ...     ).cache_result()
-            ...     return df.table_name
-            ...
-            >>> session.auto_clean_up_temp_table_enabled = True
-            >>> table_name = f(session)
-            >>> assert table_name
-            >>> gc.collect() # doctest: +SKIP
-            >>>
-            >>> # The temporary table created by cache_result will be dropped when the DataFrame is no longer referenced
-            >>> # outside the function
-            >>> session.sql(f"show tables like '{table_name}'").count()
-            0
-
-            >>> session.auto_clean_up_temp_table_enabled = False
-
         Note:
             Temporary tables will only be dropped if this parameter is enabled during garbage collection.
             If a temporary table is no longer referenced when the parameter is on, it will be dropped during garbage collection.
