c5c430e89058802d5808061736d63b115bb35fa3
Adam Ling <adam.ling@snowflake.com>
2024-12-03 20:55:53 -0800

    put at the end

--- tests/conftest.py
+++ tests/conftest.py
@@ -58,17 +58,13 @@ def is_excluded_frontend_file(path):
     return False
 
 
-def pytest_addoption(parser):
+def pytest_addoption(parser, pluginmanager):
     parser.addoption("--disable_sql_simplifier", action="store_true", default=False)
     parser.addoption("--disable_cte_optimization", action="store_true", default=False)
     parser.addoption(
         "--disable_multithreading_mode", action="store_true", default=False
     )
     parser.addoption("--skip_sql_count_check", action="store_true", default=False)
-    if not any(
-        "--local_testing_mode" in opt.names() for opt in parser._anonymous.options
-    ):
-        parser.addoption("--local_testing_mode", action="store_true", default=False)
     parser.addoption("--enable_ast", action="store_true", default=False)
     parser.addoption("--validate_ast", action="store_true", default=False)
     parser.addoption(
@@ -78,6 +74,10 @@ def pytest_addoption(parser):
         type=str,
         help="Path to the Unparser JAR built in the monorepo. To build it, run `sbt assembly` from the unparser directory.",
     )
+    if not any(
+        "--local_testing_mode" in opt.names() for opt in parser._anonymous.options
+    ):
+        parser.addoption("--local_testing_mode", action="store_true", default=False)
 
 
 def pytest_collection_modifyitems(items) -> None:
