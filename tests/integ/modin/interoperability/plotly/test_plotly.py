ee54d7b6206da9b06bdd8eb5ffb6884a2f5a5565
Labanya Mukhopadhyay <labanya.mukhopadhyay@snowflake.com>
2024-12-13 01:04:10 -0800

    update doc and test changes from review
    Signed-off-by: Labanya Mukhopadhyay <labanya.mukhopadhyay@snowflake.com>

--- tests/integ/modin/interoperability/plotly/test_plotly.py
+++ tests/integ/modin/interoperability/plotly/test_plotly.py
@@ -6,18 +6,24 @@ import modin.pandas as pd
 import numpy as np
 import plotly.express as px
 import pytest
+import pandas as native_pd
 
 import snowflake.snowpark.modin.plugin  # noqa: F401
-from tests.integ.utils.sql_counter import sql_count_checker, SqlCounter
+from tests.integ.utils.sql_counter import sql_count_checker
+from tests.integ.modin.utils import eval_snowpark_pandas_result
 
 # Integration tests for plotly.express module (https://plotly.com/python-api-reference/plotly.express.html).
 # To add tests for additional APIs,
-#     - Call the API with Snowpark pandas and native pandas df input and get the JSON representation with
+#     - Call the method with Snowpark pandas and native pandas df input and get the JSON representation with
 #     `to_plotly_json()`.
 #     - Assert correctness of the plot produced using `assert_plotly_equal` function defined below.
 
 
 def assert_plotly_equal(expect, got):
+    # referenced from cudf plotly integration test
+    # https://github.com/rapidsai/cudf/blob/main/python/cudf/cudf_pandas_tests/third_party_integration_tests/tests/
+    # test_plotly.py#L10
+
     assert type(expect) == type(got)
     if isinstance(expect, dict):
         assert expect.keys() == got.keys()
@@ -36,151 +42,172 @@ def assert_plotly_equal(expect, got):
         assert expect == got
 
 
-@pytest.fixture(scope="module")
-def df():
+@pytest.fixture()
+def test_dfs():
     nsamps = 50
     rng = np.random.default_rng(seed=42)
-    return pd.DataFrame(
-        {
-            "x": rng.random(nsamps),
-            "y": rng.random(nsamps),
-            "category": rng.integers(0, 5, nsamps),
-            "category2": rng.integers(0, 5, nsamps),
-        }
+    data = {
+        "x": rng.random(nsamps),
+        "y": rng.random(nsamps),
+        "category": rng.integers(0, 5, nsamps),
+        "category2": rng.integers(0, 5, nsamps),
+    }
+    snow_df = pd.DataFrame(data)
+    native_df = native_pd.DataFrame(data)
+    return snow_df, native_df
+
+
+@sql_count_checker(query_count=1)
+def test_scatter(test_dfs):
+    # test_dfs = dfs()
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.scatter(df, x="x", y="y").to_plotly_json(),
+        comparator=assert_plotly_equal
     )
 
 
-@sql_count_checker(query_count=2)
-def test_scatter(df):
-    snow_res = px.scatter(df, x="x", y="y").to_plotly_json()
-    native_res = px.scatter(df._to_pandas(), x="x", y="y").to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+@sql_count_checker(query_count=1)
+def test_line(test_dfs):
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.line(df, x="category", y="y").to_plotly_json(),
+        comparator=assert_plotly_equal
+    )
 
 
-@sql_count_checker(query_count=2)
-def test_line(df):
-    snow_res = px.line(df, x="category", y="y").to_plotly_json()
-    native_res = px.line(df._to_pandas(), x="category", y="y").to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+@sql_count_checker(query_count=1)
+def test_area(test_dfs):
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.area(df, x="category", y="y").to_plotly_json(),
+        comparator=assert_plotly_equal
+    )
 
 
-@sql_count_checker(query_count=2)
-def test_area(df):
-    snow_res = px.area(df, x="category", y="y").to_plotly_json()
-    native_res = px.area(df._to_pandas(), x="category", y="y").to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
-
-
-@sql_count_checker(query_count=2)
+@sql_count_checker(query_count=1)
 def test_timeline():
-    df = pd.DataFrame(
+    native_df = native_pd.DataFrame(
         [
             dict(Task="Job A", Start="2009-01-01", Finish="2009-02-28"),
             dict(Task="Job B", Start="2009-03-05", Finish="2009-04-15"),
             dict(Task="Job C", Start="2009-02-20", Finish="2009-05-30"),
         ]
     )
-    snow_res = px.timeline(
-        df, x_start="Start", x_end="Finish", y="Task"
-    ).to_plotly_json()
-    native_res = px.timeline(
-        df._to_pandas(), x_start="Start", x_end="Finish", y="Task"
-    ).to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+    snow_df = pd.DataFrame(native_df)
+    eval_snowpark_pandas_result(
+        snow_df,
+        native_df,
+        lambda df: px.timeline(
+            df, x_start="Start", x_end="Finish", y="Task"
+        ).to_plotly_json(),
+        comparator=assert_plotly_equal,
+    )
 
 
-@sql_count_checker(query_count=2)
-def test_violin(df):
-    snow_res = px.violin(df, y="y").to_plotly_json()
-    native_res = px.violin(df._to_pandas(), y="y").to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+@sql_count_checker(query_count=1)
+def test_violin(test_dfs):
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.violin(df, y="y").to_plotly_json(),
+        comparator=assert_plotly_equal
+    )
 
 
-@sql_count_checker(query_count=2)
-def test_bar(df):
-    snow_res = px.bar(df, x="category", y="y").to_plotly_json()
-    native_res = px.bar(df._to_pandas(), x="category", y="y").to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+@sql_count_checker(query_count=1)
+def test_bar(test_dfs):
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.bar(df, x="category", y="y").to_plotly_json(),
+        comparator=assert_plotly_equal
+    )
 
 
-@sql_count_checker(query_count=2)
-def test_histogram(df):
-    snow_res = px.histogram(df, x="category").to_plotly_json()
-    native_res = px.histogram(df._to_pandas(), x="category").to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+@sql_count_checker(query_count=1)
+def test_histogram(test_dfs):
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.histogram(df, x="category").to_plotly_json(),
+        comparator=assert_plotly_equal
+    )
 
 
-@sql_count_checker(query_count=2)
-def test_pie(df):
-    snow_res = px.pie(df, values="category", names="category2").to_plotly_json()
-    native_res = px.pie(
-        df._to_pandas(), values="category", names="category2"
-    ).to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+@sql_count_checker(query_count=1)
+def test_pie(test_dfs):
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.pie(df, values="category", names="category2").to_plotly_json(),
+        comparator=assert_plotly_equal
+    )
 
 
-@sql_count_checker(query_count=2)
-def test_treemap(df):
-    snow_res = px.treemap(df, names="category", values="y").to_plotly_json()
-    native_res = px.treemap(
-        df._to_pandas(), names="category", values="y"
-    ).to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+@sql_count_checker(query_count=1)
+def test_treemap(test_dfs):
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.treemap(df, names="category", values="y").to_plotly_json(),
+        comparator=assert_plotly_equal
+    )
 
 
-@sql_count_checker(query_count=2)
-def test_sunburst(df):
-    snow_res = px.sunburst(df, names="category", values="y").to_plotly_json()
-    native_res = px.sunburst(
-        df._to_pandas(), names="category", values="y"
-    ).to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+@sql_count_checker(query_count=1)
+def test_sunburst(test_dfs):
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.sunburst(df, names="category", values="y").to_plotly_json(),
+        comparator=assert_plotly_equal
+    )
 
 
-@sql_count_checker(query_count=2)
-def test_icicle(df):
-    snow_res = px.icicle(df, names="category", values="y").to_plotly_json()
-    native_res = px.icicle(
-        df._to_pandas(), names="category", values="y"
-    ).to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+@sql_count_checker(query_count=1)
+def test_icicle(test_dfs):
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.icicle(df, names="category", values="y").to_plotly_json(),
+        comparator=assert_plotly_equal
+    )
 
 
-@sql_count_checker(query_count=2)
-def test_scatter_matrix(df):
-    snow_res = px.scatter_matrix(df, dimensions=["category"]).to_plotly_json()
-    native_res = px.scatter_matrix(
-        df._to_pandas(), dimensions=["category"]
-    ).to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+@sql_count_checker(query_count=1)
+def test_scatter_matrix(test_dfs):
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.scatter_matrix(df, dimensions=["category"]).to_plotly_json(),
+        comparator=assert_plotly_equal
+    )
 
 
-@sql_count_checker(query_count=2)
-def test_funnel(df):
-    snow_res = px.funnel(df, x="x", y="y").to_plotly_json()
-    native_res = px.funnel(df._to_pandas(), x="x", y="y").to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+@sql_count_checker(query_count=1)
+def test_funnel(test_dfs):
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.funnel(df, x="x", y="y").to_plotly_json(),
+        comparator=assert_plotly_equal
+    )
 
 
-@sql_count_checker(query_count=2)
-def test_density_heatmap(df):
-    snow_res = px.density_heatmap(df, x="x", y="y").to_plotly_json()
-    native_res = px.density_heatmap(df._to_pandas(), x="x", y="y").to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+@sql_count_checker(query_count=1)
+def test_density_heatmap(test_dfs):
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.density_heatmap(df, x="x", y="y").to_plotly_json(),
+        comparator=assert_plotly_equal
+    )
 
 
-@sql_count_checker(query_count=2)
-def test_box(df):
-    snow_res = px.box(df, x="category", y="y").to_plotly_json()
-    native_res = px.box(df._to_pandas(), x="category", y="y").to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+@sql_count_checker(query_count=1)
+def test_box(test_dfs):
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.box(df, x="category", y="y").to_plotly_json(),
+        comparator=assert_plotly_equal
+    )
 
 
-def test_imshow(df):
-    df = pd.DataFrame([[1, 3], [4, 5], [7, 2]], columns=["a", "b"])
-    with SqlCounter(query_count=4):
-        snow_res = px.imshow(df, x=df.columns, y=df.index).to_plotly_json()
-    native_res = px.imshow(
-        df._to_pandas(), x=df._to_pandas().columns, y=df._to_pandas().index
-    ).to_plotly_json()
-    assert_plotly_equal(snow_res, native_res)
+@sql_count_checker(query_count=4)
+def test_imshow(test_dfs):
+    eval_snowpark_pandas_result(
+        *test_dfs,
+        lambda df: px.imshow(df, x=df.columns, y=df.index).to_plotly_json(),
+        comparator=assert_plotly_equal
+    )
