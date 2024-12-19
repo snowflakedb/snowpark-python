8318bca3781559c8a7f8c7891922edd360e8886c
Yijun Xie <yijun.xie@snowflake.com>
2024-12-18 18:22:31 -0800

    Fix linting

--- src/snowflake/snowpark/_internal/utils.py
+++ src/snowflake/snowpark/_internal/utils.py
@@ -209,7 +209,7 @@ def _pandas_importer():  # noqa: E302
         pandas = importlib.import_module("pandas")
         # since we enable relative imports without dots this import gives us an issues when ran from test directory
         from pandas import DataFrame  # NOQA
-    except ImportError as e:
+    except ImportError:
         pass
     return pandas
 
