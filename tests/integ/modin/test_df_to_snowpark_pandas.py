5f8e146af7bbd619f7556a05a1ca3502b0bfc37f
Afroz Alam <afroz.alam@snowflake.com>
2025-01-07 13:51:23 -0800

    fix sql counts

--- tests/integ/modin/test_df_to_snowpark_pandas.py
+++ tests/integ/modin/test_df_to_snowpark_pandas.py
@@ -73,7 +73,7 @@ def test_to_snowpark_pandas_basic(session, tmp_table_basic, index_col, columns) 
 
 
 @multithreaded_run()
-@sql_count_checker(query_count=15)
+@sql_count_checker(query_count=3)
 def test_to_snowpark_pandas_from_views(session, tmp_table_basic) -> None:
     snowpark_df = session.sql(
         f"SELECT ID, SHOE_MODEL FROM {tmp_table_basic} WHERE ID > 1"
