57f828a00bb1fbb38c4edce0839e8cbe0f15a349
Hazem Elmeleegy <hazem.elmeleegy@snowflake.com>
2025-01-14 17:31:46 -0800

    address comments

--- tests/integ/modin/series/test_pop.py
+++ tests/integ/modin/series/test_pop.py
@@ -8,6 +8,7 @@
 
 import modin.pandas as pd
 import pandas as native_pd
+import pytest
 
 from tests.integ.modin.utils import assert_series_equal
 from tests.integ.utils.sql_counter import sql_count_checker
@@ -24,3 +25,13 @@ def test_pop():
 
     assert snow_popped_val == native_popped_val
     assert_series_equal(snow_ser, native_ser)
+
+
+@sql_count_checker(query_count=2, join_count=1)
+def test_pop_not_found():
+    native_ser = native_pd.Series([1, 2, 3])
+    snow_ser = pd.Series(native_ser)
+    assert isinstance(snow_ser, pd.Series)
+
+    with pytest.raises(KeyError):
+        snow_ser.pop(-1)
