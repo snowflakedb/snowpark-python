62e22be05e6184c9d50729e16c8621314ca515bf
Yuyang Wang <yuyang.wang@snowflake.com>
2025-01-30 15:48:05 -0800

    add bit shift right unsigned

--- src/snowflake/snowpark/functions.py
+++ src/snowflake/snowpark/functions.py
@@ -685,6 +685,31 @@ def bitshiftleft(
 
 
 @publicapi
+def bitshiftrightunsigned(
+    to_shift_column: ColumnOrName, n: Union[Column, int], _emit_ast: bool = True
+) -> Column:
+    """Returns the bitwise negation of a numeric expression.
+
+    Example:
+        >>> df = session.createDataFrame([(-1999)], ['a'])
+        >>> df.select(bitshiftrightunsigned('a', 1)).collect()[0][0]
+        9223372036854774808
+
+        >>> df = session.createDataFrame([(42)], ['a'])
+        >>> df.select(bitshiftrightunsigned('a', 1)).collect()[0][0]
+        21
+
+        >>> df = session.createDataFrame([(-21)], ['a'])
+        >>> df.select(bitshiftrightunsigned('a', 1)).collect()[0][0]
+        9223372036854775797
+    """
+    c = _to_col_if_str(to_shift_column, "bitshiftrightunsigned")
+    max_bit = bitshiftleft(lit(1), 64)
+    unsigned_c = iff(c < 0, bitshiftright(c + max_bit, n), bitshiftright(c, n))
+    return call_builtin("bitand", unsigned_c, max_bit - 1, _emit_ast=_emit_ast)
+
+
+@publicapi
 def bitshiftright(
     to_shift_column: ColumnOrName, n: Union[Column, int], _emit_ast: bool = True
 ) -> Column:
