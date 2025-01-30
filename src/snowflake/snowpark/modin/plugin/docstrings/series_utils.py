56af1d97f0e70f7a6258f15824d03075c9ef00bd
Hazem Elmeleegy <hazem.elmeleegy@snowflake.com>
2025-01-30 00:36:32 -0800

    fix errors

--- src/snowflake/snowpark/modin/plugin/docstrings/series_utils.py
+++ src/snowflake/snowpark/modin/plugin/docstrings/series_utils.py
@@ -109,14 +109,6 @@ class StringMethods:
         0                                           this    is     a  regular  sentence
         1  https://docs.python.org/3/tutorial/index.html  None  None     None      None
         2                                           None  None  None     None      None
-
-        For slightly more complex use cases like splitting the html document name from a url, a combination of parameter settings can be used.
-
-        >>> s.str.rsplit("/", n=1, expand=True)
-                                            0           1
-        0          this is a regular sentence        None
-        1  https://docs.python.org/3/tutorial  index.html
-        2                                None        None
         """
 
     def rsplit():
