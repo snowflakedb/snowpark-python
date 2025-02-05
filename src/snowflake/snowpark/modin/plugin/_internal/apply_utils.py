8b618ca4b4828938258a116b6a12b0f0307ea409
Labanya Mukhopadhyay <labanya.mukhopadhyay@snowflake.com>
2025-02-05 12:02:35 -0800

    generate cortex funcs and rev changes
    Signed-off-by: Labanya Mukhopadhyay <labanya.mukhopadhyay@snowflake.com>

--- src/snowflake/snowpark/modin/plugin/_internal/apply_utils.py
+++ src/snowflake/snowpark/modin/plugin/_internal/apply_utils.py
@@ -106,6 +106,10 @@ SUPPORTED_SNOWFLAKE_CORTEX_FUNCTIONS_IN_APPLY = {
     snowflake.cortex.Sentiment,
 }
 
+ALL_SNOWFLAKE_CORTEX_FUNCTIONS = tuple(
+    i[1] for i in inspect.getmembers(snowflake.cortex)
+)
+
 
 class GroupbyApplySortMethod(Enum):
     """
