fd79ccf3a845e7acde72a270fbc086545755d22e
Afroz Alam <afroz.alam@snowflake.com>
2025-02-07 17:57:06 -0800

    Update schema query when attributes are available

--- src/snowflake/snowpark/_internal/analyzer/snowflake_plan.py
+++ src/snowflake/snowpark/_internal/analyzer/snowflake_plan.py
@@ -359,7 +359,11 @@ class SnowflakePlan(LogicalPlan):
         # carried over to next SelectStatement (e.g., check the implementation of df.filter()).
         cache_metadata_if_select_statement(self.source_plan, self._metadata)
         # No simplifier case relies on this schema_query change to update SHOW TABLES to a nested sql friendly query.
-        if not self.schema_query or not self.session.sql_simplifier_enabled:
+        if (
+            not self.schema_query
+            or not self.session.sql_simplifier_enabled
+            or self.session.reduce_describe_query_enabled
+        ):
             self.schema_query = schema_value_statement(attributes)
         return attributes
 
