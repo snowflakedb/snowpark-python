d0e9db0f091e45f2dcef9d3d28de061ea1a43e0e
Jamison <jamison.rose@snowflake.com>
2025-02-11 15:26:04 -0800

    SNOW-192360: [Local Testing] Fix Join On column order.

--- tests/integ/test_dataframe.py
+++ tests/integ/test_dataframe.py
@@ -1283,6 +1283,25 @@ def test_join_left_outer(session):
     assert sorted(res, key=lambda r: r[0]) == expected
 
 
+def test_join_on_order(session):
+    """
+    Test that an 'on' clause in a different order from the data frame re-orders the columns correctly.
+    """
+    df1 = session.create_dataframe([(1, "A", 3)], schema=["A", "B", "C"])
+    df2 = session.create_dataframe([(1, "A", 4)], schema=["A", "B", "D"])
+
+    df3 = df1.join(df2, on=["B", "A"])
+    assert df3.schema == StructType(
+        [
+            StructField("B", StringType(), nullable=False),
+            StructField("A", LongType(), nullable=False),
+            StructField("C", LongType(), nullable=False),
+            StructField("D", LongType(), nullable=False),
+        ]
+    )
+    Utils.check_answer(df3, [Row(B="A", A=1, C=3, D=4)])
+
+
 @pytest.mark.xfail(
     "config.getoption('local_testing_mode', default=False)",
     reason="schema_query is not supported in Local Testing",
