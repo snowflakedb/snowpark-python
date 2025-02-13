7f0d16a15c5f688c9095fbf06a2049cb76136cd7
Yuyang Wang <yuyang.wang@snowflake.com>
2025-02-13 10:23:44 -0800

    add driver into dev dependency

--- setup.py
+++ setup.py
@@ -60,6 +60,8 @@ DEVELOPMENT_REQUIREMENTS = [
     "lxml",  # used in read_xml tests
     "tox",  # used for setting up testing environments
     "snowflake.core>=1.0.0, <2",  # Catalog
+    "pyodbc",
+    "oracledb",
 ]
 
 # read the version
