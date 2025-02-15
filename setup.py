d164eaf986d28dde4b2b35fecbbfc00337fe2e43
Labanya Mukhopadhyay <labanya.mukhopadhyay@snowflake.com>
2025-02-14 16:08:22 -0800

    SNOW-1934027: Pin snowflake-ml-python version to 1.4.1 or greater
    Signed-off-by: Labanya Mukhopadhyay <labanya.mukhopadhyay@snowflake.com>

--- setup.py
+++ setup.py
@@ -202,7 +202,7 @@ setup(
             "scikit-learn",  # Snowpark pandas 3rd party library testing
             # plotly version restricted due to foreseen change in query counts in version 6.0.0+
             "plotly<6.0.0",  # Snowpark pandas 3rd party library testing
-            "snowflake-ml-python",
+            "snowflake-ml-python>=1.4.1",
         ],
         "localtest": [
             "pandas",
