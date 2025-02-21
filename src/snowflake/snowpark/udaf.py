8b2d038471c45621e10e9c1f1fafd1c466af1690
Jamison Rose <Jamison.Rose@snowflake.com>
2025-02-21 12:17:54 -0800

    SNOW-1708509: Add support for artifact_repository to udxf (#3044)

--- src/snowflake/snowpark/udaf.py
+++ src/snowflake/snowpark/udaf.py
@@ -811,6 +811,8 @@ class UDAFRegistration:
                 native_app_params=native_app_params,
                 copy_grants=copy_grants,
                 runtime_version=runtime_version_from_requirement,
+                artifact_repository=kwargs.get("artifact_repository"),
+                artifact_repository_packages=kwargs.get("artifact_repository_packages"),
             )
         # an exception might happen during registering a udaf
         # (e.g., a dependency might not be found on the stage),
