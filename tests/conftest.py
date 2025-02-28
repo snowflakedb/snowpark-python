999d6a38969e72a596b000e242b1696bd2f21905
Adam Ling <adam.ling@snowflake.com>
2025-02-27 18:21:15 -0800

    add missing option

--- tests/conftest.py
+++ tests/conftest.py
@@ -74,6 +74,7 @@ def pytest_addoption(parser, pluginmanager):
         type=str,
         help="Path to the Unparser JAR built in the monorepo.",
     )
+    parser.addoption("--join_alias_fix", action="store_true", default=False)
 
 
 def pytest_collection_modifyitems(items) -> None:
