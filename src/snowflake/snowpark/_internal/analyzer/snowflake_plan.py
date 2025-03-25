ea6e5900edbef2516aa789ab8f093892a0842219
Jianzhun Du <jianzhun.du@snowflake.com>
2025-03-25 10:40:31 -0700

    int

--- src/snowflake/snowpark/_internal/analyzer/snowflake_plan.py
+++ src/snowflake/snowpark/_internal/analyzer/snowflake_plan.py
@@ -1306,7 +1306,7 @@ class SnowflakePlanBuilder:
 
         # TODO SNOW-1983360: make it an configurable option once the UDTF scalability issue is resolved.
         # Currently it's capped at 16.
-        file_size = self.session.sql(f"ls {file_path}").collect()[0]["size"]
+        file_size = int(self.session.sql(f"ls {file_path}").collect()[0]["size"])
         num_workers = min(16, file_size // DEFAULT_CHUNK_SIZE + 1)
 
         # Create a range from 0 to N-1
