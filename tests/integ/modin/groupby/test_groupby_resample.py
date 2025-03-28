024f65efb62b1ae9dbd6e47379e9afe06021fcce
Labanya Mukhopadhyay <labanya.mukhopadhyay@snowflake.com>
2025-03-28 10:00:45 -0700

    update implementation and test
    Signed-off-by: Labanya Mukhopadhyay <labanya.mukhopadhyay@snowflake.com>

--- tests/integ/modin/groupby/test_groupby_resample.py
+++ tests/integ/modin/groupby/test_groupby_resample.py
@@ -6,27 +6,44 @@ import modin.pandas as pd
 import pandas as native_pd
 
 import snowflake.snowpark.modin.plugin  # noqa: F401
+import pytest
 from tests.integ.modin.utils import eval_snowpark_pandas_result
 from tests.integ.utils.sql_counter import SqlCounter
 
+IMPLEMENTED_GROUPBY_RESAMPLE_FUNCTIONS = ["max", "mean", "median", "min", "sum"]
+IMPLEMENTED_GROUPBY_RESAMPLE_DATEOFFSET_STRINGS = ["s", "min", "h", "D"]
 
-def test_groupby_resample():
+agg_func = pytest.mark.parametrize("agg_func", IMPLEMENTED_GROUPBY_RESAMPLE_FUNCTIONS)
+freq = pytest.mark.parametrize("freq", IMPLEMENTED_GROUPBY_RESAMPLE_DATEOFFSET_STRINGS)
+interval = pytest.mark.parametrize("interval", [1, 2, 3, 5, 15])
+
+
+@freq
+@interval
+@agg_func
+def test_groupby_resample(freq, interval, agg_func):
     idx = pd.date_range("1/1/2000", periods=8, freq="min")
     pandas_df = native_pd.DataFrame(
         data=8 * [range(3)], index=idx, columns=["a", "b", "c"]
     )
+    # creating df with missing datetime bins to be filled in by resample
     pandas_df.iloc[2, 0] = 5
     pandas_df.iloc[5, 0] = 5
     pandas_df.iloc[7, 0] = 5
     pandas_df = pandas_df.reset_index().drop(index=[3, 4, 5]).set_index("index")
     snow_df = pd.DataFrame(pandas_df)
-    with SqlCounter(query_count=1):
+    rule = f"{interval}{freq}"
+    with SqlCounter(query_count=5):
         eval_snowpark_pandas_result(
             snow_df,
             pandas_df,
-            lambda df: df.groupby("a").resample("3min", include_groups=False).sum(),
+            lambda df: getattr(
+                df.groupby("a").resample(
+                    rule=rule, closed="left", include_groups=False
+                ),
+                agg_func,
+            )(),
             test_attrs=False,
             check_freq=False,
             check_index_type=False,
         )
-    # snow_df.groupby("a").resample("3min", include_groups=False).sum()
