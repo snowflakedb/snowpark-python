b9181b377587e153b63ab97597ad19c101563940
Hazem Elmeleegy <hazem.elmeleegy@snowflake.com>
2025-04-07 10:46:07 -0700

    address comments

--- tests/integ/modin/io/test_read_snowflake_iceberg.py
+++ tests/integ/modin/io/test_read_snowflake_iceberg.py
@@ -14,7 +14,8 @@ from tests.integ.utils.sql_counter import SqlCounter
 from tests.utils import Utils
 
 
-def test_read_snowflake_iceberg_relaxed_ordering(session):
+@pytest.mark.parametrize("relaxed_ordering", [True, False])
+def test_read_snowflake_iceberg(session, relaxed_ordering):
     if "azure" in session.connection.host.split("."):
         pytest.skip("This test doesn't work for Azure.")
     if "gcp" in session.connection.host.split("."):
@@ -46,11 +47,14 @@ def test_read_snowflake_iceberg_relaxed_ordering(session):
         """
         ).collect()
 
-    with SqlCounter(query_count=2):
+    expected_query_counts = [2, 0] if relaxed_ordering else [4, 2]
+
+    with SqlCounter(query_count=expected_query_counts[0]):
         # create dataframe directly from iceberg table
-        with SqlCounter(query_count=0):
+        with SqlCounter(query_count=expected_query_counts[1]):
             # no eager query is used when relaxed_ordering is enabled
-            df = pd.read_snowflake(table_name, relaxed_ordering=True)
+            # two eager queries are used when relaxed_ordering is disabled
+            df = pd.read_snowflake(table_name, relaxed_ordering=relaxed_ordering)
 
         # convert to pandas dataframe
         pdf = df.to_pandas()
@@ -58,70 +62,13 @@ def test_read_snowflake_iceberg_relaxed_ordering(session):
         # compare two dataframes
         assert_snowpark_pandas_equals_to_pandas_without_dtypecheck(df, pdf)
 
-    with SqlCounter(query_count=2):
+    with SqlCounter(query_count=expected_query_counts[0]):
         # create dataframe from a query referencing an iceberg table
         SQL_QUERY = f"""SELECT count(*) FROM {table_name}"""
-        with SqlCounter(query_count=0):
+        with SqlCounter(query_count=expected_query_counts[1]):
             # no eager query is used when relaxed_ordering is enabled
-            df = pd.read_snowflake(SQL_QUERY, relaxed_ordering=True)
-
-        # convert to pandas dataframe
-        pdf = df.to_pandas()
-
-        # compare two dataframes
-        assert_snowpark_pandas_equals_to_pandas_without_dtypecheck(df, pdf)
-
-
-def test_read_snowflake_iceberg_no_relaxed_ordering(session):
-    if "azure" in session.connection.host.split("."):
-        pytest.skip("This test doesn't work for Azure.")
-    if "gcp" in session.connection.host.split("."):
-        pytest.skip("This test doesn't work for GCP.")
-
-    with SqlCounter(query_count=2):
-        # create iceberg table
-        table_name = Utils.random_name_for_temp_object(TempObjectType.TABLE)
-        session.sql(
-            "CREATE EXTERNAL VOLUME if not exists python_connector_iceberg_exvol"
-        ).collect()
-        session.sql(
-            f"""
-        CREATE OR REPLACE ICEBERG TABLE {table_name} (
-            "NESTED_DATA" OBJECT(
-                camelCase STRING,
-                snake_case STRING,
-                PascalCase STRING,
-                nested_map MAP(
-                    STRING,
-                    OBJECT(
-                        inner_camelCase STRING,
-                        inner_snake_case STRING,
-                        inner_PascalCase STRING
-                    )
-                )
-            )
-        ) EXTERNAL_VOLUME = 'python_connector_iceberg_exvol' CATALOG = 'SNOWFLAKE' BASE_LOCATION = 'python_connector_merge_gate';
-        """
-        ).collect()
-
-    with SqlCounter(query_count=4):
-        # create dataframe directly from iceberg table
-        with SqlCounter(query_count=2):
             # two eager queries are used when relaxed_ordering is disabled
-            df = pd.read_snowflake(table_name, relaxed_ordering=False)
-
-        # convert to pandas dataframe
-        pdf = df.to_pandas()
-
-        # compare two dataframes
-        assert_snowpark_pandas_equals_to_pandas_without_dtypecheck(df, pdf)
-
-    with SqlCounter(query_count=4):
-        # create dataframe from a query referencing an iceberg table
-        SQL_QUERY = f"""SELECT count(*) FROM {table_name}"""
-        with SqlCounter(query_count=2):
-            # two eager queries are used when relaxed_ordering is disabled
-            df = pd.read_snowflake(SQL_QUERY, relaxed_ordering=False)
+            df = pd.read_snowflake(SQL_QUERY, relaxed_ordering=relaxed_ordering)
 
         # convert to pandas dataframe
         pdf = df.to_pandas()
