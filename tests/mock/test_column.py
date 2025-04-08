bb692b87fdddcffb577880fc570ac02e86774fec
Jamison Rose <Jamison.Rose@snowflake.com>
2025-04-08 14:27:34 -0700

    SNOW-1708509: [Local Testing] Fix Column.like bug (#3187)

--- tests/mock/test_column.py
+++ tests/mock/test_column.py
@@ -4,7 +4,6 @@
 
 from snowflake.snowpark.functions import col, when, array_agg
 from snowflake.snowpark.row import Row
-
 from tests.utils import Utils
 
 
@@ -20,6 +19,41 @@ def test_like_with_non_zero_row_index(session):
     assert df.filter(col("b") > 2).select(
         col("a").like("1").alias("res")
     ).collect() == [Row(RES=False)]
+
+
+def test_like_with_several_rows(session):
+    df = session.create_dataframe(
+        [
+            [
+                "This is an apple",
+                "This is an apple",
+            ],
+            [
+                "This is an apple",
+                "This is an orange",
+            ],
+            [
+                "This is an apple",
+                "This is ",
+            ],
+            [
+                "This is an apple",
+                "Station is",
+            ],
+        ],
+        ["A", "B"],
+    )
+
+    res = df.with_column("RESULT", col("A").like(col("B")))
+    Utils.check_answer(
+        res,
+        [
+            Row("This is an apple", "This is an apple", True),
+            Row("This is an apple", "This is an orange", False),
+            Row("This is an apple", "This is ", False),
+            Row("This is an apple", "Station is", False),
+        ],
+    )
 
 
 def test_get_item(session):
