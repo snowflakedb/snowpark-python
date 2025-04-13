d76f8325da6cc975f3fbcc0e056909402f4ad636
Afroz Alam <afroz.alam@snowflake.com>
2025-04-13 18:47:48 +0530

    copy/deepcopy metadata for treenode

--- src/snowflake/snowpark/_internal/analyzer/select_statement.py
+++ src/snowflake/snowpark/_internal/analyzer/select_statement.py
@@ -764,6 +764,7 @@ class SelectStatement(Selectable):
         new._merge_projection_complexity_with_subquery = (
             self._merge_projection_complexity_with_subquery
         )
+        new._attributes = self._attributes.copy()
 
         return new
 
@@ -792,6 +793,7 @@ class SelectStatement(Selectable):
             if not self._projection_complexities
             else None
         )
+        copied._attributes = deepcopy(self._attributes)
         return copied
 
     @property
