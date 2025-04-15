814bf8b7260967f33490aa23e265df23a017774b
Hazem Elmeleegy <hazem.elmeleegy@snowflake.com>
2025-04-15 10:19:34 -0700

    SNOW-2022698: Rename relaxed_ordering param into enforce_ordering and update default setting (#3244)

--- src/snowflake/snowpark/modin/plugin/io/snow_io.py
+++ src/snowflake/snowpark/modin/plugin/io/snow_io.py
@@ -212,14 +212,14 @@ class PandasOnSnowflakeIO(BaseIO):
         name_or_query: Union[str, Iterable[str]],
         index_col: Optional[Union[str, list[str]]] = None,
         columns: Optional[list[str]] = None,
-        relaxed_ordering: bool = False,
+        enforce_ordering: bool = False,
     ):
         """
         See detailed docstring and examples in ``read_snowflake`` in frontend layer:
         src/snowflake/snowpark/modin/plugin/pd_extensions.py
         """
         return cls.query_compiler_cls.from_snowflake(
-            name_or_query, index_col, columns, relaxed_ordering=relaxed_ordering
+            name_or_query, index_col, columns, enforce_ordering=enforce_ordering
         )
 
     @classmethod
--- tests/integ/modin/series/test_to_snowflake.py
+++ tests/integ/modin/series/test_to_snowflake.py
@@ -30,7 +30,7 @@ def _verify_num_rows(session, table_name: str, expected: int) -> None:
 
 @pytest.mark.parametrize("index", [True, False])
 @pytest.mark.parametrize("index_labels", [None, ["my_index"]])
-@sql_count_checker(query_count=3, join_count=1)
+@sql_count_checker(query_count=2, join_count=1)
 def test_to_snowflake_index(test_table_name, snow_series, index, index_labels):
     snow_series.to_snowflake(
         test_table_name, if_exists="replace", index=index, index_label=index_labels
@@ -59,14 +59,14 @@ def test_to_snowflake_index_name_conflict_negative(test_table_name, snow_series)
         snow_series.to_snowflake(test_table_name, if_exists="replace", index_label="a")
 
 
-@sql_count_checker(query_count=3)
+@sql_count_checker(query_count=2)
 def test_to_snowflake_index_label_none(test_table_name):
     snow_series = pd.Series([1, 2, 3], name="a", index=native_pd.Index([4, 5, 6]))
     snow_series.to_snowflake(test_table_name, if_exists="replace", index=True)
     _verify_columns(test_table_name, ["index", "a"])
 
 
-@sql_count_checker(query_count=3)
+@sql_count_checker(query_count=2)
 def test_to_snowflake_multiindex(test_table_name, snow_series):
     index = native_pd.MultiIndex.from_arrays(
         [[1, 1, 2, 2], ["red", "blue", "red", "blue"]], names=("number", "color")
@@ -88,18 +88,18 @@ def test_to_snowflake_index_label_invalid_length_negative(test_table_name, snow_
 
 def test_to_snowflake_if_exists(session, test_table_name, snow_series):
     # Verify new table is created
-    with SqlCounter(query_count=4):
+    with SqlCounter(query_count=3):
         snow_series.to_snowflake(test_table_name, if_exists="fail", index=False)
         _verify_columns(test_table_name, ["a"])
 
     # Verify existing table is replaced with new data
-    with SqlCounter(query_count=3):
+    with SqlCounter(query_count=2):
         snow_series = pd.Series([4, 5, 6], name="b")
         snow_series.to_snowflake(test_table_name, if_exists="replace", index=False)
         _verify_columns(test_table_name, ["b"])
 
     # Verify data is appended to existing table
-    with SqlCounter(query_count=6):
+    with SqlCounter(query_count=5):
         _verify_num_rows(session, test_table_name, 3)
         snow_series.to_snowflake(test_table_name, if_exists="append", index=False)
         _verify_columns(test_table_name, ["b"])
@@ -125,7 +125,7 @@ def test_to_snowflake_if_exists_negative(session, test_table_name, snow_series):
 
 
 @pytest.mark.parametrize("index_label", VALID_PANDAS_LABELS)
-@sql_count_checker(query_count=3, join_count=1)
+@sql_count_checker(query_count=2, join_count=1)
 def test_to_snowflake_index_column_labels(index_label, test_table_name, snow_series):
     snow_series.to_snowflake(
         test_table_name, if_exists="replace", index=True, index_label=index_label
@@ -134,7 +134,7 @@ def test_to_snowflake_index_column_labels(index_label, test_table_name, snow_ser
 
 
 @pytest.mark.parametrize("col_label", VALID_PANDAS_LABELS)
-@sql_count_checker(query_count=3, join_count=1)
+@sql_count_checker(query_count=2, join_count=1)
 def test_to_snowflake_data_column_labels(col_label, test_table_name, snow_series):
     snow_series = snow_series.rename(col_label)
     snow_series.to_snowflake(test_table_name, if_exists="replace", index=False)
