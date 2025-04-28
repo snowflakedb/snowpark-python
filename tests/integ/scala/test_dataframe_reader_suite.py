2db957c000332d69da23e8f6dca23b5a05f9927d
dkaszynski <dawid.kaszynski@snowflake.com>
2025-04-28 14:46:03 +0200

    Skip tests if in local_testing_mode

--- tests/integ/scala/test_dataframe_reader_suite.py
+++ tests/integ/scala/test_dataframe_reader_suite.py
@@ -1922,143 +1922,6 @@ def test_read_multiple_csvs(session):
         Utils.drop_table(session, table_name)
 
 
-@pytest.mark.skipif(
-    "config.getoption('local_testing_mode', default=False)",
-    reason="xml not supported in local testing mode",
-)
-@pytest.mark.parametrize(
-    "file,row_tag,expected_row_count,expected_column_count",
-    [
-        [test_file_books_xml, "book", 12, 7],
-        [test_file_books2_xml, "book", 2, 6],
-        [test_file_house_xml, "House", 37, 22],
-        [test_file_house_large_xml, "House", 740, 22],
-    ],
-)
-def test_read_xml_row_tag(
-    session, file, row_tag, expected_row_count, expected_column_count
-):
-    df = session.read.option("rowTag", row_tag).xml(f"@{tmp_stage_name1}/{file}")
-    result = df.collect()
-    assert len(result) == expected_row_count
-    assert len(result[0]) == expected_column_count
-
-
-@pytest.mark.skipif(
-    "config.getoption('local_testing_mode', default=False)",
-    reason="xml not supported in local testing mode",
-)
-def test_read_xml_no_xxe(session):
-    row_tag = "bar"
-    stage_file_path = f"@{tmp_stage_name1}/{test_file_xxe_xml}"
-    df = session.read.option("rowTag", row_tag).xml(stage_file_path)
-    Utils.check_answer(df, [Row("null")])
-
-
-@pytest.mark.skipif(
-    "config.getoption('local_testing_mode', default=False)",
-    reason="xml not supported in local testing mode",
-)
-@pytest.mark.skipif(
-    IS_IN_STORED_PROC,
-    reason="SNOW-2044853: Flaky in stored procedure test",
-)
-def test_read_xml_query_nested_data(session):
-    row_tag = "tag"
-    df = session.read.option("rowTag", row_tag).xml(
-        f"@{tmp_stage_name1}/{test_file_nested_xml}"
-    )
-    assert df._all_variant_cols is True
-    Utils.check_answer(
-        df.select(
-            "'test'.num", "'test'.str", col("'test'.obj"), col("'test'.obj.bool")
-        ),
-        [Row('"1"', '"str1"', '{\n  "bool": "true",\n  "str": "str2"\n}', '"true"')],
-    )
-
-
-@pytest.mark.skipif(
-    "config.getoption('local_testing_mode', default=False)",
-    reason="xml not supported in local testing mode",
-)
-def test_read_xml_non_existing_file(session):
-    row_tag = "tag"
-    with pytest.raises(ValueError, match="does not exist"):
-        session.read.option("rowTag", row_tag).xml(
-            f"@{tmp_stage_name1}/non_existing_file.xml"
-        )
-
-
-@pytest.mark.skipif(
-    "config.getoption('local_testing_mode', default=False)",
-    reason="xml not supported in local testing mode",
-)
-@pytest.mark.parametrize(
-    "file",
-    (
-        test_file_malformed_no_closing_tag_xml,
-        test_file_malformed_not_self_closing_xml,
-        test_file_malformed_record_xml,
-    ),
-)
-def test_read_malformed_xml(session, file):
-    row_tag = "record"
-    file_path = f"@{tmp_stage_name1}/{file}"
-
-    # permissive mode
-    df = (
-        session.read.option("rowTag", row_tag)
-        .option("mode", "permissive")
-        .xml(file_path)
-    )
-    result = df.collect()
-    assert len(result) == 2
-    assert len(result[0]) == 4  # has another column '_corrupt_record'
-    assert (
-        result[0]["'_corrupt_record'"] is not None
-        or result[1]["'_corrupt_record'"] is not None
-    )
-
-    # dropmalformed mode
-    df = (
-        session.read.option("rowTag", row_tag)
-        .option("mode", "dropmalformed")
-        .xml(file_path)
-    )
-    result = df.collect()
-    assert len(result) == 1
-    assert len(result[0]) == 3
-
-    # failfast mode
-    df = (
-        session.read.option("rowTag", row_tag).option("mode", "failfast").xml(file_path)
-    )
-    with pytest.raises(SnowparkSQLException, match="Malformed XML record at bytes"):
-        df.collect()
-
-
-@pytest.mark.skipif(
-    "config.getoption('local_testing_mode', default=False)",
-    reason="xml not supported in local testing mode",
-)
-def test_read_xml_row_tag_not_found(session):
-    row_tag = "non-existing-tag"
-    df = session.read.option("rowTag", row_tag).xml(
-        f"@{tmp_stage_name1}/{test_file_books_xml}"
-    )
-
-    with pytest.raises(
-        SnowparkDataframeReaderException, match="Cannot find the row tag"
-    ):
-        df.collect()
-
-    # also works for nested query plan
-    with pytest.raises(
-        SnowparkDataframeReaderException, match="Cannot find the row tag"
-    ):
-        df.filter(lit(True)).collect()
-
-
 @pytest.mark.parametrize(
     "file_format_name,infer_schema",
     [
@@ -2067,6 +1930,10 @@ def test_read_xml_row_tag_not_found(session):
         ('"a$B12""cD"', True),
         ('"a$B12""cD"', False),
     ],
+)
+@pytest.mark.skipif(
+    "config.getoption('local_testing_mode', default=False)",
+    reason="sql not supported in local testing mode",
 )
 def test_read_file_with_enforced_existing_file_format(
     session, file_format_name, infer_schema
@@ -2098,6 +1965,10 @@ def test_read_file_with_enforced_existing_file_format(
         session.sql(f"DROP FILE FORMAT IF EXISTS {file_format_name}").collect()
 
 
+@pytest.mark.skipif(
+    "config.getoption('local_testing_mode', default=False)",
+    reason="sql not supported in local testing mode",
+)
 def test_enforce_existing_file_format_with_additional_format_type_options(session):
     file_format_name = "ABC"
 
@@ -2123,6 +1994,10 @@ def test_enforce_existing_file_format_with_additional_format_type_options(sessio
         session.sql(f"DROP FILE FORMAT IF EXISTS {file_format_name}").collect()
 
 
+@pytest.mark.skipif(
+    "config.getoption('local_testing_mode', default=False)",
+    reason="sql not supported in local testing mode",
+)
 def test_enforce_existing_file_format_without_providing_format_name(session):
     file_format_name = "ABC"
 
@@ -2145,6 +2020,10 @@ def test_enforce_existing_file_format_without_providing_format_name(session):
         session.sql(f"DROP FILE FORMAT IF EXISTS {file_format_name}").collect()
 
 
+@pytest.mark.skipif(
+    "config.getoption('local_testing_mode', default=False)",
+    reason="Read option 'FORMAT_NAME' not supported in local testing mode",
+)
 def test_enforce_existing_file_format_object_doesnt_exist(session):
     test_file_on_stage = f"@{tmp_stage_name1}/{test_file_csv}"
 
