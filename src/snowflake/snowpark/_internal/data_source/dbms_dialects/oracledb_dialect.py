e8a22ced474c7a5335752f35ee111c7f8e87428a
Yuyang Wang <yuyang.wang@snowflake.com>
2025-04-29 16:52:18 -0700

    revert change

--- src/snowflake/snowpark/_internal/data_source/dbms_dialects/oracledb_dialect.py
+++ src/snowflake/snowpark/_internal/data_source/dbms_dialects/oracledb_dialect.py
@@ -1,7 +1,7 @@
 #
 # Copyright (c) 2012-2025 Snowflake Computing Inc. All rights reserved.
 #
-from typing import List, Optional
+from typing import List
 
 from snowflake.snowpark._internal.data_source.dbms_dialects import BaseDialect
 from snowflake.snowpark._internal.utils import quote_name
@@ -13,8 +13,7 @@ class OracledbDialect(BaseDialect):
         self,
         table_or_query: str,
         schema: StructType,
-        *,
-        raw_schema: Optional[List[tuple]],
+        raw_schema: List[tuple],
     ) -> str:
         cols = []
         for field, raw_field in zip(schema.fields, raw_schema):
