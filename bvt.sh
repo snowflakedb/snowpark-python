a11405d400145ff7b91eda67b2248cd7a70d64d1
Jonathan Shi <jonathan.shi@snowflake.com>
2025-05-01 09:38:40 -0700

    update test_telemetry query counts to account for eager temp table creation, and add test_telemetry to bvt script

--- bvt.sh
+++ bvt.sh
@@ -1,1 +1,5 @@
+echo "Running telemetry tests..."
+pytest tests/integ/modin/test_telemetry.py
+
+echo "Running notebook tests..."
 jupyter nbconvert --execute --to notebook hybrid-demo/build_verification_test.ipynb
