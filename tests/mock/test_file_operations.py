d9423f5d2b2ff79ecb52aca2fe002e22f29707bf
Lawrence Ju <lawrence.ju@snowflake.com>
2025-05-29 10:19:17 -0700

    remove redundant test

--- tests/mock/test_file_operations.py
+++ tests/mock/test_file_operations.py
@@ -42,31 +42,14 @@ def test_get_and_put_snowurl(session):
                 content = f.read()
                 assert content == test_content
 
-    # Test put with a directory that hasn't been created
-    snowurl = "snow://test_file_1"
-    put_results = session.file.put(
-        normalize_local_file(test_file),
-        snowurl,
-        auto_compress=False,
-    )
-    assert len(put_results) == 1
-    put_result = put_results[0]
-    assert put_result.source == put_result.target == "test_file_1"
-    assert put_result.source_size is not None
-    assert put_result.target_size is not None
-    assert put_result.source_compression == "NONE"
-    assert put_result.target_compression == "NONE"
-    assert put_result.status == "UPLOADED"
-    assert put_result.message == ""
-
-    with tempfile.TemporaryDirectory() as temp_dir:
-        # Test that the file can be retrieved without a trailing slash
-        get_result = session.file.get(
-            snowurl,
-            temp_dir,
-        )
-        assert len(get_result) == 1
-        assert os.path.isfile(os.path.join(temp_dir, "test_file_1"))
-        with open(os.path.join(temp_dir, "test_file_1"), "rb") as f:
-            content = f.read()
-            assert content == test_content
+        with tempfile.TemporaryDirectory() as temp_dir:
+            # Test that the file can be retrieved without a trailing slash
+            get_result = session.file.get(
+                snowurl,
+                temp_dir,
+            )
+            assert len(get_result) == 1
+            assert os.path.isfile(os.path.join(temp_dir, "test_file_1"))
+            with open(os.path.join(temp_dir, "test_file_1"), "rb") as f:
+                content = f.read()
+                assert content == test_content
