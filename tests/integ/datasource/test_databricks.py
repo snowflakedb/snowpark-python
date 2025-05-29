f16e214caa66cd5a6b1751cec0a3f1c6a058d48b
Adam Ling <adam.ling@snowflake.com>
2025-05-29 16:35:10 -0700

    adding back all test

--- tests/integ/datasource/test_databricks.py
+++ tests/integ/datasource/test_databricks.py
@@ -67,8 +67,7 @@ def test_blank_databricks(session):
 
 @pytest.mark.parametrize(
     "input_type, input_value",
-    # [("table", TEST_TABLE_NAME), ("query", f"(SELECT * FROM {TEST_TABLE_NAME})")],
-    [("table", TEST_TABLE_NAME)],
+    [("table", TEST_TABLE_NAME), ("query", f"(SELECT * FROM {TEST_TABLE_NAME})")],
 )
 def test_basic_databricks(session, input_type, input_value):
     input_dict = {
@@ -87,130 +86,130 @@ def test_basic_databricks(session, input_type, input_value):
     assert df2.schema == EXPECTED_TYPE
 
 
-# @pytest.mark.parametrize(
-#     "input_type, input_value, error_message",
-#     [
-#         ("table", "NOT EXIST", "TABLE_OR_VIEW_NOT_FOUND"),
-#         ("query", "SELEC ** FORM TABLE", "PARSE_SYNTAX_ERROR"),
-#     ],
-# )
-# def test_error_case(session, input_type, input_value, error_message):
-#     input_dict = {
-#         input_type: input_value,
-#     }
-#     with pytest.raises(SnowparkDataframeReaderException, match=error_message):
-#         session.read.dbapi(create_databricks_connection, **input_dict)
+@pytest.mark.parametrize(
+    "input_type, input_value, error_message",
+    [
+        ("table", "NOT EXIST", "TABLE_OR_VIEW_NOT_FOUND"),
+        ("query", "SELEC ** FORM TABLE", "PARSE_SYNTAX_ERROR"),
+    ],
+)
+def test_error_case(session, input_type, input_value, error_message):
+    input_dict = {
+        input_type: input_value,
+    }
+    with pytest.raises(SnowparkDataframeReaderException, match=error_message):
+        session.read.dbapi(create_databricks_connection, **input_dict)
 
 
-# @pytest.mark.skipif(DEPENDENCIES_PACKAGE_UNAVAILABLE, reason="Missing 'pandas'")
-# def test_unit_data_source_data_to_pandas_df():
-#     schema = StructType(
-#         [
-#             StructField("COL1", LongType(), nullable=True),
-#             StructField("COL2", MapType(StringType(), StringType()), nullable=True),
-#         ]
-#     )
-#     data = [
-#         (1, [("key1", "value1"), ("key2", "value2")]),
-#     ]
-#     df = DatabricksDriver.data_source_data_to_pandas_df(data, schema)
-#     assert df.to_dict(orient="records") == [
-#         {"COL1": 1, "COL2": [("key1", "value1"), ("key2", "value2")]}
-#     ]
+@pytest.mark.skipif(DEPENDENCIES_PACKAGE_UNAVAILABLE, reason="Missing 'pandas'")
+def test_unit_data_source_data_to_pandas_df():
+    schema = StructType(
+        [
+            StructField("COL1", LongType(), nullable=True),
+            StructField("COL2", MapType(StringType(), StringType()), nullable=True),
+        ]
+    )
+    data = [
+        (1, [("key1", "value1"), ("key2", "value2")]),
+    ]
+    df = DatabricksDriver.data_source_data_to_pandas_df(data, schema)
+    assert df.to_dict(orient="records") == [
+        {"COL1": 1, "COL2": [("key1", "value1"), ("key2", "value2")]}
+    ]
 
 
-# def test_unicode_column_databricks(session):
-#     df = session.read.dbapi(create_databricks_connection, table="User_profile_unicode")
+def test_unicode_column_databricks(session):
+    df = session.read.dbapi(create_databricks_connection, table="User_profile_unicode")
 
-#     assert df.collect() == [Row(编号=1, 姓名="山田太郎", 国家="日本", 备注="これはUnicodeテストです")]
-
-
-# def test_double_quoted_column_databricks(session):
-#     df = session.read.dbapi(create_databricks_connection, table="User_profile")
-#     assert df.collect() == [
-#         Row(
-#             id=1,
-#             name="Yamada Taro",
-#             country="Japan",
-#             remarks="This is a test remark",
-#         )
-#     ]
+    assert df.collect() == [Row(编号=1, 姓名="山田太郎", 国家="日本", 备注="これはUnicodeテストです")]
 
 
-# @pytest.mark.parametrize(
-#     "input_type, input_value",
-#     [("table", TEST_TABLE_NAME), ("query", f"(SELECT * FROM {TEST_TABLE_NAME})")],
-# )
-# def test_udtf_ingestion_databricks(session, input_type, input_value, caplog):
-#     # we define here to avoid test_databricks.py to be pickled and unpickled in UDTF
-#     def local_create_databricks_connection():
-#         import databricks.sql
-
-#         return databricks.sql.connect(**DATABRICKS_CONNECTION_PARAMETERS)
-
-#     input_dict = {
-#         input_type: input_value,
-#     }
-#     df = session.read.dbapi(
-#         local_create_databricks_connection,
-#         **input_dict,
-#         udtf_configs={
-#             "external_access_integration": DATABRICKS_TEST_EXTERNAL_ACCESS_INTEGRATION
-#         },
-#     ).order_by("COL_BYTE", ascending=True)
-#     Utils.check_answer(df, EXPECTED_TEST_DATA)
-#     assert df.schema == EXPECTED_TYPE
-
-#     assert (
-#         "TEMPORARY  FUNCTION  data_source_udtf_" "" in caplog.text
-#         and "table(data_source_udtf" in caplog.text
-#     )
+def test_double_quoted_column_databricks(session):
+    df = session.read.dbapi(create_databricks_connection, table="User_profile")
+    assert df.collect() == [
+        Row(
+            id=1,
+            name="Yamada Taro",
+            country="Japan",
+            remarks="This is a test remark",
+        )
+    ]
 
 
-# def test_unit_udtf_ingestion():
-#     dbx_driver = DatabricksDriver(create_databricks_connection, DBMS_TYPE.DATABRICKS_DB)
-#     udtf_ingestion_class = dbx_driver.udtf_class_builder()
-#     udtf_ingestion_instance = udtf_ingestion_class()
+@pytest.mark.parametrize(
+    "input_type, input_value",
+    [("table", TEST_TABLE_NAME), ("query", f"(SELECT * FROM {TEST_TABLE_NAME})")],
+)
+def test_udtf_ingestion_databricks(session, input_type, input_value, caplog):
+    # we define here to avoid test_databricks.py to be pickled and unpickled in UDTF
+    def local_create_databricks_connection():
+        import databricks.sql
 
-#     dsp = DataSourcePartitioner(
-#         create_databricks_connection,
-#         f"(select * from {TEST_TABLE_NAME}) SORT BY COL_BYTE NULLS FIRST",
-#         is_query=True,
-#     )
-#     yield_data = list(udtf_ingestion_instance.process(dsp.partitions[0]))
-#     # databricks sort by returns the all None row as the last row regardless of NULLS FIRST/LAST
-#     # while in snowflake test data after default sort None is the first row
+        return databricks.sql.connect(**DATABRICKS_CONNECTION_PARAMETERS)
 
-#     # databricks sort by seems to be non-deterministic, we sort the data locally to stabilize the outpout
-#     yield_data = sorted(
-#         yield_data, key=lambda x: (x[0] is not None, x[0] if x[0] is not None else 0)
-#     )
+    input_dict = {
+        input_type: input_value,
+    }
+    df = session.read.dbapi(
+        local_create_databricks_connection,
+        **input_dict,
+        udtf_configs={
+            "external_access_integration": DATABRICKS_TEST_EXTERNAL_ACCESS_INTEGRATION
+        },
+    ).order_by("COL_BYTE", ascending=True)
+    Utils.check_answer(df, EXPECTED_TEST_DATA)
+    assert df.schema == EXPECTED_TYPE
 
-#     for row, expected_row in zip(
-#         yield_data, EXPECTED_TEST_DATA
-#     ):  # None data ordering is the same
-#         for index, (field, value) in enumerate(zip(EXPECTED_TYPE.fields, row)):
-#             if isinstance(field.datatype, VariantType):
-#                 # Convert ArrayType, MapType, and StructType to JSON
-#                 if "map" in field.name.lower():
-#                     assert (
-#                         (json.loads(value) == json.loads(expected_row[index]))
-#                         if value is not None
-#                         else True
-#                     )
-#                 else:
-#                     assert (
-#                         (value == json.loads(expected_row[index]))
-#                         if value is not None
-#                         else True
-#                     )
-#             elif isinstance(field.datatype, BinaryType):
-#                 # Convert BinaryType to hex string
-#                 assert (
-#                     (bytearray(bytes.fromhex(value)) == expected_row[index])
-#                     if value is not None
-#                     else True
-#                 )
-#             else:
-#                 # Keep other types as is
-#                 assert value == expected_row[index]
+    assert (
+        "TEMPORARY  FUNCTION  data_source_udtf_" "" in caplog.text
+        and "table(data_source_udtf" in caplog.text
+    )
+
+
+def test_unit_udtf_ingestion():
+    dbx_driver = DatabricksDriver(create_databricks_connection, DBMS_TYPE.DATABRICKS_DB)
+    udtf_ingestion_class = dbx_driver.udtf_class_builder()
+    udtf_ingestion_instance = udtf_ingestion_class()
+
+    dsp = DataSourcePartitioner(
+        create_databricks_connection,
+        f"(select * from {TEST_TABLE_NAME}) SORT BY COL_BYTE NULLS FIRST",
+        is_query=True,
+    )
+    yield_data = list(udtf_ingestion_instance.process(dsp.partitions[0]))
+    # databricks sort by returns the all None row as the last row regardless of NULLS FIRST/LAST
+    # while in snowflake test data after default sort None is the first row
+
+    # databricks sort by seems to be non-deterministic, we sort the data locally to stabilize the outpout
+    yield_data = sorted(
+        yield_data, key=lambda x: (x[0] is not None, x[0] if x[0] is not None else 0)
+    )
+
+    for row, expected_row in zip(
+        yield_data, EXPECTED_TEST_DATA
+    ):  # None data ordering is the same
+        for index, (field, value) in enumerate(zip(EXPECTED_TYPE.fields, row)):
+            if isinstance(field.datatype, VariantType):
+                # Convert ArrayType, MapType, and StructType to JSON
+                if "map" in field.name.lower():
+                    assert (
+                        (json.loads(value) == json.loads(expected_row[index]))
+                        if value is not None
+                        else True
+                    )
+                else:
+                    assert (
+                        (value == json.loads(expected_row[index]))
+                        if value is not None
+                        else True
+                    )
+            elif isinstance(field.datatype, BinaryType):
+                # Convert BinaryType to hex string
+                assert (
+                    (bytearray(bytes.fromhex(value)) == expected_row[index])
+                    if value is not None
+                    else True
+                )
+            else:
+                # Keep other types as is
+                assert value == expected_row[index]
