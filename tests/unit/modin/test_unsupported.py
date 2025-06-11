8a53b76eeb826922ba1499be683c7c6ffc69b52a
Hazem Elmeleegy <hazem.elmeleegy@snowflake.com>
2025-06-10 19:20:09 -0700

    fix errors

--- tests/unit/modin/test_unsupported.py
+++ tests/unit/modin/test_unsupported.py
@@ -20,7 +20,6 @@ from snowflake.snowpark.modin.plugin.compiler.snowflake_query_compiler import (
         ["read_gbq", {"query": ""}],
         ["read_clipboard", {}],
         ["read_hdf", {"path_or_buf": ""}],
-        ["read_feather", {"path": ""}],
         ["read_stata", {"filepath_or_buffer": ""}],
         ["read_sql", {"sql": "", "con": ""}],
         ["read_fwf", {"filepath_or_buffer": ""}],
