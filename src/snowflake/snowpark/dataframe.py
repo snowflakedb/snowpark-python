293325a42debc9c82a8c7b7d18b6cd88fc32d4ab
nqi <nova.qi@snowflake.com>
2025-06-16 22:25:33 -0700

    Update after meeting with Yijun

--- src/snowflake/snowpark/dataframe.py
+++ src/snowflake/snowpark/dataframe.py
@@ -121,7 +121,6 @@ from snowflake.snowpark._internal.ast.utils import (
 )
 from snowflake.snowpark._internal.error_message import SnowparkClientExceptionMessages
 from snowflake.snowpark._internal.lazy_import_utils import (
-    get_installed_pandas,
     get_pandas,
 )
 from snowflake.snowpark._internal.open_telemetry import open_telemetry_context_manager
@@ -220,6 +219,7 @@ else:
 if TYPE_CHECKING:
     import modin.pandas  # pragma: no cover
     from table import Table  # pragma: no cover
+    import pandas
 
 _logger = getLogger(__name__)
 
@@ -977,9 +977,6 @@ class DataFrame:
             _emit_ast=self._session.ast_enabled,
         )
 
-    if get_installed_pandas():
-        pandas = get_pandas()  # pragma: no cover
-
         @publicapi
         @overload
         def to_pandas(
@@ -1076,9 +1073,6 @@ class DataFrame:
                 return pandas.DataFrame(result)
 
         return result
-
-    if get_installed_pandas():
-        pandas = get_pandas()
 
         @publicapi
         @overload
