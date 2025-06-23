7b6118267d04413f7857c36c62d47d72811774e1
Yuyang Wang <yuyang.wang@snowflake.com>
2025-06-23 14:16:34 -0700

    add removed one back

--- tests/integ/conftest.py
+++ tests/integ/conftest.py
@@ -25,6 +25,7 @@ from tests.utils import (
     running_on_jenkins,
     running_on_public_ci,
 )
+import snowflake.snowpark.context as context
 
 RUNNING_ON_GH = os.getenv("GITHUB_ACTIONS") == "true"
 RUNNING_ON_JENKINS = "JENKINS_HOME" in os.environ
@@ -284,6 +285,9 @@ def session(
     session.ast_enabled = ast_enabled
     if not session._generate_multiline_queries:
         session._enable_multiline_queries()
+    context.configure_development_features(
+        enable_trace_sql_errors_to_dataframe=True, enable_eager_schema_validation=False
+    )
 
     if (RUNNING_ON_GH or RUNNING_ON_JENKINS) and not local_testing_mode:
         set_up_external_access_integration_resources(
