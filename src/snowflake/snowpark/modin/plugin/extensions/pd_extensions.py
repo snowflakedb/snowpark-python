ce8f204c5c0a3c2cc6f700b85e9853486cf0ab89
sfc-gh-mvashishtha <mahesh.vashishtha@snowflake.com>
2025-07-03 09:58:00 -0700

    Fix doctest
    Signed-off-by: sfc-gh-mvashishtha <mahesh.vashishtha@snowflake.com>

--- src/snowflake/snowpark/modin/plugin/extensions/pd_extensions.py
+++ src/snowflake/snowpark/modin/plugin/extensions/pd_extensions.py
@@ -23,6 +23,7 @@ from snowflake.snowpark.modin.plugin.extensions.timedelta_index import (  # noqa
 )
 import modin.pandas as pd
 from modin.config import context as config_context
+from pandas.util._decorators import doc
 from snowflake.snowpark.modin.plugin.utils.warning_message import (
     materialization_warning,
 )
@@ -45,14 +46,13 @@ def _snowpark_pandas_obj_check(obj: Union[DataFrame, Series]):
         raise TypeError("obj must be a Snowpark pandas DataFrame or Series")
 
 
-@register_pd_accessor("read_snowflake")
-def read_snowflake(
-    name_or_query: Union[str, Iterable[str]],
-    index_col: Union[str, list[str], None] = None,
-    columns: Optional[list[str]] = None,
-    enforce_ordering: bool = False,
-) -> DataFrame:
-    """
+# Use a template string so that we can share it between the read_snowflake
+# functions on the Snowflake and Pandas backends. We can't use the exact same
+# docstring because each doctest creates and inserts to a temp table, and also
+# creates, uses, and drops a stored procedure. Using a common table name or a
+# common stored procedure name would cause conflicts between doctests. Note
+# that we escape curly braces in this docstring by doubling them.
+_READ_SNOWFLAKE_DOC = """
     Read a Snowflake table or SQL Query to a Snowpark pandas DataFrame.
 
     Args:
@@ -105,14 +105,14 @@ def read_snowflake(
 
             * For a table referenced by `name_or_query` the base table is snapshotted and the snapshot is used to back the resulting DataFrame.
 
-            * For SELECT queries of the form `SELECT * FROM {table_name}` the base table is snapshotted as though it were referenced directly from `pd.read_snowflake`,
+            * For SELECT queries of the form `SELECT * FROM $TABLE_NAME` the base table is snapshotted as though it were referenced directly from `pd.read_snowflake`,
               and the snapshot will be used to back the resulting DataFrame as above.
 
             * In the following cases, a temporary table is created and snapshotted, and the snapshot of the temporary table is used to back the resulting
               DataFrame.
 
                 * For VIEWs, SECURE VIEWs, and TEMPORARY VIEWs, a temporary table is created as a materialized copy of the view at
-                  the time of calling `pd.read_snowflake` whether `pd.read_snowflake` is called as `pd.read_snowflake("SELECT * FROM {view_name}")` or
+                  the time of calling `pd.read_snowflake` whether `pd.read_snowflake` is called as `pd.read_snowflake("SELECT * FROM $TABLE_NAME")` or
                   `pd.read_snowflake(view_name)`.
 
                 * For more complex SELECT queries, including those with ORDER BY's or CTEs, the query is evaluated, and a temporary
@@ -130,9 +130,9 @@ def read_snowflake(
         ``index_col`` and ``columns``:
 
         >>> session = pd.session
-        >>> table_name = "RESULT"
-        >>> create_result = session.sql(f"CREATE TEMP TABLE {table_name} (A int, B int, C int)").collect()
-        >>> insert_result = session.sql(f"INSERT INTO {table_name} VALUES(1, 2, 3)").collect()
+        >>> table_name = "{table_name}"
+        >>> create_result = session.sql(f"CREATE TEMP TABLE {{table_name}} (A int, B int, C int)").collect()
+        >>> insert_result = session.sql(f"INSERT INTO {{table_name}} VALUES(1, 2, 3)").collect()
         >>> session.table(table_name).show()
         -------------------
         |"A"  |"B"  |"C"  |
@@ -203,9 +203,8 @@ def read_snowflake(
         Examples of `pd.read_snowflake` using SQL queries:
 
         >>> session = pd.session
-        >>> table_name = "RESULT"
-        >>> create_result = session.sql(f"CREATE OR REPLACE TEMP TABLE {table_name} (A int, B int, C int)").collect()
-        >>> insert_result = session.sql(f"INSERT INTO {table_name} VALUES(1, 2, 3),(-1, -2, -3)").collect()
+        >>> create_result = session.sql(f"CREATE OR REPLACE TEMP TABLE {{table_name}} (A int, B int, C int)").collect()
+        >>> insert_result = session.sql(f"INSERT INTO {{table_name}} VALUES(1, 2, 3),(-1, -2, -3)").collect()
         >>> session.table(table_name).show()
         -------------------
         |"A"  |"B"  |"C"  |
@@ -220,7 +219,7 @@ def read_snowflake(
 
           >>> import modin.pandas as pd
           >>> import snowflake.snowpark.modin.plugin
-          >>> pd.read_snowflake(f"SELECT * FROM {table_name}")   # doctest: +NORMALIZE_WHITESPACE
+          >>> pd.read_snowflake(f"SELECT * FROM {{table_name}}")   # doctest: +NORMALIZE_WHITESPACE
              A  B  C
           0  1  2  3
           1 -1 -2 -3
@@ -230,19 +229,19 @@ def read_snowflake(
           Snowflake table will be data columns. Note that duplication is allowed and
           duplicate pandas labels are maintained.
 
-          >>> pd.read_snowflake(f"SELECT * FROM {table_name}", index_col="A")   # doctest: +NORMALIZE_WHITESPACE
+          >>> pd.read_snowflake(f"SELECT * FROM {{table_name}}", index_col="A")   # doctest: +NORMALIZE_WHITESPACE
               B  C
           A
            1  2  3
           -1 -2 -3
 
-          >>> pd.read_snowflake(f"SELECT * FROM {table_name}", index_col=["A", "B"])   # doctest: +NORMALIZE_WHITESPACE
+          >>> pd.read_snowflake(f"SELECT * FROM {{table_name}}", index_col=["A", "B"])   # doctest: +NORMALIZE_WHITESPACE
                  C
           A  B
            1  2  3
           -1 -2 -3
 
-          >>> pd.read_snowflake(f"SELECT * FROM {table_name}", index_col=["A", "A", "B"])  # doctest: +NORMALIZE_WHITESPACE
+          >>> pd.read_snowflake(f"SELECT * FROM {{table_name}}", index_col=["A", "A", "B"])  # doctest: +NORMALIZE_WHITESPACE
                     C
           A  A  B
            1  1  2  3
@@ -250,19 +249,19 @@ def read_snowflake(
 
         - More complex queries can also be passed in.
 
-          >>> pd.read_snowflake(f"SELECT * FROM {table_name} WHERE A > 0")  # doctest: +NORMALIZE_WHITESPACE
+          >>> pd.read_snowflake(f"SELECT * FROM {{table_name}} WHERE A > 0")  # doctest: +NORMALIZE_WHITESPACE
              A  B  C
           0  1  2  3
 
         - SQL comments can also be included, and will be ignored.
 
-          >>> pd.read_snowflake(f"-- SQL Comment 1\\nSELECT * FROM {table_name} WHERE A > 0")
+          >>> pd.read_snowflake(f"-- SQL Comment 1\\nSELECT * FROM {{table_name}} WHERE A > 0")
              A  B  C
           0  1  2  3
 
           >>> pd.read_snowflake(f'''-- SQL Comment 1
           ... -- SQL Comment 2
-          ... SELECT * FROM {table_name}
+          ... SELECT * FROM {{table_name}}
           ... -- SQL Comment 3
           ... WHERE A > 0''')
              A  B  C
@@ -293,7 +292,7 @@ def read_snowflake(
           10           55
           11           89
 
-          >>> pd.read_snowflake(f'''WITH T1 AS (SELECT SQUARE(A) AS A2, SQUARE(B) AS B2, SQUARE(C) AS C2 FROM {table_name}),
+          >>> pd.read_snowflake(f'''WITH T1 AS (SELECT SQUARE(A) AS A2, SQUARE(B) AS B2, SQUARE(C) AS C2 FROM {{table_name}}),
           ... T2 AS (SELECT SQUARE(A2) AS A4, SQUARE(B2) AS B4, SQUARE(C2) AS C4 FROM T1),
           ... T3 AS (SELECT * FROM T1 UNION ALL SELECT * FROM T2)
           ... SELECT * FROM T3''')  # doctest: +NORMALIZE_WHITESPACE
@@ -318,7 +317,7 @@ def read_snowflake(
           ... def filter_rows(session, table_name, column_to_filter, value):
           ...   df = session.table(table_name)
           ...   return df.filter(col(column_to_filter) == value)$$
-          ... ''' + f"CALL filter_rows('{table_name}', 'A', 1)", enforce_ordering=True)
+          ... ''' + f"CALL filter_rows('{{table_name}}', 'A', 1)", enforce_ordering=True)
              A  B  C
           0  1  2  3
 
@@ -335,15 +334,15 @@ def read_snowflake(
           ... import com.snowflake.snowpark.functions._
           ... import com.snowflake.snowpark._
           ...
-          ... object Filter {
-          ...   def filterRows(session: Session, tableName: String, column_to_filter: String, value: Int): DataFrame = {
+          ... object Filter {{
+          ...   def filterRows(session: Session, tableName: String, column_to_filter: String, value: Int): DataFrame = {{
           ...       val table = session.table(tableName)
           ...       val filteredRows = table.filter(col(column_to_filter) === value)
           ...       return filteredRows
-          ...   }
-          ... }
+          ...   }}
+          ... }}
           ... $$
-          ... ''' + f"CALL filter_rows('{table_name}', 'A', -1)", enforce_ordering=True)
+          ... ''' + f"CALL filter_rows('{{table_name}}', 'A', -1)", enforce_ordering=True)
              A  B  C
           0 -1 -2 -3
 
@@ -354,18 +353,18 @@ def read_snowflake(
           >>> from snowflake.snowpark.functions import col
           >>> _ = session.sql("create or replace temp stage mystage").collect()
           >>> session.add_packages('snowflake-snowpark-python')
-          >>> @sproc(return_type=StructType([StructField("A", IntegerType()), StructField("B", IntegerType()), StructField("C", IntegerType()), StructField("D", IntegerType())]), input_types=[StringType(), StringType(), IntegerType()], is_permanent=True, name="multiply_col_by_value", stage_location="mystage")
+          >>> @sproc(return_type=StructType([StructField("A", IntegerType()), StructField("B", IntegerType()), StructField("C", IntegerType()), StructField("D", IntegerType())]), input_types=[StringType(), StringType(), IntegerType()], is_permanent=True, name="{stored_procedure_name}", stage_location="mystage")
           ... def select_sp(session_, tableName, col_to_multiply, value):
           ...     df = session_.table(table_name)
           ...     return df.select('*', (col(col_to_multiply)*value).as_("D"))
 
-          >>> pd.read_snowflake(f"CALL multiply_col_by_value('{table_name}', 'A', 2)", enforce_ordering=True)
+          >>> pd.read_snowflake(f"CALL {stored_procedure_name}('{{table_name}}', 'A', 2)", enforce_ordering=True)
              A  B  C  D
           0  1  2  3  2
           1 -1 -2 -3 -2
 
-          >>> session.sql("DROP PROCEDURE multiply_col_by_value(VARCHAR, VARCHAR, NUMBER)").collect()
-          [Row(status='MULTIPLY_COL_BY_VALUE successfully dropped.')]
+          >>> session.sql("DROP PROCEDURE {stored_procedure_name}(VARCHAR, VARCHAR, NUMBER)").collect()
+          [Row(status='{stored_procedure_name} successfully dropped.')]
 
     Note:
         The names/labels used for the parameters of the Snowpark pandas IO functions such as index_col, columns are normalized
@@ -395,7 +394,21 @@ def read_snowflake(
 
         To see what are the Normalized Snowflake Identifiers for columns of a Snowflake table, you can call SQL query
         `SELECT * FROM TABLE` or `DESCRIBE TABLE` to see the column names.
-    """
+"""
+
+
+@register_pd_accessor("read_snowflake")
+@doc(
+    _READ_SNOWFLAKE_DOC,
+    table_name="RESULT_0",
+    stored_procedure_name="MULTIPLY_COL_BY_VALUE_0",
+)
+def read_snowflake(
+    name_or_query: Union[str, Iterable[str]],
+    index_col: Union[str, list[str], None] = None,
+    columns: Optional[list[str]] = None,
+    enforce_ordering: bool = False,
+) -> DataFrame:
     from modin.core.execution.dispatching.factories.dispatcher import FactoryDispatcher
 
     return DataFrame(
@@ -411,6 +424,11 @@ def read_snowflake(
 if MODIN_IS_AT_LEAST_0_33_0:
 
     @_inherit_docstrings(read_snowflake)
+    @doc(
+        _READ_SNOWFLAKE_DOC,
+        table_name="RESULT_1",
+        stored_procedure_name="MULTIPLY_COL_BY_VALUE_1",
+    )
     def _read_snowflake_pandas_backend(
         name_or_query, index_col=None, columns=None, enforce_ordering=False
     ) -> pd.DataFrame:
