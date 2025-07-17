95b4734445f1839c6a69966274a0956197bf1486
Pawel Wojcik <pawel.wojcik@snowflake.com>
2025-07-17 15:49:28 +0200

    SNOW-2203826 one more test

--- tests/integ/test_df_flattening.py
+++ tests/integ/test_df_flattening.py
@@ -8,31 +8,40 @@ from snowflake.snowpark.window import Window
 
 def test_order_by_with_mixed_columns(session):
     df = session.createDataFrame([(1, 2, 3), (4, 5, 6)], ["a", "b", "c"])
-    result = (
-        df.select(col("c").alias("c1"), pow(2, "c").alias("d"))
-        .orderBy(col("c"), col("d"))
-        .collect()
-    )
-    assert result == [(3, 8), (6, 64)]
+    result = df.select(pow(2, "c").alias("d")).orderBy(col("c"), col("d")).collect()
+    assert result == [(8,), (64,)]
 
 
 def test_order_by_with_mixed_columns_and_window_function(session):
     df = session.createDataFrame([(1, 2, 3), (4, 5, 6)], ["a", "b", "c"])
     result = (
-        df.select(
-            rank().over(Window.order_by(col("c"))).alias("rank"), pow(2, "c").alias("d")
-        )
+        df.select(rank().over(Window.order_by(col("c"))).alias("rank"))
         .orderBy(col("c"), col("rank"))
         .collect()
     )
-    assert result == [(1, 8), (2, 64)]
+    assert result == [(1,), (2,)]
 
 
 def test_filter_with_mixed_columns(session):
     df = session.createDataFrame([(1, 2, 3), (4, 5, 6)], ["a", "b", "c"])
-    result = (
-        df.select(col("c").alias("c1"), pow(2, "c").alias("d"))
-        .filter(col("c") > col("d"))
+    result = df.select(pow(2, "c").alias("d")).filter(col("c") > col("d")).collect()
+    assert result == []
+
+
+def test_filter_and_order_by_with_mixed_columns(session):
+    df = session.createDataFrame([(1, 2, 3), (4, 5, 6)], ["a", "b", "c"])
+    filter_first = (
+        df.select(pow(2, "c").alias("d"))
+        .filter(col("c") < col("d"))
+        .orderBy(col("c"), col("d"))
         .collect()
     )
-    assert result == []
+    assert filter_first == [(8,), (64,)]
+
+    order_first = (
+        df.select(pow(2, "c").alias("d"))
+        .orderBy(col("c"), col("d"))
+        .filter(col("c") < col("d"))
+        .collect()
+    )
+    assert order_first == [(8,), (64,)]
