84237aea5df9c77166e4be4aa052fc83bbb999f5
Jamison Rose <Jamison.Rose@snowflake.com>
2025-08-19 11:58:39 -0700

    Graphite update src/snowflake/snowpark/types.py
    Co-authored-by: graphite-app[bot] <96075541+graphite-app[bot]@users.noreply.github.com>

--- src/snowflake/snowpark/types.py
+++ src/snowflake/snowpark/types.py
@@ -194,7 +194,7 @@ class TimestampType(_AtomicType):
 
     def __repr__(self) -> str:
         return (
-            f"timezone=TimestampTimeZone('{self.tz}')"
+            f"TimestampType(timezone=TimestampTimeZone('{self.tz}'))"
             if self.tzinfo != ""
             else "TimestampType()"
         )
