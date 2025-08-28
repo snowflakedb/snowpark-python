99c2db03160929db6fabbbc3c1d54b0adc6d3fb0
Arturo Herrera Aguilar <a.herreraaguilar@snowflake.com>
2025-08-28 12:12:01 -0600

    SNOW-2262972: Refactor function.py file (#3655)

--- src/snowflake/snowpark/functions.py
+++ src/snowflake/snowpark/functions.py
@@ -168,6 +168,11 @@ from typing import Callable, Dict, List, Optional, Tuple, Union, overload
 
 import snowflake.snowpark
 import snowflake.snowpark._internal.proto.generated.ast_pb2 as proto
+from snowflake.snowpark._functions import general_functions
+from snowflake.snowpark._functions.general_functions import (
+    _call_function,
+    _check_column_parameters,
+)
 import snowflake.snowpark.table_function
 from snowflake.snowpark._internal.analyzer.expression import (
     CaseWhen,
@@ -248,19 +253,6 @@ else:
     from collections.abc import Iterable
 
 
-# check function to allow test_dataframe_alias_negative to pass in AST mode.
-def _check_column_parameters(name1: str, name2: Optional[str]) -> None:
-    if not isinstance(name1, str):
-        raise ValueError(
-            f"Expects first argument to be of type str, got {type(name1)}."
-        )
-
-    if name2 is not None and not isinstance(name2, str):
-        raise ValueError(
-            f"Expects second argument to be of type str or None, got {type(name1)}."
-        )
-
-
 @overload
 @publicapi
 def col(
@@ -306,24 +298,9 @@ def col(
     *,
     _is_qualified_name: bool = False,
 ) -> Column:
-
-    _check_column_parameters(name1, name2)
-
-    if name2 is None:
-        return Column(
-            name1,
-            _is_qualified_name=_is_qualified_name,
-            _emit_ast=_emit_ast,
-            _caller_name="col",
-        )
-    else:
-        return Column(
-            name1,
-            name2,
-            _is_qualified_name=_is_qualified_name,
-            _emit_ast=_emit_ast,
-            _caller_name="col",
-        )
+    return general_functions.col(
+        name1, name2, _emit_ast, _is_qualified_name=_is_qualified_name
+    )
 
 
 @overload
@@ -419,27 +396,7 @@ def lit(
         ---------------------------------------------------------------------------------------------
         <BLANKLINE>
     """
-
-    if _emit_ast:
-        ast = proto.Expr()
-        if datatype is None:
-            build_builtin_fn_apply(ast, "lit", literal)
-        else:
-            build_builtin_fn_apply(ast, "lit", literal, datatype)
-
-        if isinstance(literal, Column):
-            # Create new Column, and assign expression of current Column object.
-            # This will encode AST correctly.
-            c = Column("", _emit_ast=False)
-            c._expression = literal._expression
-            c._ast = ast
-            return c
-        return Column(Literal(literal, datatype=datatype), _ast=ast, _emit_ast=True)
-
-    if isinstance(literal, Column):
-        return literal
-
-    return Column(Literal(literal, datatype=datatype), _ast=None, _emit_ast=False)
+    return general_functions.lit(literal, datatype, _emit_ast)
 
 
 @publicapi
@@ -10423,12 +10380,7 @@ def call_function(
         <BLANKLINE>
 
     """
-    ast = (
-        build_function_expr("call_function", [function_name, *args])
-        if _emit_ast
-        else None
-    )
-    return _call_function(function_name, *args, _ast=ast, _emit_ast=_emit_ast)
+    return general_functions.call_function(function_name, *args, _emit_ast=_emit_ast)
 
 
 @publicapi
@@ -10461,35 +10413,7 @@ def function(function_name: str, _emit_ast: bool = True) -> Callable:
         ----------------
         <BLANKLINE>
     """
-    return lambda *args: call_function(function_name, *args, _emit_ast=_emit_ast)
-
-
-def _call_function(
-    name: str,
-    *args: ColumnOrLiteral,
-    is_distinct: bool = False,
-    api_call_source: Optional[str] = None,
-    is_data_generator: bool = False,
-    _ast: proto.Expr = None,
-    _emit_ast: bool = True,
-) -> Column:
-
-    if _emit_ast and _ast is None:
-        _ast = build_function_expr(name, args)
-
-    args_list = parse_positional_args_to_list(*args)
-    expressions = [Column._to_expr(arg) for arg in args_list]
-    return Column(
-        FunctionExpression(
-            name,
-            expressions,
-            is_distinct=is_distinct,
-            api_call_source=api_call_source,
-            is_data_generator=is_data_generator,
-        ),
-        _ast=_ast,
-        _emit_ast=_emit_ast,
-    )
+    return general_functions.function(function_name, _emit_ast=_emit_ast)
 
 
 def _call_named_arguments_function(
