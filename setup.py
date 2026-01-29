c751179b3f2aaab89b30a922b2d8d193c60fda2b
Jonathan Shi <jonathan.shi@snowflake.com>
2026-01-29 15:28:42 -0800

    revert ceiling change, fix udf error

--- setup.py
+++ setup.py
@@ -28,7 +28,7 @@ INSTALL_REQ_LIST = [
     "pyyaml",
     "cloudpickle>=1.6.0,<=3.1.1,!=2.1.0,!=2.2.0",
     # `protoc` < 3.20 is not able to generate protobuf code compatible with protobuf >= 3.20.
-    "protobuf>=3.20, <7.35",  # Snowpark IR
+    "protobuf>=3.20, <6.34",  # Snowpark IR
     "python-dateutil",  # Snowpark IR
     "tzlocal",  # Snowpark IR
 ]
