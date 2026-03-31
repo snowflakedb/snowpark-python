531084317116040155da3ab5fe8599542b259731
Ben Kogan <ben.kogan@snowflake.com>
2026-03-31 15:05:28 -0700

    remove classifier

--- setup.py
+++ setup.py
@@ -250,7 +250,6 @@ setup(
         "Programming Language :: Python :: 3.11",
         "Programming Language :: Python :: 3.12",
         "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
         "Topic :: Database",
         "Topic :: Software Development",
         "Topic :: Software Development :: Libraries",
