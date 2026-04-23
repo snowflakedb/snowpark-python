18a59c12490929fd7547111b11eb940adc861ca8
Ben Kogan <ben.kogan@snowflake.com>
2026-04-23 14:53:33 -0700

    cleanup conf

--- tests/integ/conftest.py
+++ tests/integ/conftest.py
@@ -370,12 +370,6 @@ def session(
         session.sql("alter session set ENABLE_ROW_ACCESS_POLICY=true").collect()
         if sys.version_info.major == 3 and sys.version_info.minor == 14:
             session.sql("alter session set ENABLE_PYTHON_3_14=true").collect()
-            session.sql(
-                "alter session set ENABLE_DEFAULT_PYTHON_ARTIFACT_REPOSITORY=true"
-            ).collect()
-            session.sql(
-                "alter schema set DEFAULT_PYTHON_ARTIFACT_REPOSITORY=snowflake.snowpark.pypi_shared_repository"
-            ).collect()
 
     try:
         yield session
@@ -448,14 +442,6 @@ def temp_schema(connection, session, local_testing_mode) -> None:
             cursor.execute(
                 f"GRANT ALL PRIVILEGES ON SCHEMA {temp_schema_name} TO ROLE PUBLIC"
             )
-            # TODO: delete once pypi becomes default for 3.14
-            if sys.version_info.major == 3 and sys.version_info.minor == 14:
-                cursor.execute(
-                    "alter session set ENABLE_DEFAULT_PYTHON_ARTIFACT_REPOSITORY=true"
-                )
-                cursor.execute(
-                    "alter schema set DEFAULT_PYTHON_ARTIFACT_REPOSITORY=snowflake.snowpark.pypi_shared_repository"
-                )
             yield temp_schema_name
             cursor.execute(f"DROP SCHEMA IF EXISTS {temp_schema_name}")
 
