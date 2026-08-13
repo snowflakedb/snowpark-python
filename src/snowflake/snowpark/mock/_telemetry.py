4e0522d185078ba7f7a3785203a482c2976efded
Filip Pawlowski <filip.pawlowski@snowflake.com>
2026-08-13 09:54:03 +0000

    Revert "SNOW-2912540: fix SecretDetector import to match actually-installed UD build"
    This reverts commit bad7438cb3d7154fa8993bef89741c913d29aa8f.

--- src/snowflake/snowpark/mock/_telemetry.py
+++ src/snowflake/snowpark/mock/_telemetry.py
@@ -14,15 +14,16 @@ from http.client import OK
 from typing import Optional
 
 from snowflake.snowpark._internal.utils import (
+    IS_V5_DRIVER,
     get_os_name,
     get_python_version,
     get_version,
 )
 
-try:
-    from snowflake.connector.secret_detector import SecretDetector
-except ImportError:  # pragma: no cover
-    from snowflake.connector._common.secret_detector import SecretDetector  # type: ignore[no-redef]
+if IS_V5_DRIVER:
+    from snowflake.connector._common.secret_detector import SecretDetector
+else:
+    from snowflake.connector.secret_detector import SecretDetector  # type: ignore[no-redef]
 
 from .exceptions import SnowparkLocalTestingException
 
